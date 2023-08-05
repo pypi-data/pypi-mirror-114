c346603fa9a917714e287f24a802dab6059c5d61
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:07:27 +0000

    Add package sdmn/0.1.0/sdmn-0.1.0-py3-none-any.whl

---
+++ packages/sdmn/sdmn-0.1.0-py3-none-any.whl/sdmn/__init__.py
@@ -0,0 +1,9 @@
+import logging
+
+__version__ = '0.5.12'
+
+handler = logging.StreamHandler()
+handler.setFormatter(logging.Formatter('[%(levelname)s] %(message)s'))
+logger = logging.getLogger('sdmn')
+logger.addHandler(handler)
+logger.setLevel('CRITICAL')
