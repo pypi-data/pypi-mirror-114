021cd90cc9c071879c68bfecfb2a23ce9d516d02
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:07:13 +0000

    Add package pyaud/3.1.0/pyaud-3.1.0.tar.gz

---
+++ packages/pyaud/pyaud-3.1.0.tar.gz/pyaud-3.1.0/pyaud/__init__.py
@@ -0,0 +1,16 @@
+"""Plugin architecture for auditing Python packages."""
+from . import config, exceptions, plugins
+from ._environ import package
+from ._utils import branch, files, git
+
+__version__ = "3.1.0"
+
+__all__ = [
+    "branch",
+    "config",
+    "exceptions",
+    "files",
+    "git",
+    "package",
+    "plugins",
+]
