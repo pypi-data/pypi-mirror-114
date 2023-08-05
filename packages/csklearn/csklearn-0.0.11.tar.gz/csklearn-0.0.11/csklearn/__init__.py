097ea174123af91c3546a736acadb59c316f5d4e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:06:45 +0000

    Add package csklearn/0.0.11/csklearn-0.0.11.tar.gz

---
+++ packages/csklearn/csklearn-0.0.11.tar.gz/csklearn-0.0.11/csklearn/__init__.py
@@ -0,0 +1,7 @@
+from .test_pip import test_pip
+from .feature_selection import *
+from .metrics import *
+from .model_selection import *
+from .plots import *
+from .transformers import *
+from .utils import *
