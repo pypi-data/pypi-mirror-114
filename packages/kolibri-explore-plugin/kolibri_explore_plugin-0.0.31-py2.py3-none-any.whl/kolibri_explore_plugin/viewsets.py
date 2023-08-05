31107790c933ab2d3d1e274b60d08868ef1c276d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:05:47 +0000

    Add package kolibri-explore-plugin/0.0.31/kolibri_explore_plugin-0.0.31-py2.py3-none-any.whl

---
+++ packages/kolibri-explore-plugin/kolibri_explore_plugin-0.0.31-py2.py3-none-any.whl/kolibri_explore_plugin/viewsets.py
@@ -0,0 +1,35 @@
+from kolibri.core.content.api import ContentNodeSearchViewset
+from kolibri.core.content.api import ContentNodeViewset
+from kolibri.core.content.models import ContentTag
+
+
+class CustomContentNodeViewset(ContentNodeViewset):
+    def _consolidate(self, items, queryset):
+        if not items:
+            return []
+
+        tags = {}
+        for t in ContentTag.objects.filter(tagged_content__in=queryset).values(
+            "tag_name",
+            "tagged_content",
+        ):
+            if t["tagged_content"] not in tags:
+                tags[t["tagged_content"]] = [t["tag_name"]]
+            else:
+                tags[t["tagged_content"]].append(t["tag_name"])
+
+        def add_tag(item):
+            item["tags"] = tags.get(item["id"], [])
+            return item
+
+        return [add_tag(item) for item in items]
+
+    def consolidate(self, items, queryset):
+        new_items = super().consolidate(items, queryset)
+        return self._consolidate(new_items, queryset)
+
+
+class CustomContentNodeSearchViewset(
+    CustomContentNodeViewset, ContentNodeSearchViewset
+):
+    pass
