ed577d4a103ecbe5fac86831359857699a3a623d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:06:43 +0000

    Add package PyCkin3/2.0.1/PyCkin3-2.0.1.tar.gz

---
+++ packages/PyCkin3/PyCkin3-2.0.1.tar.gz/PyCkin3-2.0.1/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+    name='PyCkin3',
+    version='2.0.1',
+    py_modules=['PyCkin3'],
+    author='bobi791027',
+    author_email='13513519246@139.com',
+    description='There are many useful functions.',
+)
