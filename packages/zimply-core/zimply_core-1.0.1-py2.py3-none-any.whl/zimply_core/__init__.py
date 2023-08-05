7ee1ecdddf578dd361e5e57a4cebf2b9ed9474de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:23 +0000

    Add package zimply-core/1.0.1/zimply_core-1.0.1-py2.py3-none-any.whl

---
+++ packages/zimply-core/zimply_core-1.0.1-py2.py3-none-any.whl/zimply_core/__init__.py
@@ -0,0 +1,9 @@
+__version__ = "1.0.1"
+
+from .zim_core import Article
+from .zim_core import Namespace
+from .zim_core import SearchResult
+from .zim_core import to_bytes
+from .zim_core import ZIMClient
+from .zim_core import ZIMFile
+from .zim_core import ZIMFileIterator
