2420accca1ef97ad0d7d3d498e79314e7dec97f4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:23 +0000

    Add package yourtube/0.3.0/yourtube-0.3.0-py3-none-any.whl

---
+++ packages/yourtube/yourtube-0.3.0-py3-none-any.whl/yourtube/__init__.py
@@ -0,0 +1,14 @@
+__version__ = "0.3.0"
+
+import shlex
+import subprocess
+import pathlib
+
+dir_ = pathlib.Path(__file__).parent.resolve()
+
+
+def run():
+    # TODO once poetry-core 1.1.0 drops, this could be made more elegantly, directly with a bash script
+    # or maybe directly calling voila?
+    cmd = f"voila --theme=dark {dir_}/UI.ipynb"
+    subprocess.run(shlex.split(cmd))
