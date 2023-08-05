852c41aa46894bfbf8d3c2f16a72fea786c85fc0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:12:11 +0000

    Add package nafigator/0.1.25/nafigator-0.1.25-py2.py3-none-any.whl

---
+++ packages/nafigator/nafigator-0.1.25-py2.py3-none-any.whl/nafigator/opennaf.py
@@ -0,0 +1,64 @@
+# coding: utf-8
+
+"""Main module."""
+
+import sys
+import click
+from .nafdocument import NafDocument
+from .const import MultiwordElement
+from .const import ProcessorElement
+
+
+@click.command()
+@click.option(
+    "--input", default="data/example.naf.xml", prompt="input file", help="The input file"
+)
+def opennaf(input: str):
+    """ """
+    naf = NafDocument().open(input)
+    # print(naf.header)
+    # print(naf.version)
+    # print(naf.raw)
+    # print(naf.text)
+    # print(naf.terms)
+    # print(naf.entities)
+    # print(naf.deps)
+    # print(naf.formats)
+    # print(naf.multiwords)
+    # naf.remove_layer_elements("multiwords")
+    # print(naf.multiwords)
+    print(naf.paragraphs)
+
+    # lp = ProcessorElement(
+    #     name="processorname",
+    #     version="1.0",
+    #     timestamp=None,
+    #     beginTimestamp=None,
+    #     endTimestamp=None,
+    #     hostname=None,
+    # )
+
+    # naf.add_processor_element("recommendations", lp)
+
+    # layer = naf.layer("recommendations")
+
+    # data_recommendation = {
+    #     "id": "recommendation1",
+    #     "subjectivity": 0.5,
+    #     "polarity": 0.25,
+    #     "span": ["t37", "t39"],
+    # }
+
+    # element = naf.subelement(
+    #     element=layer, tag="recommendation", data=data_recommendation
+    # )
+
+    # naf.add_span_element(element=element, data=data_recommendation)
+
+    # print(naf.recommendations)
+
+    # print(naf.sentences)
+
+
+if __name__ == "__main__":
+    sys.exit(opennaf())
---
+++ packages/nafigator/nafigator-0.1.25-py2.py3-none-any.whl/nafigator/utils.py
@@ -0,0 +1,46 @@
+# coding: utf-8
+
+"""Utils module."""
+
+import io
+import re
+from lxml import etree
+
+
+def load_dtd(dtd_url):
+
+    dtd = None
+    r = open(dtd_url)
+    if r:
+        dtd_file_object = io.StringIO(r.read())
+        dtd = etree.DTD(dtd_file_object)
+    if dtd is None:
+        logging.error("failed to load dtd from" + str(dtd_url))
+    else:
+        logging.info("Succesfully to load dtd from" + str(dtd_url))
+    return dtd
+
+
+def time_in_correct_format(datetime_obj):
+    """
+    Function that returns the current time (UTC)
+    """
+    return datetime_obj.strftime("%Y-%m-%dT%H:%M:%SUTC")
+
+
+# Only allow legal strings in XML:
+# http://stackoverflow.com/a/25920392/2899924
+illegal_pattern = re.compile(
+    "[^\u0020-\uD7FF\u0009\u000A\u000D\uE000-\uFFFD\u10000-\u10FFFF]+"
+)
+
+
+def remove_illegal_chars(text):
+    return re.sub(illegal_pattern, "", text)
+
+
+def normalize_token_orth(orth):
+    if "\n" in orth:
+        return "NEWLINE"
+    else:
+        return remove_illegal_chars(orth)
