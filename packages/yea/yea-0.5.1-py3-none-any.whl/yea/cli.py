93f078dac7abbfc6bd9110e119151d8f748063e4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:07:31 +0000

    Add package yea/0.5.1/yea-0.5.1-py3-none-any.whl

---
+++ packages/yea/yea-0.5.1-py3-none-any.whl/yea/cli.py
@@ -0,0 +1,46 @@
+"""Command line entrypoint."""
+
+import argparse
+import sys
+
+from yea import context, runner
+
+
+def cli_list(yc):
+    print("Tests:")
+    yc._args.action = "list"
+    tr = runner.TestRunner(yc=yc)
+    tests = tr.get_tests()
+    tlen = max([len(t.test_id) for t in tests])
+    for t in tests:
+        print("  {:<{}s}: {}".format(t.test_id, tlen, t.name))
+
+
+def cli_run(yc):
+    yc._args.action = "run"
+    tr = runner.TestRunner(yc=yc)
+    tr.run()
+
+
+def cli():
+    parser = argparse.ArgumentParser()
+
+    subparsers = parser.add_subparsers(dest="action", title="action", description="Action to perform")
+    parser.add_argument("--dryrun", action="store_true", help="Dont do anything")
+    parser.add_argument("--live", action="store_true", help="Run against real server")
+
+    parse_list = subparsers.add_parser("list", aliases=["l"])
+    parse_list.set_defaults(func=cli_list)
+    parse_list.add_argument("tests", nargs="*")
+
+    parse_run = subparsers.add_parser("run", aliases=["r"])
+    parse_run.add_argument("-a", "--all", action="store_true", help="Run all")
+    parse_run.add_argument("tests", nargs="*")
+    parse_run.set_defaults(func=cli_run)
+    args = parser.parse_args()
+    if not args.action:
+        parser.print_help()
+        sys.exit(1)
+
+    yc = context.YeaContext(args=args)
+    args.func(yc)
