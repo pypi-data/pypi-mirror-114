2ea8dd2fe6a818b2e70f31330c6f3cb8e4b59546
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:06:11 +0000

    Add package chariot-plugin/1.0.8/chariot-plugin-1.0.8.tar.gz

---
+++ packages/chariot-plugin/chariot-plugin-1.0.8.tar.gz/chariot-plugin-1.0.8/setup.py
@@ -0,0 +1,63 @@
+from setuptools import setup, find_packages
+import core
+
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+
+
+
+setup(
+    # 应用名
+    name = "chariot-plugin",
+
+    # 作者
+    author = "andy",
+
+    # 作者邮箱
+    author_email = "1472942893@qq.com",
+
+    # 描述
+    description = "插件生成等功能...",
+
+    # 版本号
+    version = core.VERSION,
+
+    # 安装 当前目录下有哪些包
+    packages = find_packages(),
+
+    # 安装层级资源
+    package_data = {"core": ["res/*", "res/SDK/*"]},
+
+    include_package_data = True,
+
+    # 碳泽社区
+    url = "https://www.chariots.cn/market/introduction",
+
+    # py版本
+    python_requires = '>=3',
+
+    # 依赖
+    install_requires = [
+        "argparse",
+        "loguru",
+        "jinja2",
+        "pyyaml"
+    ],
+
+    # 入口
+    entry_points={
+        "console_scripts": [
+            "chariot-plugin = core.main:cmdline"
+        ]
+    },
+
+    # 项目的详细描述
+    long_description = long_description,
+    long_description_content_type = "text/markdown",
+
+    # 许可证
+    license = 'MIT',
+)
+
+
+
