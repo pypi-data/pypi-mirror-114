7b1ade09a595014b660b4ce1d4eb76a7c4c31e8e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:11:25 +0000

    Add package verysimplemodulesimple/0.0.1/verysimplemodulesimple-0.0.1.tar.gz

---
+++ packages/verysimplemodulesimple/verysimplemodulesimple-0.0.1.tar.gz/verysimplemodulesimple-0.0.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.0.1'
+DESCRIPTION = 'My first Python Package'
+LONG_DESCRIPTION = 'My first Python Package with a slightly longer description'
+
+setup(
+	name='verysimplemodulesimple',
+	version=VERSION,
+	author='lza',
+	description=DESCRIPTION,
+	long_description=LONG_DESCRIPTION
+)
