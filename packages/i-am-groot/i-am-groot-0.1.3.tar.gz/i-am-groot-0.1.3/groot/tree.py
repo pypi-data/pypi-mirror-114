0bb5d324281c9e39e795f1830c9eea9cc55b350a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:07:45 +0000

    Add package i-am-groot/0.1.3/i-am-groot-0.1.3.tar.gz

---
+++ packages/i-am-groot/i-am-groot-0.1.3.tar.gz/i-am-groot-0.1.3/groot/tree.py
@@ -0,0 +1,180 @@
+"""Core data structures."""
+from copy import deepcopy
+from typing import Dict, Optional
+
+import yaml
+
+from .functional import all_leaf_nodes, build_nodes, index_nodes_by_level, push_atoms
+from .node import Node
+
+__all__ = ["Tree"]
+
+
+class Tree:
+    """
+    Tree data structure.
+
+    Parameters
+    ----------
+    nodes : Dict[str, Node]
+        Lookup table for nodes.
+    name : Optional[str]
+        Name of the tree, default value `'-'.join(roots)`.
+
+    Attributes
+    ----------
+    roots : List[str]
+        List of roots.
+    atom_label : Dict[str, int]
+        Atom name to int label (0...n_atoms -1).
+    node_label : Dict[str, int]
+        Node name to overall int label (0...n_nodes-1).
+    node_level : Dict[str, int]
+        Lookup table of node level.
+    levels : List[Set[str]]
+        List of levels of nodes.
+    name : str
+        Name of the tree
+    """
+
+    def __init__(self, nodes: Dict[str, Node], name: Optional[str] = None):
+        """
+        Init tree object.
+
+        Parameters
+        ----------
+        nodes : Dict[str, Node]
+            Lookup table for nodes.
+        name : Optional[str]
+            Name of the tree, default value `'-'.join(roots)`.
+        """
+
+        _roots = [v for k, v in nodes.items() if v.is_root]
+        _roots.sort(key=lambda x: -len(x.atoms))
+        self.roots = [n.name for n in _roots]
+
+        _atoms = [v for k, v in nodes.items() if v.is_atom]
+        _atoms.sort(key=lambda x: str(x))
+        self.atom_label = {a.name: i for i, a in enumerate(_atoms)}
+
+        _node = [n for n in nodes.values()]
+        _node.sort(key=lambda x: str(x))
+        self.node_label = {n.name: i for i, n in enumerate(_node)}
+
+        self.name = name if name else "-".join(self.roots)
+
+        self.__nodes = nodes
+
+        node_level, levels = index_nodes_by_level(nodes)
+        self.node_level = node_level
+        self.levels = levels
+
+    @property
+    def nodes(self) -> Dict[str, Node]:
+        """
+        Short summary.
+
+        Returns
+        -------
+        Dict[str, Node]
+            Return nodes, making it read-only.
+        """
+        return self.__nodes
+
+    def subtree_nodes(self, root: str) -> Dict[str, Node]:
+        """
+        Get nodes of the subtree at `root`.
+
+        Parameters
+        ----------
+        root : str
+            Name of root.
+
+        Returns
+        -------
+        Dict[str, Node]
+            Lookup table of subtree nodes.
+        """
+        return {n: self.nodes[n] for n in all_leaf_nodes(root, self.nodes)}
+
+    def subtree(self, root: str) -> "Tree":
+        """
+        Generate subtree at `root`.
+
+        Parameters
+        ----------
+        root : str
+            Name of root.
+
+        Returns
+        -------
+        Tree
+            Subtree at `root`.
+        """
+        nodes = deepcopy(self.subtree_nodes(root))
+        nodes[root].parent = None
+        return Tree(nodes)
+
+    def to_dict(self) -> Dict[str, str]:
+        """
+        Serialize the tree to dictionary, i.e. child-parent map.
+
+        Returns
+        -------
+        Dict[str, str]
+            Child-parent map.
+        """
+
+        child_to_parent = dict()
+
+        for name, node in self.nodes.items():
+            child_to_parent[name] = node.parent
+
+        return child_to_parent
+
+    def to_yaml(self) -> str:
+        """
+        Serialize the tree to yaml string.
+
+        Returns
+        -------
+        str
+            Yaml string of child-parent map.
+        """
+        return yaml.dump(self.to_dict())
+
+    @classmethod
+    def from_dict(cls, child_to_parent: Dict[str, str]) -> "Tree":
+        """
+        Build tree from child-parent map.
+
+        Parameters
+        ----------
+        child_to_parent : Dict[str, str]
+            Child-parent map.
+
+        Returns
+        -------
+        Tree
+            Tree object.
+        """
+        nodes = build_nodes(child_to_parent)
+        push_atoms(nodes)
+        return cls(nodes)
+
+    @classmethod
+    def from_yaml(cls, yaml_string: str) -> "Tree":
+        """
+        Build tree from yaml string.
+
+        Parameters
+        ----------
+        yaml_string : str
+            Yaml string of child-parent map.
+
+        Returns
+        -------
+        Tree
+            Tree object.
+        """
+        return cls.from_dict(yaml.safe_load(yaml_string))
---
+++ packages/i-am-groot/i-am-groot-0.1.3.tar.gz/i-am-groot-0.1.3/groot/utils.py
@@ -0,0 +1,89 @@
+"""Provide additional utils for tree class."""
+from .node import Node
+from .tree import Tree
+
+__all__ = ["draw_tree"]
+
+
+def draw_tree(
+    tree: Tree, tree_space: int = 3, atom_marker: str = "■", show_level: bool = True
+) -> str:
+    """
+    Plot tree structure.
+
+    Parameters
+    ----------
+    tree : Tree
+        A `Tree` object.
+    tree_space : int
+        Number of space in markers.
+    atom_marker : str
+        Marker for atom nodes.
+    show_level : bool
+        If `True` levels will be added as a header
+
+    Returns
+    -------
+    str
+        Tree diagram string.
+
+    Example
+    -------
+    ```
+    draw_tree(tree, show_level=True)
+
+    0   1   2
+    ┼───┼───┼
+    a
+    ├── a.a
+    │   ├── a.a.b ■
+    │   └── a.a.a ■
+    ├── a.c ■
+    └── a.b
+        └── a.b.a ■
+    b
+    ├── b.a ■
+    └── b.b ■
+    ```
+    """
+
+    tree_space = max(int(tree_space), 1)
+
+    class Marker:
+        SPACE = " " * (tree_space + 1)
+        BRANCH = "│" + " " * tree_space
+        TEE = "├" + "─" * (tree_space - 1) + " "
+        LAST = "└" + "─" * (tree_space - 1) + " "
+
+    def draw(node: Node, prefix: str = ""):
+        """Based on stackoverflow: `List directory tree structure in python`."""
+
+        children = node.children
+        # contents each get pointers that are ├── with a final └── :
+        markers = [Marker.TEE] * (len(children) - 1) + [Marker.LAST]
+        for marker, child in zip(markers, children):
+
+            if nodes[child].is_atom:
+                yield prefix + marker + child + " " + atom_marker
+            else:
+                yield prefix + marker + child
+                extension = Marker.BRANCH if marker == Marker.TEE else Marker.SPACE
+                yield from draw(nodes[child], prefix=prefix + extension)
+
+    nodes = tree.nodes
+
+    lines = []
+    if show_level:
+        gap = " " * tree_space
+        lines.append(
+            gap.join("\b" * (len(str(l)) - 1) + str(l) for l in range(len(tree.levels)))
+        )
+        dash = "─" * tree_space
+        lines.append(dash.join("┼" for l in range(len(tree.levels))))
+
+    for root in tree.roots:
+        lines.append(root)
+        for ln in draw(nodes[root]):
+            lines.append(ln)
+
+    return "\n".join(lines)
