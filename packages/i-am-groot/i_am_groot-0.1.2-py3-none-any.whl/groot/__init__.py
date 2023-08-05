7b90f6a97e512aab2bb7bd249bd63367e08d5d0a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:07:43 +0000

    Add package i-am-groot/0.1.2/i_am_groot-0.1.2-py3-none-any.whl

---
+++ packages/i-am-groot/i_am_groot-0.1.2-py3-none-any.whl/groot/__init__.py
@@ -0,0 +1,19 @@
+"""
+The package helps manage hierarchical concept trees.
+
+Each tree can be serialized as a yaml file and each file can be loaded to
+construct the python class.
+
+Goals
+-----
+* Save the trees in most compact format
+* Be able to load the trees from serialized files
+* List all nodes
+* List nodes on a given depth
+* List all atomic nodes
+"""
+
+__version__ = "0.1.2"
+
+from . import functional, tree, utils
+from .tree import Tree
