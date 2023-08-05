e7880c7e1ef9e93953c8e7ddcdcc36b74d7a7bd5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:11:03 +0000

    Add package BlockFiRates/0.1.0/BlockFiRates-0.1.0.tar.gz

---
+++ packages/BlockFiRates/BlockFiRates-0.1.0.tar.gz/BlockFiRates-0.1.0/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['blockfirates']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['cloudscraper>=1.2.58,<2.0.0', 'lxml>=4.6.3,<5.0.0']
+
+setup_kwargs = {
+    'name': 'blockfirates',
+    'version': '0.1.0',
+    'description': 'Scrape the latest APY rates for BlockFi Interest Accounts',
+    'long_description': None,
+    'author': 'P.G. Ó Slatara',
+    'author_email': 'pgoslatara@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
