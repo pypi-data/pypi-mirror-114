f97674d1cfcb5ed4777f085dacc03ac3660796d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:07:33 +0000

    Add package eba-binomial/1.2/eba_binomial-1.2.tar.gz

---
+++ packages/eba-binomial/eba_binomial-1.2.tar.gz/eba_binomial-1.2/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='eba_binomial',
+      version='1.2',
+      description='Gaussian and Binomial distributions',
+      packages=['eba_binomial'],
+      author = 'Ebisa Asfaw',
+      author_email = 'lammiyad@yahoo.com',
+    zip_safe=False)
