c5eaf673ba39e8560f5503fa3ad5dd44e26ff040
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:00:45 +0000

    Add package chaotic-ngine/0.7.0/chaotic-ngine-0.7.0.tar.gz

---
+++ packages/chaotic-ngine/chaotic-ngine-0.7.0.tar.gz/chaotic-ngine-0.7.0/chaotic/log.py
@@ -0,0 +1,17 @@
+import os
+import sys
+import logging
+
+from dotenv import load_dotenv
+from pathlib import Path
+
+env_path = Path('.') / '.env'
+load_dotenv(dotenv_path=env_path)
+
+logging.basicConfig(
+    stream=sys.stdout,
+    level=os.environ.get('CHAOTIC_LOG_LEVEL', 'INFO').upper(),
+    format='%(asctime)s - %(name)s:%(levelname)s:%(message)s')
+
+log = logging.getLogger('chatic')
+log.debug('Init')
