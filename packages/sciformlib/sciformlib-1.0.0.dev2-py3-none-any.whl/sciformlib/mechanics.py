834d82cce807f934c6a6efe3f838eb2347db12f7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:05:53 +0000

    Add package sciformlib/1.0.0.dev2/sciformlib-1.0.0.dev2-py3-none-any.whl

---
+++ packages/sciformlib/sciformlib-1.0.0.dev2-py3-none-any.whl/sciformlib/mechanics.py
@@ -0,0 +1,132 @@
+#Formula Page
+
+import math
+
+
+#factorial
+def factorial(x):
+    if x < 0:
+        print("Invalid Input Value!")
+    elif x == 1 or x == 0:
+        return x
+    else:
+        i = 1
+        for a in range(1, x+1):
+            i *= a
+            a+=1
+        return i
+#fibonacci
+def fibonacci(x):
+    if x<0:
+        print("Incorrect Value")
+    elif x == 0:
+        return 0
+    elif x == 1 or x == 2:
+        return 1
+    else:
+        return fibonacci(x-1)+fibonacci(x-2)
+
+#sum of all positive integers 
+
+def sum_of_pos_int(x):
+    if x > 0:
+        a = 0
+        for i in range(0,x+1):
+            a += i
+        return a
+    else:
+        print("Incorrect value")
+
+#sum of positivr integers with power of x
+#by range of ri to re
+def sum_of_pos_int_pow(x,ri,re):
+    power = 0
+    base_num = x
+    rangea = ri
+    rangeb = re
+    a = 0 
+    for i in range(rangea,rangeb+1):
+        power = i
+        a += (base_num**power)
+    return a
+
+#Scientifics
+
+#Mechanics
+
+#Motion with constant velocity
+
+#Instantaneous velocity
+
+def instant_velocity(x,t):
+    v = x/t
+    return round(v,4)
+
+# Average Velocity
+
+def average_velocity(pos_init,pos_final,time_init,time_final):
+    v = (pos_final - pos_init) / (time_final - time_init)
+    return round(v,4)
+
+#Motion with constant acceleration
+
+#Displacement x
+
+def displacement_f1(vel_init,acce,time):
+    x = (vel_init*time) + (0.5*(acce*(time**2)))
+    return round(x,4)
+
+def displacement_f2(vel_init,vel_final,time):
+    x = ((vel_final+vel_init)/2)*time
+    return round(x,4)
+
+# final velocity vf
+
+def velocity_f_f1(vel_init,acce,time):
+    vf = vel_init+(acce*time)
+    return round(vf,4)
+    
+def velocity_f_f2(vel_init,acce,pos):
+    a = (vel_init**2) + (2*acce*pos)
+    vf = math.sqrt(a)
+    return round(vf,4)
+
+
+# acceleration acce
+
+def acceleration_f1(vel_init,vel_final,time):
+    acce = (vel_final - vel_init) / time
+    return round(acce,4)
+    
+def instant_acceleration(vel,time):
+    acce = vel / time
+    return round(acce,4)
+    
+def average_acceleration(vel_final,vel_init,time_final,time_init):
+    acce = (vel_final - vel_init) / (time_final - time_init)
+
+def height_vertical_f1(vel_init,time,g):
+    h = (vel_init*time) - (0.5*(g*(time**2)))
+    return round(h,4)
+    
+def height_vertical_f2(vel_final,vel_init,time):
+    h = ((vel_init-vel_final)/2) * time
+    return round(h,4)
+    
+def height_max(vel_init,g):
+    h_max = (vel_init**2) / (2*g)
+    return round(h_max,4)
+    
+def initial_velocity(g,h_max):
+    form = (2*g*h_max)
+    vel_init = math.sqrt(form)
+    return round(vel_init,4)
+
+def final_velocity_upward_f1(vel_init,g,time):
+    vf = vel_init - (g*time)
+    return round(vf, 4)
+
+
+
+
+
