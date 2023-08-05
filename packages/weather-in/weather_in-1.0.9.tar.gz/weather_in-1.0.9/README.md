d2e3710c9a1186942201a00fd01b06a4965ee355
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:10:49 +0000

    Add package weather-in/1.0.9/weather_in-1.0.9.tar.gz

---
+++ packages/weather-in/weather_in-1.0.9.tar.gz/weather_in-1.0.9/README.md
@@ -0,0 +1,23 @@
+# weather_in
+
+## What is weather_in?
+
+weather_in is a simple script for terminal use.  It utilizes the <a href = "https://openweathermap.org/current">Open Weather Map API</a> to provide quick, relevant and basic information regarding a specified locale's weather.
+
+## System Requirements
+
+* python3
+
+## Installation
+
+```pip install weather_in```
+
+## Usage
+
+To use weather_in:
+
+```weather_in City State Country (country is optional; defaults to USA)```
+
+(example: Pasadena California USA)
+
+*note: State should not be abbreviated*
---
+++ packages/weather-in/weather_in-1.0.9.tar.gz/weather_in-1.0.9/weather_in/cli.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+import requests
+import json
+import sys
+import weather_in
+from weather_in import __version__, api_key
+
+def main():
+    city = sys.argv[1]
+    state = sys.argv[2]
+    if sysargv[3]:
+        country = sys.argv[3]
+    else:
+        country = "USA"#1
+    url = "https://api.openweathermap.org/data/2.5/weather?q=%s,%s,%s&appid=%s&units=imperial" % (city, state, country, api_key)
+    try:
+        response = requests.get(url)
+        data = json.loads(response.text)
+        temp = data['main']['temp']
+        low = data['main']['temp_min']
+        high = data['main']['temp_max']
+        deets = data['weather'][0]['description']
+        print(str("the temp is: ")+str(temp))
+        print(str("low of: ")+str(low))
+        print(str("high of: ")+str(high))
+        print(str(deets))
+    except KeyError:
+        print("The API expects City State Country passed as arguments.  Example: Pasadena California USA (note: CA will not work)")
+    except:
+        print("Whoops, I'm not sure what happened here.  Good luck!")
+if __name__ == '__main__':
+    main()
