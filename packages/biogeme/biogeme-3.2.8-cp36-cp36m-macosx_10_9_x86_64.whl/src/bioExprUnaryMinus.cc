2d0143a0a5023baa53c4807043302d334b98d325
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:06:57 +0000

    Add package biogeme/3.2.8/biogeme-3.2.8-cp36-cp36m-macosx_10_9_x86_64.whl

---
+++ packages/biogeme/biogeme-3.2.8-cp36-cp36m-macosx_10_9_x86_64.whl/src/bioExprUnaryMinus.cc
@@ -0,0 +1,52 @@
+//-*-c++-*------------------------------------------------------------
+//
+// File name : bioExprUnaryMinus.cc
+// @date   Fri Apr 13 11:38:02 2018
+// @author Michel Bierlaire
+// @version Revision 1.0
+//
+//--------------------------------------------------------------------
+
+#include "bioExprUnaryMinus.h"
+#include "bioDebug.h"
+#include <sstream> 
+
+bioExprUnaryMinus::bioExprUnaryMinus(bioExpression* c) :
+  child(c) {
+  listOfChildren.push_back(c) ;
+}
+bioExprUnaryMinus::~bioExprUnaryMinus() {
+
+}
+
+const bioDerivatives* bioExprUnaryMinus::getValueAndDerivatives(std::vector<bioUInt> literalIds,
+							  bioBoolean gradient,
+							  bioBoolean hessian) {
+
+  if (gradient && theDerivatives.getSize() != literalIds.size()) {
+    theDerivatives.resize(literalIds.size()) ;
+  }
+
+  bioUInt n = literalIds.size() ;
+  const bioDerivatives* childResult = child->getValueAndDerivatives(literalIds,gradient,hessian) ;
+  theDerivatives.f = - childResult->f ;
+  if (gradient) {
+    for (bioUInt i = 0 ; i < n ; ++i) {
+      theDerivatives.g[i] = - childResult->g[i] ;
+      if (hessian) {
+	for (bioUInt j = 0 ; j < n ; ++j) {
+	  theDerivatives.h[i][j] = - childResult->h[i][j] ;
+	}
+      }
+    }
+  }
+  return &theDerivatives ;
+}
+
+bioString bioExprUnaryMinus::print(bioBoolean hp) const {
+  std::stringstream str ; 
+  str << "-" << child->print(hp) ;
+  return str.str() ;
+
+}
+
