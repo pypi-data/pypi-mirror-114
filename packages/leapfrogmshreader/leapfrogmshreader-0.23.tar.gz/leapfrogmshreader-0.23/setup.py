45b3d835bd2f6f9b5f12c107c119b26e46468d81
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:10:10 +0000

    Add package leapfrogmshreader/0.23/leapfrogmshreader-0.23.tar.gz

---
+++ packages/leapfrogmshreader/leapfrogmshreader-0.23.tar.gz/leapfrogmshreader-0.23/setup.py
@@ -0,0 +1,21 @@
+from distutils.core import setup
+setup(
+  name = 'leapfrogmshreader',        
+  packages = ['leapfrogmshreader'],  
+  version = '0.23',      
+  license='LGPL 3.0',       
+  description = 'A simple reader of leapfrog mesh files, without onerous installation requirements.',  
+  author = 'Thomas Martin',                  
+  author_email = 'tpm319@gmail.com',      
+  url = 'https://github.com/ThomasMGeo/leapfrogmshreader',     
+  keywords = ['leapfrog', 'mesh', '.msh'],   
+  install_requires=[   ],
+  classifiers=[
+    'Development Status :: 3 - Alpha',      
+    'Intended Audience :: Developers',      
+    'Topic :: Software Development :: Build Tools',  
+    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7',
+    'Programming Language :: Python :: 3.8',
+  ],
+)
