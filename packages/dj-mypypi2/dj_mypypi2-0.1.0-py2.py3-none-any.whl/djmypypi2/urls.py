5f9e0a131b1dc63f901decc3bd3ab8754d6f657f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:10:25 +0000

    Add package dj-mypypi2/0.1.0/dj_mypypi2-0.1.0-py2.py3-none-any.whl

---
+++ packages/dj-mypypi2/dj_mypypi2-0.1.0-py2.py3-none-any.whl/djmypypi2/urls.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"Application urls"
+from django.urls import path
+
+from . import views
+
+
+app_name = 'djmypypi2'
+urlpatterns = [
+    path('', views.PackageListView.as_view(), name='package-list'),
+    path('<package_name>/', views.PackageDetailView.as_view(), name='package-detail'),
+    path('@download/<archive_name>', views.download_package, name='download-package'),
+]
