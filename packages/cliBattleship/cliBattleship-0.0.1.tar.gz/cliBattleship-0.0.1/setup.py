55a2862bc8653cc9a9f06032709a7a9933b12118
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:11:04 +0000

    Add package cliBattleship/0.0.1/cliBattleship-0.0.1.tar.gz

---
+++ packages/cliBattleship/cliBattleship-0.0.1.tar.gz/cliBattleship-0.0.1/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.0.1'
+DESCRIPTION = 'A basic CLI program to play battleship against the computer'
+
+setup(
+    name='cliBattleship',
+    version=VERSION,
+    author='Tyson Van Patten',
+    author_email='tyson@descriptdata.com',
+    url='https://github.com/tvanpat/cli-battleship',
+    description=DESCRIPTION,
+    install_requires=[]
+)
