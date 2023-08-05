04ac1f911ff0bb538e0d087fbb2ddcfcb9fad5d2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:07:25 +0000

    Add package cterminal/1.0.1/cterminal-1.0.1.tar.gz

---
+++ packages/cterminal/cterminal-1.0.1.tar.gz/cterminal-1.0.1/cterminal/cio.py
@@ -0,0 +1,20 @@
+import os
+import platform
+
+if platform.system() == "Windows":
+	os.system("")
+
+class style:
+    BLACK = '\033[30m'
+    RED = '\033[31m'
+    GREEN = '\033[32m'
+    YELLOW = '\033[33m'
+    BLUE = '\033[34m'
+    MAGENTA = '\033[35m'
+    CYAN = '\033[36m'
+    WHITE = '\033[37m'
+    UNDERLINE = '\033[4m'
+    RESET = '\033[0m'
+
+def cprint(str_m):
+	print(str_m + style.RESET)
