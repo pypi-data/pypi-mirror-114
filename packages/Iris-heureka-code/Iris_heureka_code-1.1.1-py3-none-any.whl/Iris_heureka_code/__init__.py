bc849afa7b8300e70cb60c30305ce170efb4ffb6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:03:32 +0000

    Add package Iris-heureka-code/1.1.1/Iris_heureka_code-1.1.1-py3-none-any.whl

---
+++ packages/Iris-heureka-code/Iris_heureka_code-1.1.1-py3-none-any.whl/Iris_heureka_code/__init__.py
@@ -0,0 +1,8 @@
+from .Event import Event
+from .Handler import Handler
+from .StaticHandler import StaticHandler
+from .RecursiveHandler import RecursiveHandler
+
+
+__version__ = "1.1.1"
+__author__ = "heureka-code"
