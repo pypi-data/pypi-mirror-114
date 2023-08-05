fbddfc8081721cb94d3474d18a3f219ae8459898
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:59:35 +0000

    Add package mananeras/0.1.3/mananeras-0.1.3.tar.gz

---
+++ packages/mananeras/mananeras-0.1.3.tar.gz/mananeras-0.1.3/mananeras/__init__.py
@@ -0,0 +1,2 @@
+from mananeras.entities import Mananera, Participacion  # noqa
+from mananeras.reader import lee_mananera, lee_todas, todas  # noqa
---
+++ packages/mananeras/mananeras-0.1.3.tar.gz/mananeras-0.1.3/mananeras/dataset/download_articles.py
@@ -0,0 +1,12 @@
+from pathlib import Path
+from typing import List
+from urllib.request import urlretrieve
+
+
+def download_articles(url_list: List[str], download_folder):
+    download_folder = Path(download_folder)
+    download_folder.mkdir(parents=True, exist_ok=True)
+
+    for url in url_list:
+        _, _, name = url.rpartition("/")
+        urlretrieve(url, download_folder / f"{name}.html")
