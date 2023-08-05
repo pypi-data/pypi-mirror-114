6de5a534d47f47637b66a1539f84e0bf2ac60034
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:06:41 +0000

    Add package poetry-git-version-changer/1.0.2a0/poetry-git-version-changer-1.0.2a0.tar.gz

---
+++ packages/poetry-git-version-changer/poetry-git-version-changer-1.0.2a0.tar.gz/poetry-git-version-changer-1.0.2a0/setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['poetry_git_version_changer']
+
+package_data = \
+{'': ['*']}
+
+setup_kwargs = {
+    'name': 'poetry-git-version-changer',
+    'version': '1.0.2a0',
+    'description': 'An extension to support dynamic package versioning with poetry.',
+    'long_description': '```toml\n[tool.poetry-git-version-changer]\nenabled = true\nversion-file = "foo/__init__.py"\n```\n',
+    'author': 'Bijij',
+    'author_email': None,
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': 'https://github.com/bijij/unnamed-poetry-extension',
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
