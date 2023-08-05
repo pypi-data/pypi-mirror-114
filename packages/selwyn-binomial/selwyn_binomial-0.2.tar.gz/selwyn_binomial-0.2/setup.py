6645848188e663120d0888b1cfab28ae300c1e20
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:06:44 +0000

    Add package selwyn-binomial/0.2/selwyn_binomial-0.2.tar.gz

---
+++ packages/selwyn-binomial/selwyn_binomial-0.2.tar.gz/selwyn_binomial-0.2/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='selwyn_binomial', 
+        version='0.2',
+        description='package to calculate and visualise binomial distribution',
+        packages=['selwyn_binomial'],
+        author = 'rlimbu',
+        author_email = 'noreply@gmail.com',
+        zip_safe = False)
+
+
