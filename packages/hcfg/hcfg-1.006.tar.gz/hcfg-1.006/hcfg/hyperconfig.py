81d19e03d12309641e835bd7a77ad5f776cf61cb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:06:59 +0000

    Add package hcfg/1.006/hcfg-1.006.tar.gz

---
+++ packages/hcfg/hcfg-1.006.tar.gz/hcfg-1.006/hcfg/hyperconfig.py
@@ -0,0 +1,67 @@
+from os import system
+from hcfg.exceptions import *
+
+class hcfg():
+
+
+    def __init__(self) -> None:
+        pass
+    
+
+    @staticmethod
+    def readFile(fileName):
+        buffer = {}
+        if not fileName.endswith(".hcfg"):
+            raise hypFileError("[Errno 0] This is a not .hcfg file.")
+            
+
+        if not (open(fileName,"r")):
+            raise hypFileError("[Errno 1] File does not exist.")
+
+        with open(fileName,"r") as file:
+            i = 0
+            for line in file:
+                i += 1
+                object = line.split(":")
+                if len(object) <= 1:
+                    raise hypSyntaxError(f"[Errno 2] Syntax error [Line:{i}].")
+                name = object[0].strip(" ")
+                value = hcfg.assignValue(object[1])
+                buffer[name] = value
+            return buffer
+
+    @staticmethod
+    def saveFile(filename,object):
+        with open(filename, "w") as file:
+
+            if not filename.endswith(".hcfg"):
+                raise hypFileError("[Errno 0] This is a not .hcfg file.")
+            
+
+            for i in object:
+                string = f"{i} : {object[i]}\n"
+                file.writelines(string)
+                
+            
+        
+        
+
+    @staticmethod
+    def assignValue(value):
+        try:
+            val = int(value)
+            return val
+        except:
+            None
+        try:
+            val = str(value).strip().strip('"')
+            return val
+        except:
+            None
+        try:
+            val = bool(value)
+            return val
+        except:
+            None    
+
+                
