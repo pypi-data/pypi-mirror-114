84c5466a8aecbf9214215fcbc66f14b76188010f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:07:16 +0000

    Add package gpxcsv/0.2.3/gpxcsv-0.2.3-py3.9.egg

---
+++ packages/gpxcsv/gpxcsv-0.2.3-py3.9.egg/gpxcsv/__main__.py
@@ -0,0 +1,21 @@
+import argparse
+from . import gpxtofile
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('input_file',
+                        type=str,
+                        help='input gpx file, may be .gpx or .gpx.gz')
+    parser.add_argument('-o',
+                        '--output-file',
+                        type=str,
+                        help='output file name, optional',
+                        default=None)
+    parser.add_argument('-j', '--json', action='store_true', default=False)
+    args = parser.parse_args()
+    gpxtofile(args.input_file, args.output_file, json=args.json)
+
+
+if __name__ == '__main__':
+    main()
