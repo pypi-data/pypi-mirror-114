0d54dd05fe15870326aca4f64aec06258c417dec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:05:47 +0000

    Add package jlj/1.0/jlj-1.0.tar.gz

---
+++ packages/jlj/jlj-1.0.tar.gz/jlj-1.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+    name='jlj', #对外模块的名字
+    version='1.0', #版本号
+    description='第一个对外的模块，测试', #描述
+    author='jiaodengjie', #作者
+    author_email='1005563662@qq.com', #作者邮箱
+    py_modules=['jlj.jie1','jlj.jie2'] #发布的模块
+)
