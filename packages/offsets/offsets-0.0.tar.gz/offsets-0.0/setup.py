e95e0797d8a0a1b1a316bc791036cc2ef1ffac86
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:08:07 +0000

    Add package offsets/0.0/offsets-0.0.tar.gz

---
+++ packages/offsets/offsets-0.0.tar.gz/offsets-0.0/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='offsets',
+      version='0.0',
+      description='offsets csgo for python, autoupdate on, enjoy',
+      packages=['offsets'],
+      author_email='adamisaev9234@gmail.com',
+      zip_safe=False)
