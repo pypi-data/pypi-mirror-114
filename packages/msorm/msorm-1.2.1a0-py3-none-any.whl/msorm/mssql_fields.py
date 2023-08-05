9eb87a2aeac8cbb4680c0eed32c8dadf5ccd53e4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:02:59 +0000

    Add package msorm/1.2.1a0/msorm-1.2.1a0-py3-none-any.whl

---
+++ packages/msorm/msorm-1.2.1a0-py3-none-any.whl/msorm/mssql_fields.py
@@ -0,0 +1,90 @@
+import warnings
+
+from msorm import settings
+from msorm.exceptions import DeveloperToolsWarning
+
+warnings.filterwarnings("once", category=DeveloperToolsWarning)
+
+__filters__ = {
+    "gt": ">",
+    "gte": ">=",
+    "not": "!=",
+    "lt": "<",
+    "lte": "<=",
+    "like": " LIKE ",
+    "in": " IN ",
+    "not_in": " NOT IN "
+}
+__filtersf__ = {
+    "gt": lambda value: f"'f'",
+    "gte": lambda value: f"'f'",
+    "not": lambda value: f"'f'",
+    "lt": lambda value: f"'f'",
+    "lte": lambda value: f"'f'",
+    "like": lambda value: f"'f'",
+    "in": lambda value: f"""({",".join(tuple(f"'{i}'" for i in value))})""",
+    "not_in": lambda value: f"""({",".join(tuple(f"'{i}'" for i in value))})"""
+}
+
+
+class field:
+    __sub_instance__ = False
+    __field__ = "field"  # name of field
+    __properties__ = {"default": None,
+                      "null": True}
+
+    @staticmethod
+    def find_filter(field, value):
+        filed_args = field.split("__")
+        lenght = len(filed_args)
+        if lenght > 2:
+            raise ValueError(f"{field} field cannot be found in the Model")
+        name = filed_args.pop(0)
+        for item in filed_args:
+            filter = __filters__.get(item)
+            if filter:
+                return name + filter + "?"
+        return name + "=" + f"'{value}'"
+
+    def __init__(self, default=None, safe=True, null=True):
+        if not self.__sub_instance__ and safe:
+            warnings.warn("DO NOT FORGET, USING DIRECT field CLASS IS NOT SUITABLE FOR NORMAL USAGE",
+                          DeveloperToolsWarning)
+
+        self.value = default
+        self.null = null
+        self.__properties__["null"] = null
+        if default and safe:
+            self.__properties__["default"] = self.value = settings.MDC.get(self.__field__, self.__developer_field_produce)(default)
+
+    def __init_subclass__(cls, **kwargs):
+        cls.__sub_instance__ = True
+
+    @property
+    def value(self):
+
+        return self.__value
+
+    @value.setter
+    def value(self, val):
+
+        self.__value = val
+
+    @classmethod
+    def get_new(cls, *args, **kwargs):
+        new_field = cls(*args, **kwargs)
+        return new_field
+
+    def __developer_field_produce(self, val):
+        warnings.warn("DO NOT FORGET, USING DIRECT field CLASS IS NOT SUITABLE FOR NORMAL USAGE",
+                      DeveloperToolsWarning)
+        return val
+
+    def produce(self, val, field="field"):
+        if val is None and self.null:
+            return val
+
+        return settings.MDC.get(self.__field__, self.__developer_field_produce)(val, field)
+
+    def __str__(self):
+        return str(self.__value)
