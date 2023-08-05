47c4e48b6fe2b97a58074f3296d7fdeea90b6a02
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:06:40 +0000

    Add package PyCkin4/1.0.0/PyCkin4-1.0.0.tar.gz

---
+++ packages/PyCkin4/PyCkin4-1.0.0.tar.gz/PyCkin4-1.0.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+    name='PyCkin4',
+    version='1.0.0',
+    py_modules=['PyCkin4'],
+    author='bobi791027',
+    author_email='13513519246@139.com',
+    description='There are many useful functions.',
+)
