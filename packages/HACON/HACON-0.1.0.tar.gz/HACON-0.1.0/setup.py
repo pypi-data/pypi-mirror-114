b0b3723b8aa2a5787b3fe41be443dad8e0c246e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:51 +0000

    Add package HACON/0.1.0/HACON-0.1.0.tar.gz

---
+++ packages/HACON/HACON-0.1.0.tar.gz/HACON-0.1.0/setup.py
@@ -0,0 +1,42 @@
+from setuptools import setup
+
+import os
+from setuptools import setup
+
+with open('requirements.txt') as f:
+    required = f.read().splitlines()
+
+
+setup(name='HACON',
+version='0.1.0',
+description="""Lots of cyber security tool""",
+long_description="""
+# HACON
+Lots of cyber security tool
+# Install
+```
+pip3 install HACON
+```
+# Usage
+```python
+from hacon import HACON
+
+HACON.arguments("-h")
+```
+# Reminder
+Important Information and Reminder Information and programs in all repositories are created for testing purposes. Any legal responsibility belongs to the person or organization that uses it.
+
+""",
+long_description_content_type='text/markdown',
+url='https://githpipub.com/onuratakan/HACON',
+author='Onur Atakan ULUSOY',
+author_email='atadogan06@gmail.com',
+license='MIT',
+packages=["hacon"],
+package_dir={'':'src'},
+package_data={
+    "hacon": ["wordlists/*.txt"],
+},
+install_requires=required,
+python_requires='>=3.6',
+zip_safe=False)
