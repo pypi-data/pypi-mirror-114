65e1af492316e4a145e72ddc41a2d9450dfdb984
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:08:23 +0000

    Add package gplaycrawler/0.2.1/gplaycrawler-0.2.1-py3-none-any.whl

---
+++ packages/gplaycrawler/gplaycrawler-0.2.1-py3-none-any.whl/gplaycrawler/__main__.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+# set env vars (optional):
+# export PLAYSTORE_TOKEN='ya29.fooooo'
+# export PLAYSTORE_GSFID='1234567891234567890'
+# export HTTP_PROXY='http://localhost:8080'
+# export HTTPS_PROXY='http://localhost:8080'
+# export CURL_CA_BUNDLE='/usr/local/myproxy_info/cacert.pem'
+
+from gplaycrawler.main import main
+
+
+if __name__ == '__main__':
+    main()
