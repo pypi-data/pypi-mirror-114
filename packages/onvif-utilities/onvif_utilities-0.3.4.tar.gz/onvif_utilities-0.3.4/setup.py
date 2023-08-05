3691b4b48813cdff6de649dd5bd953f8e3f6e319
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:06:14 +0000

    Add package onvif-utilities/0.3.4/onvif_utilities-0.3.4.tar.gz

---
+++ packages/onvif-utilities/onvif_utilities-0.3.4.tar.gz/onvif_utilities-0.3.4/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['onvif_utilities']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['httpx>=0.17.1,<0.18.0', 'wsdiscovery>=2.0.0,<3.0.0', 'zeep>=4.0.0,<5.0.0']
+
+setup_kwargs = {
+    'name': 'onvif-utilities',
+    'version': '0.3.4',
+    'description': '',
+    'long_description': None,
+    'author': 'MohammadHossein',
+    'author_email': 'hos1377@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.9,<4.0',
+}
+
+
+setup(**setup_kwargs)
