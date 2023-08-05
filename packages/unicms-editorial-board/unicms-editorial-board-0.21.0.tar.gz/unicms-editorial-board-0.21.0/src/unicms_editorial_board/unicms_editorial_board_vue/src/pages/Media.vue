2e258f139db929a62357c49dc4b60f90c4fd89f5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:38 +0000

    Add package unicms-editorial-board/0.21.0/unicms-editorial-board-0.21.0.tar.gz

---
+++ packages/unicms-editorial-board/unicms-editorial-board-0.21.0.tar.gz/unicms-editorial-board-0.21.0/src/unicms_editorial_board/unicms_editorial_board_vue/src/pages/Media.vue
@@ -0,0 +1,72 @@
+<template>
+    <CompleteTable
+        :api_source="api_source"
+        :fields="fields"
+        :files="files"
+        :ordering="ordering"
+        :ordering_list="ordering_list"
+        :page_title="page_title"
+        :showIsActive="false" >
+
+        <template #add_new>
+            <router-link :to="{ name: 'MediaNew'}"
+                class="btn btn-success">
+                <b-icon icon="plus-circle"
+                        variant="white"></b-icon>
+                Add new
+            </router-link>
+        </template>
+
+        <template #actions="item">
+            <router-link :to="{ name: 'MediaEdit',
+                            params: { media_id: item.item.id }}"
+                class="btn-block btn btn-sm btn-info">
+                <b-icon icon="pencil-square"
+                    variant="white"></b-icon>
+                Edit
+            </router-link>
+            <router-link :to="{ name: 'MediaLocks',
+                                params: { media_id: item.item.id }}"
+                class="btn btn-block btn-sm btn-secondary">
+                <b-icon icon="lock"
+                    variant="white"></b-icon>
+                Allowed users
+            </router-link>
+        </template>
+
+    </CompleteTable>
+</template>
+<script>
+import CompleteTable from '../layout/CompleteTable.vue'
+
+export default {
+    components: {
+        CompleteTable
+    },
+    data () {
+        return {
+            api_source: '/api/editorial-board/medias/',
+            collection_id: this.$route.params.collection_id,
+            fields: [
+                'id',
+                'title',
+                {key: 'file', thClass: 'w-25'},
+                'description',
+                {key: 'file_type', label: 'Type'},
+                {key: 'file_size', label: 'Size (kb)'},
+                {key: 'file_dimensions', label: 'Dimensions'},
+                'actions'
+            ],
+            files: ['file'],
+            ordering: 'title',
+            ordering_list: [
+                { text: 'Id', value: 'id' },
+                { text: 'Title', value: 'title' },
+                { text: 'Description', value: 'description' },
+                { text: 'File type', value: 'file_type' },
+            ],
+            page_title: 'Media'
+        }
+    }
+}
+</script>
