9ee09a6b2ac6ee3dba4d76b93c01dac7a3604eab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:26 +0000

    Add package awscli-update/0.1.1/awscli-update-0.1.1.tar.gz

---
+++ packages/awscli-update/awscli-update-0.1.1.tar.gz/awscli-update-0.1.1/setup.py
@@ -0,0 +1,30 @@
+# pylint: disable=missing-module-docstring
+# pylint: disable=redefined-builtin
+
+from setuptools import setup, find_packages
+import versioneer
+
+
+with open('README.md') as f:
+    readme = f.read()
+
+with open('LICENSE') as f:
+    license = f.read()
+
+with open('requirements.txt') as f:
+    required = f.read().splitlines()
+
+setup(
+    name='awscli-update',
+    version=versioneer.get_version(),
+    cmdclass=versioneer.get_cmdclass(),
+    description='CLI tool to update AWS CLI 2',
+    long_description=readme,
+    author='Armin Grodon',
+    author_email='me@armingrodon.de',
+    url='https://github.com/x4121/awscli-update',
+    license=license,
+    packages=find_packages(exclude=('tests', 'docs')),
+    scripts=['bin/awscli-update'],
+    install_requires=required
+)
