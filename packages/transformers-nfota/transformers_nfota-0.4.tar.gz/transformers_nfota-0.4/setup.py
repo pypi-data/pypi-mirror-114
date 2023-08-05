63ab5263876cd79921ff785152a9e0af55cd779f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:05:53 +0000

    Add package transformers-nfota/0.4/transformers_nfota-0.4.tar.gz

---
+++ packages/transformers-nfota/transformers_nfota-0.4.tar.gz/transformers_nfota-0.4/setup.py
@@ -0,0 +1,15 @@
+# TODO: Fill out this file with information about your package
+
+# HINT: Go back to the object-oriented programming lesson "Putting Code on PyPi" and "Exercise: Upload to PyPi"
+
+# HINT: Here is an example of a setup.py file
+# https://packaging.python.org/tutorials/packaging-projects/
+
+from setuptools import setup
+
+setup(name='transformers_nfota',
+      version='0.4',
+      description='Various Transformer Models',
+      long_description = 'Various Transformer Models',
+      packages=['transformers_nfota'],
+      zip_safe=False)
