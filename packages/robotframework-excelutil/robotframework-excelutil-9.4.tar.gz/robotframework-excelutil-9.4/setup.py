bd45ec4fafbf389fcf43869db6a329a4d9f32621
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:59:15 +0000

    Add package robotframework-excelutil/9.4/robotframework-excelutil-9.4.tar.gz

---
+++ packages/robotframework-excelutil/robotframework-excelutil-9.4.tar.gz/robotframework-excelutil-9.4/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+with open("README.txt", "r") as fh:
+	long_description = fh.read()
+
+setup(
+
+    name='robotframework-excelutil',
+    version='9.4',
+    description='Robotframework library for excel xlsx file format',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://www.linkedin.com/in/nagesh-nagaraja-mba-pmp-csm-33515130/',
+    author='Nagesh B Nagaraja Rao',
+    author_email='nagesh.nagaraja@gmail.com',
+    packages=find_packages(),
+    install_requires=['openpyxl']
+    
+)
