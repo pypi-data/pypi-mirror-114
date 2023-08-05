579e5d08c91cf95438b671151dcc45c00f7f1dcb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:06:05 +0000

    Add package football-betting-models/0.0.2/football-betting-models-0.0.2.tar.gz

---
+++ packages/football-betting-models/football-betting-models-0.0.2.tar.gz/football-betting-models-0.0.2/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup
+from setuptools import find_packages
+
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name='football-betting-models',
+    version='0.0.2',
+    author='MikeyJW',
+    author_email='michael.watson@whoknowswins.com',
+    description='Seeding models package',
+    url='https://github.com/MikeyJW/football_betting_models',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent'
+    ],
+    packages=['betting_models'],
+    install_requires=[
+        'numpy==1.20.3',
+        'scipy==1.6.2',
+        'matplotlib==3.3.4',
+        'seaborn==0.11.1',
+        'pandas==1.2.5'
+    ],
+    python_requires='>=3.8'
+)
