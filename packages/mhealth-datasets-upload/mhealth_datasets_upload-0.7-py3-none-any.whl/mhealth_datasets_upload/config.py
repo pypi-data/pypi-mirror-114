f0757e450731911f822dd3c4b53c738a2093040e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:11:21 +0000

    Add package mhealth-datasets-upload/0.7/mhealth_datasets_upload-0.7-py3-none-any.whl

---
+++ packages/mhealth-datasets-upload/mhealth_datasets_upload-0.7-py3-none-any.whl/mhealth_datasets_upload/config.py
@@ -0,0 +1,18 @@
+outFold = 'C:/mhealth_data'
+dataFold = 'DCIM/Movie'
+# project_name = '/DATASET_PROJECT'
+
+project_name = '/work/mhealthresearchgroup/DATASET_PROJECT'
+
+
+# host = 'wockets.ccs.neu.edu'
+# port = 22
+# username = 'sftpuser'
+# keyfile_path = None
+# password = 'mHealth@NEU'
+
+host = 'xfer.discovery.neu.edu'
+port = 22
+username = 'thapachhetry.b'
+keyfile_path = None
+password = 'Anjalika1.'
