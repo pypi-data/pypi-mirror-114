bd6fe1a8352e3003b65d6d0b782fdd4af425f9ce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:04:32 +0000

    Add package helipad/1.3.2/helipad-1.3.2.tar.gz

---
+++ packages/helipad/helipad-1.3.2.tar.gz/helipad-1.3.2/helipad/helpers.py
@@ -0,0 +1,70 @@
+# ==========
+# Helper classes and functions used in multiple modules
+# None of these are intended to be user-facing
+# ==========
+
+import warnings
+
+#Pass True to check for any Ipython environment, including Spyder, for event loop purposes.
+#Otherwise check specifically whether it's an interactive notebook. However,  get_ipython() comes back
+#undefined inside callbacks. So cache the value once, the first time it runs.
+def isIpy(atall=False):
+	if atall:
+		try:
+			__IPYTHON__
+			return True
+		except NameError: return False
+	
+	if not '__helipad_ipy' in globals():
+		try:
+			globals()['__helipad_ipy'] = 'InteractiveShell' in get_ipython().__class__.__name__
+		except NameError: globals()['__helipad_ipy'] = False
+	
+	return __helipad_ipy
+
+#Generic extensible item class to store structured data
+class Item:
+	def __init__(self, **kwargs):
+		for k,v in kwargs.items():
+			setattr(self, k, v)
+
+#To handle deprecated parameter names
+class dictLike(dict):
+	def __getitem__(self, index): return super().__getitem__(self.normalize(index))
+	def __setitem__(self, index, value): super().__setitem__(self.normalize(index), value)
+	def normalize(self, index):
+		#Remove in Helipad 1.4
+		if index=='updateEvery':
+			index = 'refresh'
+			warnings.warn('The \'updateEvery\' parameter has been renamed to \'refresh\'. The ability to refer to \'updateEvery\' is deprecated and will be removed in a future version.', None, 3)
+		return index
+
+import colorsys, matplotlib.colors as mplcolor
+class Color:
+	def __init__(self, color):
+		#Can take a hex string, color name, or [r,g,b] list/tuple.
+		self.rgb = mplcolor.hex2color(color) if isinstance(color, str) else list(color)
+	
+	@property
+	def hex(self): return mplcolor.to_hex(self.rgb)
+	@property
+	def hsv(self): return list(mplcolor.rgb_to_hsv(self.rgb))
+	@property
+	def r(self): return self.rgb[0]
+	@property
+	def g(self): return self.rgb[1]
+	@property
+	def b(self): return self.rgb[2]
+	@property
+	def h(self): return self.hsv[0]
+	@property
+	def s(self): return self.hsv[1]
+	@property
+	def v(self): return self.hsv[2]
+	
+	def lighten(self, factor=3):
+		hls = colorsys.rgb_to_hls(*self.rgb)
+		return Color(colorsys.hls_to_rgb(hls[0], (1-1/factor) + hls[1]/factor, hls[2]))
+	
+	def blend(self, color2):
+		return Color(((self.r+color2.r)/2, (self.g+color2.g)/2, (self.b+color2.b)/2))
---
+++ packages/helipad/helipad-1.3.2.tar.gz/helipad-1.3.2/sample-models/crime.py
@@ -0,0 +1,56 @@
+#===============
+# SETUP
+#===============
+
+from helipad import Helipad, MultiLevel, Agent
+from math import floor
+from random import uniform, choice
+
+heli = Helipad()
+heli.name = 'Illicit Supply Chains'
+heli.order = 'random'
+
+heli.addPrimitive('firm', MultiLevel, dflt=20, priority=1)
+heli.removePrimitive('agent')
+mapPlot = heli.spatial(x=5, y=1, wrap=False)
+mapPlot.config({
+	'patchProperty': 'x',
+	'patchAspect': 2
+})
+
+#Initialize submodels
+@heli.hook
+def firmInit(firm, model):
+	firm.param('num_agent', 20)
+
+def firmsize(self): return len(self.agents['agent'])*5
+MultiLevel.size = property(firmsize)
+mapPlot.config('agentSize', 'size')
+
+#===============
+# BEHAVIOR
+#===============
+
+heli.param('num_firm', 20)
+
+@heli.hook
+def firmStep(firm, model, stage):
+	if len(firm.agents['agent']):
+		choice(firm.agents['agent']).transfer(choice(list(firm.patch.agentsOn))) #One agent moves to a random firm on the same patch
+
+@heli.hook
+def modelPostSetup(model):
+	for n,a in enumerate(model.agents['firm']):
+		stage = floor(n/4)
+		a.moveTo(uniform(stage-0.4, stage+0.4), uniform(-0.5, 0.5))
+	
+	#Create links after all firms have been assigned to a stage
+	for f in model.agents['firm']:
+		if f.patch.x > 0: f.newEdge(choice(list(f.patch.left.agentsOn)))
+
+
+#===============
+# LAUNCH THE GUI
+#===============
+
+heli.launchCpanel()
