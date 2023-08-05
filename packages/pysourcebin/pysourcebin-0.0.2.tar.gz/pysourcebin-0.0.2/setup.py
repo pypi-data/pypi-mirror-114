5b0d768ab41480c65f37c1b2b7b66ef7d5582d0c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:10:17 +0000

    Add package pysourcebin/0.0.2/pysourcebin-0.0.2.tar.gz

---
+++ packages/pysourcebin/pysourcebin-0.0.2.tar.gz/pysourcebin-0.0.2/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+  name='pysourcebin',
+  version='0.0.2',
+  description='a sourcebin api wrapper',
+  long_description=open('README.txt').read(),
+  url='',
+  author='Amashi',
+  author_email='amashisenpai386@gmail.com',
+  license='MIT',
+  classifiers=[
+  'Development Status :: 5 - Production/Stable',
+  'Intended Audience :: Education',
+  'Operating System :: Microsoft :: Windows :: Windows 10',
+  'License :: OSI Approved :: MIT License',
+  'Programming Language :: Python :: 3'
+],
+  keywords=['sourcebin', 'source', 'bin'],
+  packages=find_packages(),
+  install_requires=['']
+)
