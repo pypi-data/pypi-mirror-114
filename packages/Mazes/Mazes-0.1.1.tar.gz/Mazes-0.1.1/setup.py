939691440e6c1c3cbb93a3cd1c9bf85a9d7cbb41
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:54 +0000

    Add package Mazes/0.1.1/Mazes-0.1.1.tar.gz

---
+++ packages/Mazes/Mazes-0.1.1.tar.gz/Mazes-0.1.1/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+
+setup(
+    name="Mazes",
+    version="0.1.1",
+    install_requires=["colorama"],
+    packages=["mazes"],
+    long_description=open('README.md', 'r').read(),
+    long_description_content_type="text/markdown",
+    description="Generates and solves rectangular mazes in the terminal. Comes with Python API.",
+    entry_points={
+        'console_scripts': [
+            'mazes = mazes.cli:cli',
+        ],
+    },
+)
