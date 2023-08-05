6248fe7c46a09b02820a06d0770a824fab5d299e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:46 +0000

    Add package fatFuckSQL/0.1.5/fatFuckSQL-0.1.5.tar.gz

---
+++ packages/fatFuckSQL/fatFuckSQL-0.1.5.tar.gz/fatFuckSQL-0.1.5/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open('README.md', 'r') as rm:
+    ld = rm.read()
+
+setuptools.setup(
+    name='fatFuckSQL',
+    version='0.1.5',
+    author='tsunyoku',
+    author_email='tsunyoku@gmail.com',
+    description='Light wrapper for asyncpg and aiomysql',
+    long_description=ld,
+    long_description_content_type='text/markdown',
+    url='https://github.com/tsunyoku/fatFuckSQL',
+    packages=setuptools.find_packages(),
+    install_requires=['asyncpg', 'aiomysql'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.8'
+)
