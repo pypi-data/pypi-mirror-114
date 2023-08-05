78ee98af59e19907839c1835693b24061ca458cb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:02:29 +0000

    Add package JGB-distributions/1.3/JGB_distributions-1.3.tar.gz

---
+++ packages/JGB-distributions/JGB_distributions-1.3.tar.gz/JGB_distributions-1.3/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='JGB_distributions',
+      version='1.3',
+      description='JGB_distributions is a Python library for dealing with Gaussian (normal) and Binomial Probability distributions.',
+      packages=['JGB_distributions'],
+      author = 'Mohamed Youssef',
+      author_email = 'm.youssef315@yahoo.com',
+      zip_safe=False)
