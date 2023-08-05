5dc8c90b19a3d50871a282add8723a30f21df35f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:06:14 +0000

    Add package Aptus/3.0.1/Aptus-3.0.1.tar.gz

---
+++ packages/Aptus/Aptus-3.0.1.tar.gz/Aptus-3.0.1/test/test_options.py
@@ -0,0 +1,48 @@
+import unittest
+
+from aptus.options import *
+
+class OptionsTestTarget:
+    def __init__(self):
+        self.angle = 0
+        self.continuous = False
+        self.iter_limit = 10
+        self.outfile = None
+        self.size = [100, 100]
+
+class OptionsTest(unittest.TestCase):
+
+    def try_read_args(self, cmdline):
+        argv = cmdline.split()
+        target = OptionsTestTarget()
+        AptusOptions(target).read_args(argv)
+        return target
+
+    def testNoArgs(self):
+        target = self.try_read_args("")
+        self.assertEqual(target.angle, 0)
+        self.assertEqual(target.continuous, False)
+
+    def testSize(self):
+        target = self.try_read_args("-s 300x200")
+        self.assertEqual(target.size, [300, 200])
+        target = self.try_read_args("--size 300x200")
+        self.assertEqual(target.size, [300, 200])
+        target = self.try_read_args("--size=300x200")
+        self.assertEqual(target.size, [300, 200])
+        target = self.try_read_args("-s 300,200")
+        self.assertEqual(target.size, [300, 200])
+        target = self.try_read_args("--size 300,200")
+        self.assertEqual(target.size, [300, 200])
+        target = self.try_read_args("--size=300,200")
+        self.assertEqual(target.size, [300, 200])
+
+    def testMisc(self):
+        target = self.try_read_args("-c")
+        self.assertEqual(target.continuous, True)
+
+    def testFloatPair(self):
+        target = self.try_read_args("--center=1.5x2.5")
+        self.assertEqual(target.center, [1.5, 2.5])
+        target = self.try_read_args("--center=1.275")
+        self.assertEqual(target.center, [1.275, 1.275])
