203f6d975596d30046969d599551e3ecb133922e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:03:49 +0000

    Add package pyCSGO/1.0/pyCSGO-1.0.tar.gz

---
+++ packages/pyCSGO/pyCSGO-1.0.tar.gz/pyCSGO-1.0/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='pyCSGO',
+      version='1.0',
+      description='offsets csgo for python, autoupdate on, enjoy',
+      packages=['pyCSGO'],
+      author_email='adamisaev9234@gmail.com',
+      zip_safe=False)
