9dfda4d8b27ad53c345490adb2ff48043c627733
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:10:37 +0000

    Add package cbd-client/0.0.4/cbd_client-0.0.4-py3-none-any.whl

---
+++ packages/cbd-client/cbd_client-0.0.4-py3-none-any.whl/cbd_client/client.py
@@ -0,0 +1,81 @@
+"""Includes set of functions for handling the CDB api."""
+import urllib
+from urllib.parse import urljoin, urlparse
+
+import requests  # type: ignore
+
+
+def _uri_validator(url: str) -> bool:
+    """Checks if given str is correct uri."""
+    try:
+        result = urlparse(url)
+        return all([result.scheme, result.netloc]) and " " not in url
+    except ValueError:
+        return False
+
+
+def _prepare_failed_response(res: requests.models.Response) -> str:
+    """Prepares human readable version of failed response."""
+    return (
+        f"Unsuccessful request! \n\t"
+        f" error code: {str(res.status_code)}, \n\t"
+        f" response content {str(res.content)}"
+    )
+
+
+class CBDClient:
+    """Class to handle communication with the CBD api."""
+
+    def __init__(self, url: str) -> None:
+        """Initialises the object."""
+        if not _uri_validator(url):
+            raise ValueError(f"String {url} is not valid URL!")
+        self.url = url
+
+    def check_connection(self) -> bool:
+        """Check if app is accessible from given url in init."""
+        if urllib.request.urlopen(self.url).getcode() == 200:
+            return True
+        else:
+            return False
+
+    def hello(self) -> str:
+        """Gets the api's welcome message."""
+        return requests.get(self.url).text
+
+    def classify_string(self, text: str) -> str:
+        """
+        Performs classification on given string.
+
+        The text will be handled by application and classified into one of three classes,
+        non-harmful, cyberbullying, hate_speech
+
+        :param text: the text to classify
+
+        :return: human readable class that has been assigned to given text
+        """
+        _url = urljoin(self.url, "classify_string")
+        res = requests.post(_url, json={"text": text})
+        if res.ok:
+            return res.json()["type"]
+        else:
+            return _prepare_failed_response(res)
+
+    def get_model_info(self, prop: str) -> str:
+        """
+        Gets information about given parameter of the model.
+
+        Function just ask api to print out details of the given parameter of the model.
+        In most cases it doesn't exist so the response will not be 200.
+
+        :param prop: property of model to ask api about
+
+        :return: the string representation of value of the property
+        """
+        _url = urljoin(self.url, "get_model_info/")
+        _url = urljoin(_url, prop)
+        res = requests.get(_url)
+        if res.ok:
+            return res.json()["property"]
+        else:
+            return _prepare_failed_response(res)
