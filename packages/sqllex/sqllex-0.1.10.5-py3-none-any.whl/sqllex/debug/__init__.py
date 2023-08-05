5696756dc1fe43fa5a959f5b524f8ba3877e6324
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:11:40 +0000

    Add package sqllex/0.1.10.5/sqllex-0.1.10.5-py3-none-any.whl

---
+++ packages/sqllex/sqllex-0.1.10.5-py3-none-any.whl/sqllex/debug/__init__.py
@@ -0,0 +1,10 @@
+from sqllex.debug.logger import logger, debug_mode
+
+# logger.add(sys.stderr, level="INFO")
+# logger.configure(handlers='')
+
+
+__all__ = [
+    'logger',  # lgtm [py/undefined-export]
+    'debug_mode'  # lgtm [py/undefined-export]
+]
---
+++ packages/sqllex/sqllex-0.1.10.5-py3-none-any.whl/sqllex/types/__init__.py
@@ -0,0 +1,36 @@
+from sqllex.types.types import *
+
+__all__ = [
+    # sql
+    'SQLRequest',  # lgtm [py/undefined-export]
+    'SQLStatement',  # lgtm [py/undefined-export]
+    'ForeignKey',  # lgtm [py/undefined-export]
+    'DataType',  # lgtm [py/undefined-export]
+    'ConstantType',  # lgtm [py/undefined-export]
+    'ListDataType',  # lgtm [py/undefined-export]
+    'ColumnDataType',  # lgtm [py/undefined-export]
+    'ColumnsType',  # lgtm [py/undefined-export]
+    'DBTemplateType',  # lgtm [py/undefined-export]
+    'PathType',  # lgtm [py/undefined-export]
+    'NumStr',  # lgtm [py/undefined-export]
+    'InsertData',  # lgtm [py/undefined-export]
+    'OrOptionsType',  # lgtm [py/undefined-export]
+    'WithType',  # lgtm [py/undefined-export]
+    'WhereType',  # lgtm [py/undefined-export]
+    'OrderByType',  # lgtm [py/undefined-export]
+    'LimitOffsetType',  # lgtm [py/undefined-export]
+    'JoinType',  # lgtm [py/undefined-export]
+    'JoinArgType',  # lgtm [py/undefined-export]
+
+    # typing
+    'Literal',  # lgtm [py/undefined-export]
+    'Mapping',  # lgtm [py/undefined-export]
+    'Union',  # lgtm [py/undefined-export]
+    'List',  # lgtm [py/undefined-export]
+    'AnyStr',  # lgtm [py/undefined-export]
+    'Any',  # lgtm [py/undefined-export]
+    'Tuple',  # lgtm [py/undefined-export]
+    'Generator',  # lgtm [py/undefined-export]
+    'Iterable',  # lgtm [py/undefined-export]
+    'Sized',  # lgtm [py/undefined-export]
+]
