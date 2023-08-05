107fa44bc92807df40a5e5802d32e8c49c6c1473
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:07:16 +0000

    Add package graph-dfs/0.0.1/graph-dfs-0.0.1.tar.gz

---
+++ packages/graph-dfs/graph-dfs-0.0.1.tar.gz/graph-dfs-0.0.1/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup
+
+with open('README.md') as f:
+    long_description = f.read()
+
+setup(
+    name='graph-dfs',
+    version='0.0.1',
+    description='Module for creating graphs and performing depth first search.',
+    py_modules=['dfs'],
+    package_dir={'': 'src'},
+    url="https://github.com/daniel-ufabc/graph-dfs",
+    author="Daniel M. Martin",
+    author_email="danielmmartin@gmail.com",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.9",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent"
+    ],
+    long_description=long_description,
+    extra_requires={
+        'dev': [
+            'pytest>=3.7',
+        ]
+    }
+)
