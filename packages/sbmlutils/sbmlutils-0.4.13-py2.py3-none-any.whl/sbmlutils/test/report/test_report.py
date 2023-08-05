38177d5384e967625c4f0ff044a8a40da10af1fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:11:06 +0000

    Add package sbmlutils/0.4.13/sbmlutils-0.4.13-py2.py3-none-any.whl

---
+++ packages/sbmlutils/sbmlutils-0.4.13-py2.py3-none-any.whl/sbmlutils/test/report/test_report.py
@@ -0,0 +1,38 @@
+"""Test SBML report."""
+from pathlib import Path
+
+import pytest
+
+from sbmlutils.report import sbmlreport
+from sbmlutils.test import ALL_SBML_PATHS, GLUCOSE_SBML, GZ_SBML, sbml_paths_idfn
+
+
+def check_report(sbml_path: Path, tmp_path: Path, validate: bool) -> None:
+    """Check report creation for given SBML."""
+    d = sbmlreport.create_report(
+        sbml_path=sbml_path, output_dir=tmp_path, validate=validate
+    )
+
+    # check that created
+    assert d
+
+
+@pytest.mark.parametrize("sbml_path", [GZ_SBML], ids=sbml_paths_idfn)
+def test_report_gz(sbml_path: Path, tmp_path: Path) -> None:
+    """Test report generation for compressed models."""
+    check_report(sbml_path=sbml_path, tmp_path=tmp_path, validate=False)
+
+
+@pytest.mark.parametrize("sbml_path", [GLUCOSE_SBML], ids=sbml_paths_idfn)
+def test_serialization(sbml_path: Path, tmp_path: Path) -> None:
+    """Test report generation for compressed models."""
+    d = sbmlreport.create_report(
+        sbml_path=sbml_path, output_dir=tmp_path, validate=False
+    )
+    assert d
+
+
+@pytest.mark.parametrize("sbml_path", ALL_SBML_PATHS, ids=sbml_paths_idfn)
+def test_report(sbml_path: Path, tmp_path: Path) -> None:
+    """Test creation of report with Latex Math."""
+    check_report(sbml_path=sbml_path, tmp_path=tmp_path, validate=False)
