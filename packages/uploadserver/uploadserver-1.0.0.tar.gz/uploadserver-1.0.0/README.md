0b64f522b385db23aa607c0a2ddc5d3bfce460ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:06:31 +0000

    Add package uploadserver/1.0.0/uploadserver-1.0.0.tar.gz

---
+++ packages/uploadserver/uploadserver-1.0.0.tar.gz/uploadserver-1.0.0/README.md
@@ -0,0 +1,66 @@
+# uploadserver
+
+Python's http.server extended to include a file upload page
+
+[![License: MIT](https://img.shields.io/badge/license-MIT-blue.svg)](https://mit-license.org/)
+[![Build Status](https://travis-ci.com/Densaugeo/uploadserver.svg?branch=master)](https://travis-ci.com/github/Densaugeo/uploadserver)
+
+## Installation
+
+~~~
+python3 -m pip install --user uploadserver
+~~~
+
+## Usage
+
+~~~
+python3 -m uploadserver
+~~~
+
+Accepts the same options as [http.server](https://docs.python.org/3/library/http.server.html), plus a couple extras (documented below).
+
+After the server starts, the upload page is at /upload. For example, if the server is running at http://localhost:8000/ go to http://localhost:8000/upload .
+
+Warning: This is an upload server, and running it will allow uploads. Uploaded files will replace existing files with the same name.
+
+Now supports uploading multiple files at once! Select multiple files in the web page's file selector, or upload with cURL:
+~~~
+curl http://127.0.0.1:8000/upload -F files=@multiple-example-1.txt -F files=@multiple-example-2.txt
+~~~
+
+## Token Option
+
+Run with a simple token.
+~~~
+python3 -m uploadserver -t helloworld
+~~~
+
+Now you can upload a file with the token. For example:
+`curl http://127.0.0.1:8000/upload -F "files=@token-example.txt" -F 'token=helloworld'`
+
+Uploads without the token will be rejected. Tokens can be stolen if sent in plain HTTP, so this option is best used with HTTPS.
+
+## HTTPS Option
+
+Run with HTTPS:
+~~~
+# Generate self-signed certificate
+openssl req -x509 -out localhost.pem -keyout localhost.pem -newkey rsa:2048 -nodes -sha256 -subj '/CN=localhost'
+
+# The server root should not contain the certificate, for security reasons
+cd server-root
+python3 -m uploadserver -c localhost.pem
+~~~
+
+Note: This uses a self-signed certificate which clients such as web browser and cURL will warn about. Most browsers will allow you to proceed after adding an exception, and cURL will work if given the -k option. Using your own certificate from a certificate authority will avoid these warnings.
+
+## Breaking Changes in 1.0.0
+
+- File field in upload form renamed from `file_1` to `files`, to reflect support for multiple file upload. Scripts using cURL will need to be upadted with the new field name.
+- Successful uploads now respond with 204 No Content instead of 200 OK, so that cURL will not default to printing the upload page at the terminal.
+
+## Credits
+
+Most of `uploadserver/__main__.py` was copied from Python's `http.server`.
+
+Thanks to lishoujun for sending the first pull request! (Added the token option.)
---
+++ packages/uploadserver/uploadserver-1.0.0.tar.gz/uploadserver-1.0.0/uploadserver/__main__.py
@@ -0,0 +1,148 @@
+import http.server, sys, argparse, uploadserver, ssl, pathlib
+
+if sys.version_info.major >= 3 and sys.version_info.minor >= 7:
+    import os, functools
+
+if sys.version_info.major >= 3 and sys.version_info.minor >= 8:
+    import contextlib
+
+def ssl_wrap(socket):
+    if sys.version_info.major >= 3 and sys.version_info.minor == 6:
+        server_root = pathlib.Path('.').resolve()
+    else:
+        server_root = pathlib.Path(args.directory).resolve()
+    
+    if server_root in pathlib.Path(args.certificate).resolve().parents:
+        print('Certificate \'{}\' is inside web server root \'{}\', exiting'.format(
+            args.certificate, server_root))
+        sys.exit(3)
+    
+    try:
+        return ssl.wrap_socket(socket, certfile=args.certificate, server_side=True)
+    except FileNotFoundError:
+        print('Certificate \'{}\' not found, exiting'.format(args.certificate))
+        sys.exit(4)
+    except ssl.SSLError as e:
+        print('SSL error loading certificate \'{}\': {}, exiting'.format(args.certificate, e))
+        sys.exit(5)
+
+if sys.version_info.major == 3 and 6 <= sys.version_info.minor <= 7:
+    from http.server import BaseHTTPRequestHandler
+    
+    # The only difference in http.server.test() between Python 3.6 and 3.7 is the default value of ServerClass
+    if sys.version_info.minor == 6: from http.server import HTTPServer as DefaultHTTPServer
+    else: from http.server import ThreadingHTTPServer as DefaultHTTPServer
+    
+    # Copy of http.server.test() from Python 3.7. ssl_wrap() call added and print statement updaed for HTTPS
+    def test(HandlerClass=BaseHTTPRequestHandler,
+             ServerClass=DefaultHTTPServer,
+             protocol="HTTP/1.0", port=8000, bind=""):
+        """Test the HTTP request handler class.
+        
+        This runs an HTTP server on port 8000 (or the port argument).
+        
+        """
+        server_address = (bind, port)
+        
+        HandlerClass.protocol_version = protocol
+        with ServerClass(server_address, HandlerClass) as httpd:
+            if args.certificate: httpd.socket = ssl_wrap(httpd.socket)
+            
+            sa = httpd.socket.getsockname()
+            serve_message = "Serving {proto} on {host} port {port} ({proto_lower}://{host}:{port}/) ..."
+            print(serve_message.format(host=sa[0], port=sa[1], proto=uploadserver.PROTOCOL, 
+                proto_lower=uploadserver.PROTOCOL.lower()))
+            try:
+                httpd.serve_forever()
+            except KeyboardInterrupt:
+                print("\nKeyboard interrupt received, exiting.")
+                sys.exit(0)
+else:
+    from http.server import BaseHTTPRequestHandler
+    from http.server import ThreadingHTTPServer
+    from http.server import _get_best_family
+    
+    # Copy of http.server.test() from Python 3.8. ssl_wrap() call added and print statement updaed for HTTPS
+    def test(HandlerClass=BaseHTTPRequestHandler,
+             ServerClass=ThreadingHTTPServer,
+             protocol="HTTP/1.0", port=8000, bind=None):
+        """Test the HTTP request handler class.
+        
+        This runs an HTTP server on port 8000 (or the port argument).
+        
+        """
+        ServerClass.address_family, addr = _get_best_family(bind, port)
+        
+        HandlerClass.protocol_version = protocol
+        with ServerClass(addr, HandlerClass) as httpd:
+            if args.certificate: httpd.socket = ssl_wrap(httpd.socket)
+            
+            host, port = httpd.socket.getsockname()[:2]
+            url_host = f'[{host}]' if ':' in host else host
+            print(
+                f"Serving {uploadserver.PROTOCOL} on {host} port {port} "
+                f"({uploadserver.PROTOCOL.lower()}://{url_host}:{port}/) ..."
+            )
+            try:
+                httpd.serve_forever()
+            except KeyboardInterrupt:
+                print("\nKeyboard interrupt received, exiting.")
+                sys.exit(0)
+
+if __name__ == '__main__':
+    # In Python 3.8, http.server.test() was altered to use None instead of '' as the default for its bind parameter
+    if sys.version_info.major <= 3 and sys.version_info.minor < 8:
+        bind_default = ''
+    else:
+        bind_default = None
+    
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--cgi', action='store_true', help='Run as CGI Server')
+    parser.add_argument('--bind', '-b', default=bind_default, metavar='ADDRESS',
+        help='Specify alternate bind address [default: all interfaces]')
+    parser.add_argument('port', action='store', default=8000, type=int, nargs='?',
+        help='Specify alternate port [default: 8000]')
+    parser.add_argument('--token', '-t', action='store', default='', type=str, nargs='?',
+        help='Specify alternate token [default: \'\']')
+    parser.add_argument('--certificate', '-c', action='store', default=None,
+        help='Specify certificate to use HTTPS [default: none]')
+
+    # Directory option was added to http.server in Python 3.7
+    if sys.version_info.major >= 3 and sys.version_info.minor >= 7:
+        parser.add_argument('--directory', '-d', default=os.getcwd(),
+            help='Specify alternative directory [default:current directory]')
+    args = parser.parse_args()
+    
+    uploadserver.TOKEN = args.token
+    uploadserver.PROTOCOL = 'HTTPS' if args.certificate else 'HTTP'
+    if args.cgi:
+        handler_class = uploadserver.CGIHTTPRequestHandler
+    elif sys.version_info.major >= 3 and sys.version_info.minor >= 7:
+        handler_class = functools.partial(uploadserver.SimpleHTTPRequestHandler, directory=args.directory)
+    else:
+        handler_class = uploadserver.SimpleHTTPRequestHandler
+    
+    print('File upload available at /upload')
+    
+    # This was added to http.server's main section in Python 3.8
+    if sys.version_info.major <= 3 and sys.version_info.minor < 8:
+        test(
+            HandlerClass=handler_class,
+            port=args.port,
+            bind=args.bind,
+        )
+    else:
+        class DualStackServer(http.server.ThreadingHTTPServer):
+            def server_bind(self):
+                # suppress exception when protocol is IPv4
+                with contextlib.suppress(Exception):
+                    self.socket.setsockopt(
+                        socket.IPPROTO_IPV6, socket.IPV6_V6ONLY, 0)
+                return super().server_bind()
+        
+        test(
+            HandlerClass=handler_class,
+            ServerClass=DualStackServer,
+            port=args.port,
+            bind=args.bind,
+        )
