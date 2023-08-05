11e25bcab25235e21b5817b601bf2517f69758c7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:06:57 +0000

    Add package biogeme/3.2.8/biogeme-3.2.8-cp37-cp37m-win_amd64.whl

---
+++ packages/biogeme/biogeme-3.2.8-cp37-cp37m-win_amd64.whl/src/bioExprGreater.cc
@@ -0,0 +1,71 @@
+//-*-c++-*------------------------------------------------------------
+//
+// File name : bioExprGreater.cc
+// @date   Thu Apr 19 07:27:30 2018
+// @author Michel Bierlaire
+// @version Revision 1.0
+//
+//--------------------------------------------------------------------
+
+#include "bioExprGreater.h"
+#include <sstream>
+#include "bioDebug.h"
+#include "bioExceptions.h"
+
+bioExprGreater::bioExprGreater(bioExpression* l, bioExpression* r) :
+  left(l), right(r) {
+
+  listOfChildren.push_back(l) ;
+  listOfChildren.push_back(r) ;
+}
+
+bioExprGreater::~bioExprGreater() {
+
+}
+
+const bioDerivatives* bioExprGreater::getValueAndDerivatives(std::vector<bioUInt> literalIds,
+							     bioBoolean gradient,
+							     bioBoolean hessian) {
+  
+  if (gradient && theDerivatives.getSize() != literalIds.size()) {
+    theDerivatives.resize(literalIds.size()) ;
+  }
+
+  if (gradient || hessian) {
+    if (containsLiterals(literalIds)) {
+      std::stringstream str ;
+      str << "Expression Greater is not differentiable" << std::endl ; 
+      throw bioExceptions(__FILE__,__LINE__,str.str())  ;
+    }
+  }
+  if (gradient) {
+    if (hessian) {
+      theDerivatives.setDerivativesToZero() ;
+    }
+    else {
+      theDerivatives.setGradientToZero() ;
+    }
+  }
+
+  
+  if (left->getValue() > right->getValue()) {
+    theDerivatives.f = 1.0 ;
+  }
+  else {
+    theDerivatives.f = 0.0 ;
+  }
+
+  
+  return &theDerivatives ;
+}
+
+bioString bioExprGreater::print(bioBoolean hp) const {
+  std::stringstream str ;
+  if (hp) {
+    str << ">(" << left->print(hp) << "," << right->print(hp) << ")" ;
+  }
+  else {
+    str << "(" << left->print(hp) << ">" << right->print(hp) << ")" ;
+  }
+  return str.str() ;
+}
