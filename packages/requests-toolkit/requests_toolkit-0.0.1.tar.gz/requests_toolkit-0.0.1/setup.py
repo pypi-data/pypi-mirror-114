7273bba3792d529954a6dfcc9a57f8e020004a60
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:06:16 +0000

    Add package requests-toolkit/0.0.1/requests_toolkit-0.0.1.tar.gz

---
+++ packages/requests-toolkit/requests_toolkit-0.0.1.tar.gz/requests_toolkit-0.0.1/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name='requests_toolkit',
+    version='0.0.1',
+    description='Decorators and other tools for handling common patterns with the python requests module.',
+    py_modules=["requests_toolkit"],
+    package_dir={'': 'src'},
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
+        "Operating System :: OS Independent",
+    ],
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    install_requires=[
+        "requests ~= 2.26.0",
+    ],
+    extras_require={
+        "dev": [
+            "pytest >= 6.2.4",
+        ]
+    },
+    url="https://github.com/benjaminCallaghan/requests_toolkit",
+    author="benjaminCallaghan",
+    author_email="benjamin.r.callaghan@gmail.com",
+)
---
+++ packages/requests-toolkit/requests_toolkit-0.0.1.tar.gz/requests_toolkit-0.0.1/tests/test_toolkit.py
@@ -0,0 +1,72 @@
+import unittest
+from unittest.mock import *
+import pytest
+import requests
+
+
+from requests_toolkit import (validate_keys, handle_http_error, ExpectedKeyError)
+import requests_toolkit
+
+
+class TestToolkit(unittest.TestCase):
+    class MockRequestsResponse:
+        def __init__(self, status_code: int, data: list or dict):
+            self.status_code = status_code
+            self.data = data
+            if self.status_code in (200, 300):
+                self.ok = True
+            elif self.status_code in (400, 500):
+                self.ok = False
+
+        def json(self):
+            return self.data
+
+    def test_handle_http_error_200_response(self):
+        @handle_http_error
+        def make_request():
+            response = requests.Response()
+            response.status_code = 200
+            return response
+        assert isinstance(make_request(), type(requests.Response()))
+
+    def test_handle_http_error_400_response_raises_exception(self):
+        @handle_http_error
+        def make_request():
+            response = requests.Response()
+            response.status_code = 400
+            return response
+        with pytest.raises(Exception):
+            make_request()
+
+    def test_validate_keys_match_returns_response(self):
+        @validate_keys(expected_keys=["key1", "key2"])
+        def make_request():
+            return TestToolkit.MockRequestsResponse(status_code=200, data=[{"key1": "data1", "key2": "data2"}])
+        make_request()
+
+    def test_validate_keys_missing_key_raises_exception(self):
+        @validate_keys(expected_keys=["key1", "key2"])
+        def make_request():
+            return TestToolkit.MockRequestsResponse(status_code=200, data=[{"key1": "data1"}])
+        with pytest.raises(Exception):
+            make_request()
+
+    def test_validate_keys_extra_found_key_raises_exception(self):
+        @validate_keys(expected_keys=["key1", "key2"], suppress_exception_for_extra_found_keys=False)
+        def make_request():
+            return TestToolkit.MockRequestsResponse(status_code=200, data=[{"key1": "data1", "key2": "data2", "key3": "data3"}])
+        with pytest.raises(Exception):
+            make_request()
+
+    def test_validate_keys_extra_found_key_exception_suppressed(self):
+        @validate_keys(expected_keys=["key1", "key2"])
+        def make_request():
+            return TestToolkit.MockRequestsResponse(status_code=200, data=[{"key1": "data1", "key2": "data2", "key3": "data3"}])
+        make_request()
+
+    def test_validate_keys_no_keys_raises_exception(self):
+        @validate_keys(expected_keys=["key1", "key2"])
+        def make_request():
+            return TestToolkit.MockRequestsResponse(status_code=200, data=[{}])
+        with pytest.raises(Exception):
+            make_request()
