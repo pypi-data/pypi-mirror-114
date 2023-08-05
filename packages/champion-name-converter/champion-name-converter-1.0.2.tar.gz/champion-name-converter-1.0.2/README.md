0c73dec31bae3bc8164836d8370102dbfca4fa31
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:07:38 +0000

    Add package champion-name-converter/1.0.2/champion-name-converter-1.0.2.tar.gz

---
+++ packages/champion-name-converter/champion-name-converter-1.0.2.tar.gz/champion-name-converter-1.0.2/README.md
@@ -0,0 +1,20 @@
+# Champion Name Converter
+
+Installing using pip: 
+
+`
+pip install champion-name-converter
+`
+
+Usage:
+
+```python
+
+from converter import Converter
+
+convert = Converter()
+
+champion_id = convert.champion_to_id("Akshan")
+print(champion_id)
+
+```
---
+++ packages/champion-name-converter/champion-name-converter-1.0.2.tar.gz/champion-name-converter-1.0.2/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, Extension
+
+latest_version = "1.0.2"
+
+with open('README.md', encoding='utf-8') as f:
+    long_description = f.read()
+
+
+setup(
+  name = 'champion-name-converter',
+  long_description=long_description,
+  long_description_content_type='text/markdown',
+  packages = ['champion-converter'],   
+  version = latest_version,
+  license='MIT',
+  description = 'Convert League of Legends Champions Name to ID',   
+  author = 'flowd1337',                   
+  author_email = 'john_doe@gmail.com',      
+  url = 'https://github.com/flowd1337/champion-name-converter/',   
+  download_url = 'https://github.com/flowd1337/champion-name-converter/archive/refs/tags/{}.tar.gz'.format(latest_version),
+  keywords = ['League', 'League of Legends', 'Champion Name Converter'],
+  install_requires=[
+          'requests',
+      ],
+  classifiers=[
+    'Development Status :: 4 - Beta',      
+    'Intended Audience :: Developers',      
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',   
+    'Programming Language :: Python :: 3',     
+  ],
+)
