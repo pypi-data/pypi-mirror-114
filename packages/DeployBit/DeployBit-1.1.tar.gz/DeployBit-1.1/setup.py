1af7f800265f1c5c0410cee82fddf4f05232c2ee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:00:44 +0000

    Add package DeployBit/1.1/DeployBit-1.1.tar.gz

---
+++ packages/DeployBit/DeployBit-1.1.tar.gz/DeployBit-1.1/setup.py
@@ -0,0 +1,23 @@
+from distutils.core import setup
+setup(
+  name = 'DeployBit',
+  packages = ['DeployBit'],
+  version = '1.1',
+  license='MIT',   
+  description = 'A New Door to Salesforce',
+  author = 'Padmnabh Munde',
+  author_email = 'padmnabhmunde77@gmail.com',
+  url = 'https://github.com/padmnabhmunde77/DeployBit',
+  download_url = 'https://github.com/padmnabhmunde77/DeployBit/archive/refs/tags/1.0.tar.gz' ,
+  keywords = ['Salesforce', 'DeployBit','DeployBit Python', 'Deploy','Deployment','Retrieve','Metadata API','SOAP API','Query','SOQL'],
+  classifiers=[
+    'Development Status :: 3 - Alpha',     
+    'Intended Audience :: Developers',
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+  ],
+)
