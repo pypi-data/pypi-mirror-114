0309c0a0351a3a19c35bc3517743a49e253e7d90
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:07:35 +0000

    Add package weaviate-client/3.0.0/weaviate-client-3.0.0.tar.gz

---
+++ packages/weaviate-client/weaviate-client-3.0.0.tar.gz/weaviate-client-3.0.0/weaviate/connect/__init__.py
@@ -0,0 +1,9 @@
+"""
+Module communication to a Weaviate instance. Used to connect to
+Weaviate and run REST requests.
+"""
+
+__all__ = ['Connection']
+
+from .connection import Connection
+from .constants import *
