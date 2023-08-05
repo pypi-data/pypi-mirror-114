5145ca3f15f2912e58a47ec54a6059b23bd3fac5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:06:14 +0000

    Add package Aptus/3.0.1/Aptus-3.0.1-cp39-cp39-win_amd64.whl

---
+++ packages/Aptus/Aptus-3.0.1-cp39-cp39-win_amd64.whl/aptus/__init__.py
@@ -0,0 +1,16 @@
+""" Aptus Mandelbrot set viewer and renderer.
+"""
+
+__version__ = '3.0.1'
+
+import os.path
+
+def data_file(fname):
+    """ Return the path to a data file of ours.
+    """
+    return os.path.join(os.path.split(__file__)[0], fname)
+
+class AptusException(Exception):
+    """ Any Aptus-raised exception.
+    """
+    pass
