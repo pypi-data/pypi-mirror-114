1dafec1a3ea4486941084109f9019448da12df15
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:07:23 +0000

    Add package pyfanyi/0.0.1/pyfanyi-0.0.1.tar.gz

---
+++ packages/pyfanyi/pyfanyi-0.0.1.tar.gz/pyfanyi-0.0.1/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+    name='pyfanyi',
+    version='0.0.1',
+    description='a translate library',
+    author='stripe-python',
+    author_email='13513519246@139.com',
+    py_modules=['pyfanyi'],
+)
