275e19edbe9c2034766816f1705bc508c55e3c4f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:07:22 +0000

    Add package socialscience.ai/0.0.8/socialscience.ai-0.0.8.tar.gz

---
+++ packages/socialscience.ai/socialscience.ai-0.0.8.tar.gz/socialscience.ai-0.0.8/setup.py
@@ -0,0 +1,25 @@
+import setuptools
+import os
+
+
+setuptools.setup(
+    name='socialscience.ai'
+    , version='0.0.8'
+    , author='Nick Oh'
+    , author_email="socialscience.ai@gmail.com"
+    , description="AI/ML toolkits for social scientists"
+    , long_description="AI/ML toolkits for social scientists"
+    , long_description_content_type="text/markdown"
+    , install_requires = [
+        "numpy",
+        "matplotlib", 
+        "scipy",  
+        "pandas"
+    ]
+    , packages=setuptools.find_packages()
+    , classifiers = [
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
