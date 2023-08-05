b0a27a2ea74f4cda6be4bae2e96094e8e1c6291f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:01:35 +0000

    Add package PoL-gauss-bino-distribution/0.2/PoL_gauss_bino_distribution-0.2.tar.gz

---
+++ packages/PoL-gauss-bino-distribution/PoL_gauss_bino_distribution-0.2.tar.gz/PoL_gauss_bino_distribution-0.2/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='PoL_gauss_bino_distribution',
+      version='0.2',
+      description='Gaussian & Binomial distributions',
+      packages=['PoL_gauss_bino_distribution'],
+      auther = 'Kuldeep Saxena',
+      author_email = 'saxenak02@gmail.com',
+      zip_safe=False)
