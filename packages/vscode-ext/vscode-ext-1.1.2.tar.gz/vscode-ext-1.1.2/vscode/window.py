0c8b3f748dad9a7b78f0ab373f9750f40530c812
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:07:07 +0000

    Add package vscode-ext/1.1.2/vscode-ext-1.1.2.tar.gz

---
+++ packages/vscode-ext/vscode-ext-1.1.2.tar.gz/vscode-ext-1.1.2/vscode/window.py
@@ -0,0 +1,17 @@
+# IPC Commands are in the following format
+# {2digitcode}: {arg1}|||{arg2}|||{argn}
+
+def _base(code, text, *args):
+    print(f'{code}: {text}' + '|||'*bool(args) + '|||'.join(args), flush=True, end='')
+    res = input()
+    return res    
+
+def show_info_message(text, *args):
+    return _base('IM', text, *args)
+
+def show_warn_message(text, *args):
+    return _base('WM', text, *args)
+
+def show_error_message(text, *args):
+    return _base('EM', text, *args)
+
