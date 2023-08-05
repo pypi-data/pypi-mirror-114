45e0cec7d88b9cc4bb6e5d4d03dafb660e425473
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:35 +0000

    Add package hibpy/0.0.2/hibpy-0.0.2.tar.gz

---
+++ packages/hibpy/hibpy-0.0.2.tar.gz/hibpy-0.0.2/setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="hibpy",
+    version="0.0.2",
+    author="Geographs",
+    author_email="87452561+Geographs@users.noreply.github.com",
+    description="Python CLI application to check if you have been pwned.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Geographs/HIBP",
+    project_urls={
+        "Bug Tracker": "https://github.com/Geographs/HIBP/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.6",
+)
