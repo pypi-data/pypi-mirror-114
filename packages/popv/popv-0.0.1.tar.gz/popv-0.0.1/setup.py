de0ba601b5fa0c58e17a5a403f1b5e4e2176767d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:08:10 +0000

    Add package popv/0.0.1/popv-0.0.1.tar.gz

---
+++ packages/popv/popv-0.0.1.tar.gz/popv-0.0.1/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="popv",
+    version="0.0.1",
+    description="The PopV algorithm",
+    long_description="The PopV algorithm for unsupervised label transfer from reference datasets.",
+    long_description_content_type="text/markdown",
+    author="Nir Yosef",
+    author_email="alexander.tarashansky@czbiohub.org",
+    keywords="scrnaseq analysis label transfer annotation",
+    python_requires=">=3.7",
+    # py_modules=["SAM", "utilities", "SAMGUI"],
+    install_requires=[
+        "gdown",
+        "obonet",
+        "bbknn",
+        "OnClass",
+        "scvi-tools",
+        "imgkit",
+        "scanorama",
+        "scanpy",
+        "tensorflow"
+    ],
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+)
