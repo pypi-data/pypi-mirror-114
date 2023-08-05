114cd97adc485027bc451f7f2f89c0f6033b7dc8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:00:07 +0000

    Add package draft-distributions/0.1/draft_distributions-0.1.tar.gz

---
+++ packages/draft-distributions/draft_distributions-0.1.tar.gz/draft_distributions-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='draft_distributions',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['draft_distributions'],
+      author = 'Vincent',
+      author_email = 'vincentbiasa6767@gmail.com',
+      zip_safe=False)
