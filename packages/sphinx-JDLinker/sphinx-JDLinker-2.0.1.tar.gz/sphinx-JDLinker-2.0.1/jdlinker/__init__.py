4f0c56ed0975d4adf43a82d308e14513ed2fdfe6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:08:39 +0000

    Add package sphinx-JDLinker/2.0.1/sphinx-JDLinker-2.0.1.tar.gz

---
+++ packages/sphinx-JDLinker/sphinx-JDLinker-2.0.1.tar.gz/sphinx-JDLinker-2.0.1/jdlinker/__init__.py
@@ -0,0 +1,12 @@
+
+from . import jdlinker
+
+__version = '2.0.1'
+
+def setup(app):
+    app.add_role('javadoc', jdlinker.javadoc_role)
+    app.add_config_value('javadoc_links', {}, 'env')
+    app.add_config_value('javadoc_dump', False, 'env')
+    app.add_directive('javadoc-import', jdlinker.JavaDocImportDirective)
+    app.connect('env-purge-doc', jdlinker.purge_imports)
+    app.connect('env-merge-info', jdlinker.merge_imports)
