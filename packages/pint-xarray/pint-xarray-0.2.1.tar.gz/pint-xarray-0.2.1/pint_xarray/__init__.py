9395eaff641fc52953b92fac445e48bc87de4c22
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:06:41 +0000

    Add package pint-xarray/0.2.1/pint-xarray-0.2.1.tar.gz

---
+++ packages/pint-xarray/pint-xarray-0.2.1.tar.gz/pint-xarray-0.2.1/pint_xarray/__init__.py
@@ -0,0 +1,27 @@
+try:
+    from importlib.metadata import version
+except ImportError:
+    from importlib_metadata import version
+
+import pint
+
+from . import accessors, formatting, testing  # noqa: F401
+from .accessors import default_registry as unit_registry
+from .accessors import setup_registry
+
+try:
+    __version__ = version("pint-xarray")
+except Exception:
+    # Local copy or not installed with setuptools.
+    # Disable minimum version checks on downstream libraries.
+    __version__ = "999"
+
+
+pint.Quantity._repr_inline_ = formatting.inline_repr
+
+
+__all__ = [
+    "testing",
+    "unit_registry",
+    "setup_registry",
+]
