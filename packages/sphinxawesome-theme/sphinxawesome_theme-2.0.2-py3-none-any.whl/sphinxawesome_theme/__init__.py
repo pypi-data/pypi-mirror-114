a25e9ab17360a0d0dcb62f2aa238f59b4d31d1d5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:07:42 +0000

    Add package sphinxawesome-theme/2.0.2/sphinxawesome_theme-2.0.2-py3-none-any.whl

---
+++ packages/sphinxawesome-theme/sphinxawesome_theme-2.0.2-py3-none-any.whl/sphinxawesome_theme/__init__.py
@@ -0,0 +1,54 @@
+"""The Sphinx awesome theme as a Python package.
+
+:copyright: Copyright Kai Welke.
+:license: MIT, see LICENSE_ for details
+
+.. _LICENSE: https://github.com/kai687/sphinxawesome-theme/blob/master/LICENSE
+"""
+
+try:
+    from importlib.metadata import PackageNotFoundError, version  # type: ignore
+except ImportError:  # pragma: no cover
+    # ignore mypy error about incompatible imports
+    from importlib_metadata import version, PackageNotFoundError  # type: ignore
+
+from os import environ, path
+from typing import Any, Dict
+
+from sphinx.application import Sphinx
+
+try:
+    # obtain version from `pyproject.toml` via `importlib.metadata.version()`
+    __version__ = version(__name__)
+except PackageNotFoundError:  # pragma: no cover
+    __version__ = "unknown"
+
+
+def setup(app: "Sphinx") -> Dict[str, Any]:
+    """Register the theme and its extensions wih Sphinx.
+
+    The setup function of this theme accomplishes the following:
+
+    - add the HTML theme
+    - activate the ``sphinxawesome.sampdirective`` extension
+    - activate the ``sphinxawesome_theme.highlighting`` extension
+    - activate the ``sphinxawesome_theme.html_translator`` extension
+    - add the ``AdmonitionID`` as post-transform
+    - execute the ``post_process_html`` code when the build has finished
+    """
+    app.add_html_theme("sphinxawesome_theme", path.abspath(path.dirname(__file__)))
+    app.setup_extension("sphinxawesome.sampdirective")
+    app.setup_extension("sphinxawesome_theme.highlighting")
+    app.setup_extension("sphinxawesome_theme.html_translator")
+    app.setup_extension("sphinxawesome_theme.admonition_ids")
+    app.setup_extension("sphinxawesome_theme.jinja_filters")
+
+    # if this environment variable is defined, skip the postprocessing
+    if "SPHINX_AWESOME_THEME_NO_POSTPROCESSING" not in environ:
+        app.setup_extension("sphinxawesome_theme.postprocess")
+
+    return {
+        "version": __version__,
+        "parallel_read_safe": True,
+        "parallel_write_safe": True,
+    }
