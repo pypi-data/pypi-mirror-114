762631da89b44017f98ce7238b7b37cff98546ea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:06:11 +0000

    Add package AzureK/2.5.2/AzureK-2.5.2.tar.gz

---
+++ packages/AzureK/AzureK-2.5.2.tar.gz/AzureK-2.5.2/setup.py
@@ -0,0 +1,17 @@
+import setuptools
+
+setuptools.setup(
+  name="AzureK",
+  version="2.5.2",
+  author="flhsjjk",
+  author_email="flhsjjk@mail.ustc.edu.cn",
+  description="科研用",
+  long_description= "待续",
+  long_description_content_type="text/markdown",
+  url="",
+  classifiers=[
+  "Programming Language :: Python :: 3.8",
+  "License :: OSI Approved :: MIT License",
+  "Operating System :: OS Independent",
+  ],
+)
