193e57c5652796a231b958ab229501666fe21a15
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:46 +0000

    Add package straitjacket/202107.1020/straitjacket-202107.1020-py3-none-any.whl

---
+++ packages/straitjacket/straitjacket-202107.1020-py3-none-any.whl/straitjacket/__init__.py
@@ -0,0 +1,9 @@
+# This file is part of the straitjacket project
+# https://github.com/mbarkhau/straitjacket
+#
+# Copyright (c) 2018-2021 Manuel Barkhau (mbarkhau@gmail.com) - MIT License
+# SPDX-License-Identifier: MIT
+
+__version__ = "v202107.1020"
+
+from . import sjfmt  # noqa
