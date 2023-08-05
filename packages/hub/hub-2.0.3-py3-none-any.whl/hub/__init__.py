35c90240220b9fa3942267eedc40cce829564ea4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:08:45 +0000

    Add package hub/2.0.3/hub-2.0.3-py3-none-any.whl

---
+++ packages/hub/hub-2.0.3-py3-none-any.whl/hub/__init__.py
@@ -0,0 +1,27 @@
+import numpy as np
+
+__pdoc__ = {
+    "core": False,
+    "api": False,
+    "cli": False,
+    "client": False,
+    "constants": False,
+    "integrations": False,
+    "tests": False,
+}
+
+from .api.dataset import dataset
+from .api.read import read
+from .util.bugout_reporter import hub_reporter
+
+load = dataset.load
+empty = dataset.empty
+like = dataset.like
+__all__ = ["dataset", "read", "__version__", "load", "empty", "like"]
+
+__version__ = "2.0.3"
+__encoded_version__ = np.array(__version__)
+
+hub_reporter.tags.append(f"version:{__version__}")
+hub_reporter.system_report(publish=True)
+hub_reporter.setup_excepthook(publish=True)
