bd1d0a571b355b21b22e90d320a7666eeb09db24
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:36 +0000

    Add package machine-common-sense/0.4.3/machine_common_sense-0.4.3-py3-none-any.whl

---
+++ packages/machine-common-sense/machine_common_sense-0.4.3-py3-none-any.whl/machine_common_sense/plotter.py
@@ -0,0 +1,231 @@
+import io
+import math
+
+import ai2thor
+import matplotlib
+import PIL
+
+matplotlib.use('Agg')
+from typing import Dict, List, NamedTuple
+
+import matplotlib.pyplot as plt
+from shapely import geometry
+
+
+class XZHeading(NamedTuple):
+    x: float
+    z: float
+
+
+class Robot(NamedTuple):
+    x: float
+    y: float
+    z: float
+    rotation: float
+
+
+class Object(NamedTuple):
+    held: bool
+    visible: bool
+    uuid: str
+    color: str
+    bounds: list
+
+
+class TopDownPlotter():
+
+    ROBOT_PLOT_WIDTH = 0.2
+    ROBOT_PLOT_LABEL = "robot"
+    ROBOT_COLOR = 'xkcd:gray'
+    DEFAULT_COLOR = "xkcd:black"
+    HEADING_LENGTH = 0.4
+    MINIMUM_ROOM_DIMENSION = -5
+    MAXIMUM_ROOM_DIMENSION = 5
+    BORDER = 0.05
+
+    def __init__(self, team: str, scene_name: str):
+        self._team = team
+        if '/' in scene_name:
+            scene_name = scene_name.rsplit('/', 1)[1]
+        self._scene_name = scene_name
+
+    def plot(self, scene_event: ai2thor.server.Event,
+             step_number: int,
+             goal_id: str = None
+             ) -> PIL.Image.Image:
+
+        plt = self._initialize_plot(step_number=step_number)
+        self._draw_objects(self._find_plottable_objects(scene_event), goal_id)
+        self._draw_robot(scene_event.metadata.get('agent', None))
+        img = self._export_plot(plt)
+        plt.close()
+        return img
+
+    def _find_plottable_objects(
+            self, scene_event: ai2thor.server.Event) -> List:
+        '''Find plottable objects from the scene data.
+
+        Plottable objects include normal scene objects as well as
+        occluder and wall structural objects.
+        '''
+        structural_objects = scene_event.metadata.get('structuralObjects', [])
+        filtered_structural_objects = [
+            obj for obj in structural_objects
+            if not obj.get('objectId', '').startswith('ceiling') and not
+            obj.get('objectId', '').startswith('floor')
+        ]
+        objects = scene_event.metadata.get('objects', [])
+        return filtered_structural_objects + objects
+
+    def _initialize_plot(self, step_number: int) -> None:
+        '''Create the plot'''
+        plt.xlim(self.MINIMUM_ROOM_DIMENSION, self.MAXIMUM_ROOM_DIMENSION)
+        plt.ylim(self.MINIMUM_ROOM_DIMENSION, self.MAXIMUM_ROOM_DIMENSION)
+        plt.text(
+            self.MAXIMUM_ROOM_DIMENSION + self.BORDER,
+            self.MINIMUM_ROOM_DIMENSION + self.BORDER,
+            step_number)
+        plt.title(f"{self._team} {self._scene_name}")
+        return plt
+
+    def _export_plot(self, plt: matplotlib.pyplot) -> PIL.Image.Image:
+        '''Export the plot to a PIL Image'''
+        fig = plt.gcf()
+        buf = io.BytesIO()
+        fig.savefig(buf)
+        buf.seek(0)
+        img = PIL.Image.open(buf)
+        return img
+
+    def _draw_robot(self, robot_metadata: Dict) -> None:
+        '''Plot the robot position and heading'''
+        if robot_metadata is None:
+            return None
+        robot = self._create_robot(robot_metadata)
+        self._draw_robot_position(robot)
+        self._draw_robot_heading(robot)
+
+    def _draw_robot_position(self, robot: Robot) -> None:
+        '''Draw the robot's scene XZ position in the plot'''
+        circle = plt.Circle(
+            (robot.x, robot.z),
+            radius=self.ROBOT_PLOT_WIDTH,
+            color=self.ROBOT_COLOR,
+            label=self.ROBOT_PLOT_LABEL)
+        plt.gca().add_patch(circle)
+
+    def _draw_robot_heading(self, robot: Robot) -> None:
+        '''Draw the heading vector starting from the robot XZ position'''
+        heading = self._calculate_heading(
+            rotation_angle=360.0 - robot.rotation,
+            heading_length=self.HEADING_LENGTH
+        )
+        heading = plt.Line2D((robot.x, robot.x + heading.x),
+                             (robot.z, robot.z + heading.z),
+                             color=self.ROBOT_COLOR,
+                             lw=1)
+        plt.gca().add_line(heading)
+
+    def _draw_objects(self, objects: Dict,
+                      goal_id: str = None) -> None:
+        '''Plot the object bounds for each object in the scene'''
+        for o in objects:
+            obj = self._create_object(o)
+            if obj.bounds is not None:
+                obj_pts = [(pt['x'], pt['z']) for pt in obj.bounds]
+                polygon = geometry.MultiPoint(obj_pts).convex_hull
+                pts = polygon.exterior.coords
+                self._draw_object_bounds(obj, pts)
+                if goal_id is not None and o['objectId'] == goal_id:
+                    self._draw_goal(o['position'])
+
+    def _draw_goal(self, position: Object) -> None:
+        '''Draw the goal object of the scene'''
+        plt.scatter(
+            position['x'],
+            position['z'],
+            c=self._convert_color("gold"),
+            s=300,
+            marker="*",
+            zorder=5,
+            alpha=.7,
+            edgecolors=self._convert_color("black"),
+            linewidths=.5
+        )
+
+    def _draw_object_bounds(self, obj: Object, points: List) -> None:
+        '''Draw the scene object'''
+        poly = plt.Polygon(points,
+                           color=obj.color,
+                           fill=obj.color if obj.visible or
+                           obj.held else '',
+                           ec=self.DEFAULT_COLOR,
+                           label=obj.uuid)
+        plt.gca().add_patch(poly)
+
+    def _calculate_heading(self, rotation_angle: float,
+                           heading_length: float) -> XZHeading:
+        '''Calculate XZ heading vector from the rotation angle'''
+        s = math.sin(math.radians(rotation_angle))
+        c = math.cos(math.radians(rotation_angle))
+        vec_x = 0 * c - heading_length * s
+        vec_z = 0 * s + heading_length * c
+        return XZHeading(vec_x, vec_z)
+
+    def _create_robot(self, robot_metadata: Dict) -> Robot:
+        '''Extract robot position and rotation information from the metadata'''
+        position = robot_metadata.get('position', None)
+        if position is not None:
+            x = position.get('x', None)
+            y = position.get('y', None)
+            z = position.get('z', None)
+        else:
+            x = 0.0
+            y = 0.0
+            z = 0.0
+
+        rotation = robot_metadata.get('rotation', None)
+        if rotation is not None:
+            rotation_y = rotation.get('y', None)
+        else:
+            rotation_y = 0.0
+
+        return Robot(x, y, z, rotation_y)
+
+    def _create_object(self, object_metadata: Dict) -> Object:
+        '''Create the scene object from its metadata'''
+
+        held = object_metadata.get('isPickedUp', None)
+        visible = object_metadata.get('visibleInCamera', None)
+        uuid = object_metadata.get('objectId', None)
+
+        colors = object_metadata.get('colorsFromMaterials', [])
+        if len(colors):
+            color = self._convert_color(colors[0])
+        else:
+            color = self._convert_color('')
+
+        bounds = object_metadata.get('objectBounds', None)
+        if bounds is not None:
+            corners = bounds.get('objectBoundsCorners', None)
+        else:
+            corners = None
+
+        return Object(
+            held=held,
+            visible=visible,
+            uuid=uuid,
+            color=color,
+            bounds=corners
+        )
+
+    def _convert_color(self, color: str) -> str:
+        '''Convert color string to xkcd string'''
+        # use default of black if no color present
+        if not color:
+            color = 'black'
+        # white color does not show up in plot but ivory does
+        if color == 'white':
+            color = 'ivory'
+        # prefix with xkcd string
+        return 'xkcd:' + color
---
+++ packages/machine-common-sense/machine_common_sense-0.4.3-py3-none-any.whl/tests/test_mcs.py
@@ -0,0 +1,232 @@
+import unittest
+
+import machine_common_sense as mcs
+from machine_common_sense.config_manager import ConfigManager
+from machine_common_sense.controller_logger import ControllerLogger
+from machine_common_sense.controller_media import (
+    DepthImageEventHandler, DepthVideoEventHandler, HeatmapVideoEventHandler,
+    ImageVideoEventHandler, ObjectMaskImageEventHandler,
+    SceneImageEventHandler, SegmentationVideoEventHandler,
+    TopdownVideoEventHandler)
+from machine_common_sense.history_writer import HistoryEventHandler
+from machine_common_sense.setup import add_subscribers
+
+from .mock_controller import MockController
+
+
+class TestMCS(unittest.TestCase):
+
+    def test_create_controller(self):
+        # TODO How do we test this without starting the whole app?
+        pass
+
+    def test_add_subscribers(self):
+        ctrl = MockController()
+        cfg = ConfigManager()
+        ctrl._subscribers.clear()
+
+        def return_true():
+            return True
+
+        def return_false():
+            return False
+
+        cfg.is_history_enabled = return_false
+        cfg.is_save_debug_images = return_false
+        cfg.is_depth_maps_enabled = return_false
+        cfg.is_object_masks_enabled = return_false
+        cfg.is_video_enabled = return_false
+        add_subscribers(ctrl, cfg)
+        self.assertEqual(len(ctrl._subscribers), 2)
+        classes = (ControllerLogger, HistoryEventHandler)
+        self.assertIsInstance(ctrl._subscribers[0], classes)
+        self.assertIsInstance(ctrl._subscribers[1], classes)
+
+        ctrl._subscribers.clear()
+        cfg.is_save_debug_images = return_true
+        cfg.is_depth_maps_enabled = return_false
+        cfg.is_object_masks_enabled = return_false
+        cfg.is_video_enabled = return_false
+        add_subscribers(ctrl, cfg)
+        self.assertEqual(len(ctrl._subscribers), 4)
+        self.assertIsInstance(ctrl._subscribers[0], DepthImageEventHandler)
+        self.assertIsInstance(ctrl._subscribers[1], SceneImageEventHandler)
+        self.assertIsInstance(ctrl._subscribers[2], ControllerLogger)
+        self.assertIsInstance(ctrl._subscribers[3], HistoryEventHandler)
+
+        ctrl._subscribers.clear()
+        cfg.is_save_debug_images = return_false
+        cfg.is_depth_maps_enabled = return_false
+        cfg.is_object_masks_enabled = return_false
+        cfg.is_video_enabled = return_true
+        add_subscribers(ctrl, cfg)
+        self.assertEqual(len(ctrl._subscribers), 5)
+        self.assertIsInstance(ctrl._subscribers[0], ImageVideoEventHandler)
+        self.assertIsInstance(
+            ctrl._subscribers[1],
+            TopdownVideoEventHandler)
+        self.assertIsInstance(
+            ctrl._subscribers[2],
+            HeatmapVideoEventHandler)
+        self.assertIsInstance(ctrl._subscribers[3], ControllerLogger)
+        self.assertIsInstance(ctrl._subscribers[4], HistoryEventHandler)
+
+        ctrl._subscribers.clear()
+        cfg.is_save_debug_images = return_false
+        cfg.is_depth_maps_enabled = return_false
+        cfg.is_object_masks_enabled = return_true
+        cfg.is_video_enabled = return_true
+        add_subscribers(ctrl, cfg)
+        self.assertEqual(len(ctrl._subscribers), 6)
+        self.assertIsInstance(ctrl._subscribers[0], ImageVideoEventHandler)
+        self.assertIsInstance(
+            ctrl._subscribers[1],
+            TopdownVideoEventHandler)
+        self.assertIsInstance(
+            ctrl._subscribers[2],
+            HeatmapVideoEventHandler)
+        self.assertIsInstance(
+            ctrl._subscribers[3],
+            SegmentationVideoEventHandler)
+        self.assertIsInstance(ctrl._subscribers[4], ControllerLogger)
+        self.assertIsInstance(ctrl._subscribers[5], HistoryEventHandler)
+
+        ctrl._subscribers.clear()
+        cfg.is_save_debug_images = return_true
+        cfg.is_depth_maps_enabled = return_true
+        cfg.is_object_masks_enabled = return_false
+        cfg.is_video_enabled = return_false
+        add_subscribers(ctrl, cfg)
+        self.assertEqual(len(ctrl._subscribers), 4)
+        self.assertIsInstance(ctrl._subscribers[0], DepthImageEventHandler)
+        self.assertIsInstance(ctrl._subscribers[1], SceneImageEventHandler)
+        self.assertIsInstance(ctrl._subscribers[2], ControllerLogger)
+        self.assertIsInstance(ctrl._subscribers[3], HistoryEventHandler)
+
+        ctrl._subscribers.clear()
+        cfg.is_save_debug_images = return_true
+        cfg.is_depth_maps_enabled = return_false
+        cfg.is_object_masks_enabled = return_true
+        cfg.is_video_enabled = return_false
+        add_subscribers(ctrl, cfg)
+        self.assertEqual(len(ctrl._subscribers), 5)
+        self.assertIsInstance(ctrl._subscribers[0], DepthImageEventHandler)
+        self.assertIsInstance(ctrl._subscribers[1], SceneImageEventHandler)
+        self.assertIsInstance(
+            ctrl._subscribers[2],
+            ObjectMaskImageEventHandler)
+        self.assertIsInstance(ctrl._subscribers[3], ControllerLogger)
+        self.assertIsInstance(ctrl._subscribers[4], HistoryEventHandler)
+
+        ctrl._subscribers.clear()
+        cfg.is_save_debug_images = return_false
+        cfg.is_depth_maps_enabled = return_true
+        cfg.is_object_masks_enabled = return_true
+        cfg.is_video_enabled = return_true
+        add_subscribers(ctrl, cfg)
+        self.assertEqual(len(ctrl._subscribers), 7)
+        self.assertIsInstance(ctrl._subscribers[0], ImageVideoEventHandler)
+        self.assertIsInstance(
+            ctrl._subscribers[1],
+            TopdownVideoEventHandler)
+        self.assertIsInstance(
+            ctrl._subscribers[2],
+            HeatmapVideoEventHandler)
+        self.assertIsInstance(
+            ctrl._subscribers[3],
+            DepthVideoEventHandler)
+        self.assertIsInstance(
+            ctrl._subscribers[4],
+            SegmentationVideoEventHandler)
+        self.assertIsInstance(ctrl._subscribers[5], ControllerLogger)
+        self.assertIsInstance(ctrl._subscribers[6], HistoryEventHandler)
+
+        ctrl._subscribers.clear()
+        cfg.is_save_debug_images = return_true
+        cfg.is_depth_maps_enabled = return_true
+        cfg.is_object_masks_enabled = return_true
+        cfg.is_video_enabled = return_true
+        add_subscribers(ctrl, cfg)
+        self.assertEqual(len(ctrl._subscribers), 10)
+        self.assertIsInstance(ctrl._subscribers[0], DepthImageEventHandler)
+        self.assertIsInstance(
+            ctrl._subscribers[1],
+            SceneImageEventHandler)
+        self.assertIsInstance(
+            ctrl._subscribers[2],
+            ObjectMaskImageEventHandler)
+        self.assertIsInstance(ctrl._subscribers[3], ImageVideoEventHandler)
+        self.assertIsInstance(
+            ctrl._subscribers[4],
+            TopdownVideoEventHandler)
+        self.assertIsInstance(
+            ctrl._subscribers[5],
+            HeatmapVideoEventHandler)
+        self.assertIsInstance(
+            ctrl._subscribers[6],
+            DepthVideoEventHandler)
+        self.assertIsInstance(
+            ctrl._subscribers[7],
+            SegmentationVideoEventHandler)
+        self.assertIsInstance(ctrl._subscribers[8], ControllerLogger)
+        self.assertIsInstance(ctrl._subscribers[9], HistoryEventHandler)
+
+    def test_load_scene_file_json(self):
+        actual, status = mcs.load_scene_json_file("tests/test_scene.json")
+        expected = {
+            "ceilingMaterial": "Walls/WallDrywallWhite",
+            "floorMaterial": "Fabrics/RUG4",
+            "wallMaterial": "Walls/YellowDrywall",
+            "objects": [{
+                "id": "testBall",
+                "type": "sphere",
+                "materialFile": "Plastics/BlueRubber",
+                "physics": True,
+                "shows": [{
+                    "stepBegin": 0,
+                    "position": {
+                        "x": 0,
+                        "y": 0.5,
+                        "z": 3
+                    },
+                    "scale": {
+                        "x": 0.25,
+                        "y": 0.25,
+                        "z": 0.25
+                    }
+                }],
+                "forces": [{
+                    "stepBegin": 1,
+                    "stepEnd": 1,
+                    "vector": {
+                        "x": 50,
+                        "y": 0,
+                        "z": 0
+                    }
+                }]
+            }]
+        }
+        self.assertEqual(actual, expected)
+        self.assertEqual(status, None)
+
+    def test_load_scene_file_json_is_invalid(self):
+        actual, status = mcs.load_scene_json_file(
+            "tests/test_scene_invalid.json")
+        self.assertEqual(actual, {})
+        self.assertEqual(
+            status,
+            "The given file 'tests/test_scene_invalid.json' does not " +
+            "contain valid JSON."
+        )
+
+    def test_load_scene_file_json_is_missing(self):
+        actual, status = mcs.load_scene_json_file(
+            "tests/test_scene_missing.json")
+        self.assertEqual(actual, {})
+        self.assertEqual(
+            status,
+            "The given file 'tests/test_scene_missing.json' cannot be found.")
+
+
+if __name__ == '__main__':
+    unittest.main()
