f079f3b29cb40e1bd58ea8b0d906b742f04d360d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:11:22 +0000

    Add package eve-panel/0.3.19/eve_panel-0.3.19.tar.gz

---
+++ packages/eve-panel/eve_panel-0.3.19.tar.gz/eve_panel-0.3.19/eve_panel/auth/__init__.py
@@ -0,0 +1,32 @@
+"""
+auth module
+====================================
+Authentication and Authorization handling
+"""
+import pkg_resources
+import logging
+
+from .base import EveAuthBase, EveNoAuth
+from .basic_auth import EveBasicAuth
+from .bearer import EveBearerAuth
+from .oauth2 import Oauth2DeviceFlow
+
+log = logging.getLogger(__name__)
+
+
+AUTH_CLASSES = {
+    None: EveNoAuth,
+    "Basic": EveBasicAuth,
+    "Bearer": EveBearerAuth,
+    "Oauth2 Device Flow": Oauth2DeviceFlow,
+}
+
+for entry_point in pkg_resources.iter_entry_points('eve_panel.auth'):
+    try:
+        AUTH_CLASSES[entry_point.name] = entry_point.load()
+    except Exception as e:
+        log.warning(f'Could not load {entry_point.name}.')
+        log.debug(str(e))
+import pkg_resources
+
+DEFAULT_AUTH = None
