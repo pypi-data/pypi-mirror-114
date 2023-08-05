3ec078e7b23dd61e91fd4e3968b14cab50206eff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:06:50 +0000

    Add package xiu/0.0.6.dev1/xiu-0.0.6.dev1.tar.gz

---
+++ packages/xiu/xiu-0.0.6.dev1.tar.gz/xiu-0.0.6.dev1/setup.py
@@ -0,0 +1,30 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="xiu",
+    version="0.0.6dev1", # Latest version .
+    author="R2FsCg",
+    author_email="r2fscg@gmail.com",
+    description="XIUXIU",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/private_repo/codefast",
+    packages=setuptools.find_packages(),
+     entry_points={
+        'console_scripts': [
+            'data=xiu.urls:display_shared_links'
+        ]
+    },
+
+    install_requires=[
+	    'codefast'
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
