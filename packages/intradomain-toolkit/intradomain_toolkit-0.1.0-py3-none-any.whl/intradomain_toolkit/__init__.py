9d1a9d4934d86c0a52e5dbbd2c47c6bf30930b6d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:05:40 +0000

    Add package intradomain-toolkit/0.1.0/intradomain_toolkit-0.1.0-py3-none-any.whl

---
+++ packages/intradomain-toolkit/intradomain_toolkit-0.1.0-py3-none-any.whl/intradomain_toolkit/__init__.py
@@ -0,0 +1,9 @@
+"""
+intradomain_toolkit
+
+A context diambiguation library for natural language texts.
+"""
+
+__version__ = "0.1.0"
+__author__ = 'Ambarish Moharil & Arpit Sharma'
+__credits__ = 'Indian Institute of Science Education and Research, Bhopal'
