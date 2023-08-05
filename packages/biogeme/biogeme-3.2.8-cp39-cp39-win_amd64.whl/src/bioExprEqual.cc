0f102d8cb7678523bffdee50b654a620fdaefb6f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:06:58 +0000

    Add package biogeme/3.2.8/biogeme-3.2.8-cp39-cp39-win_amd64.whl

---
+++ packages/biogeme/biogeme-3.2.8-cp39-cp39-win_amd64.whl/src/bioExprEqual.cc
@@ -0,0 +1,76 @@
+//-*-c++-*------------------------------------------------------------
+//
+// File name : bioExprEqual.cc
+// @date   Thu Apr 19 07:16:34 2018
+// @author Michel Bierlaire
+// @version Revision 1.0
+//
+//--------------------------------------------------------------------
+
+#include "bioExprEqual.h"
+#include <sstream>
+#include "bioDebug.h"
+#include "bioExceptions.h"
+
+bioExprEqual::bioExprEqual(bioExpression* l, bioExpression* r) :
+  left(l), right(r) {
+
+  listOfChildren.push_back(l) ;
+  listOfChildren.push_back(r) ;
+}
+
+bioExprEqual::~bioExprEqual() {
+
+}
+
+  
+const bioDerivatives* bioExprEqual::getValueAndDerivatives(std::vector<bioUInt> literalIds,
+						     bioBoolean gradient,
+						     bioBoolean hessian) {
+
+    if (gradient && theDerivatives.getSize() != literalIds.size()) {
+      theDerivatives.resize(literalIds.size()) ;
+    }
+
+  if (gradient) {
+    if (containsLiterals(literalIds)) {
+      std::stringstream str ;
+      str << "Expression Equal is not differentiable" << std::endl ; 
+      throw(bioExceptions(__FILE__,__LINE__,str.str())) ;
+    }
+    if (hessian) {
+      theDerivatives.setDerivativesToZero() ;
+    }
+    else {
+      theDerivatives.setGradientToZero() ;
+    }
+  }
+  
+  if (left == NULL) {
+    throw bioExceptNullPointer(__FILE__,__LINE__,"left") ;
+  }
+  if (right == NULL) {
+    throw bioExceptNullPointer(__FILE__,__LINE__,"right") ;
+  }
+  
+  const bioDerivatives* l = left->getValueAndDerivatives(literalIds,false,false) ;
+  const bioDerivatives* r = right->getValueAndDerivatives(literalIds,false,false) ;
+  if (l->f == r->f) {
+    theDerivatives.f = 1.0 ;
+  }
+  else {
+    theDerivatives.f = 0.0 ;
+  }
+  return &theDerivatives ;
+}
+
+bioString bioExprEqual::print(bioBoolean hp) const {
+  std::stringstream str ;
+  if (hp) {
+    str << "==(" << left->print(hp) << "," << right->print(hp) << ")" ;
+  }
+  else {
+    str << "(" << left->print(hp) << "==" << right->print(hp) << ")" ;
+  }
+  return str.str() ;
+}
