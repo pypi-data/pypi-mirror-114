c20f2edcf4c830a958903cad795963fcf6fc7e56
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:06:41 +0000

    Add package nspb/1.1.0/nspb-1.1.0.tar.gz

---
+++ packages/nspb/nspb-1.1.0.tar.gz/nspb-1.1.0/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+import pathlib
+
+HERE = pathlib.Path(__file__).parent
+
+README = (HERE / "README.md").read_text()
+
+setup(
+    name="nspb",
+    version="1.1.0",
+    description="ProgressBar for python",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url="https://github.com/realpython/reader",
+    author="Alexandre Hachet",
+    author_email="info@realpython.com",
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+    ],
+    packages=["nspb"],
+    include_package_data=True,
+)
