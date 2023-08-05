83a57ed7e98afad995ade084ecc8a8d8ca614099
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:33 +0000

    Add package lowdash/1.2.0/lowdash-1.2.0.tar.gz

---
+++ packages/lowdash/lowdash-1.2.0.tar.gz/lowdash-1.2.0/setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['lowdash']
+
+package_data = \
+{'': ['*']}
+
+setup_kwargs = {
+    'name': 'lowdash',
+    'version': '1.2.0',
+    'description': 'A python implementation of lodash in javascript',
+    'long_description': None,
+    'author': 'abh80',
+    'author_email': None,
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>=3.6,<4.0',
+}
+
+
+setup(**setup_kwargs)
