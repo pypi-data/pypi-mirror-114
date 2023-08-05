9d451635cec7d74f4282b7420ee070afabb2ec71
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:07:39 +0000

    Add package gravityRecorder/1.5.0/gravityRecorder-1.5.0-py3-none-any.whl

---
+++ packages/gravityRecorder/gravityRecorder-1.5.0-py3-none-any.whl/gravityRecorder/tests/test_objects.py
@@ -0,0 +1,5 @@
+""" Модуль содержит тестовые объеткы, используемые в других тестах """
+from gravityRecorder.main import Recorder
+
+
+test_recorder = Recorder('wdb', 'watchman', 'hect0r1337', '192.168.100.118')
