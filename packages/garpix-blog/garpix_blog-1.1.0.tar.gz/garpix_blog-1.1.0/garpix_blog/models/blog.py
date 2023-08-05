c6b849b0fa0e3e5682ec191a46208fa7d3c135c4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:10:52 +0000

    Add package garpix-blog/1.1.0/garpix_blog-1.1.0.tar.gz

---
+++ packages/garpix-blog/garpix_blog-1.1.0.tar.gz/garpix_blog-1.1.0/garpix_blog/models/blog.py
@@ -0,0 +1,26 @@
+from django.conf import settings
+from django.utils.module_loading import import_string
+from garpix_page.models import BasePage
+
+from garpix_blog.mixins import PolymorphicActiveMixin
+
+BlogMixin = import_string(settings.GARPIX_BLOG_MIXIN)
+
+
+class BlogPage(BasePage, BlogMixin, PolymorphicActiveMixin):
+
+    def get_context(self, request=None, *args, **kwargs):
+        from garpix_blog.models import CategoryPage, PostPage
+        context = super().get_context(request, *args, **kwargs)
+        categories = CategoryPage.on_site.filter(is_active=True, parent=kwargs['object'])[:10]
+        posts = PostPage.on_site.filter(is_active=True, parent=kwargs['object'])[:10]
+        context.update({
+            'categories': categories,
+            'posts': posts
+        })
+        return context
+
+    class Meta:
+        verbose_name = "Блог"
+        verbose_name_plural = "Блоги"
+        ordering = ('-created_at',)
