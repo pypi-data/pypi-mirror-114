0f15a5528534ba14c126591db6d03d5891282a29
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:26 +0000

    Add package loudify/0.0.post17/loudify-0.0.post17-py2.py3-none-any.whl

---
+++ packages/loudify/loudify-0.0.post17-py2.py3-none-any.whl/loudify/client.py
@@ -0,0 +1,50 @@
+"""Async Client cli."""
+
+from . import client_async_api
+from . import parser
+
+
+def main(argv=None):
+    """
+    Main function for cli client.
+
+    Args:
+        Addres: broker addres to connect to
+        Port: port to connect to
+    Returns:
+        None
+    """
+    args = parser.parse_args_client(argv)
+    parser.setup_logging(args.loglevel)
+    verbose = False
+    if args.loglevel == 10:
+        verbose = True
+
+    client = client_async_api.Client("tcp://" + args.address + ":" + str(args.port), verbose)
+
+    requests = 10
+
+    for i in range(requests):
+        request = b"Hello world"
+        try:
+            client.send(b"echo",  request)
+        except KeyboardInterrupt:
+            print("send interrupted, aborting")
+            return
+
+    count = 0
+    while count < requests:
+        try:
+            reply = client.recv()
+        except KeyboardInterrupt:
+            break
+        else:
+            # also break on failure to reply:
+            if reply is None:
+                break
+        count += 1
+    # print("%i requests/replies processed" % count)
+
+
+if __name__ == "__main__":
+    main()
