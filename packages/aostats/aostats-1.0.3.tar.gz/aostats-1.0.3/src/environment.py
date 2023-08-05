a35bf84d0d242bb3fb76ed84fef8df86ec6d561f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:00:48 +0000

    Add package aostats/1.0.3/aostats-1.0.3.tar.gz

---
+++ packages/aostats/aostats-1.0.3.tar.gz/aostats-1.0.3/src/environment.py
@@ -0,0 +1,25 @@
+import os
+
+TEST_ENV_ENABLED = bool(os.getenv('TESTING'))
+
+
+class TestType:
+    Simple = 'simple'
+    ZoneChange = 'zone_change'
+    Healing = 'healing'
+
+
+_test_types = {
+    TestType.Simple: TestType.Simple,
+    TestType.ZoneChange: TestType.ZoneChange,
+    TestType.Healing: TestType.Healing
+}
+
+if TEST_ENV_ENABLED:
+    test_type = os.getenv('TESTING')
+
+    if test_type not in _test_types:
+        raise Exception('Unavailable GUI test type, avaliable values: {}'.format(
+            ','.join(_test_types.keys())))
+
+    TEST_TYPE = _test_types[test_type.lower()]
