fb355f4503a6b4f888b71d927bbbd51c99bbd503
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:06:07 +0000

    Add package Aptus/3.0/Aptus-3.0-cp39-cp39-manylinux_2_5_i686.manylinux1_i686.manylinux_2_12_i686.manylinux2010_i686.whl

---
+++ packages/Aptus/Aptus-3.0-cp39-cp39-manylinux_2_5_i686.manylinux1_i686.manylinux_2_12_i686.manylinux2010_i686.whl/aptus/cmdline.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+
+import sys
+
+from PIL import Image
+
+from aptus.compute import AptusCompute
+from aptus.progress import IntervalProgressReporter, ConsoleProgressReporter
+from aptus.options import AptusOptions
+
+
+class AptusCmdApp():
+    def main(self, args):
+        """ The main for the Aptus command-line tool.
+        """
+        compute = AptusCompute()
+        opts = AptusOptions(compute)
+        opts.read_args(args)
+        compute.create_mandel()
+
+        compute.progress = IntervalProgressReporter(60, ConsoleProgressReporter())
+        compute.compute_pixels()
+        pix = compute.color_mandel()
+        im = Image.fromarray(pix)
+        if compute.supersample > 1:
+            print("Resampling image...")
+            im = im.resize(compute.size, Image.ANTIALIAS)
+        compute.write_image(im, compute.outfile)
+
+
+def main(argv=None):
+    if argv is None:
+        argv = sys.argv[1:]
+    AptusCmdApp().main(argv)
+
+
+if __name__ == '__main__':
+    main()
