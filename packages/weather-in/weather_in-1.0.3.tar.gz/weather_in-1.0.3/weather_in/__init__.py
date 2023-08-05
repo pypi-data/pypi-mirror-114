536bdb1bea8010090212e55a090b2ead330dd1cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:25 +0000

    Add package weather-in/1.0.3/weather_in-1.0.3.tar.gz

---
+++ packages/weather-in/weather_in-1.0.3.tar.gz/weather_in-1.0.3/weather_in/__init__.py
@@ -0,0 +1,11 @@
+__version__ = '1.0.3'
+
+import requests
+import json
+import sys
+
+api_key = "f054a8afdf8d658df832afeccd66c806"
+
+city = sys.argv[1]
+state = sys.argv[2]
+url = "https://api.openweathermap.org/data/2.5/weather?q=%s,%s&appid=%s&units=imperial" % (city, state, api_key)
