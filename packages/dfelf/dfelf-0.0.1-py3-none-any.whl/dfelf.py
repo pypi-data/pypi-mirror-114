88dcf7157ec436466d7426bb84b3c608505b7001
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:11:55 +0000

    Add package dfelf/0.0.1/dfelf-0.0.1-py3-none-any.whl

---
+++ packages/dfelf/dfelf-0.0.1-py3-none-any.whl/dfelf.py
@@ -0,0 +1,6 @@
+# coding: utf-8
+
+from datafileelf.DataFileElf import DataFileElf
+from datafileelf.CSVFileElf import CSVFileElf
+from datafileelf.PDFFileElf import PDFFileElf
+from datafileelf.ImageFileElf import ImageFileElf
