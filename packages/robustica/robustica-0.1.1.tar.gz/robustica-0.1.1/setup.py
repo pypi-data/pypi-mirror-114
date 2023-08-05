744e562755e66527ef4d8f2f302c5f3cf383ac24
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:00:50 +0000

    Add package robustica/0.1.1/robustica-0.1.1.tar.gz

---
+++ packages/robustica/robustica-0.1.1.tar.gz/robustica-0.1.1/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+setup(
+    name="robustica",
+    version="0.1.1",
+    packages=["robustica"],
+    python_requires=">=3.8",
+    package_data={"": ["LICENSE", "*.md","*.ipynb","*.yml"]},
+    install_requires=[
+        "numpy",
+        "pandas",
+        "scipy",
+        "scikit-learn",
+        "scikit-learn-extra",
+        "joblib",
+        "tqdm",
+    ],
+    author="Miquel Anglada Girotto",
+    author_email="miquel.anglada@crg.eu",
+    description="Fully cumstomizable robust Independent Components Analysis (ICA)",
+    url="https://github.com/CRG-CNAG/robustica",
+    project_urls={"Issues": "https://github.com/CRG-CNAG/robustica/issues"},
+)
