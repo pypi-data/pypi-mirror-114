21d7fa8e9a40c63ad050802ced5b0430cef1f3d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:06:56 +0000

    Add package torchdyn/1.0.0/torchdyn-1.0.0.tar.gz

---
+++ packages/torchdyn/torchdyn-1.0.0.tar.gz/torchdyn-1.0.0/setup.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['torchdyn',
+ 'torchdyn.core',
+ 'torchdyn.datasets',
+ 'torchdyn.models',
+ 'torchdyn.nn',
+ 'torchdyn.numerics']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['ipykernel',
+ 'ipywidgets',
+ 'matplotlib',
+ 'poethepoet>=0.10.0,<0.11.0',
+ 'pytorch-lightning',
+ 'scipy',
+ 'sklearn',
+ 'torch>=1.8.1,<2.0.0',
+ 'torchvision']
+
+setup_kwargs = {
+    'name': 'torchdyn',
+    'version': '1.0.0',
+    'description': 'Your PyTorch package for neural differential equations',
+    'long_description': None,
+    'author': 'Michael Poli',
+    'author_email': None,
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
