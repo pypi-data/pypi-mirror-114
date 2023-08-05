629c98792fa8128e857fb7e4548c7e15c9d36f6e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:02:54 +0000

    Add package antipywidgets/0.0.2/antipywidgets-0.0.2-py3-none-any.whl

---
+++ packages/antipywidgets/antipywidgets-0.0.2-py3-none-any.whl/antipywidgets/jobs_runner.py
@@ -0,0 +1,56 @@
+import ipysheet
+import ipywidgets as widgets
+from ipywidgets import Button, GridBox, Layout, ButtonStyle, VBox, HBox, Label, Valid, Textarea
+import traceback
+from typing import Dict, Callable
+from IPython.display import display
+
+
+def make_runner(jobs: Dict[str, Callable]) -> Callable:
+    """
+    Display initial run board
+
+    Parameters
+    ----------
+    jobs: Dict[str,Callable]
+        Jobs to run
+
+    Returns
+    -------
+    Callable
+        Should be called to run jobs
+    """
+
+
+    def make_new_row(name):
+        name = Label(value=name, layout=Layout(width='10%'))
+
+        status = VBox([Label(value="Pending...")], layout= Layout(width='10%')) #Valid(value=status, )
+
+        message = Textarea(value="", disabled=True, layout=Layout(width='80%'))
+
+        return [name,status,message]
+    
+    jobs_and_rows = [{"job_name": job_name, "job_func":job_func, "row": make_new_row(job_name)} for job_name, job_func in jobs.items() ]
+
+    table = VBox([HBox(i["row"]) for i in jobs_and_rows] )
+
+    display(table)
+    
+    def run_func():
+        for job in jobs_and_rows:
+            job_name = job["job_name"]
+            job_func = job["job_func"]
+            row = job["row"]
+            print(f"Run '{job_name}'")
+            status = row[1]
+            message = row[2]
+            try:
+                _ = job_func()
+                
+                status.children = [Valid(value=True, description="Passed")]
+            except Exception as e:
+                err = traceback.format_exc()
+                status.children = [Valid(value=False, description="Failed")]
+                message.value = err
+    return run_func
