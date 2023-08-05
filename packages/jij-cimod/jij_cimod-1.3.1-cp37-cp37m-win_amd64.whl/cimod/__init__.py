ae5fe8d7fba461b32ff6be08e6cb98de5ffd88c3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:08:22 +0000

    Add package jij-cimod/1.3.1/jij_cimod-1.3.1-cp37-cp37m-win_amd64.whl

---
+++ packages/jij-cimod/jij_cimod-1.3.1-cp37-cp37m-win_amd64.whl/cimod/__init__.py
@@ -0,0 +1,26 @@
+
+
+""""""  # start delvewheel patch
+def _delvewheel_init_patch_0_0_13():
+    import os
+    import sys
+    libs_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir, '.'))
+    if sys.version_info[:2] >= (3, 8):
+        os.add_dll_directory(libs_dir)
+    else:
+        from ctypes import WinDLL
+        with open(os.path.join(libs_dir, '.load-order-jij_cimod-1.3.1')) as file:
+            load_order = file.read().split()
+        for lib in load_order:
+            WinDLL(os.path.join(libs_dir, lib))
+
+
+_delvewheel_init_patch_0_0_13()
+del _delvewheel_init_patch_0_0_13
+# end delvewheel patch
+
+
+from .__version import __version__
+from .vartype import SPIN, BINARY, Vartype
+from .model.binary_quadratic_model import make_BinaryQuadraticModel, make_BinaryQuadraticModel_from_JSON, BinaryQuadraticModel
+from .model.binary_polynomial_model import make_BinaryPolynomialModel, make_BinaryPolynomialModel_from_JSON, BinaryPolynomialModel
