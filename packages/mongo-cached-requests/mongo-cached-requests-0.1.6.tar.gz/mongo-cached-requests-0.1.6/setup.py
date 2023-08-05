8c52aebba0ecd16dd50dd31d58f9be8230e3821d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:07:15 +0000

    Add package mongo-cached-requests/0.1.6/mongo-cached-requests-0.1.6.tar.gz

---
+++ packages/mongo-cached-requests/mongo-cached-requests-0.1.6.tar.gz/mongo-cached-requests-0.1.6/setup.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+package_dir = \
+{'': 'src'}
+
+packages = \
+['mongo_cached_requests']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['pymongo>=3.11.4,<4.0.0', 'requests>=2.25.1,<3.0.0']
+
+setup_kwargs = {
+    'name': 'mongo-cached-requests',
+    'version': '0.1.6',
+    'description': '',
+    'long_description': None,
+    'author': 'Patrick Fenerty',
+    'author_email': 'patrick@fenerty.me',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'package_dir': package_dir,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
