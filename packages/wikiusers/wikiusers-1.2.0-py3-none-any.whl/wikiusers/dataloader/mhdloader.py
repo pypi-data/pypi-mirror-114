1ae3e6b5d19658618674ab76ee4975cc9a566e4a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:08:31 +0000

    Add package wikiusers/1.2.0/wikiusers-1.2.0-py3-none-any.whl

---
+++ packages/wikiusers/wikiusers-1.2.0-py3-none-any.whl/wikiusers/dataloader/mhdloader.py
@@ -0,0 +1,74 @@
+import requests
+from typing import Union
+from pathlib import Path
+from whdtscraper import fetch_wikies, fetch_dumps, fetch_latest_version, WIKI_URL
+
+from wikiusers import logger
+
+
+class MhdLoader:
+
+    def __init__(self, datasets_dir: Union[str, Path], lang: str):
+        datasets_dir = Path(datasets_dir)
+        self.wiki_dir = datasets_dir.joinpath('mhd')
+        self.lang = lang
+
+    def __download(self, file_path: str, file_path_obj: Path, tsv_url: str) -> None:
+        file_path_obj.parent.mkdir(exist_ok=True, parents=True)
+        response = requests.get(tsv_url, allow_redirects=True)
+        open(file_path_obj, 'wb').write(response.content)
+        logger.debug(f'Downloaded {file_path}', lang=self.lang, scope='scraper')
+
+    def __download_if_needed(self, file_path: str, size_in_bytes: int, tsv_url: str) -> None:
+        logger.debug(f'Syncing {file_path}', lang=self.lang, scope='scraper')
+        file_path_obj = self.wiki_dir.joinpath(file_path)
+
+        if not file_path_obj.is_file():
+            self.__download(file_path, file_path_obj, tsv_url)
+        elif abs(file_path_obj.stat().st_size - size_in_bytes) > 50e6:
+            logger.warn(f'{file_path} already exists but with unexpected size, redownloading',
+                        lang=self.lang, scope='scraper')
+            file_path_obj.unlink()
+            self.__download(file_path, file_path_obj, tsv_url)
+        else:
+            logger.debug(f'{file_path} already exists', lang=self.lang, scope='scraper')
+
+    def sync_wikies(self, /, version: str = 'latest'):
+        logger.info('Syncing tsv files', lang=self.lang, scope='scraper')
+
+        if (version == 'latest'):
+            last_version_obj = fetch_latest_version()
+            version = last_version_obj['version']
+
+        wiki = f'{self.lang}wiki'
+        dumps = fetch_dumps(version, wiki)
+
+        for dump in dumps:
+            url: str = dump['url']
+            size_in_bytes: int = int(dump['bytes'])
+            self.__download_if_needed(url.replace(f'{WIKI_URL}/', ''), size_in_bytes, url)
+
+        logger.succ(f'Synced tsv files', lang=self.lang, scope='scraper')
+
+    def get_tsv_files(self, /, version: str = 'latest') -> list[Path]:
+        if version == 'latest':
+            version_path = list(self.wiki_dir.glob('*'))[-1]
+        else:
+            version_path = self.wiki_dir.joinpath(version)
+
+        wiki = f'{self.lang}wiki'
+        wiki_path = version_path.joinpath(wiki)
+        files = [file for file in wiki_path.iterdir() if file.is_file()]
+        return sorted(files, key=lambda f: f.stem)
+
+    def get_available_langs(self) -> list[str]:
+        unfiltered_langs = sorted([wiki['wiki'].replace('wiki', '') for wiki in fetch_wikies('latest', wikitype='wiki')])
+        return [lang for lang in unfiltered_langs if len(lang) == 2]
+
+    def get_local_langs(self) -> list[str]:
+        versions = sorted([dir.name for dir in self.wiki_dir.iterdir() if dir.is_dir()], reverse=True)
+        if not versions:
+            return []
+
+        path = self.wiki_dir.joinpath(versions[0])
+        return sorted([dir.name.replace('wiki', '') for dir in path.iterdir() if dir.is_dir()])
---
+++ packages/wikiusers/wikiusers-1.2.0-py3-none-any.whl/wikiusers/rawprocessor/processor.py
@@ -0,0 +1,111 @@
+from pathlib import Path
+from typing import Optional, Tuple, Union
+from joblib import Parallel, delayed
+from pymongo import MongoClient
+
+from wikiusers import logger
+from wikiusers import settings
+from wikiusers.dataloader import MhdLoader
+from wikiusers.rawprocessor.utils import Analyzer
+
+
+class RawProcessor:
+
+    def __init_loader(self, lang: str) -> None:
+        loader = MhdLoader(self.datasets_dir, lang)
+        if self.sync_data:
+            loader.sync_wikies()
+        return loader
+
+    def __check_if_collection_already_exists(self, lang: str) -> bool:
+        connection = MongoClient(self.dburl)
+        database = connection.get_database(self.database)
+        collection = database.get_collection(f'{lang}wiki_raw')
+
+        db_collections = database.list_collection_names()
+        if collection.name in db_collections:
+            if self.force:
+                logger.warn('Collection already exists: dropping',
+                            lang=lang, scope='RAWPROCESSOR')
+                collection.drop()
+            elif self.skip:
+                logger.warn('Collection already exists, skipping',
+                           lang=lang, scope='RAWPROCESSOR')
+
+                return True
+            else:
+                logger.warn('Collection already exists, it does not matter',
+                           lang=lang, scope='RAWPROCESSOR')
+        return False
+
+    def __get_tsv_month_and_year(self, tsv_file_name: str) -> Tuple[Optional[int], Optional[int]]:
+        time_str = tsv_file_name.split('.')[-3]
+        parts = time_str.split('-')
+
+        if len(parts) > 1:
+            [year, month] = parts
+        else:
+            year = parts[0]
+
+        try:
+            month = int(month)
+        except:
+            month = None
+
+        try:
+            year = int(year)
+        except:
+            year = None
+
+        return month, year
+
+    def __init__(
+        self,
+        dburl: str = settings.DEFAULT_DATABASE_URL,
+        sync_data: bool = settings.DEFAULT_SYNC_DATA,
+        datasets_dir: Union[Path, str] = settings.DEFAULT_DATASETS_DIR,
+        langs: Union[str, list[str]] = settings.DEFAULT_LANGUAGE,
+        parallelize: bool = settings.DEFAULT_PARALLELIZE,
+        n_processes: int = settings.DEFAULT_N_PROCESSES,
+        database: str = settings.DEFAULT_DATABASE_PREFIX,
+        force: bool = settings.DEFAULT_FORCE,
+        skip: bool = settings.DEFAULT_SKIP,
+        erase_datasets: bool = settings.DEFAULT_ERASE_DATASETS
+    ):
+        self.dburl = dburl
+        self.sync_data = sync_data
+        self.datasets_dir = Path(datasets_dir)
+        self.langs = [langs] if type(langs) == str else langs
+        self.parallelize = parallelize
+        self.n_processes = n_processes
+        self.database = f'{database}_raw'
+        self.force = force
+        self.skip = skip
+        self.erase_datasets = erase_datasets
+
+    def _process_file(self, lang: str, path: Path) -> None:
+        logger.info(f'Starting processing {path}', lang=lang, scope='RAWPROCESSOR')
+        month, year = self.__get_tsv_month_and_year(path.name)
+        analyzer = Analyzer(path, month, year, lang, self.dburl, self.database)
+        analyzer.analyze()
+        logger.succ(f'Finished processing {path}', lang=lang, scope='RAWPROCESSOR')
+        if self.erase_datasets:
+            path.unlink()
+            logger.info(f'Deleted {path}', lang=lang, scope='RAWPROCESSOR')
+
+    def process(self) -> None:
+        for lang in self.langs:
+            if self.__check_if_collection_already_exists(lang):
+                continue
+
+            loader = self.__init_loader(lang)
+            datasets_paths = loader.get_tsv_files()
+
+            if self.parallelize:
+                Parallel(n_jobs=self.n_processes)(
+                    delayed(self._process_file)(lang, path)
+                    for path in datasets_paths
+                )
+            else:
+                for path in datasets_paths:
+                    self._process_file(lang, path)
