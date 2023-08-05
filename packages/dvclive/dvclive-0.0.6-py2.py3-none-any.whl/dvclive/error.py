d3ada3c43a2a4ae82378ee97a3d3bccb225a4921
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:08:26 +0000

    Add package dvclive/0.0.6/dvclive-0.0.6-py2.py3-none-any.whl

---
+++ packages/dvclive/dvclive-0.0.6-py2.py3-none-any.whl/dvclive/error.py
@@ -0,0 +1,10 @@
+class DvcLiveError(Exception):
+    pass
+
+
+class InitializationError(DvcLiveError):
+    def __init__(self):
+        super().__init__(
+            "Initialization error - no call was made to `dvclive.init()` "
+            " or `dvclive.log()` "
+        )
