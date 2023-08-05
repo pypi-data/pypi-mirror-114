fd0b1ab3f5cab84e65faeade90049d1576fe134a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:07:55 +0000

    Add package sublinput/1.0.1/sublinput-1.0.1-py3-none-any.whl

---
+++ packages/sublinput/sublinput-1.0.1-py3-none-any.whl/sublinput/__init__.py
@@ -0,0 +1,13 @@
+"""An input pygame alternative for sublime text users."""
+
+from sublinput.ui import InputBoxScreen
+import builtins
+
+
+def new_input(__prompt: str = '') -> str:
+    """A function that creates a new input box screen and return it's value."""
+    with InputBoxScreen() as input_box:
+        return input_box(__prompt)
+
+
+builtins.input = new_input
