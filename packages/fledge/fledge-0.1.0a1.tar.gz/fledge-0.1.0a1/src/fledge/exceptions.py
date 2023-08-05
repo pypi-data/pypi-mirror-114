6a818fc8d2c75f72e260c0fbd19fc66119f3696f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:04:29 +0000

    Add package fledge/0.1.0a1/fledge-0.1.0a1.tar.gz

---
+++ packages/fledge/fledge-0.1.0a1.tar.gz/fledge-0.1.0a1/src/fledge/exceptions.py
@@ -0,0 +1,7 @@
+"""Exceptions."""
+
+
+class NoPathError(Exception):
+    """Raised when no path to the desired frame or projection exists."""
+
+    pass
---
+++ packages/fledge/fledge-0.1.0a1.tar.gz/fledge-0.1.0a1/src/fledge/named.py
@@ -0,0 +1,72 @@
+# flake8: noqa
+# TODO: re-add to flake8
+from __future__ import annotations
+
+from typing import Any
+from typing import Final
+from typing import final
+from typing import Optional
+
+
+def is_named(item: Any) -> bool:
+    return issubclass(type(item), Named)
+
+
+class Named(object):
+    """Named objects are uniquely identifiable by their `name` attribute.
+
+    NOTE: this does not preclude objects of different classes from sharing names if the user
+    provides them. This is not a problem unless the objects are stored in the same registry, which
+    handles that case.
+
+    Args:
+        name (Optional[str], optional): name of the instance. If not provided, the name is derived
+            from the classname.
+
+    Raises:
+        ValueError: if the provided name is already in use by an instance of the same class.
+
+    """
+
+    name: str
+
+    # The instance count is never decremented.
+    _instance_count: Final[int] = 0
+
+    # Names are removed from this when deleted, allowing for reuse.
+    _instance_names: Final[set] = set()
+
+    def __init__(self, name: Optional[str] = None) -> None:
+        if name is None:
+            self.name = (
+                f"{self.__class__.__name__.lower()}_{self.__class__._instance_count}"
+            )
+        elif name not in self._instance_names:
+            self.name = name
+        else:
+            raise ValueError("cannot reuse name: {}".format(name))
+
+        self.__class__._instance_count += 1
+
+    def __hash__(self):
+        return hash(self.name)
+
+    def __del__(self):
+        if self.name in self._instance_names:
+            self._instance_names.remove(self.name)
+
+    def rename(self, name: str) -> Named:
+        """Rename the named object.
+
+        Sub-classes may wish to update a registry containing the object.
+
+        """
+        if self.name == name:
+            return self
+        elif name in self._instance_names:
+            raise ValueError(f"name already taken: {name}")
+
+        self._instance_names.remove(self.name)
+        self._instance_names.add(name)
+        self.name = name
+        return self
