9aa4e934974185a1b7f5056ea13011c4d4015b5c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:07:44 +0000

    Add package nxviz/0.7.0/nxviz-0.7.0.tar.gz

---
+++ packages/nxviz/nxviz-0.7.0.tar.gz/nxviz-0.7.0/tests/test_nxviz.py
@@ -0,0 +1,26 @@
+"""Tests for nxviz high level API."""
+
+from itertools import cycle
+from matplotlib import pyplot as plt
+
+import nxviz as nv
+
+import pytest
+
+
+@pytest.mark.usefixtures("dummyG")
+def test_api(dummyG):
+    """Tests that the high level APIs work properly."""
+    apifuncs = nv.arc, nv.circos, nv.parallel, nv.hive, nv.matrix
+
+    for func in apifuncs:
+        fig, ax = plt.subplots()
+        func(dummyG, group_by="group", sort_by="value", edge_alpha_by="edge_value")
+
+
+def test_classes(dummyG):
+    """Tests that the object oriented APIs execute."""
+    objects = nv.ArcPlot, nv.CircosPlot, nv.MatrixPlot, nv.HivePlot
+    for obj in objects:
+        fig, ax = plt.subplots()
+        obj(dummyG, node_grouping="group", node_order="value", node_color="group")
