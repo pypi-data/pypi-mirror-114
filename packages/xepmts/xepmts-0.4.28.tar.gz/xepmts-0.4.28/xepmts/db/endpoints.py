de558736959782b66fbefc99d695de50b823e816
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:11:46 +0000

    Add package xepmts/0.4.28/xepmts-0.4.28.tar.gz

---
+++ packages/xepmts/xepmts-0.4.28.tar.gz/xepmts-0.4.28/xepmts/db/endpoints.py
@@ -0,0 +1,22 @@
+import httpx
+import logging
+import xepmts_endpoints
+from xepmts_endpoints import endpoints
+
+
+log = logging.getLogger(__name__)
+
+def clean_nones(d):
+    return {k:v for k,v in d.items() if v is not None}
+
+def get_endpoints(servers, endpoint_path='endpoints'):
+    for server in servers:
+        uri = "/".join([server.rstrip('/'), endpoint_path.lstrip('/')])
+        log.info(f"Attempting to read endpoints from {uri}")
+        r = httpx.get(uri, timeout=15)
+        if not r.is_error:
+            log.info('Endpoints read succesfully from server.')
+            return {k: clean_nones(v) for k,v in r.json().items()}
+
+    log.error("Failed to read endpoint definitions from server, loading defaults.")
+    return xepmts_endpoints.get_endpoints()
