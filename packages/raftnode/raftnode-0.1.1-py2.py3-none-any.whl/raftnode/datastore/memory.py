b45d249293cca51f9d91cdfd01f4f6975935e5f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:00:38 +0000

    Add package raftnode/0.1.1/raftnode-0.1.1-py2.py3-none-any.whl

---
+++ packages/raftnode/raftnode-0.1.1-py2.py3-none-any.whl/raftnode/datastore/memory.py
@@ -0,0 +1,53 @@
+from raftnode.datastore.Idatastore import IDatastore
+
+
+class MemoryStore(IDatastore):
+
+    '''
+    A class that implements IDatastore. It is responsible
+    for storing data in-memory and retrieving it 
+    using python dictionary
+    '''
+
+    def __init__(self):
+        self.__db = dict()
+
+    def connect(self):
+        '''
+        create/connect to the in-memory data store
+        '''
+        return self.__db
+
+    def put(self, key: str, value, **kwargs):
+        '''
+        insert values into the in-memory datastore
+
+        :param key: key using which data will be stored
+                    in the dictionary
+        :type key: str
+
+        :param value: the actual data to be stored in the
+                    dictionary
+        :type value: any
+        '''
+        try:
+            self.__db.update({key: value})
+        except Exception as e:
+            raise e
+
+    def get(self, key: str, **kwargs) -> dict:
+        '''
+        fetch data form the in-memory datastore
+
+        :param key: key using which data will be fetched from the
+                    dictionary
+        :type key: str
+
+        :returns: data in dictionary format
+        :rtype: dict
+        '''
+        try:
+            data = self.__db.get(key, None)
+            return data
+        except Exception as e:
+            raise e
---
+++ packages/raftnode/raftnode-0.1.1-py2.py3-none-any.whl/raftnode/raftnode.py
@@ -0,0 +1,49 @@
+"""Main module."""
+from threading import Thread
+from raftnode import logger
+from raftnode.election import Election
+from raftnode.store import Store
+from raftnode.transport import Transport
+
+
+class RaftNode(Transport):
+
+    def __init__(self, my_ip: str, peers: list, timeout: int, **kwargs):
+        self.__store = Store(**kwargs)
+        self.__transport = Transport(my_ip, timeout=timeout)
+        self.__election = Election(
+            transport=self.__transport, store=self.__store)
+        self.__peers = peers
+
+    def run(self):
+        '''
+        start the server, add peers and election timer
+        '''
+        logger.info('starting transport')
+        self.start_transport()
+        logger.info('adding peers')
+        self.start_adding_peers(peers=self.__peers)
+        logger.info('initializing timeout')
+        # self.start_timeout()
+
+    def start_transport(self):
+        '''
+        start the socket server for this node
+        '''
+        Thread(target=self.__transport.serve, args=(self.__election,)).start()
+
+    def start_adding_peers(self, peers):
+        '''
+        if peers are specified at the runtime, add them to
+        the list of peers
+        '''
+        if peers:
+            for peer in peers:
+                Thread(target=self.__transport.req_add_peer,
+                       args=(peer,)).start()
+
+    def start_timeout(self):
+        '''
+        start the election timer
+        '''
+        self.__election.init_timeout()
