708a2236e66a14ba529660d588f9af6b40f4a000
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:07:42 +0000

    Add package hrl-pybullet-envs/0.2.28/hrl_pybullet_envs-0.2.28-py3-none-any.whl

---
+++ packages/hrl-pybullet-envs/hrl_pybullet_envs-0.2.28-py3-none-any.whl/hrl_pybullet_envs/envs/MjAnt.py
@@ -0,0 +1,92 @@
+# modified from: pybulletgym/envs/mujoco/robots/locomotors/ant.py
+
+import numpy as np
+from gym.spaces import Box
+from pybullet_envs.gym_locomotion_envs import WalkerBaseBulletEnv, AntBulletEnv
+from pybullet_envs.robot_locomotors import Ant, WalkerBase
+
+
+class MjAnt(Ant):
+    def __init__(self, start_pos=(0, 0), action_dim=8, obs_dim=23):
+        WalkerBase.__init__(self, "ant.xml", "torso", action_dim=action_dim, obs_dim=obs_dim, power=2.5)
+        self.start_pos = start_pos
+        self.observation_space = Box(-np.inf, np.inf, shape=(23,))
+
+    def calc_state(self):
+        # super().calc_state()
+        j = np.array([j.current_relative_position() for j in self.ordered_joints],
+                     dtype=np.float32).flatten()
+        # even elements [0::2] position, scaled to -1..+1 between limits
+        # odd elements  [1::2] angular speed, scaled to show -1..+1
+        self.joint_speeds = j[1::2]
+        self.joints_at_limit = np.count_nonzero(np.abs(j[0::2]) > 0.99)
+
+        body_pose = self.robot_body.pose()
+        parts_xyz = np.array([p.pose().xyz() for p in self.parts.values()]).flatten()
+        self.body_xyz = (parts_xyz[0::3].mean(), parts_xyz[1::3].mean(), body_pose.xyz()[2])
+        self.body_real_xyz = body_pose.xyz()
+        self.body_rpy = body_pose.rpy()
+        z = self.body_xyz[2]
+        if self.initial_z == None:
+            self.initial_z = z
+        self.walk_target_dist = np.linalg.norm(
+            [self.walk_target_y - self.body_real_xyz[1], self.walk_target_x - self.body_real_xyz[0]])
+
+        pose = self.parts['torso'].get_pose()
+        qpos = np.hstack((pose, [j.get_position() for j in self.ordered_joints])).flatten()
+        velocity = self.parts['torso'].speed()
+        qvel = np.hstack((velocity[0], velocity[1], [j.get_velocity() for j in self.ordered_joints])).flatten()
+
+        return np.concatenate([
+            qpos.flat[2:],  # self.sim.data.qpos.flat[2:],
+            qvel.flat,  # self.sim.data.qvel.flat,
+            # np.clip(self.sim.data.cfrc_ext, -1, 1).flat,
+        ])
+
+    def alive_bonus(self, z, pitch):
+        return +1 if z - self.initial_z > 0.26 else -1  # 0.25 is central sphere rad, die if it scrapes the ground
+
+
+class AntMjEnv(WalkerBaseBulletEnv):
+    def __init__(self, start_pos=(0, 0), progress_weight=1., ctrl_cost_weight=1., survive_reward_weight=1.):
+        self.robot = MjAnt(start_pos=start_pos, obs_dim=25)
+        super().__init__(self.robot)
+
+        self.progress_weight = progress_weight
+        self.ctrl_cost_weight = ctrl_cost_weight
+        self.survive_reward_weight = survive_reward_weight
+
+    def step(self, a):
+        if not self.scene.multiplayer:
+            self.robot.apply_action(a)
+            self.scene.global_step()
+
+        state = self.robot.calc_state()  # also calculates self.joints_at_limit
+
+        # state[0] is body height above ground, body_rpy[1] is pitch
+        self._alive = float(self.robot.alive_bonus(state[0] + self.robot.initial_z, self.robot.body_rpy[1]))
+        done = self._isDone()
+        if not np.isfinite(state).all():
+            print("~INF~", state)
+            done = True
+
+        potential_old = self.potential
+        self.potential = self.robot.calc_potential()
+
+        progress = self.progress_weight * float(self.potential - potential_old)
+        ctrl_cost = self.ctrl_cost_weight * .5 * np.square(a).sum()
+        survive_reward = self.survive_reward_weight * 1.0
+        reward = progress - ctrl_cost + survive_reward
+
+        self.update_foot_contacts()
+
+        return state, reward, bool(done), {'ctrl_cost': ctrl_cost, 'survive_reward': survive_reward,
+                                           'progress':  progress}
+
+    def update_foot_contacts(self):
+        for i, f in enumerate(self.robot.feet):
+            contact_ids = set((x[2], x[4]) for x in f.contact_list())
+            if self.ground_ids & contact_ids:
+                self.robot.feet_contact[i] = 1.0
+            else:
+                self.robot.feet_contact[i] = 0.0
---
+++ packages/hrl-pybullet-envs/hrl_pybullet_envs-0.2.28-py3-none-any.whl/hrl_pybullet_envs/envs/ant_maze/ant_maze_bullet_env.py
@@ -0,0 +1,99 @@
+import numpy as np
+from pybullet_envs.gym_locomotion_envs import AntBulletEnv
+
+from hrl_pybullet_envs.envs.ant_maze.maze_scene import MazeScene
+from hrl_pybullet_envs.utils import debug_draw_point, PositionEncoding
+from gym.spaces import Box
+import gym
+
+_eval_target = [-2, 3.5]
+_targets = ([2, -3.5], [2, 0], [2, 3.5], [0, 3.5], _eval_target)
+
+
+class AntMazeBulletEnv(AntBulletEnv):
+    """
+    At evaluation time, we evaluate the agent only on its ability to reach (0,16). We define a “success” as being within
+    an L2 distance of 5 from the target on the ultimate step of the episode. - Data efficient HRL
+    """
+
+    def __init__(self, n_bins: int = 10, sensor_range: float = 5, sensor_span: float = 2 * np.pi, targets=_targets,
+                 target_encoding: PositionEncoding = 0, tol=1.5, inner_rew_weight=0, seed=None, debug=0):
+        super().__init__()
+        self.robot.start_pos_x, self.robot.start_pos_y, self.robot.start_pos_z = -2, -3.5, 0.25
+
+        self.n_bins = n_bins
+        self.sensor_range = float(sensor_range)
+        self.sensor_span = sensor_span
+
+        self.targets = targets
+        self.tol = tol
+        self.inner_rew_weight = inner_rew_weight
+
+        if isinstance(target_encoding, int):
+            target_encoding = PositionEncoding(target_encoding)
+        self.target_encoding = target_encoding
+        self.target: np.ndarray = np.array(_eval_target)
+
+        self.rs, _ = gym.utils.seeding.np_random(seed)
+
+        self.debug = debug
+
+        self.action_space = self.robot.action_space
+
+        self.observation_space = Box(-np.inf, np.inf, shape=(self.robot.observation_space.shape[0] - 2 + n_bins + 2,))
+
+    def create_single_player_scene(self, bullet_client):
+        self.stadium_scene = MazeScene(bullet_client, 9.8, 0.0165 / 4, 4)
+        return self.stadium_scene
+
+    def _get_obs(self, ant_obs):
+        wall_obs = self.scene.sense_walls(self.n_bins, self.sensor_span, self.sensor_range,
+                                          self.robot.body_real_xyz[:2], self.robot_body.pose().rpy()[2], self.debug > 1)
+
+        target_obs = []
+        vec_to_target = self.target - self.robot_body.pose().xyz()[:2]
+
+        if self.target_encoding == PositionEncoding.normed_vec:
+            target_obs = vec_to_target / np.linalg.norm(vec_to_target)
+        elif self.target_encoding == PositionEncoding.angle:
+            angle_to_target = np.arctan2(*vec_to_target[::-1]) - self.robot_body.pose().rpy()[2]
+            target_obs = [np.sin(angle_to_target), np.cos(angle_to_target)]
+
+        return np.concatenate((target_obs, [ant_obs[0]], ant_obs[3:], wall_obs))
+
+    def step(self, a):
+        if self.debug > 0:
+            debug_draw_point(self.scene._p, *self.target, colour=[0.1, 0.5, 0.7])
+        ant_obs, inner_rew, d, i = super().step(a)
+        obs = self._get_obs(ant_obs)
+
+        rew = inner_rew * self.inner_rew_weight
+
+        if self.robot.walk_target_dist < self.tol:
+            rew += 1
+            d = True
+
+        return obs, rew, d, i
+
+    def seed(self, seed=None):
+        super().seed(seed)
+        self.rs, seed = gym.utils.seeding.np_random(seed)
+        return [seed]
+
+    def reset(self):
+        if not hasattr(self, '_p'):
+            super().reset()
+
+        start_xyz = [self.robot.start_pos_x, self.robot.start_pos_y, self.robot.start_pos_z]
+        self.target = self.targets[self.rs.randint(0, len(self.targets))]
+        super().reset()
+
+        # Allows for correct inner reward
+        self.walk_target_x, self.walk_target_y = self.target
+        self.robot.walk_target_x, self.robot.walk_target_y = self.target
+
+        self._p.resetBasePositionAndOrientation(self.robot.objects[0], start_xyz, [0, 0, 0, 1])
+        self.robot.robot_specific_reset(self.scene._p)
+        ant_obs = self.robot.calc_state()
+
+        return self._get_obs(ant_obs)
