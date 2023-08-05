19878c00ae55d42b46231ced1ce3c9e6b26363ea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:07:40 +0000

    Add package MGEmasker/0.1.13/MGEmasker-0.1.13.tar.gz

---
+++ packages/MGEmasker/MGEmasker-0.1.13.tar.gz/MGEmasker-0.1.13/setup.py
@@ -0,0 +1,29 @@
+import setuptools
+import os
+import glob
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setuptools.setup(
+    name="MGEmasker",
+    version="0.1.13",
+    author="Anthony Underwood",
+    author_email="au3@sanger.ac.uk",
+    long_description=read("README.md"),
+    long_description_content_type="text/markdown",
+    license="MIT",
+    packages=setuptools.find_packages(),
+    package_data={'mge_masker': ['mge_patterns.txt', 'tests/data/*']},
+    include_package_data=True,
+    entry_points={
+        'console_scripts': [
+            'mge_masker=mge_masker.run_mge_masker:main',
+            'mgemasker=mge_masker.run_mge_masker:main'
+        ]
+    },
+    python_requires='>=3',
+    install_requires=['biopython >= 1.7.9', 'rich'],
+    setup_requires=['pytest-runner'],
+    tests_require=['pytest', 'coverage']
+)
