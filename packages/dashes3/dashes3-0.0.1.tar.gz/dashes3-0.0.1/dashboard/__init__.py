32c5947df641861637b20f7003524a9e8eeb17cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:06:45 +0000

    Add package dashes3/0.0.1/dashes3-0.0.1.tar.gz

---
+++ packages/dashes3/dashes3-0.0.1.tar.gz/dashes3-0.0.1/dashboard/__init__.py
@@ -0,0 +1,1 @@
+from dashes3.dashboard.dashboard import main
---
+++ packages/dashes3/dashes3-0.0.1.tar.gz/dashes3-0.0.1/utils/__init__.py
@@ -0,0 +1,4 @@
+from dashes3.utils.fetch_url import fetch_url
+from dashes3.utils.load_css import local_css
+from dashes3.utils.load_data import load_data
+from dashes3.utils.load_time_series import load_time_series
