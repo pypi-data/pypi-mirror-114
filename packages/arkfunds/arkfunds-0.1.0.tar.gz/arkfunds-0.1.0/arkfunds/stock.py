4e59b3acb450d46402c7d9e9155be4bff389af34
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:08:38 +0000

    Add package arkfunds/0.1.0/arkfunds-0.1.0.tar.gz

---
+++ packages/arkfunds/arkfunds-0.1.0.tar.gz/arkfunds-0.1.0/arkfunds/stock.py
@@ -0,0 +1,67 @@
+from datetime import date
+from .arkfunds import ArkFunds
+
+
+class Stock(ArkFunds):
+    """Class for accessing Stock data"""
+
+    def __init__(self, symbol: str):
+        """Initialize
+
+        Args:
+            symbol (str): Stock ticker
+        """
+        super().__init__()
+        self.symbol = symbol
+
+    def profile(self):
+        """Get Stock profile information
+
+        Returns:
+            dict
+        """
+        params = {
+            "symbol": self.symbol,
+        }
+
+        res = self._get(key="stock", endpoint="profile", params=params)
+
+        return res.json()
+
+    def fund_ownership(self):
+        """Get Stock Fund Ownership
+
+        Returns:
+            pandas.DataFrame
+        """
+        params = {
+            "symbol": self.symbol,
+        }
+
+        res = self._get(key="stock", endpoint="ownership", params=params).json()
+
+        return self._dataframe(res, key="stock", endpoint="ownership")
+
+    def trades(
+        self, direction: str = None, date_from: date = None, date_to: date = None
+    ):
+        """Get Stock Trades
+
+        Args:
+            direction (str, optional): 'Buy' or 'Sell'. Defaults to None.
+            date_from (date, optional): From-date in ISO 8601 format.. Defaults to None.
+            date_to (date, optional): To-date in ISO 8601 format.. Defaults to None.
+
+        Returns:
+            pandas.DataFrame
+        """
+        params = {
+            "symbol": self.symbol,
+            "direction": [direction.lower() if direction else None],
+            "date_from": date_from,
+            "date_to": date_to,
+        }
+
+        res = self._get(key="stock", endpoint="trades", params=params).json()
+
+        return self._dataframe(res, key="stock", endpoint="trades")
