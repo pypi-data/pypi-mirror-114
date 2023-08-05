00eefec45d66495a0dd6cb789c9be6e18ca1e37d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:05:38 +0000

    Add package fies/0.1.0/fies-0.1.0.tar.gz

---
+++ packages/fies/fies-0.1.0.tar.gz/fies-0.1.0/fies/test.py
@@ -0,0 +1,20 @@
+
+# ファイル入出力ツール [fies]
+# 【動作確認 / 使用例】
+
+import sys
+from sout import sout
+from ezpip import load_develop
+fies = load_develop("fies", "../", develop_flag = True)
+
+# json書き出し
+fies["./test.json"] = {"hoge": 23, "dummy_data": "fuga"}
+
+# json読み込み
+sout(fies["./test.json"])
+
+# プレーンテキスト書き出し
+fies["./test.txt"] = "hogehoge"
+
+# プレーンテキスト読み込み
+print(fies["./test.txt"])
