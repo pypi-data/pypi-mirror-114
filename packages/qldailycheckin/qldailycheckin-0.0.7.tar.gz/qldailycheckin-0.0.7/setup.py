4f82e4831069cdc2dcdf140f390bff05f733a80f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:05:53 +0000

    Add package qldailycheckin/0.0.7/qldailycheckin-0.0.7.tar.gz

---
+++ packages/qldailycheckin/qldailycheckin-0.0.7.tar.gz/qldailycheckin-0.0.7/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup,find_packages
+
+setup(
+    name='qldailycheckin',
+    version='0.0.7',
+    author='yuxian158',
+    author_email='yuxian158@gmail.com',
+    url='https://sitoi.gitee.io/dailycheckin/',
+    packages=find_packages(),
+    install_requires=[],
+    entry_points={
+        'console_scripts': [
+            'qldailycheckin=dailycheckin.main:checkin'
+        ]
+    }
+)
