f651af1f6f844184224d3e7fe82d4e1132eb7d2b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:02:27 +0000

    Add package deemon/1.1/deemon-1.1.tar.gz

---
+++ packages/deemon/deemon-1.1.tar.gz/deemon-1.1/README.md
@@ -0,0 +1,37 @@
+![](deemon/assets/images/logo.png "deemon")
+
+[About](#about) | [Upgrading from 0.4.x](#migrating) | [Installation](#installation) | [Support](#support) | [Documentation](https://digitalec.github.io/deemon)
+
+![PyPI](https://img.shields.io/pypi/v/deemon?style=flat-square)
+![PyPI - Python Version](https://img.shields.io/pypi/pyversions/deemon?style=flat-square)
+![PyPI - License](https://img.shields.io/pypi/l/deemon?style=flat-square)
+[![Downloads](https://pepy.tech/badge/deemon)](https://pepy.tech/project/deemon)
+![GitHub last commit](https://img.shields.io/github/last-commit/digitalec/deemon?style=flat-square)
+![Discord](https://img.shields.io/discord/831356172464160838?style=flat-square)
+
+
+
+### About
+deemon is an automation tool that relies on the deemix library and
+the deezer-py API module to monitor a specified list of artists for new releases
+
+### Support
+[Open an Issue](https://github.com/digitalec/deemon/issues/new) | [Discord](https://discord.gg/KzNCG2tkvn)
+
+
+### Upgrading from 0.4.x or earlier
+Please note, the deemon CLI has changed significantly and no longer accepts many of the switches/commands that were previously used. This was done to provide a better, more natural, user experience.
+
+Please take a look at the help via `deemon --help` or by reading the [documentation](https://digitalec.github.io/deemon).
+
+### Installation
+
+#### Using pip
+
+```bash
+$ pip install deemon
+```
+
+#### From source
+```bash
+$ pip install -r requirements.txt
---
+++ packages/deemon/deemon-1.1.tar.gz/deemon-1.1/deemon/app/batch.py
@@ -0,0 +1,51 @@
+from deemon.app import Deemon, download, monitor
+from deemon.app.refresh import Refresh
+from pathlib import Path
+import tqdm
+import logging
+import sys
+
+logger = logging.getLogger(__name__)
+
+
+class BatchJobs(Deemon):
+
+    def import_artists(self, path):
+        import_artists = path
+        # TODO check db for existing artist
+        if import_artists:
+            if Path(import_artists).is_file():
+                with open(import_artists, encoding="utf8") as f:
+                    import_list = f.read().splitlines()
+                    num_to_import = len(import_list)
+                    logger.debug(f"Detected {num_to_import} artist(s) to import")
+            elif Path(import_artists).is_dir():
+                import_list = [x.relative_to(import_artists) for x in sorted(Path(import_artists).iterdir()) if x.is_dir()]
+                num_to_import = len(import_list)
+                logger.debug(f"Detected {num_to_import} artist(s) to import")
+            else:
+                logger.error(f"File or directory not found: {import_artists}")
+                sys.exit(1)
+
+            progress = tqdm.tqdm(import_list, bar_format='{l_bar}{bar}| {n_fmt}/{total_fmt} ({elapsed}{postfix})')
+
+            import_count = 0
+            import_error = 0
+            for artist in progress:
+                progress.set_description("Importing")
+                ma = monitor.Monitor()
+                ma.artist = artist
+                imported = ma.start_monitoring()
+                if imported == 1:
+                    import_count += 1
+                elif imported == 2:
+                    import_error += 1
+            if import_count > 0:
+                logger.info(f"** Successful: {import_count} | Errors: {import_error}")
+            else:
+                logger.info(f"** No new artists to import | Errors: {import_error}")
+            self.db.commit()
+
+            print()
+            refresh = Refresh()
+            refresh.refresh()
