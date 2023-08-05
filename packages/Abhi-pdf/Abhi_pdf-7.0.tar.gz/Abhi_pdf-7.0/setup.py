d29ebeb2c9ec7d8b3cf8013b301f8d6d213533c7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:06:09 +0000

    Add package Abhi-pdf/7.0/Abhi_pdf-7.0.tar.gz

---
+++ packages/Abhi-pdf/Abhi_pdf-7.0.tar.gz/Abhi_pdf-7.0/setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+from setuptools import setup, find_packages
+import requests
+import os
+setup(
+    name='Abhi_pdf',
+    version='7.0',
+    author='Tector Pro',
+    description='Testing something',
+    packages=find_packages()
+)
