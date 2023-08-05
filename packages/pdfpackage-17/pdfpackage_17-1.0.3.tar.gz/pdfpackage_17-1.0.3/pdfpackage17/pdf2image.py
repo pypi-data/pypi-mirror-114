de2465b346d6306b517a326a3f72ea6c15057827
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:05:59 +0000

    Add package pdfpackage-17/1.0.3/pdfpackage_17-1.0.3.tar.gz

---
+++ packages/pdfpackage-17/pdfpackage_17-1.0.3.tar.gz/pdfpackage_17-1.0.3/pdfpackage17/pdf2image.py
@@ -0,0 +1,14 @@
+""" This module provides functions to convert a PDF to text."""
+
+
+def convert(path):
+    """ 
+    Convert the given PDF to image.
+
+    Parameters:
+    path (str): The path to a PDF file.
+
+    Returns:
+    image: The content of the PDF file as image.
+    """
+    print("pdf2image")
