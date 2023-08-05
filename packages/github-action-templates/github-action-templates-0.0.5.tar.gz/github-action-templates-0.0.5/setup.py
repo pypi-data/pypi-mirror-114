dce3179a488cfa08dc084a13293b7541515c26a1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:10:26 +0000

    Add package github-action-templates/0.0.5/github-action-templates-0.0.5.tar.gz

---
+++ packages/github-action-templates/github-action-templates-0.0.5.tar.gz/github-action-templates-0.0.5/setup.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+from setuptools import setup, find_packages
+from os import path
+
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name="github-action-templates",
+    version="0.0.5",
+    description="Template GitHub Actions",
+    author="Cian Hatton",
+    author_email="cianhatton@gmail.com",
+    url="https://github.com/chatton/github-action-templates",
+    packages=find_packages(),
+    long_description=long_description,
+    long_description_content_type='text/markdown'
+)
