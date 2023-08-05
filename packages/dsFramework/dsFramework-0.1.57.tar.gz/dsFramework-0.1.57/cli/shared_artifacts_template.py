0105ef15b7b4d6af4a43bdb45c2f2cd2cae9d045
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:08:47 +0000

    Add package dsFramework/0.1.57/dsFramework-0.1.57.tar.gz

---
+++ packages/dsFramework/dsFramework-0.1.57.tar.gz/dsFramework-0.1.57/cli/shared_artifacts_template.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+import json
+from dsframework.base.pipeline.artifacts.shared_artifacts import ZIDS_SharedArtifacts
+
+class generatedClass(ZIDS_SharedArtifacts):
+
+    def __init__(self) -> None:
+        super().__init__()
