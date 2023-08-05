0b732b253ead611f654eef898ed3cf44bc1a73a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:00:21 +0000

    Add package srf_app_helper/1.0.9/srf_app_helper-1.0.9.tar.gz

---
+++ packages/srf_app_helper/srf_app_helper-1.0.9.tar.gz/srf_app_helper-1.0.9/setup.py
@@ -0,0 +1,13 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='srf_app_helper',
+    version='1.0.9',
+    packages=['core'],
+    description="为srf项目打造的app收集管理程序",
+    author="WangLaoSi", # 作者
+    author_email='103745315@qq.com', # 作者邮箱
+    url="https://gitee.com/Wang_LaoSi/srf_app_helper", # 项目主页
+    download_url='https://gitee.com/Wang_LaoSi/srf_app_helper/repository/archive/master.zip', # 下载地址
+    install_requires=['sanic-plugin-toolkit','sanic_rest_framework'] # 依赖
+)
