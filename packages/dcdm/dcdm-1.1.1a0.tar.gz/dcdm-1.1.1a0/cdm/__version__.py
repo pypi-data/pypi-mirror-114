14d86345ac83eae2b6b6354d89701c180aab15fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:08:19 +0000

    Add package dcdm/1.1.1a0/dcdm-1.1.1a0.tar.gz

---
+++ packages/dcdm/dcdm-1.1.1a0.tar.gz/dcdm-1.1.1a0/cdm/__version__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+
+VERSION = (1, 1, 1)
+PRERELEASE = "alpha"  # alpha, beta or rc
+REVISION = None
+
+
+def generate_version(version, prerelease=None, revision=None):
+    version_parts = [".".join(map(str, version))]
+    if prerelease:
+        version_parts.append("-{}".format(prerelease))
+    if revision:
+        version_parts.append(".{}".format(revision))
+    return "".join(version_parts)
+
+
+__title__ = "dcdm"
+__description__ = "cdm is a commend line Download Manger"
+__url__ = "https://github.com/DoostanKhas/cdm "
+__version__ = generate_version(VERSION, prerelease=PRERELEASE, revision=REVISION)
+__author__ = "doostanKhas"
+__author_email__ = "doostanKhas@gmail.com"
+__license__ = "Apache 2.0"
