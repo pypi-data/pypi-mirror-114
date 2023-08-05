f9551cdcbc98921609ee537d2fff49870531a1e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:05:54 +0000

    Add package example-LR/1.0.0/example_LR-1.0.0.tar.gz

---
+++ packages/example-LR/example_LR-1.0.0.tar.gz/example_LR-1.0.0/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages,setup
+#导入setup函数
+setup(
+    name= 'example_LR',#模块名
+    version= '1.0.0',#版本
+    packages=find_packages(),#目录的所有文件
+    author= 'liurui',#作者
+    author_email='11@189.com',#邮箱
+    description= '测试example_LR'
+)
