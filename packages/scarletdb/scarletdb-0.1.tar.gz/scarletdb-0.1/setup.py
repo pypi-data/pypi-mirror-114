51144228cb1c72c4d0fefe3a28201ae694dee7b3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:10:54 +0000

    Add package scarletdb/0.1/scarletdb-0.1.tar.gz

---
+++ packages/scarletdb/scarletdb-0.1.tar.gz/scarletdb-0.1/setup.py
@@ -0,0 +1,23 @@
+from distutils.core import setup
+setup(
+  name = 'scarletdb',         # How you named your package folder (MyLib)
+  packages = ['scarletdb'],   # Chose the same as "name"
+  version = '0.1',      # Start with a small number and increase it with every change you make
+  license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
+  description = 'TYPE YOUR DESCRIPTION HERE',   # Give a short description about your library
+  author = 'YOUR NAME',                   # Type in your name
+  author_email = 'xfinnbar@gmail.com',      # Type in your E-Mail
+  url = 'https://github.com/xd-pro/scarletdb',   # Provide either the link to your github or to your website
+  download_url = 'https://github.com/Xd-pro/ScarletDB/archive/refs/tags/0.1-beta.tar.gz',    # I explain this later on
+  keywords = ['db', 'database', 'scarlet'],   # Keywords that define your package best
+  install_requires=[            # I get to this in a second
+          'replit',
+      ],
+  classifiers=[
+    'Development Status :: 4 - Beta',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
+    'Intended Audience :: Developers',      # Define that your audience are developers
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',   # Again, pick a license
+    'Programming Language :: Python :: 3.8',
+  ],
+)
