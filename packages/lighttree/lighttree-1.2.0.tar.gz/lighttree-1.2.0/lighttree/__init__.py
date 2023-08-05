586c3cfe8ea78fe45c705a0a3c633d8d44429b0d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:09:46 +0000

    Add package lighttree/1.2.0/lighttree-1.2.0.tar.gz

---
+++ packages/lighttree/lighttree-1.2.0.tar.gz/lighttree-1.2.0/lighttree/__init__.py
@@ -0,0 +1,4 @@
+from .interactive import TreeBasedObj
+from .tree import Tree, Node, Key, KeyedNode, KeyedTree
+
+__all__ = ["Tree", "Node", "TreeBasedObj", "Key", "KeyedNode", "KeyedTree"]
---
+++ packages/lighttree/lighttree-1.2.0.tar.gz/lighttree-1.2.0/lighttree/node.py
@@ -0,0 +1,54 @@
+import uuid
+from typing import Optional, Any, Tuple
+
+NodeId = str
+
+
+class Node(object):
+    def __init__(
+        self,
+        identifier: Optional[NodeId] = None,
+        auto_uuid: bool = True,
+        keyed: bool = True,
+        accept_children: bool = True,
+        repr_: Optional[str] = None,
+        data: Any = None,
+    ) -> None:
+        """
+        :param identifier: node identifier, must be unique per tree
+        """
+        if identifier is None:
+            if not auto_uuid:
+                raise ValueError("Required identifier")
+            identifier = str(uuid.uuid4())
+        self.identifier = identifier
+        self.keyed = keyed
+        self.accept_children = accept_children
+        self.repr = repr_
+        self.data = data
+
+    def line_repr(self, depth: int, **kwargs: Any) -> Tuple[str, str]:
+        """Control how node is displayed in tree representation.
+        _
+        ├── one                                           end
+        │   └── two                                     myEnd
+        └── three
+        """
+        if self.repr is not None:
+            return self.repr, ""
+        if not self.accept_children:
+            return str(self.data), ""
+        if self.keyed:
+            return "{}", ""
+        return "[]", ""
+
+    def __eq__(self, other: Any) -> bool:
+        if not isinstance(other, self.__class__):
+            return False
+        return self.identifier == other.identifier
+
+    def __str__(self) -> str:
+        return "%s, id=%s" % (self.__class__.__name__, self.identifier)
+
+    def __repr__(self) -> str:
+        return self.__str__()
