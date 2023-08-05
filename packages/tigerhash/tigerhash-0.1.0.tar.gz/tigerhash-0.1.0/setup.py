82cb777eea716d7b31bb2d1f1f35d32124d2a896
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:03:11 +0000

    Add package tigerhash/0.1.0/tigerhash-0.1.0.tar.gz

---
+++ packages/tigerhash/tigerhash-0.1.0.tar.gz/tigerhash-0.1.0/setup.py
@@ -0,0 +1,27 @@
+from setuptools import find_packages, setup
+from distutils.core import Extension
+from Cython.Distutils import build_ext
+from Cython.Build import cythonize
+
+setup(
+    name='tigerhash',
+    version='0.1.0',
+    description='Here you can find Python3 lib with different hash structs for CDC task.',
+    author='Alex Glushko',
+    author_email='aglushko@hse.ru',
+    url='https://github.com/Badcat330/TigerHash',
+    classifiers=[
+        "Programming Language :: Cython",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+        "Development Status :: 1 - Planning"
+    ],
+    package_dir={"": "tigerhash"},
+    packages=find_packages(where="tigerhash"),
+    python_requires=">=3.9",
+    install_requires=['cython'],
+    setup_requires=['pytest-runner'],
+    tests_require=['pytest==4.4.1'],
+    test_suite='tests',
+    ext_modules=cythonize("tigerhash/tigerhash.pyx", language="c++"),
+)
