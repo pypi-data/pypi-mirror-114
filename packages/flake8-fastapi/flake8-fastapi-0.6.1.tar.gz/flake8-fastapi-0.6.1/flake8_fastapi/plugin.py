411072dbb1badf52898e3ad782a66ea8ed632c88
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:11:36 +0000

    Add package flake8-fastapi/0.6.1/flake8-fastapi-0.6.1.tar.gz

---
+++ packages/flake8-fastapi/flake8-fastapi-0.6.1.tar.gz/flake8-fastapi-0.6.1/flake8_fastapi/plugin.py
@@ -0,0 +1,24 @@
+from flake8_plugin_utils import Plugin
+
+from flake8_fastapi import __version__
+from flake8_fastapi.visitors import (
+    CORSMiddlewareOrder,
+    GenericExceptionHandler,
+    NoContentResponse,
+    RouteDecorator,
+    RouterPrefix,
+    UndocumentedHTTPException,
+)
+
+
+class FastAPIPlugin(Plugin):
+    name = "flake8-fastapi"
+    version = __version__
+    visitors = [
+        RouteDecorator,
+        RouterPrefix,
+        GenericExceptionHandler,
+        CORSMiddlewareOrder,
+        NoContentResponse,
+        UndocumentedHTTPException,
+    ]
