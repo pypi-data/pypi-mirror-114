837e8c3d51d65c89d6ba8e066de5ab6aa2d03de9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:06:11 +0000

    Add package journalize/0.0.0/journalize-0.0.0.tar.gz

---
+++ packages/journalize/journalize-0.0.0.tar.gz/journalize-0.0.0/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='journalize',
+    packages=["journalize"],
+    version='0.0.0',
+    author='Perzan',
+    author_email='PerzanDevelopment@gmail.com',
+    description="Please email me if you would like this project transferred"
+)
