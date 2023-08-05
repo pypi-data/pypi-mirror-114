e66e7e9ad30688714ed850935e92c140b6a4c656
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:10:48 +0000

    Add package analytix/2.0.0.dev7/analytix-2.0.0.dev7-py3-none-any.whl

---
+++ packages/analytix/analytix-2.0.0.dev7-py3-none-any.whl/analytix/__init__.py
@@ -0,0 +1,10 @@
+__productname__ = "analytix"
+__version__ = "2.0.0.dev7"
+__description__ = "A simple yet powerful API wrapper to make getting analytical information from the YouTube Analytics API easier than ever."
+__url__ = "https://github.com/parafoxia/analytix"
+__docs__ = "https://analytix.readthedocs.io/en/latest/"
+__author__ = "Ethan Henderson"
+__license__ = "BSD-3-Clause"
+__bugtracker__ = "https://github.com/parafoxia/analytix/issues"
+
+from .youtube.analytics.api import YouTubeAnalytics
