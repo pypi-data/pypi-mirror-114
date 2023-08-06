5ce0552ba2c5b4b6e19686311f2f45f1be16a8e5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:41:51 +0000

    Add package ModelOfPrintList/1.0.0/ModelOfPrintList-1.0.0.tar.gz

---
+++ packages/ModelOfPrintList/ModelOfPrintList-1.0.0.tar.gz/ModelOfPrintList-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name            = 'ModelOfPrintList',
+    version         = '1.0.0',
+    py_modules      = ['ModelOfPrintList'],
+    author          = 'trouvaille',
+    author_email    = '765856804@qq.com',
+    url             = 'https://blog.csdn.net/qq_52315758',
+    description     = 'A simple printer of nested lists',
+    )
