bf4a5f102285f05bd6be2062d4767f4f7590272e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:08:11 +0000

    Add package ranorex-report-parser/0.1.5/ranorex-report-parser-0.1.5.tar.gz

---
+++ packages/ranorex-report-parser/ranorex-report-parser-0.1.5.tar.gz/ranorex-report-parser-0.1.5/src/ranorex_report_parser/elements/test_module.py
@@ -0,0 +1,26 @@
+from datetime import datetime
+
+from .base import ReportElementWithStatus, ReportStatus
+from .item import ItemElement
+
+class TestModuleElement(ReportElementWithStatus):
+    def __init__(self, start_time: datetime, duration: int, status: ReportStatus, display_name: str):
+        super().__init__(start_time, duration, status)
+
+        self.display_name = display_name
+
+        self.id = ""
+        self.module_type = ""
+
+        self._items = []
+
+    def __str__(self):
+        return f'<TestModuleElement display_name: "{self.display_name}" status: {self.status}>'
+
+    def add_item(self, item: ItemElement):
+        self.add_element(item)
+        self._items.append(item)
+
+    def parse_attributes(self, attribs: dict):
+        self.id = attribs['moduleid']
+        self.module_type = attribs['moduletype']
