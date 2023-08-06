6257df4a9a2dc2c735e7f8ed0fd690526e841246
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:49:42 +0000

    Add package aiobungie/0.2.5b2/aiobungie-0.2.5b2-py3-none-any.whl

---
+++ packages/aiobungie/aiobungie-0.2.5b2-py3-none-any.whl/aiobungie/internal/__init__.pyi
@@ -0,0 +1,4 @@
+from . import enums as enums
+from .assets import Image as Image
+from .helpers import deprecated as deprecated
+from .time import Time as Time
---
+++ packages/aiobungie/aiobungie-0.2.5b2-py3-none-any.whl/aiobungie/types/__init__.py
@@ -0,0 +1,11 @@
+"""
+aiobungie.types
+---------------
+
+Typings for aiobungie API
+
+MIT License
+Copyright (c) 2020 - Present nxtlo
+"""
+
+from __future__ import annotations
