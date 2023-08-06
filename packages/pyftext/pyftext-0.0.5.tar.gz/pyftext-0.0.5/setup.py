db13b2965dc0ec99b0f98263f93b573565cc466d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:42:20 +0000

    Add package pyftext/0.0.5/pyftext-0.0.5.tar.gz

---
+++ packages/pyftext/pyftext-0.0.5.tar.gz/pyftext-0.0.5/setup.py
@@ -0,0 +1,28 @@
+import os
+from setuptools import find_packages, setup
+
+HERE = os.path.abspath(os.path.dirname(__file__))
+
+with open(os.path.join(HERE, "README.md")) as fid:
+    README = fid.read()
+
+setup(
+    name="pyftext",
+    version="0.0.5",
+    description="Ascii Word Writing",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url="https://github.com/yazilimfuryasi/pyftext",
+    author="yazilimfuryasi@gmail.com",
+    author_email="yazilimfuryasi@gmail.com",
+    license="MIT",
+    platforms="any",
+    packages=find_packages(),
+    python_requires=">=3.9.2",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.9",
+    ],
+    include_package_data=True,
+    install_requires=["Pillow"],
+)
