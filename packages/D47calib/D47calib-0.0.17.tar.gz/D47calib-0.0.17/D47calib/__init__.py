22e4bebc1343a3fc8c47e6115908cb566b703c3d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:30:26 +0000

    Add package D47calib/0.0.17/D47calib-0.0.17.tar.gz

---
+++ packages/D47calib/D47calib-0.0.17.tar.gz/D47calib-0.0.17/D47calib/__init__.py
@@ -0,0 +1,15 @@
+'''
+D47calib library
+'''
+
+__author__    = 'Mathieu Daëron'
+__contact__   = 'daeron@lsce.ipsl.fr'
+__copyright__ = 'Copyright (c) 2021 Mathieu Daëron'
+__license__   = 'Modified BSD License - https://opensource.org/licenses/BSD-3-Clause'
+__date__      = '2021-07-29'
+__version__   = '0.0.17'
+
+def bar():
+	print('bar')
+	
+	
