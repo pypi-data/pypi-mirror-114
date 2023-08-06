e817edf9460611c78a6bfa3ec8e7db79186a9fd1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:08:25 +0000

    Add package sdc-api-py/1.3.2/sdc_api_py-1.3.2-py3-none-any.whl

---
+++ packages/sdc-api-py/sdc_api_py-1.3.2-py3-none-any.whl/sdc_api_py/classes/Bots.py
@@ -0,0 +1,33 @@
+from typing import Union
+from discord.ext.commands import Bot, AutoShardedBot
+
+
+class Global:
+    SDC_token = ""
+    _time = 60
+    logging_level = None
+
+
+class Bots:
+
+    def __init__(self, bot: Union[Bot, AutoShardedBot], token: str, logging:bool = True):
+        if not token.startswith("SDC "):
+            token = "SDC " + token
+        self.SDC_token = token
+        self.bot = bot
+        Global.SDC_token = self.SDC_token
+        Global.logging_level = logging
+
+    def create_loop(self, time=60):
+        time = int(time)
+
+        if time < 30:
+            time = 30
+
+        Global._time = time
+        try:
+            self.bot.load_extension(".classes.extension", package="sdc_api_py")
+        except Exception as e:
+            print(e)
+        else:
+            print(f"SDC: Цикл успешно запущен. Статистика будет обновляться каждые {Global._time} минут")
