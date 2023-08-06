40840aaf8e486213fd808418189fd13a692c0fd3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:29:36 +0000

    Add package Amplo/0.4.0/Amplo-0.4.0.tar.gz

---
+++ packages/Amplo/Amplo-0.4.0.tar.gz/Amplo-0.4.0/tests/Unit/AutoML/test_data_processing.py
@@ -0,0 +1,40 @@
+import shutil
+import unittest
+import pandas as pd
+from sklearn.datasets import load_iris
+from sklearn.datasets import load_boston
+from Amplo.AutoML import DataProcessing
+from Amplo.Utils import check_dataframe_quality
+
+
+class TestDataProcessing(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        cls.classification, y = load_iris(return_X_y=True, as_frame=True)
+        cls.classification['target'] = y
+        x, y = load_boston(return_X_y=True)
+        cls.regression = pd.DataFrame({'target': y})
+        for i in range(x.shape[1]):
+            cls.regression['feature_{}'.format(i)] = x[:, i]
+
+    def test_regression(self):
+        dp = DataProcessing('target', folder='tmp/')
+        cleaned = dp.clean(self.regression)
+        assert check_dataframe_quality(cleaned)
+
+        # Cleanup
+        shutil.rmtree('tmp')
+
+    def test_classification(self):
+        dp = DataProcessing('target', folder='tmp/')
+        cleaned = dp.clean(self.classification)
+        assert check_dataframe_quality(cleaned)
+
+        # Cleanup
+        shutil.rmtree('tmp')
+
+    def test_standalone(self):
+        dp = DataProcessing()
+        cleaned = dp.clean(self.regression)
+        assert check_dataframe_quality(cleaned)
