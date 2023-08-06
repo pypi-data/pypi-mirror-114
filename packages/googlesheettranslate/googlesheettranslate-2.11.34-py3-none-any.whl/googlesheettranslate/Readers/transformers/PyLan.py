9c55f8c6bd466f50526bb35eea62be5e9df8b0d1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:08:12 +0000

    Add package googlesheettranslate/2.11.34/googlesheettranslate-2.11.34-py3-none-any.whl

---
+++ packages/googlesheettranslate/googlesheettranslate-2.11.34-py3-none-any.whl/googlesheettranslate/Readers/transformers/PyLan.py
@@ -0,0 +1,7 @@
+from .itransformer import InterfaceTransform
+
+
+class PyLan(InterfaceTransform):
+    def __init__(self):
+        """ implementation of interface transformation """
+        super().__init__()
