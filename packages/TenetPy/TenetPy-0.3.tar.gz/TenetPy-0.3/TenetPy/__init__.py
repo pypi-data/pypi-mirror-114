3d0e06b9108945242d88677f98b2d470af65322d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:24:12 +0000

    Add package TenetPy/0.3/TenetPy-0.3.tar.gz

---
+++ packages/TenetPy/TenetPy-0.3.tar.gz/TenetPy-0.3/TenetPy/__init__.py
@@ -0,0 +1,7 @@
+from TenetPy.PalindromeWord import PalindromeWord
+from TenetPy.PalindromeList import PalindromeList
+from TenetPy.Reverser import Reverser
+from TenetPy.PalindromeWordInList import PalindromeWordInList
+from TenetPy.WordLengthInList import WordLengthInList
+from TenetPy.Vowel import Vowel
+from TenetPy.ReverserList import ReverserList
