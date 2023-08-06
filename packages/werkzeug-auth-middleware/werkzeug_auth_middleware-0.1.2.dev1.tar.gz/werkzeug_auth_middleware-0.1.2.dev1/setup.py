b9719e176e8e829fa4c6cba5728237b690bebbe5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:14:04 +0000

    Add package werkzeug-auth-middleware/0.1.2.dev1/werkzeug_auth_middleware-0.1.2.dev1.tar.gz

---
+++ packages/werkzeug-auth-middleware/werkzeug_auth_middleware-0.1.2.dev1.tar.gz/werkzeug_auth_middleware-0.1.2.dev1/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['werkzeug_auth_middleware']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['Werkzeug==2.0.1', 'requests==2.26.0']
+
+setup_kwargs = {
+    'name': 'werkzeug-auth-middleware',
+    'version': '0.1.2.dev1',
+    'description': '',
+    'long_description': None,
+    'author': 'sorxcode',
+    'author_email': 'sorxcode@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.6,<4.0',
+}
+
+
+setup(**setup_kwargs)
