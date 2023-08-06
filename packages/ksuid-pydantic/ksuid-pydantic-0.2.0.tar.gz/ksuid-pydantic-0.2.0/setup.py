bdd851a5fef6cfd695d4b8db6386282e5f0c24e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:12:59 +0000

    Add package ksuid-pydantic/0.2.0/ksuid-pydantic-0.2.0.tar.gz

---
+++ packages/ksuid-pydantic/ksuid-pydantic-0.2.0.tar.gz/ksuid-pydantic-0.2.0/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['ksuid_pydantic']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['cyksuid>=1.1.0,<2.0.0', 'pydantic>=1.8.2,<2.0.0']
+
+setup_kwargs = {
+    'name': 'ksuid-pydantic',
+    'version': '0.2.0',
+    'description': '',
+    'long_description': None,
+    'author': 'Justin Rubek',
+    'author_email': 'justin@koloni.me',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.9,<4.0',
+}
+
+
+setup(**setup_kwargs)
