4ecd108cec11c1cfcdec61b4736cf46cfe19a01c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:10:08 +0000

    Add package cubist/0.0.6/cubist-0.0.6.tar.gz

---
+++ packages/cubist/cubist-0.0.6.tar.gz/cubist-0.0.6/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, Extension
+from Cython.Build import cythonize
+import glob
+import numpy as np
+
+exts = [
+    Extension(
+        name='_cubist',
+        sources=["cubist/_cubist.pyx"] + glob.glob("cubist/src/*.c"),
+        include_dirs=["cubist/src", np.get_include()]
+    )
+]
+
+with open("README.md", 'r') as f:
+    long_description = f.read()
+
+with open("requirements.txt", 'r') as f:
+    requires = f.read()
+
+setup(
+    name="cubist",
+    version="0.0.6",
+    description="A Python port of the R Cubist library.",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    ext_modules=cythonize(exts),
+    zip_safe=False,
+    include_package_data=True,
+    install_requires=requires,
+    url="https://github.com/pjaselin/Cubist"
+)
