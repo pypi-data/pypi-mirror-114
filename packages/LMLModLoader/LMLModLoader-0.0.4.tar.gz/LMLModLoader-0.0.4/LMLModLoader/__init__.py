0e5b2150639ef50ab9dd28e746763692001f1d13
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:31:15 +0000

    Add package LMLModLoader/0.0.4/LMLModLoader-0.0.4.tar.gz

---
+++ packages/LMLModLoader/LMLModLoader-0.0.4.tar.gz/LMLModLoader-0.0.4/LMLModLoader/__init__.py
@@ -0,0 +1,14 @@
+import os
+
+def register_mods(path):
+    modsPath = os.getenv('USERPROFILE')+"\\LoudML"
+    for s in os.listdir(modsPath):
+        modPath = modsPath + "\\" + s
+        for line in open(modPath):
+            l = line.split()
+            if l[0] == "setScene":
+                open(f"{path}\\{l[1]}.txt","w").write(" ".join(l[2:]).replace('"',''))
+
+
+
+        
---
+++ packages/LMLModLoader/LMLModLoader-0.0.4.tar.gz/LMLModLoader-0.0.4/setup.py
@@ -0,0 +1,11 @@
+
+from setuptools import setup
+
+setup(
+    name='LMLModLoader',
+    version='0.0.4',
+    description='The Required PyPi module for loading Forte Mods.',
+    long_description='The Required PyPi module for loading Forte Mods.',
+    packages=['LMLModLoader']
+    )
+
