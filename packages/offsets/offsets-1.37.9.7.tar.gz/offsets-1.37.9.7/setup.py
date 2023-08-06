4a862a5cbe81ed76b4b0e84e5667cef90f81d86c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:50:32 +0000

    Add package offsets/1.37.9.7/offsets-1.37.9.7.tar.gz

---
+++ packages/offsets/offsets-1.37.9.7.tar.gz/offsets-1.37.9.7/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='offsets',
+      version='1.37.9.7',
+      description='offsets csgo for python, autoupdate on, enjoy',
+      packages=['offsets'],
+      author_email='adamisaev9234@gmail.com',
+      zip_safe=False)
