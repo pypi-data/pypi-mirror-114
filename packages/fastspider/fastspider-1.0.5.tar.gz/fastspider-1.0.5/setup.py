ce05bdc15fa8ff47189298223d60203a72ecf62e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:12:06 +0000

    Add package fastspider/1.0.5/fastspider-1.0.5.tar.gz

---
+++ packages/fastspider/fastspider-1.0.5.tar.gz/fastspider-1.0.5/setup.py
@@ -0,0 +1,33 @@
+# encoding=utf-8
+
+import os
+
+import setuptools
+
+with open(os.path.join(os.path.join(os.path.dirname(__file__), "fastspider"), "VERSION"), "rb") as f:
+	version = f.read().decode().strip()
+
+with open("README.md", 'rb') as f:
+	readme = f.read().decode().strip()
+
+all_requires = []
+with open(os.path.join(os.path.join(os.path.dirname(__file__), "fastspider"), "requirments.txt"), "rb") as f:
+	requires = f.readlines()
+	for package in requires:
+		if package.decode().strip():
+			all_requires.append(package.decode().strip())
+
+setuptools.setup(
+	name="fastspider",
+	packages=setuptools.find_packages(),
+	version=version,
+	description="fastspider 爬虫框架",
+	long_description=readme,
+	author="wanghaifei",
+	author_email="wanghaifei36@126.com",
+	url="https://github.com/coco369/fastspider.git",
+	python_requires=">=3.6",
+	install_requires=all_requires,
+	entry_points={"console_scripts": ["fastspider=fastspider.commands.cmdline:execute"]},
+	classifiers=["Programming Language :: Python :: 3"],
+)
