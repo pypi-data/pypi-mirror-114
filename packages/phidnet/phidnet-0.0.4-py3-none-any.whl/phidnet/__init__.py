09a809d14da22190da8d8e5d28e1785c2d3b1359
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:24:33 +0000

    Add package phidnet/0.0.4/phidnet-0.0.4-py3-none-any.whl

---
+++ packages/phidnet/phidnet-0.0.4-py3-none-any.whl/phidnet/__init__.py
@@ -0,0 +1,14 @@
+__all__ = ['activation',
+           'error',
+           'matrix',
+           'model',
+           'network_data',
+           'one_hot_encode',
+           'set',
+           'feedforward',
+           'loss',
+           'gradient',
+           'optimizer',
+           'load']
+
+from phidnet import *
