e02b6d77ffdaf5c905d674136be0fbe9115a0307
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:25:58 +0000

    Add package double-pendulum/1.0.5/double-pendulum-1.0.5.tar.gz

---
+++ packages/double-pendulum/double-pendulum-1.0.5.tar.gz/double-pendulum-1.0.5/setup.py
@@ -0,0 +1,33 @@
+import setuptools
+
+with open("README.md",'r') as ld:
+    long_description = ld.read()
+
+setuptools.setup(
+    name="double-pendulum",
+    version="1.0.5",
+    author="Nekurone",
+    author_email="florencesecure@protonmail.com",
+    description="A small double pendulum simulator",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Nekurone/double-pendulum-ascii/",
+    package_dir={'':'double-pendulum'},
+    packages=setuptools.find_packages(
+        where='double-pendulum'
+    ),
+    entry_points =
+        {
+        "console_scripts": [
+        "double-pendulum=pendulum:main"
+        ]
+        },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU Lesser General Public License v2 (LGPLv2)",
+        "Operating System :: OS Independent",
+        "Intended Audience :: End Users/Desktop",
+        "Topic :: Terminals"
+    ],
+    python_requires=">=3.6",
+)
