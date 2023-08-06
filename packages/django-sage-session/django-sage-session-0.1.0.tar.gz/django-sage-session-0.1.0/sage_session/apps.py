2000da44498a3aa81916551a27432e1a065f237e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:50:46 +0000

    Add package django-sage-session/0.1.0/django-sage-session-0.1.0.tar.gz

---
+++ packages/django-sage-session/django-sage-session-0.1.0.tar.gz/django-sage-session-0.1.0/sage_session/apps.py
@@ -0,0 +1,7 @@
+from django.apps import AppConfig
+
+
+class SageSessionConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'sage_session'
+    verbose_name = 'User Session'
