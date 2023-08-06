810df81884912439bb69886c61ae0eea318e0c00
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:22:24 +0000

    Add package auto-editor/21.30.1/auto-editor-21.30.1.tar.gz

---
+++ packages/auto-editor/auto-editor-21.30.1.tar.gz/auto-editor-21.30.1/auto_editor/render/audio.py
@@ -0,0 +1,100 @@
+'''render/audio.py'''
+
+from __future__ import division
+
+import os
+
+from auto_editor.utils.func import get_new_length, fnone
+from auto_editor.utils.progressbar import ProgressBar
+from auto_editor.scipy.wavfile import read, write
+
+def convertAudio(ffmpeg, in_file, inp, out_file, codec, log):
+    if(fnone(codec)):
+        codec = inp.audio_streams[0]['codec']
+
+    ffmpeg.run(['-i', in_file, '-acodec', codec, out_file])
+
+def handleAudio(ffmpeg, in_file, audioBit, samplerate, temp, log):
+    # type: (...) -> str
+    temp_file = os.path.join(temp, 'faAudio.wav')
+
+    log.checkType(samplerate, 'samplerate', str)
+    cmd = ['-i', in_file]
+    if(not fnone(audioBit)):
+        cmd.extend(['-b:a', audioBit])
+    cmd.extend(['-ac', '2', '-ar', samplerate, '-vn', temp_file])
+
+    ffmpeg.run(cmd)
+    log.conwrite('')
+
+    return temp_file
+
+def fastAudio(in_file, out_file, chunks, speeds, log, fps, machineReadable, hideBar):
+    import numpy as np
+
+    def custom_speeds(a):
+        return len([x for x in a if x != 1 and x != 99999]) > 0
+
+    if(custom_speeds(speeds)):
+        from audiotsm2 import phasevocoder
+        from audiotsm2.io.array import ArrReader, ArrWriter
+
+    if(len(chunks) == 1 and chunks[0][2] == 0):
+        log.error('Trying to create an empty file.')
+
+    samplerate, audioData = read(in_file)
+
+    newL = get_new_length(chunks, speeds, fps)
+    # Get the new length in samples with some extra leeway.
+    estLeng = int(newL * samplerate * 1.5) + int(samplerate * 2)
+
+    # Create an empty array for the new audio.
+    newAudio = np.zeros((estLeng, 2), dtype=np.int16)
+
+    channels = 2
+    yPointer = 0
+
+    audioProgress = ProgressBar(len(chunks), 'Creating new audio', machineReadable,
+        hideBar)
+
+    for chunkNum, chunk in enumerate(chunks):
+        audioSampleStart = int(chunk[0] / fps * samplerate)
+        audioSampleEnd = int(audioSampleStart + (samplerate / fps) * (chunk[1] - chunk[0]))
+
+        theSpeed = speeds[chunk[2]]
+        if(theSpeed != 99999):
+            spedChunk = audioData[audioSampleStart:audioSampleEnd]
+
+            if(theSpeed == 1):
+                yPointerEnd = yPointer + spedChunk.shape[0]
+                newAudio[yPointer:yPointerEnd] = spedChunk
+            else:
+                spedupAudio = np.zeros((0, 2), dtype=np.int16)
+                with ArrReader(spedChunk, channels, samplerate, 2) as reader:
+                    with ArrWriter(spedupAudio, channels, samplerate, 2) as writer:
+                        phasevocoder(reader.channels, speed=theSpeed).run(
+                            reader, writer
+                        )
+                        spedupAudio = writer.output
+
+                yPointerEnd = yPointer + spedupAudio.shape[0]
+                newAudio[yPointer:yPointerEnd] = spedupAudio
+
+            myL = chunk[1] - chunk[0]
+            mySamples = (myL / fps) * samplerate
+            newSamples = int(mySamples / theSpeed)
+
+            yPointer = yPointer + newSamples
+        else:
+            # Speed is too high so skip this section.
+            yPointerEnd = yPointer
+
+        audioProgress.tick(chunkNum)
+
+    log.debug('Total Samples: {}'.format(yPointer))
+    log.debug('Samples per Frame: {}'.format(samplerate / fps))
+    log.debug('Expected video length: {}'.format(yPointer / (samplerate / fps)))
+    newAudio = newAudio[:yPointer]
+    write(out_file, samplerate, newAudio)
+
+    log.conwrite('')
---
+++ packages/auto-editor/auto-editor-21.30.1.tar.gz/auto-editor-21.30.1/auto_editor/utils/video.py
@@ -0,0 +1,72 @@
+'''utils/video.py'''
+
+# Internal Libraries
+import os
+from shutil import move, copy
+
+# Included Libraries
+from .func import fnone
+from auto_editor.render.audio import fastAudio
+
+def handleAudioTracks(ffmpeg, write_file, args, tracks, chunks, speeds, fps, temp, log):
+    for t in range(tracks):
+        temp_file = os.path.join(temp, '{}.wav'.format(t))
+        new_file = os.path.join(temp, 'new{}.wav'.format(t))
+        fastAudio(temp_file, new_file, chunks, speeds, log, fps,
+            args.machine_readable_progress, args.no_progress)
+
+        if(not os.path.isfile(new_file)):
+            log.bug('Audio file not created.')
+
+    if(args.export_as_audio):
+        if(args.keep_tracks_seperate):
+            log.warning("Audio files don't have multiple tracks.")
+
+        if(tracks == 1):
+            move(os.path.join(temp, '0.wav'), write_file)
+            return False
+
+        cmd = []
+        for t in range(tracks):
+            cmd.extend(['-i', os.path.join(temp, '{}.wav'.format(t))])
+        cmd.extend(['-filter_complex', 'amix=inputs={}:duration=longest'.format(tracks),
+            write_file])
+        ffmpeg.run(cmd)
+
+        return False
+    return True
+
+def mux_rename_video(ffmpeg, write_file, args, tracks, temp, log):
+
+    if(tracks == 0):
+        move(os.path.join(temp, 'spedup.mp4'), write_file)
+        log.conwrite('')
+        return
+
+    cmd = []
+    if(args.keep_tracks_seperate):
+        for t in range(tracks):
+            cmd.extend(['-i', os.path.join(temp, 'new{}.wav'.format(t))])
+        cmd.extend(['-i', os.path.join(temp, 'spedup.mp4')])
+        for t in range(tracks):
+            cmd.extend(['-map', '{}:a:0'.format(t)])
+        cmd.extend(['-map', '{}:v:0'.format(tracks)])
+    else:
+        # Merge all the audio tracks into one.
+        new_a_file = os.path.join(temp, 'newAudioFile.wav')
+        if(tracks > 1):
+            for t in range(tracks):
+                cmd.extend(['-i', os.path.join(temp, 'new{}.wav'.format(t))])
+            cmd.extend(['-filter_complex', 'amerge=inputs={}'.format(tracks), '-ac', '2',
+                new_a_file])
+            ffmpeg.run(cmd)
+        else:
+            copy(os.path.join(temp, 'new0.wav'), new_a_file)
+        cmd = ['-i', new_a_file, '-i', os.path.join(temp, 'spedup.mp4')]
+
+    cmd.extend(['-c:v', 'copy'])
+    if(not fnone(args.audio_codec)):
+        cmd.extend(['-c:a', args.audio_codec])
+    cmd.append(write_file)
+    ffmpeg.run(cmd)
+    log.conwrite('')
