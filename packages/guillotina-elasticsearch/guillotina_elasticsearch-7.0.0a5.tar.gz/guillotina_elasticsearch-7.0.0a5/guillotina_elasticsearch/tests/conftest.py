fcf32a8e49e8fdce3e2a18b7e6fd588d7a42a777
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:31:31 +0000

    Add package guillotina-elasticsearch/7.0.0a5/guillotina_elasticsearch-7.0.0a5.tar.gz

---
+++ packages/guillotina-elasticsearch/guillotina_elasticsearch-7.0.0a5.tar.gz/guillotina_elasticsearch-7.0.0a5/guillotina_elasticsearch/tests/conftest.py
@@ -0,0 +1,23 @@
+from pytest_docker_fixtures import images
+
+
+image_version = "7.5.1"
+
+images.configure(
+    "elasticsearch",
+    "docker.elastic.co/elasticsearch/elasticsearch",
+    image_version,
+    env={
+        "xpack.security.enabled": None,  # unset
+        "discovery.type": "single-node",
+        "http.host": "0.0.0.0",
+        "transport.host": "127.0.0.1",
+    },
+)
+
+
+pytest_plugins = [
+    "pytest_docker_fixtures",
+    "guillotina.tests.fixtures",
+    "guillotina_elasticsearch.tests.fixtures",
+]
