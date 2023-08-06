44b7a6551adba190afacfe4be1f366b6e2b8efc7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:41:56 +0000

    Add package distributions-apurb/0.1/distributions_apurb-0.1.tar.gz

---
+++ packages/distributions-apurb/distributions_apurb-0.1.tar.gz/distributions_apurb-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='distributions_apurb',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['distributions_apurb'],
+      zip_safe=False)
