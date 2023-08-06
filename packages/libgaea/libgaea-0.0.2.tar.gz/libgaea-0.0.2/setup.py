d823b52bc330a8858126d6b1f65e879bb37ca38e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:10:10 +0000

    Add package libgaea/0.0.2/libgaea-0.0.2.tar.gz

---
+++ packages/libgaea/libgaea-0.0.2.tar.gz/libgaea-0.0.2/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['gaea']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['PySide6>=6.1.2,<7.0.0', 'sh>=1.14.2,<2.0.0']
+
+setup_kwargs = {
+    'name': 'libgaea',
+    'version': '0.0.2',
+    'description': 'Spawner of the understory',
+    'long_description': '# gaea\nSpawn and manage your personal websites\n\nDownload and run the most recent [release](https://github.com/canopy/gaea/releases)\nfor your operating system.\n',
+    'author': 'Angelo Gladding',
+    'author_email': 'self@angelogladding.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.9',
+}
+
+
+setup(**setup_kwargs)
