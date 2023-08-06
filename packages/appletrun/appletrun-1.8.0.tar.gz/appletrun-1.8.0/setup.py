11b7de6ba1326c425edfbc9fbdc51a6588b7adcb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:13:03 +0000

    Add package appletrun/1.8.0/appletrun-1.8.0.tar.gz

---
+++ packages/appletrun/appletrun-1.8.0.tar.gz/appletrun-1.8.0/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['appletrun']
+
+package_data = \
+{'': ['*']}
+
+entry_points = \
+{'console_scripts': ['appletrun = appletrun._main_:main']}
+
+setup_kwargs = {
+    'name': 'appletrun',
+    'version': '1.8.0',
+    'description': '✨ Appletrun takes java file as input and runs the applet instantly with out compilation and html files',
+    'long_description': '# APPLETRUN\n\n![](https://img.shields.io/pypi/v/appletrun?style=for-the-badge)\n![](https://img.shields.io/apm/l/vim-mode?style=for-the-badge)\n![](https://img.shields.io/badge/requires-Java%20%3C%3D%208-red?style=for-the-badge)\n\nAppletRun runs applet code from given java file in a new window\n\n## 👉 Installation\n\n```\npip install appletrun\n```\n\nif not working try `python -m pip install appletrun` or `python3 -m pip install appletrun`\n\n## 👉 Usage\n\n```\n appletrun nameOfFile.java\n```\n\n<p align="center">\n<img src="https://user-images.githubusercontent.com/52039218/127250568-51bfd770-49c3-4d32-9da3-18627d9abde3.gif"/>\n </p>\n\n## 👉 Issues\n\nIf you found any bugs or issue post here\nhttps://github.com/Royal-lobster/appletrun-python/issues\n',
+    'author': 'Royal-lobster',
+    'author_email': 'srujangs8@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': 'https://github.com/Royal-lobster/appletrun-python',
+    'packages': packages,
+    'package_data': package_data,
+    'entry_points': entry_points,
+    'python_requires': '>=3.10,<4.0',
+}
+
+
+setup(**setup_kwargs)
