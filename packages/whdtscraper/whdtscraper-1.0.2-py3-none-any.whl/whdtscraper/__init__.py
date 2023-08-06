1e5c9ffbca724fb60364332e3555dbcf5668308d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:54:18 +0000

    Add package whdtscraper/1.0.2/whdtscraper-1.0.2-py3-none-any.whl

---
+++ packages/whdtscraper/whdtscraper-1.0.2-py3-none-any.whl/whdtscraper/__init__.py
@@ -0,0 +1,4 @@
+import warnings
+warnings.warn("Note: this package is deprecated in favour of mhdscraper", DeprecationWarning)
+
+from whdtscraper.whdtscraper import fetch_latest_version, fetch_versions, fetch_wikies, fetch_dumps, WIKI_URL
