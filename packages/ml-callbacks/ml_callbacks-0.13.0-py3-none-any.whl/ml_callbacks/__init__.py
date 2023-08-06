42f2e84db03ef9a3e16249885eed81552ab88335
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:25:31 +0000

    Add package ml-callbacks/0.13.0/ml_callbacks-0.13.0-py3-none-any.whl

---
+++ packages/ml-callbacks/ml_callbacks-0.13.0-py3-none-any.whl/ml_callbacks/__init__.py
@@ -0,0 +1,5 @@
+__version__ = "0.13.0"
+
+# imports
+
+__import__('sys').setrecursionlimit(1800)
---
+++ packages/ml-callbacks/ml_callbacks-0.13.0-py3-none-any.whl/ml_callbacks/enviroment_callback.py
@@ -0,0 +1,4 @@
+class EnviromentInterface:
+    def model_saving(self, model, path: str):
+        # interface
+        pass
