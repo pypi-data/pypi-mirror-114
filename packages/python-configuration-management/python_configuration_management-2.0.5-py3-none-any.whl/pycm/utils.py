177d1e3718ec70f82aaa8188cdd9d9ac3994a024
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:22:04 +0000

    Add package python-configuration-management/2.0.5/python_configuration_management-2.0.5-py3-none-any.whl

---
+++ packages/python-configuration-management/python_configuration_management-2.0.5-py3-none-any.whl/pycm/utils.py
@@ -0,0 +1,38 @@
+import os
+from getpass import getpass
+from pathlib import Path
+
+from dotenv import load_dotenv
+
+from pycm.root import PYCM_ROOT
+from pycm.secrets import decrypt_value, encrypt_value
+
+
+def load_env(environment: str):
+    env_path = Path(PYCM_ROOT) / f".env-{environment}"
+    if os.path.isfile(env_path):
+        load_dotenv(dotenv_path=env_path, verbose=True)
+        return
+
+    raise AssertionError(
+        f"env not found: {env_path}. This file is required and must contain your ENC_KEY"
+    )
+
+
+def gather_user_input():
+    key_name = input("Enter key name: ")
+    key_value = getpass("Enter key value: ")
+    return key_name, encrypt_value(key_value)
+
+
+def normalize_config_data(data: dict, use_secrets: bool):
+    normalized = dict()
+    for key, meta in data.items():
+        if type(meta) == dict:
+            if not use_secrets:
+                continue
+            value = meta["value"]
+            normalized[key] = decrypt_value(value)
+        else:
+            normalized[key] = meta
+    return normalized
