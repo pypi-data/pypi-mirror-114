468fde4d540c5cb24bb7b0a6ded8928ceb507ffe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:41:50 +0000

    Add package Viola-SV/1.0.0.dev10/Viola_SV-1.0.0.dev10-py3-none-any.whl

---
+++ packages/Viola-SV/Viola_SV-1.0.0.dev10-py3-none-any.whl/viola/_exceptions.py
@@ -0,0 +1,10 @@
+class TableNotFoundError(KeyError):
+    pass
+
+class InfoNotFoundError(KeyError):
+    pass
+
+class ContigNotFoundError(KeyError):
+    pass
+class IllegalArgumentError(ValueError):
+    pass
