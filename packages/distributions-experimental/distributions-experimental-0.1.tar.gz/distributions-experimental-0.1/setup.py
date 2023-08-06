5dfc7d379b4a9ff23f8103c0a5971ce821278a48
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:14:02 +0000

    Add package distributions-experimental/0.1/distributions-experimental-0.1.tar.gz

---
+++ packages/distributions-experimental/distributions-experimental-0.1.tar.gz/distributions-experimental-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='distributions-experimental',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['distributions-experimental'],
+      zip_safe=False)
