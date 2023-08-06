0370930ee3c5297aa345dd5c4201975bc1094a46
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:19:21 +0000

    Add package xesrepair/0.0.1/xesrepair-0.0.1-py3-none-any.whl

---
+++ packages/xesrepair/xesrepair-0.0.1-py3-none-any.whl/xesrepair/common.py
@@ -0,0 +1,31 @@
+import sys
+import subprocess
+import locale
+import os
+
+PYTHONW_EXE = sys.executable.replace(
+    "xes_py_helper.exe", "pythonw.exe").replace("python.exe", "pythonw.exe")
+
+def get_user_libs_path():
+    '''
+    :return 获取用户目录的学而思直播地址
+    '''
+    user_en = locale.getpreferredencoding()
+    if user_en == "cp936":
+        user_lib_path = os.path.expanduser(r"~\学而思直播\code\site-packages")
+    else:
+        user_lib_path = os.path.expanduser(r"~\xescode\site-packages")
+    return user_lib_path
+
+USER_LIB_PATH = get_user_libs_path()
+
+def create_python_process(args):
+    try:
+        print("start create_python_process")
+        process_res = subprocess.check_output(args)
+        print("end create_python_process")
+        return process_res
+    except Exception as e: 
+        print(e)
+        return e
+
