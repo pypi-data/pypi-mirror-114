dafbd3fff48572310cded99cdc09bf6feca7597d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:42:46 +0000

    Add package data-lelo/0.0.1/data_lelo-0.0.1.tar.gz

---
+++ packages/data-lelo/data_lelo-0.0.1.tar.gz/data_lelo-0.0.1/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+import os
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+VERSION = '0.0.1'
+DESCRIPTION = 'Fetch market data for the NSE Exchange'
+LONG_DESCRIPTION = 'Through the yahoo finance api, we can easily fetch this data'
+
+# Setting up
+setup(
+    name="data_lelo",
+    version=VERSION,
+    author="Abhishek Mittal",
+    author_email="abhishekmittal964@gmail.com",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=LONG_DESCRIPTION,
+    packages=find_packages(),
+    install_requires=['requests','os','dotenv','datetime'],
+    keywords=['python','finance','NSE','data','market','trading','investing'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
