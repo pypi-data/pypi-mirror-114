31ac711c6501dc06b73e2dd0ca4e60fae86bb46b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:08:35 +0000

    Add package pybsrnqc/0.1.42/pybsrnqc-0.1.42.tar.gz

---
+++ packages/pybsrnqc/pybsrnqc-0.1.42.tar.gz/pybsrnqc-0.1.42/pybsrnqc/config.py
@@ -0,0 +1,24 @@
+import importlib.resources
+import json
+
+
+def load(name_coef, coef, name_coef_min=None, coef_min=None):
+
+    json_lines = []
+
+    with importlib.resources.path("pybsrnqc", "qcrad_conf.json") as data_path:
+        with open(data_path, 'r') as f:
+            dic_coefs = json.load(f)
+
+    with importlib.resources.path("pybsrnqc", "qcrad_conf.json") as data_path:
+        with open(data_path, 'w') as open_file:
+            open_file.writelines('\n'.join(json_lines))
+
+    if name_coef_min is not None:
+        dic_coefs['COEF'][name_coef_min] = coef_min
+
+    dic_coefs['COEF'][name_coef] = coef
+
+    with importlib.resources.path("pybsrnqc", "qcrad_conf.json") as data_path:
+        with open(data_path, 'w') as open_file:
+            json.dump(dic_coefs, open_file)
