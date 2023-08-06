1a2ce61e1593d0057c56f3eede662b404c1b0013
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:16:53 +0000

    Add package imgkerneliu/0.0.3/imgkerneliu-0.0.3.tar.gz

---
+++ packages/imgkerneliu/imgkerneliu-0.0.3.tar.gz/imgkerneliu-0.0.3/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="imgkerneliu",
+    version="0.0.3",
+    author="ken",
+    author_email="liuyunclouder@gmail.com",
+    description="Image kernel.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/liuyunclouder/imgkerneliu",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
