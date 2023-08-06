ef3490a800d127c123921a14200f166103ba9710
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:12:05 +0000

    Add package HibiAPI/0.7.2/HibiAPI-0.7.2.tar.gz

---
+++ packages/HibiAPI/HibiAPI-0.7.2.tar.gz/HibiAPI-0.7.2/hibiapi/__init__.py
@@ -0,0 +1,15 @@
+r"""
+  _    _ _ _     _          _____ _____  
+ | |  | (_) |   (_)   /\   |  __ \_   _| 
+ | |__| |_| |__  _   /  \  | |__) || |   
+ |  __  | | '_ \| | / /\ \ |  ___/ | |   
+ | |  | | | |_) | |/ ____ \| |    _| |_  
+ |_|  |_|_|_.__/|_/_/    \_\_|   |_____| 
+
+An alternative implement of Imjad API
+Project: https://github.com/mixmoe/HibiAPI
+"""  # noqa:W291,W293
+
+from importlib.metadata import version
+
+__version__ = version("hibiapi")
