ff8346bc72c8ac7a61bbbe906e70b25dd84c5e93
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:13:15 +0000

    Add package et-micc3/3.0.0/et_micc3-3.0.0-py3-none-any.whl

---
+++ packages/et-micc3/et_micc3-3.0.0-py3-none-any.whl/et_micc3/__init__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+
+"""
+Package et_micc3
+=======================================
+
+Top-level package for et_micc3.
+"""
+
+import et_micc3.cli_micc3
+__version__ = "3.0.0"
+
+
+def hello(who='world'):
+    """'Hello world' method.
+
+    :param str who: whom to say hello to
+    :returns: a string
+    """
+    result = "Hello " + who
+    return result
+
+# Your code here...
