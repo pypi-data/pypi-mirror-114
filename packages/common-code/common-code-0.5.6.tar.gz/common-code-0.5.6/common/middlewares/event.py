f2251cdde847732b467b538e79169506fe27cecb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:29:09 +0000

    Add package common-code/0.5.6/common-code-0.5.6.tar.gz

---
+++ packages/common-code/common-code-0.5.6.tar.gz/common-code-0.5.6/common/middlewares/event.py
@@ -0,0 +1,41 @@
+import json
+from queue import Queue
+from threading import Thread
+
+from django.utils.deprecation import MiddlewareMixin
+
+from common.logging_config import logger
+from middleware.estools import ESClient
+
+
+class EventMiddleware(MiddlewareMixin):
+
+    def start(self, host, port, username=None, password=None, index='event'):
+        self.q = Queue()
+        self.index = index
+        self.es_client = ESClient()
+        self.es = self.es_client.conn(host=host, port=port, username=username, password=password)
+        Thread(target=self.send_event).start()
+
+    def process_response(self, request, response):
+        try:
+            if response.status_code < 300 and request.method != 'GET':
+                data = self.parsing(request, response)
+                if data:
+                    self.q.put(data)
+        except Exception as e:
+            logger.exception(e)
+        return response
+
+    def parsing(self, request, response):
+        data = json.loads(request.body)
+        return data
+
+    def send_event(self):
+        while True:
+            try:
+                body = self.q.get()
+                self.es_client.index(index=self.index, body=body)
+
+            except Exception as e:
+                logger.exception(e)
