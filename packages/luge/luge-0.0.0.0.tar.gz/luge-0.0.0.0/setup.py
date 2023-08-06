a139d6c4153ba5459030703f31acf723874073a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:21:24 +0000

    Add package luge/0.0.0.0/luge-0.0.0.0.tar.gz

---
+++ packages/luge/luge-0.0.0.0.tar.gz/luge-0.0.0.0/setup.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+# encoding: utf-8
+
+
+from setuptools import setup, find_packages
+
+setup(
+    name="luge",
+    version="0.0.0.0",
+    license="MIT Licence",
+
+    url="",
+    author="",
+    author_email="",
+
+    packages=find_packages(),
+    include_package_data=True,
+    platforms="any",
+    install_requires=[]
+)
