ec957e7e5bec65ede3925410ac9332d31eaa82ea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:50:29 +0000

    Add package seleniumbase/1.63.18/seleniumbase-1.63.18.tar.gz

---
+++ packages/seleniumbase/seleniumbase-1.63.18.tar.gz/seleniumbase-1.63.18/examples/test_contains_selector.py
@@ -0,0 +1,9 @@
+from seleniumbase import BaseCase
+
+
+class ContainsSelectorTests(BaseCase):
+    def test_contains_selector(self):
+        self.open("https://xkcd.com/2207/")
+        self.assert_text("Math Work", "#ctitle")
+        self.click('a:contains("Next")')
+        self.assert_text("Drone Fishing", "#ctitle")
