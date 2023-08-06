2119f7d461cbb4d5879a1f6ad404a11b508cbbdf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:23:30 +0000

    Add package sc-backtest/0.0.5/sc_backtest-0.0.5-py3-none-any.whl

---
+++ packages/sc-backtest/sc_backtest-0.0.5-py3-none-any.whl/sc_backtest/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jul 30 13:22:34 2021
+
+@author: chang.sun
+"""
+
+import sc_backtest.technical_analysis as technical_analysis
+from .simpletest import simpletest
+from .backtest import *
