ba2ecd2dac7e90ee74e03761b0496909c083c086
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:40:14 +0000

    Add package mdmparis-defense-finder/0.0.7/mdmparis-defense-finder-0.0.7.tar.gz

---
+++ packages/mdmparis-defense-finder/mdmparis-defense-finder-0.0.7.tar.gz/mdmparis-defense-finder-0.0.7/defense_finder_cli/main.py
@@ -0,0 +1,48 @@
+import click
+import defense_finder
+import defense_finder_updater
+import defense_finder_posttreat
+
+@click.group()
+def cli():
+    """Systematic search of all known anti-phage systems by MDM Labs, Paris.
+
+    Prior to using defense-finder:
+
+    - install hmmsearch: http://hmmer.org/documentation.html
+
+    - get the models (run this every so often to stay up to date):
+
+        $ defense-finder update
+
+    Tool repository: https://github.com/mdmparis/defense-finder.
+    """
+    pass
+
+@cli.command()
+def update():
+    """Fetches the latest defense finder models.
+
+    The models will be downloaded from mdmparis repositories and installed on macsydata.
+
+    This will make them available to macsyfinder and ultimately to defense-finder.
+
+    Models repository: https://github.com/mdmparis/defense-finder-models.
+    """
+    defense_finder_updater.update_models()
+
+@cli.command()
+@click.argument('file')
+@click.option('-o', '--out-dir', 'outdir', help='The target directory where to store the results. Defaults to the current directory.')
+def run(file: str, outdir: str):
+    """Search for known anti-phage defense systems in a protein.
+
+    Point the 'file' argument to the file where the .faa protein sequence is defined.
+
+    Output is written to /tmp/defense-finder/
+    """
+    with open(file) as f:
+        defense_finder.run(f)
+
+    defense_finder_posttreat.run(outdir)
+
