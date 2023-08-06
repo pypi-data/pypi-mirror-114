9f52ddc76be10127919e5cb7f90a795bb0be53bf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:32:00 +0000

    Add package vinodchandrannester/1.3.0/vinodchandrannester-1.3.0.tar.gz

---
+++ packages/vinodchandrannester/vinodchandrannester-1.3.0.tar.gz/vinodchandrannester-1.3.0/setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+
+setup(
+
+       name         = 'vinodchandrannester',
+       version      = '1.3.0',
+       py_modules   = ['vinodchandrannester'],
+       author       = 'Vinod Chandran',
+       author_email = 'vinodchandranav@gmail.com',
+       url          = 'http://www.flyingtux.com',
+       description  = 'A simple printer of nested lists',
+
+       )
