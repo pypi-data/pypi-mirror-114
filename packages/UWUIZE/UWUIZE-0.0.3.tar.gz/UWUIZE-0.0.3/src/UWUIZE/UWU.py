895cd1a86a41e1bc94ba28fc700494444ed814c5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:16:14 +0000

    Add package UWUIZE/0.0.3/UWUIZE-0.0.3.tar.gz

---
+++ packages/UWUIZE/UWUIZE-0.0.3.tar.gz/UWUIZE-0.0.3/src/UWUIZE/UWU.py
@@ -0,0 +1,9 @@
+def uwuize(beans):
+	string_to_mod = beans
+	string_to_mod = string_to_mod.replace("r", "w")
+	string_to_mod = string_to_mod.replace("R", "W")
+	string_to_mod = string_to_mod.replace("l", "w")
+	string_to_mod = string_to_mod.replace("L", "W")
+	return string_to_mod
+
+uwuize("sorry")
