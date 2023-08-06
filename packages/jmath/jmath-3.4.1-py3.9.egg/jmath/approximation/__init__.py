41b5643b40a43de00260d488b77c32c5fee28429
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:20:29 +0000

    Add package jmath/3.4.1/jmath-3.4.1-py3.9.egg

---
+++ packages/jmath/jmath-3.4.1-py3.9.egg/jmath/approximation/__init__.py
@@ -0,0 +1,17 @@
+'''
+    jmath/approximation/__init__.py
+
+    Tools for approximating mathematical equations.
+
+    Optional Packages
+    -----------------
+
+    jmath.approximation.euler_method
+            The euler method for approximating the integrals of differential equations.
+'''
+
+# - Namespace
+
+__path__ = __import__('pkgutil').extend_path(__path__, __name__)
+
+# No Defaults
