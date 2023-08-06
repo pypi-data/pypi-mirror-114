550eaeab40bad731db5d1a1436c5a6cc00a9fe6a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:33:01 +0000

    Add package envo/0.9.16/envo-0.9.16-py3-none-any.whl

---
+++ packages/envo/envo-0.9.16-py3-none-any.whl/envo/emergency_env.py
@@ -0,0 +1,46 @@
+from pathlib import Path
+from typing import Any, Dict, List, Optional, Tuple  # noqa: F401
+
+import envo  # noqa: F401
+from envo import (  # noqa: F401
+    Env,
+    Plugin,
+    VirtualEnv,
+    boot_code,
+    command,
+    const,
+    context,
+    logger,
+    oncreate,
+    ondestroy,
+    onload,
+    onstderr,
+    onstdout,
+    onunload,
+    postcmd,
+    precmd,
+    run,
+)
+
+
+class EmergencyEnv(Env):  # type: ignore
+    class Meta(Env.Meta):  # type: ignore
+        root = Path(__file__).parent.absolute()
+        stage: str = "emergency"
+        emoji: str = const.emojis["emergency"]
+        plugins: List[Plugin] = []
+        name: str = "emergency"
+        version: str = "0.1.0"
+        watch_files: List[str] = []
+        ignore_files: List[str] = []
+
+    class Environ(Env.Environ):
+        ...
+
+    e: Environ
+
+    def init(self) -> None:
+        pass
+
+
+ThisEnv = EmergencyEnv
