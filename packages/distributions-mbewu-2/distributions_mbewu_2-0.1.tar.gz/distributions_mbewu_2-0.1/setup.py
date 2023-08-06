9a3c11806c9678e012778775e35e7861d7247368
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:12:26 +0000

    Add package distributions-mbewu-2/0.1/distributions_mbewu_2-0.1.tar.gz

---
+++ packages/distributions-mbewu-2/distributions_mbewu_2-0.1.tar.gz/distributions_mbewu_2-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='distributions_mbewu_2',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['distributions_mbewu_2'],
+      author='James Mbewu',
+      author_email='james.mbewu@gmail.com',
+      zip_safe=False)
