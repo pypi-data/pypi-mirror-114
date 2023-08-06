4d4a3296c881ffb6d71c6bb0094d6587e7e07f04
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:06:52 +0000

    Add package intting/0.1.4/intting-0.1.4.tar.gz

---
+++ packages/intting/intting-0.1.4.tar.gz/intting-0.1.4/intting/__init__.py
@@ -0,0 +1,4 @@
+print('Thanks for you use the intting')
+print('Email:a1234567890001919@outlook.com')
+import intting.operating
+import intting.transformation
---
+++ packages/intting/intting-0.1.4.tar.gz/intting-0.1.4/intting/operating.py
@@ -0,0 +1,75 @@
+def calculate(num1,num2,operating = 'addition',mode = 'int',roundTo = 0):
+    # operating has 3 parameter:'int'/'float'/'round'
+    # calculate has operating mode,is'addition''add''+'/'subtraction''sub''-'/'multiplication''mul''*'
+    if operating == 'addition' or operating == 'add' or operating == '+':
+        if mode == 'int':
+            return num1 + num2
+        elif mode == 'float':
+            num1 = float(num1)
+            num2 = float(num2)
+            return num1 + num2
+        elif mode == 'round':
+            return round((num1 + num2),int(roundTo))
+        else:
+            raise TypeError('The error mode')
+    elif operating == 'subtraction' or operating == 'sub' or operating == '-':
+        if mode == 'int':
+            return num1 - num2
+        elif mode == 'float':
+            num1 = float(num1)
+            num2 = float(num2)
+            return num1 - num2
+        elif mode == 'round':
+            return round((num1 - num2),int(roundTo))
+        else:
+            raise TypeError('The error mode')
+    elif operating == 'multiplication' or operating == 'mul' or operating == '*':
+        if mode == 'int':
+            return num1 * num2
+        elif mode == 'float':
+            num1 = float(num1)
+            num2 = float(num2)
+            return num1 * num2
+        elif mode == 'round':
+            return round((num1 * num2),int(roundTo))
+        else:
+            raise TypeError('The error mode')
+    elif operating == 'division' or operating == 'div' or operating == '/':
+        if mode == 'int':
+            return num1 / num2
+        elif mode == 'float':
+            num1 = float(num1)
+            num2 = float(num2)
+            return num1 / num2
+        elif mode == 'round':
+            return round((num1 / num2),int(roundTo))
+        else:
+            raise TypeError('The error mode')
+    else:
+        raise TypeError('The error operating mode')
+
+def count(List):
+    string = ''
+    for i in List:
+        string = '{}{}'.format(string,i)
+    return len(string)
+
+def add_at(num1,num2,idx):
+    end_num = str(num1)
+    add_num = str(num2)
+    end_num_list = end_num.split('')
+    end_num_list.insert(idx,add_num)
+    return int(''.join(end_num_list))
+
+def text_int(text):
+    try:
+        int(text)
+    except:
+        return False
+    else:
+        return True
+
+def copyright():
+    print('It is zjy self having')
+
+
