d57d0ba6a44cc8f6e88f5d9a044a81ff502df4ad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:12:10 +0000

    Add package threedigrid-builder/0.3.0/threedigrid_builder-0.3.0-cp37-cp37m-manylinux1_x86_64.whl

---
+++ packages/threedigrid-builder/threedigrid_builder-0.3.0-cp37-cp37m-manylinux1_x86_64.whl/threedigrid_builder/grid/channels.py
@@ -0,0 +1,49 @@
+from threedigrid_builder.base import array_of
+from threedigrid_builder.constants import CalculationType
+from threedigrid_builder.constants import ContentType
+from threedigrid_builder.grid import linear
+from threedigrid_builder.grid.cross_section_locations import interpolate
+
+import pygeos
+
+
+__all__ = ["Channels"]
+
+
+class Channel:
+    id: int
+    code: str
+    the_geom: pygeos.Geometry
+    dist_calc_points: float
+    connection_node_start_id: int
+    connection_node_end_id: int
+    calculation_type: CalculationType
+
+
+@array_of(Channel)
+class Channels(linear.BaseLinear):
+    content_type = ContentType.TYPE_V2_CHANNEL
+
+    def get_1d2d_properties(self, nodes, node_idx, locations):
+        """Compute properties (is_closed, dpumax) of 1D-2D channel flowlines.
+
+        Args:
+            nodes (Nodes): All nodes
+            node_idx (array of int): indices into nodes for which to compute properties
+            locations (CrossSectionLocations): for the bank_levels
+
+        Returns:
+            tuple of:
+            - is_closed (bool): always False
+            - dpumax (array of float): interpolated between CS location bank_levels
+        """
+        # dpumax is interpolated between cross section location bank levels
+        dpumax = interpolate(
+            nodes.cross_loc1[node_idx],
+            nodes.cross_loc2[node_idx],
+            nodes.cross_weight[node_idx],
+            locations,
+            "bank_level",
+        )
+
+        return False, dpumax
