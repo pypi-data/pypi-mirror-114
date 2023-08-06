ea08246131152984fc87a964e51e11b5b0edcadc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:18:30 +0000

    Add package spwc/0.9.0/spwc-0.9.0.tar.gz

---
+++ packages/spwc/spwc-0.9.0.tar.gz/spwc-0.9.0/README.rst
@@ -0,0 +1,5 @@
+================================
+Space Physics WebServices Client
+================================
+
+The package has moved `here <https://pypi.python.org/pypi/speasy>`_
---
+++ packages/spwc/spwc-0.9.0.tar.gz/spwc-0.9.0/spwc/__init__.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+"""Top-level package for Space Physics WebServices Client."""
+
+__author__ = """Alexis Jeandet"""
+__email__ = 'alexis.jeandet@member.fsf.org'
+__version__ = '0.9.0'
+
+raise DeprecationWarning("""
+SPWC is renamed to SPEAZY please have a look here https://github.com/SciQLop/speasy
+Replace all your spwc imports by speasy
+""")
