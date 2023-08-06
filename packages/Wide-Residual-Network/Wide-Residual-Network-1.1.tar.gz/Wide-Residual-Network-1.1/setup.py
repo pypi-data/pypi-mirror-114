32760c950b0989b080e83d55de31e88c7aae2096
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:31:16 +0000

    Add package Wide-Residual-Network/1.1/Wide-Residual-Network-1.1.tar.gz

---
+++ packages/Wide-Residual-Network/Wide-Residual-Network-1.1.tar.gz/Wide-Residual-Network-1.1/setup.py
@@ -0,0 +1,11 @@
+import setuptools
+from setuptools import setup
+
+setup(name='Wide-Residual-Network',
+      version='1.1',
+      description="This package is for creating a wide residual convolutional neural network with keras and tensorflow as backend. This architecture was first introduced in the paper on https://arxiv.org/abs/1605.07146.",
+      author="Patrick Brus",
+      license="MIT",
+      url="https://github.com/patrickbrus/Wide-Residual-Network-Package",
+      packages=setuptools.find_packages(),
+      zip_safe=False)
