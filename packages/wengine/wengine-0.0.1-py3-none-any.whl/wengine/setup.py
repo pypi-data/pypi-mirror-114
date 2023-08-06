75d322159d200db64d4e067bfc731b25f39ea696
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:22:00 +0000

    Add package wengine/0.0.1/wengine-0.0.1-py3-none-any.whl

---
+++ packages/wengine/wengine-0.0.1-py3-none-any.whl/wengine/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='wengine',
+    version='0.0.1',
+    packages=find_packages(),
+    author='PunchyArchy',
+    author_email='ksmdrmvscthny@gmail.com',
+    install_requires=[
+        'gskud-point',
+        'qbs',
+        'sigur_interact',
+        'weightsplitter',
+        'wsqluse',
+        'whikoperator',
+        'qodex_dk',
+        'gc_module_cua',
+        'gravityRecorder'
+    ],
+)
