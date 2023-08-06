461582d436548adcd20f68f79e07482e45b5c790
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:21:37 +0000

    Add package emot/3.1/emot-3.1.tar.gz

---
+++ packages/emot/emot-3.1.tar.gz/emot-3.1/setup.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+"""
+Setup script for emot
+"""
+from setuptools import setup
+
+setup(
+    name='emot',
+    version="3.1",
+    author='Neel Shah',
+    author_email='neelknightme@gmail.com',
+    description="Emoji and Emoticons detection package for Python",
+    keywords=['emoji, emoticons'],
+    include_package_data=True,
+    license="GNU GENERAL PUBLIC LICENSE",
+    packages=['emot'],
+    url="https://github.com/NeelShah18/emo",
+    zip_safe=True,
+    download_url="https://github.com/NeelShah18/emot/archive/refs/tags/v3.0.tar.gz",
+)
