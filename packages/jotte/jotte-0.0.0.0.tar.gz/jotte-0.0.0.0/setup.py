b77af49735ede7cc3dca698f91c2c527640fb0d5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:10:27 +0000

    Add package jotte/0.0.0.0/jotte-0.0.0.0.tar.gz

---
+++ packages/jotte/jotte-0.0.0.0.tar.gz/jotte-0.0.0.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name="jotte",
+    version="0.0.0.0",
+    description="i forgor 💀",
+    url="https://github.com/jottew/jotte",
+    author="jotte",
+    license="MIT")
