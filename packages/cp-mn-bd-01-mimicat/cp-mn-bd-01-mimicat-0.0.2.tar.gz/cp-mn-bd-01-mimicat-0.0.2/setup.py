752c54d541b3d0e2440ebd9b901ff0f687b53442
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:15:33 +0000

    Add package cp-mn-bd-01-mimicat/0.0.2/cp-mn-bd-01-mimicat-0.0.2.tar.gz

---
+++ packages/cp-mn-bd-01-mimicat/cp-mn-bd-01-mimicat-0.0.2.tar.gz/cp-mn-bd-01-mimicat-0.0.2/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='cp-mn-bd-01-mimicat',
+    version='0.0.2',
+    packages=['payload'],
+    package_dir={'': 'src'},
+    url='https://illustria.io',
+    license='MIT',
+    author='Bogdan Kortnov',
+    author_email='bogdan@illustria.io',
+    description='(C) CNSIDE (P) Payload - (M) Malicious (N) Non Harmful - (B) Binary (D) Dormant -  01 - Mimikatz'
+)
---
+++ packages/cp-mn-bd-01-mimicat/cp-mn-bd-01-mimicat-0.0.2.tar.gz/cp-mn-bd-01-mimicat-0.0.2/src/payload/main.py
@@ -0,0 +1,8 @@
+# This is a payload script for illustria CNSIDE System.
+
+def main():
+    print("Hello there from illustria CNSIDE")
+
+
+if __name__ == '__main__':
+    main()
