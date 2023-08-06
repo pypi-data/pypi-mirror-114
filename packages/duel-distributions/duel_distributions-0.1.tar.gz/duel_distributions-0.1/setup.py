4f7efc261de5ca0e42b32a972295ec0c090bae90
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:16:12 +0000

    Add package duel-distributions/0.1/duel_distributions-0.1.tar.gz

---
+++ packages/duel-distributions/duel_distributions-0.1.tar.gz/duel_distributions-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='duel_distributions',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['duel_distributions'],
+      author = 'Chen Zhao',
+      zip_safe=False)
