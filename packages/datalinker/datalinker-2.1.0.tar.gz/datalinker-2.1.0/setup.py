9f0ce0bd14f9c2838264053fe9b8c7daac03549d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:41:58 +0000

    Add package datalinker/2.1.0/datalinker-2.1.0.tar.gz

---
+++ packages/datalinker/datalinker-2.1.0.tar.gz/datalinker-2.1.0/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+		name="datalinker",
+		version="2.1.0",
+		author="Satish Chougule",
+		author_email="satishrajc@gmail.com",
+		description="""Variable sharing mechanism between Python
+					fies/folders and across the project/process""",
+		long_description=long_description,
+		long_description_content_type="text/markdown",
+		url="https://github.com/Satishrajc/datalinker",
+		keywords=['DATA SHARING', 'VARIABLE SHARING', 'SHARING'],
+		py_modules=["datalinker", "test_1", "test_2"],
+		package_dir={"": "src"},
+		install_requires=[],
+		setup_requires=[],
+		classifiers=[
+				"Programming Language :: Python :: 3",
+				"License :: OSI Approved :: MIT License",
+				"Operating System :: OS Independent",
+				],
+		python_requires='>=3.6',
+)
