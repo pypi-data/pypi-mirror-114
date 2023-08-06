d5ca1a4ef1bd50c666c27f79b062602361f536ad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:14:09 +0000

    Add package bysondb/0.1.1/bysondb-0.1.1-py3-none-any.whl

---
+++ packages/bysondb/bysondb-0.1.1-py3-none-any.whl/bysondb/__init__.py
@@ -0,0 +1,17 @@
+"""
+bysonDB
+~~~~~~~~~~~~~~~~~~~
+
+A simple BSON-based database for Python
+
+:license: MIT
+
+"""
+
+__title__ = "bysondb"
+__author__ = "Patrick Brennan (AM2i9)"
+__license__ = "MIT"
+__version__ = "0.1.0"
+
+from .db import BysonDB
+from .document_db import BysonDocumentDB
