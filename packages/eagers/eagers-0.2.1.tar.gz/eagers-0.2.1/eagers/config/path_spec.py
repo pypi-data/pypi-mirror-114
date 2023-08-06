8280fd5a8ae7fe63b540ed4c264422457168e29d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:43:26 +0000

    Add package eagers/0.2.1/eagers-0.2.1.tar.gz

---
+++ packages/eagers/eagers-0.2.1.tar.gz/eagers-0.2.1/eagers/config/path_spec.py
@@ -0,0 +1,52 @@
+"""Path specifications."""
+
+import pathlib
+
+from .user import DATA_CACHE, USER_DIR
+
+
+# Suffixes.
+CSV_SUFFIX = '.csv'
+EPW_SUFFIX = '.epw'
+HDF5_SUFFIX = '.h5'
+IDF_SUFFIX = '.idf'
+EIO_SUFFIX = '.eio'
+
+# File names.
+DATABASE_FILENAME = 'projects'
+DEFAULT_OUTPUT_FILENAME = 'output'
+DISPATCH_RESULT_FILENAME = 'dispatch_result.html'
+
+# Paths.
+TLD_ABSPATH = pathlib.Path(__file__).parents[2]
+EXCEL_INTERFACE_PROJECT_HELP = (
+    TLD_ABSPATH / 'eagers' / 'read' / 'excel_interface_project_help.txt'
+)
+DATA_CACHE_PATH = pathlib.Path(DATA_CACHE).expanduser()
+USER_DIR_PATH = pathlib.Path(USER_DIR).expanduser()
+USER_DIR_DATA_RETRIEVAL = USER_DIR_PATH / 'data_retrieval'
+USER_DIR_EPLUS = USER_DIR_PATH / 'eplus_validation'
+USER_DIR_HYDRO_DATA = USER_DIR_PATH / 'hydro_data'
+USER_DIR_SIMRESULTS = USER_DIR_PATH / 'simulation_results'
+USER_DIR_DATASETS = USER_DIR_PATH / 'data_sets'
+USER_DIR_HDF5_DATASETS = USER_DIR_DATASETS / 'hdf5'
+USER_DIR_HDF5_NODEDATA_DATASETS = USER_DIR_HDF5_DATASETS / 'nodedata'
+USER_DIR_HDF5_WEATHER_DATASETS = USER_DIR_HDF5_DATASETS / 'weather'
+USER_DIR_BUILDINGS = USER_DIR_PATH / 'eplus_buildings'
+USER_DIR_ENERGYPLUS_WEATHER = USER_DIR_PATH / 'eplus_weather'
+USER_DIR_PROJECTS = USER_DIR_PATH / 'projects'
+USER_DIR_TESTDATA = USER_DIR_PATH / 'test_data'
+AUTO_CREATED_DIRS = [
+    DATA_CACHE_PATH,
+    USER_DIR_DATA_RETRIEVAL,
+    USER_DIR_EPLUS,
+    USER_DIR_HYDRO_DATA,
+    USER_DIR_SIMRESULTS,
+    USER_DIR_DATASETS,
+    USER_DIR_HDF5_NODEDATA_DATASETS,
+    USER_DIR_HDF5_WEATHER_DATASETS,
+    USER_DIR_BUILDINGS,
+    USER_DIR_ENERGYPLUS_WEATHER,
+    USER_DIR_PROJECTS,
+    USER_DIR_TESTDATA,
+]
