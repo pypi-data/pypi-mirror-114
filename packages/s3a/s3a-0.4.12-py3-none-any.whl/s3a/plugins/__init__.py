d00c4e0d3a1433509cb6e4718840b22de23e565c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:26:06 +0000

    Add package s3a/0.4.12/s3a-0.4.12-py3-none-any.whl

---
+++ packages/s3a/s3a-0.4.12-py3-none-any.whl/s3a/plugins/__init__.py
@@ -0,0 +1,27 @@
+import pydoc
+from functools import lru_cache
+from s3a.shims import entry_points
+
+def INTERNAL_PLUGINS():
+  from .misc import MainImagePlugin, CompTablePlugin, RandomToolsPlugin, EditPlugin, HelpPlugin, MultiPredictionsPlugin
+  from .usermetrics import UserMetricsPlugin
+  from .file import FilePlugin
+  from .tablefield import VerticesPlugin
+
+  return [FilePlugin, EditPlugin, MultiPredictionsPlugin, VerticesPlugin, MainImagePlugin, CompTablePlugin,
+          RandomToolsPlugin, HelpPlugin, UserMetricsPlugin]
+
+@lru_cache()
+def EXTERNAL_PLUGINS():
+  discoveredPlgs = entry_points().get('s3a.plugins', [])
+  externPlgs = []
+  # noinspection PyTypeChecker
+  for ep in discoveredPlgs:
+    # ALL_PLUGINS is usually exposed here
+    member = pydoc.locate(ep.value)
+    if member:
+      plgs = member()
+      for plg in plgs:
+        if plg not in externPlgs:
+          externPlgs.append(plg)
+  return externPlgs
