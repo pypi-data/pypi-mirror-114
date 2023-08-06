bdb8da0217140481a59b9c36cc2d0bc27b5417d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:08:36 +0000

    Add package MindsDB/2.43.0/MindsDB-2.43.0.tar.gz

---
+++ packages/MindsDB/MindsDB-2.43.0.tar.gz/MindsDB-2.43.0/mindsdb/api/http/namespaces/stream.py
@@ -0,0 +1,101 @@
+import os
+from flask import request
+from flask_restx import Resource, abort
+from flask import current_app as ca
+from mindsdb.utilities.log import log
+from mindsdb.api.http.namespaces.configs.streams import ns_conf
+
+from mindsdb.interfaces.storage.db import session
+from mindsdb.interfaces.storage.db import Stream as StreamDB
+from mindsdb.streams.base.base_stream import StreamTypes
+
+from mindsdb.interfaces.database.integrations import (
+    get_db_integration,
+)
+
+# def get_integration(name):
+#     integrations = ca.config_obj.get('integrations', {})
+#     return integrations.get(name, {})
+
+def get_predictors():
+    # full_predictors_list = [*request.native_interface.get_models(),*request.custom_models.get_models()]
+    full_predictors_list = [*request.native_interface.get_models()]
+    return [x["name"] for x in full_predictors_list
+            if x["status"] == "complete" and x["current_phase"] == 'Trained']
+
+
+def get_streams():
+    streams = session.query(StreamDB).all()
+    return [to_dict(stream) for stream in streams]
+
+
+def to_dict(stream):
+    return {"connect_info": stream.connection_params,
+            "advanced_info": stream.advanced_params,
+            "type": stream._type,
+            "predictor": stream.predictor,
+            "stream_in": stream.stream_in,
+            "stream_out": stream.stream_out,
+            "stream_anomaly": stream.stream_anomaly,
+            "integration": stream.integration,
+            "name": stream.name}
+
+
+@ns_conf.route('/')
+class StreamList(Resource):
+    @ns_conf.doc("get_streams")
+    def get(self):
+        return {'streams': get_streams()}
+
+
+@ns_conf.route('/<name>')
+@ns_conf.param('name', 'Key-value storage stream')
+class Stream(Resource):
+    @ns_conf.doc("get_stream")
+    def get(self, name):
+        streams = get_streams()
+        for stream in streams:
+            if stream["name"] == name:
+                return stream
+        abort(404, f"Can\'t find steam: {name}")
+
+    @ns_conf.doc("put_stream")
+    def put(self, name):
+        params = request.json.get('params')
+        if not isinstance(params, dict):
+            abort(400, "type of 'params' must be dict")
+        for param in ["predictor", "stream_in", "stream_out", "integration_name"]:
+            if param not in params:
+                abort(400, f"'{param}' is missed.")
+        integration_name = params['integration_name']
+        integration_info = get_db_integration(integration_name, request.company_id, False)
+        if not integration_info:
+            abort(400, f"integration '{integration_name}' doesn't exist.")
+        if integration_info["type"] not in ['redis', 'kafka']:
+            abort(400, f"only integration of redis or kafka might be used to crate redis streams. got: '{integration_info.type}' type")
+        connection_params = params.get('connect', {})
+        advanced_params = params.get('advanced', {})
+        predictor = params['predictor']
+        stream_in = params['stream_in']
+        stream_out = params['stream_out']
+        stream_anomaly = params.get('stream_anomaly', stream_out)
+        _type = params.get('type', 'forecast')
+        if predictor not in get_predictors():
+            abort(400, f"requested predictor '{predictor}' is not ready or doens't exist")
+        stream = StreamDB(_type=_type, name=name, connection_params=connection_params, advanced_params=advanced_params,
+                          predictor=predictor, stream_in=stream_in, stream_out=stream_out,
+                          integration=integration_name, company_id=request.company_id, stream_anomaly=stream_anomaly)
+
+        session.add(stream)
+        session.commit()
+        return {"status": "success", "stream_name": name}, 200
+
+    @ns_conf.doc("delete_stream")
+    def delete(self, name):
+        try:
+            session.query(StreamDB).filter_by(company_id=request.company_id, name=name).delete()
+            session.commit()
+        except Exception as e:
+            log.error(e)
+            abort(400, str(e))
+        return '', 200
---
+++ packages/MindsDB/MindsDB-2.43.0.tar.gz/MindsDB-2.43.0/mindsdb/integrations/base/integration.py
@@ -0,0 +1,115 @@
+import os
+from abc import ABC, abstractmethod
+from mindsdb.utilities.config import Config, STOP_THREADS_EVENT
+from mindsdb.interfaces.storage.db import session, Stream
+from mindsdb.utilities.log import log
+from mindsdb.interfaces.database.integrations import get_db_integration
+
+
+class Integration(ABC):
+    def __init__(self, config, name):
+        self.config = config
+        self.name = name
+        self.mindsdb_database = config['api']['mysql']['database']
+
+    @abstractmethod
+    def setup(self):
+        pass
+
+    @abstractmethod
+    def _query(self, query, fetch=False):
+        pass
+
+    @abstractmethod
+    def register_predictors(self, model_data_arr):
+        pass
+
+    @abstractmethod
+    def unregister_predictor(self, name):
+        pass
+
+
+class StreamIntegration(Integration):
+    """Base abstract class for stream integrations like redis and kafka."""
+    def __init__(self, config, name):
+        Integration.__init__(self, config, name)
+        self.streams = {}
+        self.company_id = os.environ.get('MINDSDB_COMPANY_ID', None)
+        self.stop_event = STOP_THREADS_EVENT
+        self.log = log
+
+    def get_stream_from_db(self, db_record):
+        kwargs = {"type": db_record._type,
+                  "name": db_record.name,
+                  "predictor": db_record.predictor,
+                  "input_stream": db_record.stream_in,
+                  "output_stream": db_record.stream_out,
+                  "anomaly_stream": db_record.stream_anomaly}
+        return self.get_stream_from_kwargs(**kwargs)
+
+    def exist_in_db(self):
+        return get_db_integration(self.name, self.company_id) is not None
+
+    def delete_stream(self, predictor):
+        """Deletes stream from database and stops it work by
+        setting up a special threading.Event flag."""
+        stream_name = f"{self.name}_{predictor}"
+        self.log.debug(f"Integration {self.name}: deleting {stream_name}")
+        session.query(Stream).filter_by(company_id=self.company_id, integration=self.name, name=stream_name).delete()
+        session.commit()
+        if stream_name in self.streams:
+            self.streams[stream_name].set()
+            del self.streams[stream_name]
+
+    def delete_all_streams(self):
+        for stream in self.streams.copy():
+            self.streams[stream].set()
+            del self.streams[stream]
+        session.query(Stream).filter_by(company_id=self.company_id, integration=self.name).delete()
+        session.commit()
+
+    def stop_streams(self):
+        for stream in self.streams:
+            self.streams[stream].set()
+
+    def stop_deleted_streams(self):
+        existed_streams = session.query(Stream).filter_by(company_id=self.company_id, integration=self.name)
+        actual_streams = [x.name for x in existed_streams]
+
+        for stream in self.streams.copy():
+            if stream not in actual_streams:
+                # this stream is still running but it has been deleted from database.
+                # need to stop it.
+                self.log.error(f"INTEGRATION {self.name}: deleting {stream} stream.")
+                self.streams[stream].set()
+                del self.streams[stream]
+
+    def setup(self):
+        """Launches streams stored in db and
+        Launches a worker in separate thread which waits
+        data from control stream and creates a particular Streams."""
+
+        self.start_stored_streams()
+        # launch worker which reads control steam and spawn streams
+        self.start()
+
+    @abstractmethod
+    def start_stored_streams(self):
+        pass
+
+    @abstractmethod
+    def work(self):
+        pass
+
+    @abstractmethod
+    def start(self):
+        pass
+
+    def _query(self, query, fetch=False):
+        pass
+
+    def register_predictors(self, model_data_arr):
+        pass
+
+    def unregister_predictor(self, name):
+        pass
