0bbaf56e851bdc7f52dd8d33962b9b57e85d1db7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:30:33 +0000

    Add package NearBeach/0.28.11/NearBeach-0.28.11.tar.gz

---
+++ packages/NearBeach/NearBeach-0.28.11.tar.gz/NearBeach-0.28.11/NearBeach/views/error_views.py
@@ -0,0 +1,60 @@
+from django.http import HttpResponse
+from django.template import loader
+from django.contrib.auth.decorators import login_required
+
+
+@login_required(login_url='login',redirect_field_name="")
+def error_403(request, exception):
+    """
+
+    :param request:
+    :param destination:
+    :param location_id:
+    :return:
+    """
+
+    t = loader.get_template('403.html')
+
+    c = {
+        'nearbeach_title': 'NearBeach Forbidden',
+    }
+
+    return HttpResponse(t.render(c, request))
+
+
+@login_required(login_url='login',redirect_field_name="")
+def error_404(request, exception):
+    """
+
+    :param request:
+    :param destination:
+    :param location_id:
+    :return:
+    """
+
+    t = loader.get_template('404.html')
+
+    c = {
+        'nearbeach_title': 'NearBeach Not Found',
+    }
+
+    return HttpResponse(t.render(c, request))
+
+
+@login_required(login_url='login',redirect_field_name="")
+def error_500(request):
+    """
+
+    :param request:
+    :param destination:
+    :param location_id:
+    :return:
+    """
+
+    t = loader.get_template('500.html')
+
+    c = {
+        'nearbeach_title': 'NearBeach Server Error',
+    }
+
+    return HttpResponse(t.render(c, request))
