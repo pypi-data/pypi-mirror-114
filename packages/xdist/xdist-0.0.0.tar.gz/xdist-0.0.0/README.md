c7fed3027a180eb6bb1d3ebda1690b6d60cde532
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:16:46 +0000

    Add package xdist/0.0.0/xdist-0.0.0.tar.gz

---
+++ packages/xdist/xdist-0.0.0.tar.gz/xdist-0.0.0/README.md
@@ -0,0 +1,7 @@
+# xdist
+
+[![PyPI version](https://badge.fury.io/py/xdist.svg)](https://badge.fury.io/py/xdist)  [![codecov](https://codecov.io/gh/doerlbh/xdist/branch/main/graph/badge.svg?token=)](https://codecov.io/gh/doerlbh/xdist) [![Travis_CI](https://travis-ci.com/doerlbh/xdist.svg?token=&branch=main)](https://travis-ci.com/doerlbh/xdist) [![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+
+Python library
+
+Created by [Baihan Lin](www.baihan.nyc), Columbia University
---
+++ packages/xdist/xdist-0.0.0.tar.gz/xdist-0.0.0/setup.py
@@ -0,0 +1,31 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="xdist",
+    version="0.0.0",
+    author="Baihan Lin",
+    author_email="doerlbh@gmail.com",
+    description="Python library of word2vec-based similiarity embeddings",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/doerlbh/xdist",
+    project_urls={
+        "Bug Tracker": "https://github.com/doerlbh/xdist/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    license="MIT",
+    package_dir={"": "xdist"},
+    packages=setuptools.find_packages(where="xdist"),
+    python_requires=">=3.6",
+    install_requires=["numpy>=1.16.5", "pandas"],
+    test_suite="nose.collector",
+    tests_require=["nose"],
+    include_package_data=True,
+)
