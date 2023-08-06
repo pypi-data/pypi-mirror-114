cec45b2b99e6de976e647030a9f34c62899c558e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:12:21 +0000

    Add package MinecraftaPY/0.0.2/MinecraftaPY-0.0.2-py3-none-any.whl

---
+++ packages/MinecraftaPY/MinecraftaPY-0.0.2-py3-none-any.whl/hypixel/__init__.py
@@ -0,0 +1,10 @@
+"""
+hypixel_py
+
+Simple Python wrapper for the Hypixel API.
+
+:copyright: (c) 2021-present sweting
+:license: MIT, see LICENSE for more details.
+"""
+
+from .hypixel import HypixelConnection
