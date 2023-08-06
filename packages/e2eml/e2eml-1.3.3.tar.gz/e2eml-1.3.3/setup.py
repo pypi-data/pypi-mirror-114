c513d40e4a1cf61d2a3486e49055688598bb0fc3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:12:12 +0000

    Add package e2eml/1.3.3/e2eml-1.3.3.tar.gz

---
+++ packages/e2eml/e2eml-1.3.3.tar.gz/e2eml-1.3.3/setup.py
@@ -0,0 +1,49 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as readme_file:
+    readme = readme_file.read()
+
+requirements = ["ipython>=6",
+                "babel>=2.9.1"
+                "boostaroota>=1.3",
+                "category_encoders==2.2.2",
+                "imblearn>=0.0",
+                "ipython>=7.10.0",
+                "jupyter_core>=4.7.0",
+                "lightgbm==3.1.0",
+                "matplotlib>=3.3.4",
+                "ngboost>=0.3.1",
+                "nltk>=3.2.4",
+                "numpy>=1.19.4",
+                "optuna>=2.5.0",
+                "pandas==1.1.5",
+                "pip>=21.0.1",
+                "plotly>=4.14.3",
+                "psutil==5.8.0",
+                "seaborn>=0.11.1",
+                "scikit-learn==0.22.0",
+                "scipy==1.6.3",
+                "setuptools >= 51.1.0",
+                "shap>=0.39.0",
+                "spacy>=3.0.6",
+                "vowpalwabbit>=8.11.0",
+                "wheel==0.36.2",
+                "xgboost>=1.3.3",
+                ]
+
+setup(
+    name="e2eml",
+    version="1.3.3",
+    author="Thomas Meißner",
+    author_email="meissnercorporation@gmx.de",
+    description="An end to end solution for automl.",
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    url="https://github.com/ThomasMeissnerDS/e2e_ml",
+    packages=find_packages(),
+    install_requires=requirements,
+    classifiers=[
+        "Programming Language :: Python :: 3.8",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+    ],
+)
