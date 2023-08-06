e6e1294a2a6a91a323964b581c0b78951ea7d2d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:12:46 +0000

    Add package ngutils/0.9.8/ngutils-0.9.8-py3-none-any.whl

---
+++ packages/ngutils/ngutils-0.9.8-py3-none-any.whl/ngutils/__init__.py
@@ -0,0 +1,12 @@
+from ngutils.functions import *
+
+__version__ = '0.9.8'
+
+__all__ = [
+    'accel_steps',
+    'phrase_build',
+    'read_urls_contents',
+    'tune_steps',
+    'view_types',
+]
+
