9a4571b1fe3cc053ca5f7cf0560e225caead98b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:41:49 +0000

    Add package AmoghPasswordValidator/0.0.2/AmoghPasswordValidator-0.0.2.tar.gz

---
+++ packages/AmoghPasswordValidator/AmoghPasswordValidator-0.0.2.tar.gz/AmoghPasswordValidator-0.0.2/setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("C:/Users/Lenovo/Documents/container2/README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="AmoghPasswordValidator",
+    version="0.0.2",
+    author="Amogh Jhamb",
+    author_email="sheerin@brainynbright.com",
+    description="Password validator",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Brainy-N-Bright/python/blob/main/Amogh",
+    project_urls={
+        "Bug Tracker": "https://github.com/pypa/sampleproject/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "AmoghPasswordValidator"},
+    packages=setuptools.find_packages(where="AmoghPasswordValidator"),
+    python_requires=">=3.6",
+)
