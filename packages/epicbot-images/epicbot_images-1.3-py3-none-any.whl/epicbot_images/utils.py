254a3ad0a09e9a0c11169f760b0dce6048bb4208
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:17:05 +0000

    Add package epicbot-images/1.3/epicbot_images-1.3-py3-none-any.whl

---
+++ packages/epicbot-images/epicbot_images-1.3-py3-none-any.whl/epicbot_images/utils.py
@@ -0,0 +1,15 @@
+from textwrap import TextWrapper
+
+def wrap_text(width, text) -> str:
+    """
+    Making it so that the text doesn't flow outside the image by adding a bunch of `\n`s.
+    """
+    wrapper = TextWrapper(width=width)
+    t = ""
+    _t_list = wrapper.wrap(text=text)
+    for _t in _t_list:
+        t += f"{_t}\n"
+    return t
+
+# this utilizes your mo-
+# :flint:
