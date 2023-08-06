fa9839ef94719f7ce45f24175a16aa5223413839
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:16:20 +0000

    Add package rule-estimator/0.4.1/rule_estimator-0.4.1.tar.gz

---
+++ packages/rule-estimator/rule_estimator-0.4.1.tar.gz/rule_estimator-0.4.1/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='rule_estimator',
+    version='0.4.1',
+    package_dir={'rule_estimator': 'rule_estimator'},  # the one line where all the magic happens
+    package_data={
+        'rule_estimator': ['data/*'],
+    },
+    packages=find_packages(),
+    description='Scikit-learn compatible business rule estimator',
+    long_description='Scikit-learn compatible business rule estimator, with dashboard included',
+    license='Apache-2',
+    classifiers=["Development Status :: 3 - Alpha"],
+    install_requires=['pandas', 'numpy', 'scikit-learn', 'oyaml', 'python-igraph', 'dash>=1.21', 'dash-bootstrap-components'],
+    python_requires='>=3.6',
+    author='Oege Dijk',
+    author_email='oegedijk@gmail.com',
+    keywords=['machine learning'],
+    url='https://github.com/oegedijk/rule_estimator',
+)
