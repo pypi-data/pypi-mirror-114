9a0c172d04ae8d3ef23d8d68be217ffeec44ab2c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:51:04 +0000

    Add package djangoldp-notification/2.2.11/djangoldp_notification-2.2.11.tar.gz

---
+++ packages/djangoldp-notification/djangoldp_notification-2.2.11.tar.gz/djangoldp_notification-2.2.11/djangoldp_notification/views.py
@@ -0,0 +1,13 @@
+from djangoldp.serializers import LDPSerializer
+from djangoldp.views import LDPViewSet
+from djangoldp.pagination import LDPPagination
+
+
+class LDPNotificationsPagination(LDPPagination):
+    default_limit = 80
+
+
+class LDPNotificationsViewSet(LDPViewSet):
+    '''overridden LDPViewSet to force pagination'''
+    pagination_class = LDPNotificationsPagination
+    depth = 0
