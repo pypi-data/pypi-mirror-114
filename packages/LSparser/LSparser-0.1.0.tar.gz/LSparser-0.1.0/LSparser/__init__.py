04b641c3e0de4300213d707edefaa1dcc0fa9b61
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:13:25 +0000

    Add package LSparser/0.1.0/LSparser-0.1.0.tar.gz

---
+++ packages/LSparser/LSparser-0.1.0.tar.gz/LSparser-0.1.0/LSparser/__init__.py
@@ -0,0 +1,10 @@
+"""
+    Command Parser with Long/Short options/arguments\n
+    使用command.Command类创建指令模板，为模板添加长短选项（Options）、回调函数、别名、帮助信息等\n
+    使用parser.CommandParser类创建对象，根据情况调用tryParse或parse解析指令
+"""
+
+from .__version__ import __version__
+from .command import Command,CommandParser,OPT,ParseResult
+from .event import Events
+#这样就可以 from LSParser import *
