4552337d05b2187a1e7707b56d1e2d2506317bdb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:29:17 +0000

    Add package darbiadev-manageorders/0.2.0/darbiadev-manageorders-0.2.0.tar.gz

---
+++ packages/darbiadev-manageorders/darbiadev-manageorders-0.2.0.tar.gz/darbiadev-manageorders-0.2.0/setup.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['darbiadev_manageorders', 'darbiadev_manageorders.lib']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['requests>=2.26.0,<3.0.0']
+
+extras_require = \
+{'docs': ['sphinx>=4.1.1,<5.0.0',
+          'sphinx-autodoc-annotation>=1.0-1,<2.0',
+          'sphinxcontrib-packages>=1.0.1,<2.0.0',
+          'sphinxcontrib-napoleon>=0.7,<0.8',
+          'sphinxcontrib-apidoc>=0.3.0,<0.4.0',
+          'sphinx-rtd-theme>=0.5.2,<0.6.0',
+          'toml>=0.10.2,<0.11.0'],
+ 'tests': ['pytest>=6.2.4,<7.0.0', 'tox>=3.24.0,<4.0.0']}
+
+setup_kwargs = {
+    'name': 'darbiadev-manageorders',
+    'version': '0.2.0',
+    'description': 'darbiadev-manageorders',
+    'long_description': '# darbiadev-manageorders\n',
+    'author': 'Bradley Reynolds',
+    'author_email': 'bradley.reynolds@darbia.dev',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': 'https://github.com/DarbiaDev/darbiadev-manageorders',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'extras_require': extras_require,
+    'python_requires': '>=3.9,<4.0',
+}
+
+
+setup(**setup_kwargs)
