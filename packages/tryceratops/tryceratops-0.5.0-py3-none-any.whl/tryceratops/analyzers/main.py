c225c42894ad22db73257aab7c32558c88b15efe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:41:57 +0000

    Add package tryceratops/0.5.0/tryceratops-0.5.0-py3-none-any.whl

---
+++ packages/tryceratops/tryceratops-0.5.0-py3-none-any.whl/tryceratops/analyzers/main.py
@@ -0,0 +1,94 @@
+import logging
+from dataclasses import dataclass
+from typing import List, Set, Type
+
+from tryceratops.filters import GlobalFilter
+from tryceratops.types import ParsedFilesType
+from tryceratops.violations import Violation
+
+from . import call as call_analyzers
+from . import exception_block as except_analyzers
+from . import try_block as try_analyzers
+from .base import BaseAnalyzer
+
+logger = logging.getLogger(__name__)
+
+
+ANALYZER_CLASSES = {
+    call_analyzers.CallTooManyAnalyzer,  # type: ignore
+    call_analyzers.CallRaiseVanillaAnalyzer,  # type: ignore
+    call_analyzers.CallRaiseLongArgsAnalyzer,  # type: ignore
+    call_analyzers.CallAvoidCheckingToContinueAnalyzer,  # type: ignore
+    except_analyzers.ExceptReraiseWithoutCauseAnalyzer,
+    except_analyzers.ExceptVerboseReraiseAnalyzer,
+    except_analyzers.ExceptBroadPassAnalyzer,
+    except_analyzers.LogErrorAnalyzer,
+    except_analyzers.LogObjectAnalyzer,
+    try_analyzers.TryConsiderElseAnalyzer,
+    try_analyzers.TryShouldntRaiseAnalyzer,
+}
+
+
+def _get_analyzer_chain(global_filter: GlobalFilter) -> Set[BaseAnalyzer]:
+    analyzers = {
+        analyzercls()
+        for analyzercls in ANALYZER_CLASSES
+        if global_filter.should_run_analyzer(analyzercls)
+    }
+    return analyzers
+
+
+@dataclass
+class RuntimeError:
+    filename: str
+    analyzer: Type[BaseAnalyzer]
+    exception: Exception
+
+
+class Runner:
+    def __init__(self):
+        self.runtime_errors: List[RuntimeError] = []
+        self.violations: List[Violation] = []
+        self.analyzed_files: int = 0
+        self.excluded_files: int = 0
+
+    def _clear(self):
+        self.violations = []
+        self.runtime_errors = []
+        self.excluded_files = 0
+
+    def analyze(self, trees: ParsedFilesType, global_filter: GlobalFilter) -> List[Violation]:
+        analyzers = _get_analyzer_chain(global_filter)
+        self._clear()
+        self.analyzed_files = len(trees)
+
+        for filename, tree, filefilter in trees:
+            if global_filter.should_skip_file(filename):
+                self.analyzed_files -= 1
+                self.excluded_files += 1
+                continue
+
+            for analyzer in analyzers:
+                try:
+                    found_violations = analyzer.check(tree, filename)
+                    valid_violations = [
+                        violation
+                        for violation in found_violations
+                        if not filefilter.ignores_violation(violation)
+                    ]
+                    self.violations += valid_violations
+                except Exception as ex:
+                    logger.exception(
+                        f"Exception raised when running {type(analyzer)} on {filename}"
+                    )
+                    self.runtime_errors.append(RuntimeError(filename, type(analyzer), ex))
+
+        return self.violations
+
+    @property
+    def had_issues(self) -> bool:
+        return len(self.runtime_errors) > 0
+
+    @property
+    def any_violation(self) -> bool:
+        return len(self.violations) > 0
---
+++ packages/tryceratops/tryceratops-0.5.0-py3-none-any.whl/tryceratops/flake_plugin.py
@@ -0,0 +1,58 @@
+import ast
+import importlib.metadata
+from tokenize import TokenInfo
+from typing import Any, Generator, Iterable, List, Tuple, Type
+
+from tryceratops.analyzers.main import Runner
+from tryceratops.files.discovery import load_config
+from tryceratops.files.parser import parse_ignore_tokens
+from tryceratops.filters import FileFilter, GlobalFilter
+from tryceratops.violations.violations import Violation
+
+PACKAGE_NAME = "tryceratops"
+GLOBAL_DUMMY_FILTER = GlobalFilter(False, ignore_violations=[], exclude_dirs=[])
+FLAKE8_VIOLATION_TYPE = Tuple[int, int, str, Type[Any]]
+# line, offset, message, class
+
+
+class TryceratopsAdapterPlugin:
+    name = PACKAGE_NAME
+    version = importlib.metadata.version(PACKAGE_NAME)
+
+    def __init__(
+        self,
+        tree: ast.AST,
+        filename: str = "undefined",
+        file_tokens: Iterable[TokenInfo] = [],
+    ):
+        self._tree = tree
+        self._filename = filename
+        self._runner = Runner()
+
+        ignore_lines = list(parse_ignore_tokens(file_tokens))
+        self._file_filter = FileFilter(ignore_lines)
+        self._global_filter = self._create_global_filter(filename)
+
+    def _create_global_filter(self, filename: str) -> GlobalFilter:
+        pyproj_config = load_config([filename])
+        if pyproj_config:
+            return GlobalFilter.create_from_config(pyproj_config)
+
+        return GLOBAL_DUMMY_FILTER
+
+    def _execute_analyzer(self) -> List[Violation]:
+        tryceratops_input = [
+            (
+                self._filename,
+                self._tree,
+                self._file_filter,
+            )
+        ]
+        return self._runner.analyze(tryceratops_input, self._global_filter)
+
+    def run(self) -> Generator[FLAKE8_VIOLATION_TYPE, None, None]:
+        violations = self._execute_analyzer()
+
+        for violation in violations:
+            msg = f"{violation.code} {violation.description}"
+            yield violation.line, violation.col, msg, type(self)
