daef016a3d2974b6b2d782ae80cd530b72a8d4af
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:15:10 +0000

    Add package binary-refinery/0.4.1/binary-refinery-0.4.1.tar.gz

---
+++ packages/binary-refinery/binary-refinery-0.4.1.tar.gz/binary-refinery-0.4.1/refinery/units/formats/archive/xtace.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+import acefile
+
+from . import ArchiveUnit
+from ....lib.structures import MemoryFile
+
+
+class xtace(ArchiveUnit):
+    """
+    Extract files from an ACE archive.
+    """
+    def unpack(self, data):
+        ace = acefile.open(MemoryFile(data, read_as_bytes=True))
+        for member in ace.getmembers():
+            member: acefile.AceMember
+            comment = {} if not member.comment else {'comment': member.comment}
+            yield self._pack(
+                member.filename,
+                member.datetime,
+                lambda a=ace, m=member: a.read(m, pwd=self.args.pwd),
+                **comment
+            )
