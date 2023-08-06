1370b1354f2cc0fb934345733d4870514464096c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:42:28 +0000

    Add package PySIO/0.1.0/PySIO-0.1.0.tar.gz

---
+++ packages/PySIO/PySIO-0.1.0.tar.gz/PySIO-0.1.0/setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='PySIO',
+      version='0.1.0',
+      description='Parser for SLAC\'s Serial Input Output file format.',
+      author='Karol Krizka',
+      author_email='kkrizka@gmail.com',
+      packages=['sio'])
+
