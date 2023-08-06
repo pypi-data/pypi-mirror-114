70e34ebd2de85cb2d30d97bba2c58369c151dad0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:42:16 +0000

    Add package KeyLabs/1.1/KeyLabs-1.1-py3.9.egg

---
+++ packages/KeyLabs/KeyLabs-1.1-py3.9.egg/keylabs/__init__.py
@@ -0,0 +1,14 @@
+from .client import api
+    
+__all__ = (
+    "KeyLabs", "get_hwid",
+
+    "InvalidApplicationInformation",
+    "ApplicationDoesntExist",
+    "KeyLabsException",
+
+    "InitializeResult",
+    "RegisterResult",
+    "KeyLabsResult",
+    "Subscription",
+)
