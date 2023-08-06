4d37a277529b8ba63ed426ca5457d71318a1de8b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:14:47 +0000

    Add package loghooks/0.1.0/loghooks-0.1.0-py3-none-any.whl

---
+++ packages/loghooks/loghooks-0.1.0-py3-none-any.whl/loghooks/webhooks.py
@@ -0,0 +1,23 @@
+import discord
+import aiohttp
+
+
+def create_adapter(session: aiohttp.ClientSession):
+    if discord.__version__.startswith("1.7"):
+        adapter = discord.AsyncWebhookAdapter(session)
+        return adapter
+    elif discord.__version__.startswith("2"):
+        return session
+    
+
+
+def create_webhook(webhook_url: str, session: aiohttp.ClientSession) -> discord.Webhook:
+    adapter_or_session = create_adapter(session)
+    if discord.__version__.startswith("1.7"):
+        webhook = discord.Webhook.from_url(webhook_url, adapter = adapter_or_session)
+        return webhook
+    elif discord.__version__.startswith("2"):
+        webhook = discord.Webhook.from_url(webhook_url, session = adapter_or_session)
+        return webhook
+
+
