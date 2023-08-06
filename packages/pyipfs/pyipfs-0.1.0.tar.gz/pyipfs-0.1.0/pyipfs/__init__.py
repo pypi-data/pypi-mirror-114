fff894f683d7bd410a3646df9c87bd05bb6172b1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:19:32 +0000

    Add package pyipfs/0.1.0/pyipfs-0.1.0.tar.gz

---
+++ packages/pyipfs/pyipfs-0.1.0.tar.gz/pyipfs-0.1.0/pyipfs/__init__.py
@@ -0,0 +1,7 @@
+"""
+pyipfs init file
+"""
+
+__version__ = "0.1.0"
+__author__ = 'Raaj'
+__credits__ = 'CodingBeast, ...'
---
+++ packages/pyipfs/pyipfs-0.1.0.tar.gz/pyipfs-0.1.0/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+
+setup(
+    name='pyipfs',
+    version='0.1.0',    
+    description='intract ipfs server with python',
+    url='https://github.com/codingbeast/ipfs',
+    author='Raaj',
+    author_email='Raj0kumar00@gmail.com',
+    license='MIT License',
+    packages=['pyipfs'],
+    install_requires=['requests',                   
+                      ],
+
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: MIT License',  
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ],
+)
