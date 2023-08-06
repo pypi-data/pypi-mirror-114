44ce475df1d93e8f973f2d2c5871ce019869aeab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:43:01 +0000

    Add package phoenyx/0.3.3b7/phoenyx-0.3.3b7.tar.gz

---
+++ packages/phoenyx/phoenyx-0.3.3b7.tar.gz/phoenyx-0.3.3b7/setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+
+with open("requirements.txt", "r", encoding="utf-8") as f:
+    requires = f.read().split("\n")
+
+setuptools.setup(
+    name="phoenyx",
+    version="0.3.3b7",
+    author="Thomas Byr",
+    author_email="thomas-c2000@outlook.fr",
+    description="A drawing and physics engine for Pygame",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/ThomasByr/phoenyx.git",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.9",
+    install_requires=requires,
+)
