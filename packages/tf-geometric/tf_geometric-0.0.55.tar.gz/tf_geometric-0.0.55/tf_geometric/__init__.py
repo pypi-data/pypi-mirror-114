248f4c849022319b0f5bde2428d928e17088a3d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:12:59 +0000

    Add package tf-geometric/0.0.55/tf_geometric-0.0.55.tar.gz

---
+++ packages/tf-geometric/tf_geometric-0.0.55.tar.gz/tf_geometric-0.0.55/tf_geometric/__init__.py
@@ -0,0 +1,13 @@
+# coding=utf-8
+import tensorflow as tf
+
+if tf.__version__[0] == "1":
+    tf.enable_eager_execution()
+
+
+from . import nn, utils, data, datasets, layers, sparse
+from tf_geometric.data.graph import Graph, BatchGraph
+from tf_geometric.sparse.sparse_adj import SparseAdj
+
+
+
