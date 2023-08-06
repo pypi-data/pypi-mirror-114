f512ec64d9212b100f6216c459cd1ec72cab6f47
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:24:30 +0000

    Add package lumimqtt/1.0.13/lumimqtt-1.0.13-py3-none-any.whl

---
+++ packages/lumimqtt/lumimqtt-1.0.13-py3-none-any.whl/lumimqtt/device.py
@@ -0,0 +1,21 @@
+"""
+Basic Device class
+"""
+import os
+
+
+class Device:
+    MQTT_VALUES = None
+
+    def __init__(self, name, device_file, topic=None):
+        self.name = name
+        self.device_file = device_file
+        self.topic = topic or name
+
+    def read_raw(self, device_file=None):
+        if not device_file:
+            device_file = self.device_file
+        if os.path.exists(device_file):
+            with open(device_file, 'r') as f:
+                return f.read().strip()
+        return None
