d91a20102a227da7f4ee379d09b829eee1c09c76
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:12:02 +0000

    Add package b2a/2021.7.28.3/b2a-2021.7.28.3-py3-none-any.whl

---
+++ packages/b2a/b2a-2021.7.28.3-py3-none-any.whl/b2a/bdyplat.py
@@ -0,0 +1,111 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+"""
+@File    :  bdyplat.py
+@Date    :  2021/06/17
+@Author  :  Yaronzz
+@Version :  1.0
+@Contact :  yaronhuang@foxmail.com
+@Desc    :
+"""
+import aigpy
+from baidupcs_py.baidupcs import BaiduPCSApi
+from tqdm import *
+
+from b2a.platformImp import *
+
+
+class BdyKey(object):
+    def __init__(self):
+        super().__init__()
+        self.api = None
+        self.cookies = ''
+
+    def login(self, cookiesStr: str) -> bool:
+        try:
+            cookies = dict()
+            array = cookiesStr.split(';')
+            for item in array:
+                values = item.split('=')
+                cookies[values[0].strip(' ')] = values[1]
+            self.api = BaiduPCSApi(bduss=cookies['BDUSS'], cookies=cookies)
+            self.cookies = cookiesStr
+            return True
+        except:
+            return False
+
+
+class BdyPlat(PlatformImp):
+    def __init__(self):
+        super().__init__()
+
+    def list(self, remotePath: str, includeSubDir: bool = False) -> List[FileAttr]:
+        array = []
+        if len(remotePath) <= 0:
+            remotePath = '/'
+        res = self.key.api.list(remotePath)
+        for item in res:
+            obj = FileAttr()
+            obj.isfile = item.is_file
+            obj.name = aigpy.path.getFileName(item.path)
+            obj.path = item.path
+            obj.size = item.size
+            obj.uid = item.fs_id
+            array.append(obj)
+
+            if includeSubDir and item.is_dir:
+                subarr = self.list(item.path, includeSubDir)
+                array.extend(subarr)
+        return array
+
+    def downloadFile(self, remoteFilePath: str, localFilePath: str) -> bool:
+        path = aigpy.path.getDirName(localFilePath)
+        name = aigpy.path.getFileName(localFilePath)
+        check = aigpy.path.mkdirs(path)
+
+        stream = self.key.api.file_stream(remoteFilePath)
+        if not stream:
+            return False
+
+        curSize = 0
+        totalSize = len(stream)
+        with open(localFilePath, 'wb+') as f:
+            with tqdm.wrapattr(stream, "read", desc='下载中:', miniters=1, total=totalSize, ascii=True) as fs:
+                while True:
+                    data = fs.read(2097152)
+                    f.write(data)
+                    curSize += len(data)
+                    if curSize >= totalSize:
+                        break
+
+        stream.close()
+        return True
+
+        # headers = {
+        #     "Cookie ": "; ".join(
+        #         [f"{k}={v if v is not None else ''}" for k, v in self.key.api.cookies.items()]
+        #     ),
+        #     "User-Agent": "netdisk;2.2.51.6;netdisk;10.0.63;PC;android-android",
+        #     "Connection": "Keep-Alive",
+        # }
+
+        # link = self.key.api.download_link(remoteFilePath)
+        # if not link or len(link) <= 0:
+        #     return False
+        # check, message = aigpy.net.downloadFile(link, localFilePath, showProgress=True)
+        # return check
+
+    def uploadFile(self, localFilePath: str, remoteFilePath: str) -> bool:
+        return False
+
+    def downloadLink(self, remoteFilePath: str):
+        link = self.key.api.download_link(remoteFilePath)
+        return link
+
+    def uploadLink(self, localFilePath: str, remoteFilePath: str):
+        return None
+
+    def isFileExist(self, remoteFilePath: str) -> bool:
+        return self.key.api.is_file(remoteFilePath)
+
+
