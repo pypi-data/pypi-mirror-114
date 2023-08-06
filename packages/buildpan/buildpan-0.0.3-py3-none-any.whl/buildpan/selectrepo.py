f160cffb8ae502b3f1d0fc9817712b406dcd33b8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:29:19 +0000

    Add package buildpan/0.0.3/buildpan-0.0.3-py3-none-any.whl

---
+++ packages/buildpan/buildpan-0.0.3-py3-none-any.whl/buildpan/selectrepo.py
@@ -0,0 +1,49 @@
+
+"""
+    Title: selectrepo.py
+    Module Name: allrepo.py
+    Author: Akash Dwivedi
+    Language: Python
+    Date Created: 26-07-2021
+    Date Modified: 29-07-2021
+    Description:
+        ###############################################################
+        ## Selects a specific repository   ## 
+         ###############################################################
+ """
+import git
+import os
+import click
+import git
+import sys
+from github import Github
+
+
+@click.command()
+@click.option('--token', '-e', default="dev", prompt='Enter token ', help='Shows all the repo in the account')
+
+def selectrepo(token):
+    '''Select the repo in the account '''
+    """
+    param : token is the argument which is a aceess token for getting access to the
+    github repository.
+    Variables Used:
+    token : Stores the github access token 
+    repo : stores the reposirory 
+    content: Stores the files avaialable in the files in the repository
+    return: Nil
+    """
+    g=Github(token)
+    print(g)
+    i=1
+    for repo in g.get_user().get_repos():
+     print(i," ",repo.name)
+    i=i+1
+    name=input("Enter the repo name")
+    repo=g.get_repo("AkashAi7/"+name)
+    reponame="AkashAi7/"+name
+    print(reponame)
+    content=repo.get_contents("")
+    for file in content:
+       print("FILE NAME ARE ",file)
+   
