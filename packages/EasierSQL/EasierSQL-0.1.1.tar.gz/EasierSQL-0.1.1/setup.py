5288f1531ed615ebc893a3e72a2a2f0ff9475326
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:32:04 +0000

    Add package EasierSQL/0.1.1/EasierSQL-0.1.1.tar.gz

---
+++ packages/EasierSQL/EasierSQL-0.1.1.tar.gz/EasierSQL-0.1.1/setup.py
@@ -0,0 +1,20 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="EasierSQL",
+    version='0.1.1',
+    description="A wrapper for SQLite, Most of the queries wrapped into commands for ease.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(),
+    url='https://github.com/RefinedDev/EasierSQL',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
