b7a6004f49a6780aa3f5e1efe82f4ccbaca0b00c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:34:26 +0000

    Add package ctlml-commons/0.0.25/ctlml_commons-0.0.25-py3-none-any.whl

---
+++ packages/ctlml-commons/ctlml_commons-0.0.25-py3-none-any.whl/ctlml_commons/entity/config.py
@@ -0,0 +1,25 @@
+from dataclasses import dataclass, field
+from typing import Dict
+
+
+@dataclass(frozen=True)
+class Config:
+    email: str
+    users_key: str
+    delimiter: str
+    testing: bool
+    debug: bool
+    f_it_mode: bool
+    profile_info: bool
+    buy_one_sell_one_mode: bool
+    window_length: int
+    window_secs: int
+    random_investors: int
+    error_sleep_time_sec: int
+    storage_bucket: str
+    total_loss_threshold: float = -2000
+    update_candidate_tickers_in_sec: int = 60 * 3 # 3 minutes
+    max_order_wait_attempts: int = 50
+    dirs: Dict[str, str] = field(default_factory=dict)
+    queues: Dict[str, str] = field(default_factory=dict)
+    urls: Dict[str, str] = field(default_factory=dict)
