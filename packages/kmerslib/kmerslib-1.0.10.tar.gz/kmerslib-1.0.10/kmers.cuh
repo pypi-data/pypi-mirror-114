0f78e2753a519f76a9d7d24c804631d3fcdeb22c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:11:21 +0000

    Add package kmerslib/1.0.10/kmerslib-1.0.10.tar.gz

---
+++ packages/kmerslib/kmerslib-1.0.10.tar.gz/kmerslib-1.0.10/kmers.cuh
@@ -0,0 +1,10 @@
+#ifndef KMERS_H
+#define KMERS_H
+#include <string>
+namespace kmerslib{
+
+    int makeFFPS(std::string prefixDirName,int startk, int endk, int deviceN);
+
+}
+
+#endif //KMERS_H
---
+++ packages/kmerslib/kmerslib-1.0.10.tar.gz/kmerslib-1.0.10/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, Extension
+
+kmerslib_core = Extension('kmerslib.core',
+	sources=['kmerslib/core.cpp'],
+	include_dirs = ['.'],
+	libraries=['kmerslib'])
+
+setup(name = 'kmerslib',
+	author = 'Isaias May Canche', 
+	author_email='isaias.mc@chetumal.tecnm.mx',
+	version = '1.0.10',
+	description="An example project showing how to build a pip-installable Python package that invokes custom CUDA/C++ code.",
+	ext_modules=[kmerslib_core],
+	packages=['kmerslib'],
+	options={"bdist_wheel": {"universal": True}})
