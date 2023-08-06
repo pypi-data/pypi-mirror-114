7d4c7a6ce606a6e837ba34ab2f2be128f6ba93ea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:11:33 +0000

    Add package dsnd-distributions-sk-7/15.3.2/dsnd_distributions_sk_7-15.3.2.tar.gz

---
+++ packages/dsnd-distributions-sk-7/dsnd_distributions_sk_7-15.3.2.tar.gz/dsnd_distributions_sk_7-15.3.2/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='dsnd_distributions_sk_7',
+      version='15.3.2',
+      description='Gaussian and Binomial distributions',
+      packages=['dsnd_distributions_sk_7'],
+      zip_safe=False)
