4f3493cd413ee8e40ec9ae56a2bb944758553b79
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:16:28 +0000

    Add package ImageCreator/0.0.2/ImageCreator-0.0.2.tar.gz

---
+++ packages/ImageCreator/ImageCreator-0.0.2.tar.gz/ImageCreator-0.0.2/ImageCreator/__init__.py
@@ -0,0 +1,22 @@
+import platform
+import os
+
+
+# When the package is a pip package, this can be removed
+import sys
+import pathlib
+abspath = os.path.abspath(sys.argv[0])
+dname = os.path.dirname(abspath)
+sys.path.append(os.path.dirname(dname))
+sys.path.append(str(pathlib.Path(__file__).parent.absolute()))
+#
+
+from ImageCreator.core.use import create, create_on_press, create_on_modified, create_template
+
+
+def create_from_params(params_path, save_path=None, save_name=None):
+    return create(params_path, save_path, save_name)
+
+
+def start():
+    import GUI
