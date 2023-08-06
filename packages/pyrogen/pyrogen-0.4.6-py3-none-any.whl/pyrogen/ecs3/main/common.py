aba2e5a0f6bf87c4f12c91f90c6efd517d4f968a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:18:44 +0000

    Add package pyrogen/0.4.6/pyrogen-0.4.6-py3-none-any.whl

---
+++ packages/pyrogen/pyrogen-0.4.6-py3-none-any.whl/pyrogen/ecs3/main/common.py
@@ -0,0 +1,33 @@
+
+class Buttons():
+
+    ANY    = -1
+    A      =  0
+    B      =  1
+    X      =  2
+    Y      =  3
+    LB     =  4
+    RB     =  5
+    VIEW   =  6
+    MENU   =  7
+    LSTICK =  8
+    RSTICK =  9
+
+    MOUSE_LEFT   = 1
+    MOUSE_CENTER = 2
+    MOUSE_RIGHT  = 4
+
+
+class Gamepads():
+
+    ANY = -1
+
+class Axis():
+
+    ANY = -1
+    LX = "x"
+    LY = "y"
+    RX = "rx"
+    RY = "ry"
+    Z  = "z"
+
