38cf8e81c5a6deaa3804d5dea23656f6859ee6b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:40:14 +0000

    Add package mdmparis-defense-finder/0.0.8/mdmparis-defense-finder-0.0.8.tar.gz

---
+++ packages/mdmparis-defense-finder/mdmparis-defense-finder-0.0.8.tar.gz/mdmparis-defense-finder-0.0.8/defense_finder/__init__.py
@@ -0,0 +1,17 @@
+import subprocess
+
+def run(f):
+    script = """
+DIR="/tmp/defense-finder"
+rm -rf $DIR
+mkdir $DIR
+macsyfinder \
+  --db-type ordered_replicon \
+  --sequence-db {file} \
+  --models defense-finder-models all \
+  --out-dir $DIR \
+  --coverage-profile 0.1 \
+  --w 4
+    """.format(file=f.name)
+
+    subprocess.run(script, shell=True)
