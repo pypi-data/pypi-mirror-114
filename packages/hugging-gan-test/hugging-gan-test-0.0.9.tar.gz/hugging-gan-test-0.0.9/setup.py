55fad1a00072ec6475adfe76ae6fa5613610e05b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:33:49 +0000

    Add package hugging-gan-test/0.0.9/hugging-gan-test-0.0.9.tar.gz

---
+++ packages/hugging-gan-test/hugging-gan-test-0.0.9.tar.gz/hugging-gan-test-0.0.9/setup.py
@@ -0,0 +1,30 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="hugging-gan-test",
+    version="0.0.9",
+    author="Javi and Vicc",
+    author_email="vipermu97@gmail.com",
+    description="Testing pip",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/javismiles/HuggingGAN",
+    project_urls={
+        "Docs": "https://github.com/javismiles/HuggingGAN/docs",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    # package_dir={
+    #     "": ".",
+    #     "taming": "./bigotis/models/taming/modeling_taming",
+    # },
+    packages=setuptools.find_packages(),
+    python_requires=">=3.6",
+    install_requires = []
+)
