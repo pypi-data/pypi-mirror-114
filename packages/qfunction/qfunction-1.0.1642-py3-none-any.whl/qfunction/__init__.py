38cbcf141c2843ca162ee6ed7a43383340ec3356
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:07:36 +0000

    Add package qfunction/1.0.1642/qfunction-1.0.1642-py3-none-any.whl

---
+++ packages/qfunction/qfunction-1.0.1642-py3-none-any.whl/qfunction/__init__.py
@@ -0,0 +1,2 @@
+from qfunction.fundamentals import *
+#from qfunction.quantum.quantum_circuit import QuantumCircuit
---
+++ packages/qfunction/qfunction-1.0.1642-py3-none-any.whl/qfunction/base/__init__.py
@@ -0,0 +1,5 @@
+from qfunction.base.trigonometry import q_cos,q_sin
+from qfunction.base.base import q_exp,radian,limit
+#from qfunction.base import trigonometry
+
+all = [q_cos,q_sin,q_exp,radian,limit]
