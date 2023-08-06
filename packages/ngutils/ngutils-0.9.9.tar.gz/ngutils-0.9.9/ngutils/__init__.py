ee0bc4a56aa44797bbb9d9930f2183f736fba47c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:40:12 +0000

    Add package ngutils/0.9.9/ngutils-0.9.9.tar.gz

---
+++ packages/ngutils/ngutils-0.9.9.tar.gz/ngutils-0.9.9/ngutils/__init__.py
@@ -0,0 +1,12 @@
+from ngutils.functions import *
+
+__version__ = '0.9.9'
+
+__all__ = [
+    'accel_steps',
+    'build_phrase',
+    'read_urls_contents',
+    'tune_steps',
+    'view_types',
+]
+
