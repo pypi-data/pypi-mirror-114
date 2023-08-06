af0ad004245615fae3f00d62237bf8c842c8cdf3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:12:00 +0000

    Add package pyorit/1.0/pyorit-1.0.tar.gz

---
+++ packages/pyorit/pyorit-1.0.tar.gz/pyorit-1.0/README.md
@@ -0,0 +1,11 @@
+![Logo](https://user-images.githubusercontent.com/57337800/127297823-7f5d3ac8-e9bd-406d-987b-f99b5293a403.jpg)
+
+# pyorit Library
+OrangIT Base Library
+
+[![PyPI - Version](https://img.shields.io/pypi/v/pyorit?style=for-the-badge)](https://pypi.org/project/pyorit)
+[![PyPI - Downloads](https://img.shields.io/pypi/dm/pyorit?label=DOWNLOADS&style=for-the-badge)](https://pypi.org/project/pyorit)
+
+
+# Credits
+* [Lonami](https://github.com/LonamiWebs/) for [Telethon](https://github.com/LonamiWebs/Telethon)
---
+++ packages/pyorit/pyorit-1.0.tar.gz/pyorit-1.0/pyorit/__init__.py
@@ -0,0 +1,36 @@
+# OrangIT - UserBot
+# Copyright (C) 2021 OrangIT
+#
+# This file is a part of < https://github.com/Projectgeez/OrangIT/ >
+# PLease read the GNU Affero General Public License in
+# <https://www.github.com/OrangIT/OrangIT/blob/main/LICENSE/>.
+
+from .connections import *
+
+LOGS = LOGS
+
+udB = redis_connection()
+
+OrangIT_bot, asst = client_connection()
+
+vcasst, vcClient, CallsClient = vc_connection(udB)
+
+if not udB.get("HNDLR"):
+    udB.set("HNDLR", ".")
+
+if not udB.get("SUDO"):
+    udB.set("SUDO", "False")
+
+if not udB.get("SUDOS"):
+    udB.set("SUDOS", "777000")
+
+if not udB.get("BLACKLIST_CHATS"):
+    udB.set("BLACKLIST_CHATS", "[]")
+
+if not udB.get("DUAL_HNDLR"):
+    udB.set("DUAL_HNDLR", "/")
+
+HNDLR = udB.get("HNDLR")
+
+Evar = udB.get("SUDO_HNDLR")
+SUDOHNDLR = Evar if Evar else HNDLR
