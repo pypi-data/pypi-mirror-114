eda2fe3c680903b4bb46e82834ddb84b77cac161
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:43:15 +0000

    Add package helpful-django/0.8.8/helpful_django-0.8.8-py3-none-any.whl

---
+++ packages/helpful-django/helpful_django-0.8.8-py3-none-any.whl/helpful_django/__init__.py
@@ -0,0 +1,6 @@
+"""
+The Helpful Django Module is a little collection of some extensions
+and prebuilt apps for the Django Framework. Some of these extensions
+are unnecessary and some others are very important or helpful (also
+mentioned in the name of this module).
+"""
