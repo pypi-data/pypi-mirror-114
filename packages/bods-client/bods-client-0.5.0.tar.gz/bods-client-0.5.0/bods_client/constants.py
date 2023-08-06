6f94a1330e907beb68afa3f960344f1b660e103b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:52:41 +0000

    Add package bods-client/0.5.0/bods-client-0.5.0.tar.gz

---
+++ packages/bods-client/bods-client-0.5.0.tar.gz/bods-client-0.5.0/bods_client/constants.py
@@ -0,0 +1,18 @@
+BODS_API_URL = "https://data.bus-data.dft.gov.uk/api"
+
+V1 = BODS_API_URL + "/v1"
+
+V1_TIMETABLES_URL = V1 + "/dataset"
+V1_FARES_URL = V1 + "/fares/dataset"
+V1_GTFS_RT_URL = V1 + "/gtfsrtdatafeed"
+V1_SIRI_VM_URL = V1 + "/datafeed"
+
+TIMETABLES_PATH = "dataset"
+FARES_PATH = "fares/dataset"
+GTFS_RT_PATH = "gtfsrtdatafeed"
+SIRI_VM_PATH = "datafeed"
+
+DATASET_STATUSES = ["published", "error", "expired", "inactive"]
+
+OK_200 = 200
+NOT_FOUND_404 = 404
