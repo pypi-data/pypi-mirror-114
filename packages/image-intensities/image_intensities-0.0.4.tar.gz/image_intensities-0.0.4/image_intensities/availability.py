6035ac2ccf31ff43782acea49367185b32366bd2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:40:46 +0000

    Add package image-intensities/0.0.4/image_intensities-0.0.4.tar.gz

---
+++ packages/image-intensities/image_intensities-0.0.4.tar.gz/image_intensities-0.0.4/image_intensities/availability.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+from luckydonaldUtils.logger import logging
+
+__author__ = 'luckydonald'
+
+logger = logging.getLogger(__name__)
+if __name__ == '__main__':
+    logging.add_colored_handler(level=logging.DEBUG)
+# end if
+
+AVAILABLE_BACKENDS = {
+    "pure_python": True,
+    "compiled_cython": None,
+    "compiled_cffi": None,
+    "native_cffi": None,
+    "native_ctypes": None,
+}
+
+try:
+    from image_intensities.pure_python import rgb_sums, sums_to_luma, rgb_luma_from_filename
+    AVAILABLE_BACKENDS["pure_python"] = True
+except Exception as e:
+    AVAILABLE_BACKENDS["pure_python"] = False
+    print(f'pure_python failed: {e!s}')
+# end try
+
+try:
+    from image_intensities.compiled_cython import rgb_sums, sums_to_luma, rgb_luma_from_filename
+    AVAILABLE_BACKENDS["compiled_cython"] = True
+except Exception as e:
+    AVAILABLE_BACKENDS["compiled_cython"] = False
+    print(f'compiled_cython failed: {e!s}')
+# end try
+
+try:
+    from image_intensities.compiled_cffi import jpeg_intensities, png_intensities, rgb_luma_from_filename
+    AVAILABLE_BACKENDS["compiled_cffi"] = True
+except Exception as e:
+    AVAILABLE_BACKENDS["compiled_cffi"] = False
+    print(f'compiled_cffi failed: {e!s}')
+# end try
+
+try:
+    from image_intensities.native_cffi import jpeg_intensities, png_intensities, rgb_luma_from_filename
+    AVAILABLE_BACKENDS["native_cffi"] = True
+except Exception as e:
+    AVAILABLE_BACKENDS["native_cffi"] = False
+    print(f'native_cffi failed: {e!s}')
+# end try
+
+try:
+    from image_intensities.native_ctypes import jpeg_intensities, png_intensities, rgb_luma_from_filename
+    AVAILABLE_BACKENDS["native_ctypes"] = True
+except Exception as e:
+    AVAILABLE_BACKENDS["native_ctypes"] = False
+    print(f'native_ctypes failed: {e!s}')
+# end try
+
---
+++ packages/image-intensities/image_intensities-0.0.4.tar.gz/image_intensities-0.0.4/image_intensities/compiled_cffi.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+from cffi import FFI
+from luckydonaldUtils.logger import logging
+from luckydonaldUtils.encoding import to_binary as b
+
+__author__ = 'luckydonald'
+
+logger = logging.getLogger(__name__)
+if __name__ == '__main__':
+    logging.add_colored_handler(level=logging.DEBUG)
+# end if
+
+from pathlib import Path
+from .pure_python import Luma
+from .shared_cffi import lib_jpeg_intensities, lib_rgb_luma_from_filename
+from .shared_cffi import lib_png_intensities
+
+so_files = Path(__file__).joinpath('_native_code').absolute().glob("**/*image_intensities*.so")
+try:
+    so_file = str(next(so_files))
+except:
+    logger.warning('Loading optimized library via CFFI failed, file not found.')
+    raise ImportError('File not found.')
+# end if
+ffi = FFI()
+ffi.cdef("""
+    extern struct intensity_data {
+        double nw;
+        double ne;
+        double sw;
+        double se;
+        int error;
+    } intensity_data;
+
+    struct intensity_data jpeg_intensities(const char *file_name);
+    struct intensity_data png_intensities(const char *file_name);
+""")
+try:
+    lib = ffi.dlopen(so_file)
+except OSError as e:
+    logger.warning('Loading optimized library via CFFI failed:', exc_info=True)
+    raise ImportError('OSError: ' + str(e))
+# end if
+
+
+def jpeg_intensities(filename) -> Luma:
+    return lib_jpeg_intensities(b(filename), lib)
+# end def
+
+
+def png_intensities(filename) -> Luma:
+    return lib_png_intensities(b(filename), lib)
+# end def
+
+
+def rgb_luma_from_filename(filename) -> Luma:
+    return lib_rgb_luma_from_filename(filename, b(filename), lib)
+# end def
