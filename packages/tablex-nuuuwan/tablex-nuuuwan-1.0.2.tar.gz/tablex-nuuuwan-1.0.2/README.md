f4d790b4ea22cabfe288fa6eed23f7dfa5b3b524
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:31:07 +0000

    Add package tablex-nuuuwan/1.0.2/tablex-nuuuwan-1.0.2.tar.gz

---
+++ packages/tablex-nuuuwan/tablex-nuuuwan-1.0.2.tar.gz/tablex-nuuuwan-1.0.2/README.md
@@ -0,0 +1,27 @@
+# tablex
+
+## Setup and Install
+
+```
+pip install tablex-nuuuwan
+```
+
+*The source code for this library can be found at https://github.com/nuuuwan/tablex.*
+
+
+## Release History
+
+### 1.0.0
+* [2021-07-30 08:41AM] Basic Tabula to Matrix
+* [2021-07-30 10:58AM] Add camelot unittest
+* [2021-07-30 11:05AM] Remove upload data and tweet workflows
+* [2021-07-30 11:10AM] Add install GhostScript to Workflows Take
+* [2021-07-30 11:25AM] Make test_camelot non-trivial
+* [2021-07-30 11:46AM] Add more example files
+* [2021-07-30 12:20PM] Improve Parse Dates
+* [2021-07-30 03:04PM] Cleanup Old Date Regex
+
+### 1.0.1
+* RELEASE ONLY
+## 1.0.2 (PRE-RELEASE)
+### 1.0.2 (PRE-RELEASE)
---
+++ packages/tablex-nuuuwan/tablex-nuuuwan-1.0.2.tar.gz/tablex-nuuuwan-1.0.2/setup.py
@@ -0,0 +1,37 @@
+'''Setup.'''
+
+import setuptools
+
+DIST_NAME = 'tablex'
+version = '1.0.2'
+
+setuptools.setup(
+    name='%s-nuuuwan' % DIST_NAME,
+    version=version,
+    author='Nuwan I. Senaratna',
+    author_email='nuuuwan@gmail.com',
+    description='',
+    long_description='',
+    long_description_content_type='text/markdown',
+    url='https://github.com/nuuuwan/%s' % DIST_NAME,
+    project_urls={
+        'Bug Tracker': 'https://github.com/nuuuwan/%s/issues' % DIST_NAME,
+    },
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    package_dir={'': 'src'},
+    packages=setuptools.find_packages(where='src'),
+    python_requires='>=3.6',
+    install_requires=[
+        'bs4',
+        'selenium',
+        'tweepy',
+        'utils-nuuuwan',
+        'pytest',
+        'tabula-py',
+        'camelot-py[cv]',
+    ],
+)
