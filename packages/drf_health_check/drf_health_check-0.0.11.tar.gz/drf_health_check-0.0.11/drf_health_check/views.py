ecc10c7e2bf002429e0c31b93e84d7bbbe55434f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:09:32 +0000

    Add package drf_health_check/0.0.11/drf_health_check-0.0.11.tar.gz

---
+++ packages/drf_health_check/drf_health_check-0.0.11.tar.gz/drf_health_check-0.0.11/drf_health_check/views.py
@@ -0,0 +1,17 @@
+from typing import Any
+
+from django.http import HttpRequest, JsonResponse
+from drf_spectacular.utils import extend_schema
+from rest_framework.permissions import AllowAny
+from rest_framework.views import APIView
+
+from .health_status import HealthChecker
+from .serializers import AnnotateHealthSerializer
+
+
+class HealthView(APIView):
+    permission_classes = [AllowAny]
+
+    @extend_schema(request=None, responses=AnnotateHealthSerializer())
+    def get(self, request: HttpRequest, *args: Any, **kwargs: Any) -> JsonResponse:
+        return JsonResponse({"status": HealthChecker.get_health_status()})
