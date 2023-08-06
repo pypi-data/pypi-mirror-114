33f778b2cc07720d328d27dccd8a4269ad029b5e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:17:01 +0000

    Add package pyMILES/0.0.3/pyMILES-0.0.3.tar.gz

---
+++ packages/pyMILES/pyMILES-0.0.3.tar.gz/pyMILES-0.0.3/src/pyMILES/embedding_test.py
@@ -0,0 +1,50 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jun 19 20:47:14 2021
+
+@author: vorst
+"""
+
+# Python imports
+import unittest
+
+# Third party imports
+import numpy as np
+
+# Local imports
+from embedding import embed_bag, embed_all_bags 
+
+# Globals
+
+
+#%% 
+
+# TODO Write tests
+
+class EmbeddingTest(unittest.TestCase):
+    
+    def setUp(self):
+        return None
+    
+    def test_(self):
+        return None
+    def test_(self):
+        return None
+    def test_(self):
+        return None
+    def test_(self):
+        return None
+    def test_(self):
+        return None
+    def test_(self):
+        return None
+    def test_(self):
+        return None
+    def test_(self):
+        return None
+    def test_(self):
+        return None
+    def test_(self):
+        return None
+    def test_(self):
+        return None
