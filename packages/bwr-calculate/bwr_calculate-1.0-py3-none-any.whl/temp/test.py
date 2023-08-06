b7d19dc5c6205deed6418fa37efe28762905ee18
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:19:29 +0000

    Add package bwr-calculate/1.0/bwr_calculate-1.0-py3-none-any.whl

---
+++ packages/bwr-calculate/bwr_calculate-1.0-py3-none-any.whl/temp/test.py
@@ -0,0 +1,14 @@
+
+def func_test():
+    print("Successfully Imported test.py file")
+
+def print_name(name):
+    print(f'Hello {name}')
+
+def print_test_age(age):
+    print_age(age)
+
+def add(a, b):
+    "This program adds two numbers and return the result"
+    result = a + b
+    return result
