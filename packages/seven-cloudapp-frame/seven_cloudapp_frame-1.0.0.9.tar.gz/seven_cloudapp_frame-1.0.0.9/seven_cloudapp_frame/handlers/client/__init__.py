ea4704ce102b86a97d7841856c107c128d79ed03
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:09:56 +0000

    Add package seven-cloudapp-frame/1.0.0.9/seven_cloudapp_frame-1.0.0.9.tar.gz

---
+++ packages/seven-cloudapp-frame/seven_cloudapp_frame-1.0.0.9.tar.gz/seven_cloudapp_frame-1.0.0.9/seven_cloudapp_frame/handlers/client/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+:Author: HuangJianYi
+:Date: 2021-07-15 11:59:42
+@LastEditTime: 2021-07-28 09:21:22
+@LastEditors: HuangJianYi
+:description: 
+"""
+__all__ = ["address", "user", "theme"]
