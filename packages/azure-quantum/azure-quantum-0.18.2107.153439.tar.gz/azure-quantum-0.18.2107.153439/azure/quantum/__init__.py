74924e7cf4a2446b29462d0863481f567109f4c0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:29:36 +0000

    Add package azure-quantum/0.18.2107.153439/azure-quantum-0.18.2107.153439.tar.gz

---
+++ packages/azure-quantum/azure-quantum-0.18.2107.153439.tar.gz/azure-quantum-0.18.2107.153439/azure/quantum/__init__.py
@@ -0,0 +1,12 @@
+##
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License.
+##
+import logging
+from .version import __version__
+
+from .job import *
+from .workspace import *
+
+logger = logging.getLogger(__name__)
+logger.info(f"version: {__version__}")
