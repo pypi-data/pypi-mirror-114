5f6047b7e802bb6ee7a3ecfb48fda0e1e0ae3dba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:42:57 +0000

    Add package pypi-Deploy-Demo/0.0.4/pypi-Deploy-Demo-0.0.4.tar.gz

---
+++ packages/pypi-Deploy-Demo/pypi-Deploy-Demo-0.0.4.tar.gz/pypi-Deploy-Demo-0.0.4/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+import pathlib
+
+HERE = pathlib.Path(__file__).parent
+README = (HERE / "README.md").read_text()
+
+setup(
+    name='pypi-Deploy-Demo',
+    description='pypi-Deploy-Demo',
+    long_description=README,
+    long_description_content_type="text/markdown",
+    packages=find_packages(),
+    version='0.0.4',
+    license='MIT',
+    author='Kushagra Bainsla',
+    author_email='kushagrabainsla@gmail.com',
+    url='https://github.com/Kushagrabainsla/build-flask-app',
+    install_requires=['PyInquirer'],
+    entry_points = {
+        'console_scripts': ['pypi-Deploy-Demo=pypi_Deploy_Demo.main:main'],
+    },
+)
