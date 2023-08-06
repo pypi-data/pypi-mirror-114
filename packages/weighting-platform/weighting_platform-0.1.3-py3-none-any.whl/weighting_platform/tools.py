bd60d1c63a3e4950c48cdfd2bee697dd158a4c8c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:23:44 +0000

    Add package weighting-platform/0.1.3/weighting_platform-0.1.3-py3-none-any.whl

---
+++ packages/weighting-platform/weighting_platform-0.1.3-py3-none-any.whl/weighting_platform/tools.py
@@ -0,0 +1,17 @@
+from traceback import format_exc
+from weighting_platform.functions import logging_functions
+
+
+def round_decorator(round_operator):
+    """ Декоратор, оборачивающий оператора раунда взвешивания """
+    def wrapper(*args, **kwargs):
+        try:
+            if args[0].status_ready:
+                round_operator(*args, **kwargs)
+            else:
+                return {"status": False, 'info': 'Раунд в процессе'}
+        except:
+            logging_functions.fix_round_fail(args[0].gdw, format_exc())
+            pass
+    return wrapper
+
