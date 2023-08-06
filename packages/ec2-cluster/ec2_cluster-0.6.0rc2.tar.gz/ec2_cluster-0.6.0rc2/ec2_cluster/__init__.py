c5043c9d5fb0cc3c58944740c8df421e769f532f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:41:51 +0000

    Add package ec2-cluster/0.6.0rc2/ec2_cluster-0.6.0rc2.tar.gz

---
+++ packages/ec2-cluster/ec2_cluster-0.6.0rc2.tar.gz/ec2_cluster-0.6.0rc2/ec2_cluster/__init__.py
@@ -0,0 +1,10 @@
+name = "ec2-cluster"
+
+from pathlib import Path
+version_file = Path(__file__).parent/"VERSION"
+__version__ = version_file.open('r').read().strip()
+
+from ec2_cluster.config.config import ClusterConfig, ClusterConfigValidationError
+from ec2_cluster.instances.cluster import Cluster
+from ec2_cluster.shells.control import ClusterShell
+
