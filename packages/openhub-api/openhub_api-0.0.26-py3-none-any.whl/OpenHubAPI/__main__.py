2ba02c3e9f94ec521ed2467b02b461d9b9d862a8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:15:30 +0000

    Add package openhub-api/0.0.26/openhub_api-0.0.26-py3-none-any.whl

---
+++ packages/openhub-api/openhub_api-0.0.26-py3-none-any.whl/OpenHubAPI/__main__.py
@@ -0,0 +1,22 @@
+import socket
+
+from .manage.manage import main
+
+
+def get_my_ip():
+    """
+    Find my IP address
+    :return:
+    """
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.connect(("8.8.8.8", 80))
+    ip = s.getsockname()[0]
+    s.close()
+    return ip
+
+
+ip = str(get_my_ip())
+
+# main('makemigrations data')
+# main('migrate data')
+main('runserver ' + ip + ':8000')
