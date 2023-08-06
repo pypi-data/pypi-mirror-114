ccb78b53c08ea1deac5e8efdffd24f55067eb3e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:41:49 +0000

    Add package XAUTH/1.1.15/XAUTH-1.1.15.tar.gz

---
+++ packages/XAUTH/XAUTH-1.1.15.tar.gz/XAUTH-1.1.15/xauth/__init__.py
@@ -0,0 +1,14 @@
+from .client import api
+    
+__all__ = (
+    "KeyAuth", "get_hwid",
+
+    "InvalidApplicationInformation",
+    "ApplicationDoesntExist",
+    "KeyAuthException",
+
+    "InitializeResult",
+    "RegisterResult",
+    "KeyAuthResult",
+    "Subscription",
+)
