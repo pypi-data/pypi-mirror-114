4332e06b9839bf306c0fae4f7b4ff8d2e3e2bf73
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:19:02 +0000

    Add package denseweight/0.1.1/denseweight-0.1.1.tar.gz

---
+++ packages/denseweight/denseweight-0.1.1.tar.gz/denseweight-0.1.1/setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="denseweight",
+    version="0.1.1",
+    author="Michael Steininger",
+    author_email="steininger@informatik.uni-wuerzburg.de",
+    description="The imbalanced regression method DenseWeight",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/steimi/denseweight",
+    project_urls={
+        "Bug Tracker": "https://github.com/steimi/denseweight/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    packages=setuptools.find_packages(where="denseweight"),
+    install_requires=["numpy>=1.20.0", "KDEpy", "scikit-learn"],
+    python_requires=">=3.6",
+)
