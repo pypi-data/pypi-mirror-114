10a11f954e564943bff3ad5237a67dc5ce3e9b1b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:50:07 +0000

    Add package pybacting/0.1.1/pybacting-0.1.1.tar.gz

---
+++ packages/pybacting/pybacting-0.1.1.tar.gz/pybacting-0.1.1/src/pybacting/api.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+"""Java components via :mod:`scyjava`."""
+
+from scyjava import config, jimport
+
+__all__ = [
+    "cdk",
+]
+
+ENDPOINT = "io.github.egonw.bacting:managers-cdk:0.0.20"
+CDKMANAGER_PATH = "net.bioclipse.managers.CDKManager"
+WORKSPACE_ROOT = "."
+
+config.add_endpoints(ENDPOINT)
+cdk_class = jimport(CDKMANAGER_PATH)
+cdk = cdk_class(WORKSPACE_ROOT)
