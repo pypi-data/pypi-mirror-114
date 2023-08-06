25a671f6c383a593291d006878c41199a8172528
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:25:32 +0000

    Add package i-mongodb/1.0.0a3/i_mongodb-1.0.0a3.tar.gz

---
+++ packages/i-mongodb/i_mongodb-1.0.0a3.tar.gz/i_mongodb-1.0.0a3/i_mongodb/__init__.py
@@ -0,0 +1,9 @@
+"""A set of functions to retrieve and save data into MongoDB.
+"""
+
+# import package modules
+from i_mongodb.config import Config
+from i_mongodb.i_mongodb import MongoDBInterface
+
+
+__version__ = '1.0.0-alpha.3'
