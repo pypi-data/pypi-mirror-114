3835b2551ebb7e5a623038cc16c60d5230e1dc37
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:09:04 +0000

    Add package round-to-n-significant-digits/0.1.5/round_to_n_significant_digits-0.1.5.tar.gz

---
+++ packages/round-to-n-significant-digits/round_to_n_significant_digits-0.1.5.tar.gz/round_to_n_significant_digits-0.1.5/README.rst
@@ -0,0 +1,62 @@
+==============================
+round_to_n_significant_digits
+==============================
+
+.. image:: https://img.shields.io/github/last-commit/stas-prokopiev/round_to_n_significant_digits
+   :target: https://img.shields.io/github/last-commit/stas-prokopiev/round_to_n_significant_digits
+   :alt: GitHub last commit
+
+.. image:: https://img.shields.io/github/license/stas-prokopiev/round_to_n_significant_digits
+    :target: https://github.com/stas-prokopiev/round_to_n_significant_digits/blob/master/LICENSE.txt
+    :alt: GitHub license<space><space>
+
+.. image:: https://img.shields.io/pypi/v/round_to_n_significant_digits
+   :target: https://img.shields.io/pypi/v/round_to_n_significant_digits
+   :alt: PyPI
+
+.. image:: https://img.shields.io/pypi/pyversions/round_to_n_significant_digits
+   :target: https://img.shields.io/pypi/pyversions/round_to_n_significant_digits
+   :alt: PyPI - Python Version
+
+
+.. contents:: **Table of Contents**
+
+Overview.
+=========================
+round_to_n_significant_digits is a one function PYPI package made only with one
+goal to round float numbers in python to asked number of signifacant digits
+
+
+.. code-block:: python
+
+    import round_to_n_significant_digits
+
+    round_to_n_significant_digits.rtnsd(0.23234, 2)  # 0.23
+    round_to_n_significant_digits.rtnsd(0.235, 2)  # 0.24
+    round_to_n_significant_digits.rtnsd(105.3, 1)  # 100
+    round_to_n_significant_digits.rtnsd(125.3, 2)  # 130
+
+Installation via pip:
+======================
+
+.. code-block:: bash
+
+    pip install round_to_n_significant_digits
+
+Links
+=====
+
+    * `PYPI <https://pypi.org/project/round_to_n_significant_digits/>`_
+    * `GitHub <https://github.com/stas-prokopiev/round_to_n_significant_digits>`_
+
+Contacts
+========
+
+    * Email: stas.prokopiev@gmail.com
+    * `vk.com <https://vk.com/stas.prokopyev>`_
+    * `Facebook <https://www.facebook.com/profile.php?id=100009380530321>`_
+
+License
+=======
+
+This project is licensed under the MIT License.
---
+++ packages/round-to-n-significant-digits/round_to_n_significant_digits-0.1.5.tar.gz/round_to_n_significant_digits-0.1.5/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+package_dir = \
+{'': 'src'}
+
+packages = \
+['round_to_n_significant_digits']
+
+package_data = \
+{'': ['*']}
+
+setup_kwargs = {
+    'name': 'round-to-n-significant-digits',
+    'version': '0.1.5',
+    'description': 'PYPI package with only 1 function to round float numbers',
+    'long_description': '==============================\nround_to_n_significant_digits\n==============================\n\n.. image:: https://img.shields.io/github/last-commit/stas-prokopiev/round_to_n_significant_digits\n   :target: https://img.shields.io/github/last-commit/stas-prokopiev/round_to_n_significant_digits\n   :alt: GitHub last commit\n\n.. image:: https://img.shields.io/github/license/stas-prokopiev/round_to_n_significant_digits\n    :target: https://github.com/stas-prokopiev/round_to_n_significant_digits/blob/master/LICENSE.txt\n    :alt: GitHub license<space><space>\n\n.. image:: https://img.shields.io/pypi/v/round_to_n_significant_digits\n   :target: https://img.shields.io/pypi/v/round_to_n_significant_digits\n   :alt: PyPI\n\n.. image:: https://img.shields.io/pypi/pyversions/round_to_n_significant_digits\n   :target: https://img.shields.io/pypi/pyversions/round_to_n_significant_digits\n   :alt: PyPI - Python Version\n\n\n.. contents:: **Table of Contents**\n\nOverview.\n=========================\nround_to_n_significant_digits is a one function PYPI package made only with one\ngoal to round float numbers in python to asked number of signifacant digits\n\n\n.. code-block:: python\n\n    import round_to_n_significant_digits\n\n    round_to_n_significant_digits.rtnsd(0.23234, 2)  # 0.23\n    round_to_n_significant_digits.rtnsd(0.235, 2)  # 0.24\n    round_to_n_significant_digits.rtnsd(105.3, 1)  # 100\n    round_to_n_significant_digits.rtnsd(125.3, 2)  # 130\n\nInstallation via pip:\n======================\n\n.. code-block:: bash\n\n    pip install round_to_n_significant_digits\n\nLinks\n=====\n\n    * `PYPI <https://pypi.org/project/round_to_n_significant_digits/>`_\n    * `GitHub <https://github.com/stas-prokopiev/round_to_n_significant_digits>`_\n\nContacts\n========\n\n    * Email: stas.prokopiev@gmail.com\n    * `vk.com <https://vk.com/stas.prokopyev>`_\n    * `Facebook <https://www.facebook.com/profile.php?id=100009380530321>`_\n\nLicense\n=======\n\nThis project is licensed under the MIT License.\n',
+    'author': 'stanislav',
+    'author_email': 'stas.prokopiev@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'package_dir': package_dir,
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>=3.6',
+}
+
+
+setup(**setup_kwargs)
