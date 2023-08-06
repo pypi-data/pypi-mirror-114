f87af171b44f725a22ec173ab3ebaac39a565067
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:33:14 +0000

    Add package torchglyph/0.2.0/torchglyph-0.2.0.tar.gz

---
+++ packages/torchglyph/torchglyph-0.2.0.tar.gz/torchglyph-0.2.0/torchglyph/dataset.py
@@ -0,0 +1,123 @@
+import itertools
+import uuid
+from collections import namedtuple
+from pathlib import Path
+from typing import Iterable, Any, Type
+from typing import Union, List, Tuple, NamedTuple, Dict
+
+from torch.utils.data import DataLoader as TorchDataLoader
+from torch.utils.data import Dataset as TorchDataset
+from tqdm import tqdm
+
+from torchglyph.io import DownloadMixin
+from torchglyph.pipe import Pipe
+
+__all__ = [
+    'Dataset',
+    'DataLoader',
+]
+
+
+class Dataset(TorchDataset, DownloadMixin):
+    def __init__(self, pipes: List[Dict[str, Pipe]], **kwargs) -> None:
+        super(Dataset, self).__init__()
+
+        self.pipes: Dict[str, Pipe] = {
+            name: pipe
+            for ps in pipes
+            for name, pipe in ps.items()
+        }
+        self.Batch: Type = namedtuple(
+            typename=f'Batch_{str(uuid.uuid4())[:8]}',
+            field_names=list(self.pipes.keys()),
+        )
+        if self.Batch.__name__ not in globals():
+            globals()[self.Batch.__name__] = self.Batch
+
+        self.data: Dict[str, List[Any]] = {}
+
+        for datum, ps in zip(zip(*self.load(**kwargs)), pipes):
+            for name, pipe in ps.items():
+                self.data.setdefault(name, []).extend(datum)
+
+    def transpose(self) -> None:
+        names, data = zip(*self.data.items())
+        names, data = list(names), zip(*data)
+        self.data = [self.Batch(**dict(zip(names, datum))) for datum in data]
+
+    def __getitem__(self, index: int) -> NamedTuple:
+        return self.data[index]
+
+    def __len__(self) -> int:
+        return len(self.data)
+
+    @property
+    def vocabs(self) -> NamedTuple:
+        return self.Batch(**{
+            name: pipe.vocab
+            for name, pipe in self.pipes.items()
+        })
+
+    def collate_fn(self, batch: List[NamedTuple]) -> NamedTuple:
+        data = self.Batch(*zip(*batch))
+        return self.Batch(*[
+            self.pipes[name].collate_fn(datum)
+            for name, datum in zip(self.Batch._fields, data)
+        ])
+
+    @classmethod
+    def load(cls, **kwargs) -> Iterable[Any]:
+        raise NotImplementedError
+
+    def dump(self, fp, batch: NamedTuple, prediction: Any, *args, **kwargs) -> None:
+        raise NotImplementedError
+
+    def eval(self, path: Path, **kwargs):
+        raise NotImplementedError
+
+    def viz(self, path: Path, **kwargs):
+        raise NotImplementedError
+
+    @classmethod
+    def new(cls, **kwargs) -> Tuple['DataLoader', ...]:
+        raise NotImplementedError
+
+
+class DataLoader(TorchDataLoader):
+    @property
+    def vocabs(self) -> NamedTuple:
+        return self.dataset.vocabs
+
+    @classmethod
+    def new(cls, datasets: Tuple[Dataset, ...],
+            batch_size: Union[int, Tuple[int, ...]],
+            shuffle: bool = True, drop_last: bool = False) -> Tuple['DataLoader', ...]:
+        assert len(datasets) > 0
+
+        batch_sizes = batch_size
+        if isinstance(batch_size, int):
+            batch_sizes = itertools.repeat(batch_size)
+
+        iteration = tqdm(
+            desc='processing datasets',
+            total=len(datasets) * (len(datasets[0].pipes) + 1),
+        )
+        for dataset in datasets:
+            for name, pipe in dataset.pipes.items():
+                pipe.postprocess_(dataset, name=name)
+                iteration.update(1)
+                iteration.set_postfix_str(f'{name}')
+            dataset.transpose()
+            iteration.update(1)
+            iteration.set_postfix_str('transpose')
+        iteration.close()
+
+        return tuple(
+            DataLoader(
+                dataset=dataset, batch_size=batch_size,
+                collate_fn=dataset.collate_fn,
+                shuffle=shuffle if index == 0 else False,
+                drop_last=drop_last if index == 0 else False,
+            )
+            for index, (dataset, batch_size) in enumerate(zip(datasets, batch_sizes))
+        )
---
+++ packages/torchglyph/torchglyph-0.2.0.tar.gz/torchglyph-0.2.0/torchglyph/nn/embedding.py
@@ -0,0 +1,62 @@
+from einops import rearrange
+from torch import nn, Tensor
+from torch.nn.utils.rnn import PackedSequence
+from torchglyph.vocab import Vocab
+from torchrua import PackedMeta, PackedSequential
+
+__all__ = [
+    'TokenEmbedding',
+    'CharLstmEmbedding',
+]
+
+
+class TokenEmbedding(nn.Embedding, metaclass=PackedMeta):
+    def __init__(self, embedding_dim: int, freeze: bool = False, *, vocab: Vocab = None,
+                 num_embeddings: int = 0, padding_idx: int = None) -> None:
+        if vocab is not None:
+            super(TokenEmbedding, self).__init__(
+                embedding_dim=embedding_dim,
+                num_embeddings=len(vocab),
+                padding_idx=vocab.pad_idx,
+                _weight=vocab.vectors,
+            )
+        else:
+            super(TokenEmbedding, self).__init__(
+                embedding_dim=embedding_dim,
+                num_embeddings=num_embeddings,
+                padding_idx=padding_idx,
+                _weight=None,
+            )
+        self.weight.requires_grad = not freeze
+
+    def extra_repr(self) -> str:
+        if not self.weight.requires_grad:
+            return ', '.join([super(TokenEmbedding, self).extra_repr(), 'frozen'])
+        return super(TokenEmbedding, self).extra_repr()
+
+
+class CharLstmEmbedding(nn.Module):
+    def __init__(self, hidden_dim: int = 50, num_layers: int = 1,
+                 bias: bool = True, bidirectional: bool = True, dropout: float = 0.5, *,
+                 char_embedding: TokenEmbedding) -> None:
+        super(CharLstmEmbedding, self).__init__()
+
+        self.embedding = PackedSequential(
+            char_embedding,
+            nn.Dropout(dropout),
+        )
+
+        self.rnn = nn.LSTM(
+            input_size=char_embedding.embedding_dim,
+            hidden_size=hidden_dim,
+            num_layers=num_layers, bias=bias,
+            bidirectional=bidirectional,
+        )
+
+        self.num_directions = 2 if self.rnn.bidirectional else 1
+        self.embedding_dim = self.rnn.hidden_size * self.num_directions
+
+    def forward(self, indices: PackedSequence) -> Tensor:
+        embedding = self.embedding(indices)
+        _, (encoding, _) = self.rnn(embedding)
+        return rearrange(encoding, '(l d) b x -> l b (d x)', d=self.num_directions)[-1]
