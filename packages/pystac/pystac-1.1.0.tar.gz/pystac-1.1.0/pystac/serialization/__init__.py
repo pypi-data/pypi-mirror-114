1e23e81c090304680dce82e5eba6c71aea37756d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:42:00 +0000

    Add package pystac/1.1.0/pystac-1.1.0.tar.gz

---
+++ packages/pystac/pystac-1.1.0.tar.gz/pystac-1.1.0/pystac/serialization/__init__.py
@@ -0,0 +1,7 @@
+from pystac.serialization.identify import (
+    STACVersionRange,
+    identify_stac_object,
+    identify_stac_object_type,
+)
+from pystac.serialization.common_properties import merge_common_properties
+from pystac.serialization.migrate import migrate_to_latest
---
+++ packages/pystac/pystac-1.1.0.tar.gz/pystac-1.1.0/tests/utils/stac_io_mock.py
@@ -0,0 +1,25 @@
+from typing import Any, Union
+from unittest.mock import Mock
+
+import pystac
+
+
+class MockStacIO(pystac.StacIO):
+    """Creates a mock that records StacIO calls for testing and allows
+    clients to replace StacIO functionality, all within a context scope.
+    """
+
+    def __init__(self) -> None:
+        self.mock = Mock()
+
+    def read_text(
+        self, source: Union[str, pystac.Link], *args: Any, **kwargs: Any
+    ) -> str:
+        self.mock.read_text(source)
+        return pystac.StacIO.default().read_text(source)
+
+    def write_text(
+        self, dest: Union[str, pystac.Link], txt: str, *args: Any, **kwargs: Any
+    ) -> None:
+        self.mock.write_text(dest, txt)
+        pystac.StacIO.default().write_text(dest, txt)
