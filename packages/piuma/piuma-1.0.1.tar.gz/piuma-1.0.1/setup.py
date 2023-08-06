727dbf7427df83d637d23607483963d7e036daf9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:53:54 +0000

    Add package piuma/1.0.1/piuma-1.0.1.tar.gz

---
+++ packages/piuma/piuma-1.0.1.tar.gz/piuma-1.0.1/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="piuma",
+    version="1.0.1",
+    license="gpl-3.0",
+    author="Alessandro De Leo",
+    author_email="emit07@protonmail.com",
+    description="An ultra-lighweight document oriented database",
+    packages=find_packages(),
+    keywords=['python', 'database', 'document oriented'],
+    url="https://github.com/emit07/piuma",
+    python_requires='>=3.6'
+)
