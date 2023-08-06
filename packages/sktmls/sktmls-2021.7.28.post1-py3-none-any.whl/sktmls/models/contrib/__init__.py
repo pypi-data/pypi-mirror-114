b02138bc1643c6ebbc94b8005c4ddca311a93a54
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:21:14 +0000

    Add package sktmls/2021.7.28.post1/sktmls-2021.7.28.post1-py3-none-any.whl

---
+++ packages/sktmls/sktmls-2021.7.28.post1-py3-none-any.whl/sktmls/models/contrib/__init__.py
@@ -0,0 +1,65 @@
+from .default_model import DefaultLightGBMModel, DefaultXGBoostModel, DefaultCatBoostModel, DefaultGenericModel
+from .sample_model import SampleModel
+from .sample_rule_model import SampleRuleModel
+from .random_pick_model import RandomPickModel, PeriodicRandomPickModel
+from .info_unpaid_rule_model import InfoUnpaidRuleModel
+from .mbr_boostpark_baseline_model import MbrBoostparkBaselineModel
+from .mbr_category_baseline_model import MbrCategoryBaselineModel
+from .mbr_interest_all_baseline_model import MbrInterestAllBaselineModel
+from .mbr_similar_benefit_model import MbrSimilarBenefitModel
+from .mbr_popular_benefit_model import MbrPopularBenefitModel
+from .mbr_custom_benefit_random_model import MbrCustomBenefitRandomModel
+from .mbr_valid_category_baseline_model import MbrValidCategoryBaselineModel
+from .info_defect_rule_model import InfoDefectRuleModel
+from .shuffle_list_model import ShuffleListModel, PeriodicShuffleListModel
+from .lightgbm_random_context_model import LightGBMRandomContextModel
+from .vas_xcloud_rule_model import VasXcloudRuleModel
+from .fee_no_equip_single_emb_model import FeeNoEquipSingleEmbModel
+from .random_score_model import RandomScoreModel, PeriodicRandomScoreModel
+from .generic_context_model import GenericContextModel
+from .generic_logic_model import GenericLogicModel
+from .conditional_generic_logic_model import ConditionalGenericLogicModel
+from .tw_random_greeting_rule_model import TwRandomGreetingRuleModel
+from .lightgbm_device_model import LightGBMDeviceModel
+from .catboost_pointwise_ranking_model import CatBoostPointwiseRankingModel
+from .mab_ranking_rule_model import MABRankingRuleModel
+from .sample_pytorch_model import SamplePyTorchModel
+from .catboost_device_model import CatboostDeviceModel
+from .commbot_catboost_device_model import CommbotCatboostDeviceModel
+
+__all__ = [
+    "DefaultLightGBMModel",
+    "DefaultXGBoostModel",
+    "DefaultCatBoostModel",
+    "DefaultGenericModel",
+    "SampleModel",
+    "SampleRuleModel",
+    "RandomPickModel",
+    "PeriodicRandomPickModel",
+    "InfoUnpaidRuleModel",
+    "MbrBoostparkBaselineModel",
+    "MbrCategoryBaselineModel",
+    "MbrInterestAllBaselineModel",
+    "MbrSimilarBenefitModel",
+    "MbrPopularBenefitModel",
+    "MbrCustomBenefitRandomModel",
+    "MbrValidCategoryBaselineModel",
+    "InfoDefectRuleModel",
+    "ShuffleListModel",
+    "PeriodicShuffleListModel",
+    "LightGBMRandomContextModel",
+    "VasXcloudRuleModel",
+    "FeeNoEquipSingleEmbModel",
+    "RandomScoreModel",
+    "PeriodicRandomScoreModel",
+    "GenericContextModel",
+    "GenericLogicModel",
+    "ConditionalGenericLogicModel",
+    "TwRandomGreetingRuleModel",
+    "LightGBMDeviceModel",
+    "CatBoostPointwiseRankingModel",
+    "MABRankingRuleModel",
+    "SamplePyTorchModel",
+    "CatboostDeviceModel",
+    "CommbotCatboostDeviceModel",
+]
