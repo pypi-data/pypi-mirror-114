fc3cd4c21c3eebc46a2a41949c1f9cdaa8cac11f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:40:17 +0000

    Add package intents/0.2.0/intents-0.2.0.tar.gz

---
+++ packages/intents/intents-0.2.0.tar.gz/intents-0.2.0/intents/connectors/_experimental/alexa/export.py
@@ -0,0 +1,148 @@
+"""
+Features:
+
+- [OK] intent
+- [OK] example utterances
+- [OK] example utterances with parameter references
+- [OK] system entity slots
+- [OK] custom entity slots
+- [  ] plain text responses
+- [  ] input/output contexts
+- [OK] multi language
+
+Note that there are some relevant TODOs in this module (e.g. intent names may be
+ambiguous, punctuation is stripped from utterances, and so on..)
+"""
+import re
+from typing import List, Dict
+from dataclasses import asdict
+
+from intents import Intent
+from intents.model.entity import Entity
+from intents.language import intent_language, intent_language_data, entity_language, entity_language_data, agent_supported_languages, LanguageCode
+from intents.connectors._experimental.alexa import agent_schemas as ask_schema
+from intents.helpers.data_classes import custom_asdict_factory
+
+# TODO: model in framework
+DEFAULT_INTENTS = [
+    ask_schema.LanguageModelIntent(
+        name="AMAZON.CancelIntent",
+        samples=[]
+    ),
+    ask_schema.LanguageModelIntent(
+        name="AMAZON.HelpIntent",
+        samples=[]
+    ),
+    ask_schema.LanguageModelIntent(
+        name="AMAZON.StopIntent",
+        samples=[]
+    ),
+    ask_schema.LanguageModelIntent(
+        name="AMAZON.NavigateHomeIntent",
+        samples=[]
+    )
+]
+
+def render(connector: "AlexaConnector") -> Dict[LanguageCode, dict]:
+    languages: List[LanguageCode] = agent_supported_languages(connector.agent_cls)
+    result = {}
+    for lang in languages:
+        rendered = render_agent(connector, lang)
+        rendered.interactionModel.languageModel.intents.extend(DEFAULT_INTENTS)
+        result[lang] = asdict(rendered, dict_factory=custom_asdict_factory())
+    return result
+
+def render_agent(connector: "AlexaConnector", lang: LanguageCode) -> ask_schema.Agent:
+    return ask_schema.Agent(
+        interactionModel=render_interaction_model(connector, lang)
+    )
+
+def render_interaction_model(connector: "AlexaConnector", lang: LanguageCode) -> ask_schema.InteractionModel:
+    return ask_schema.InteractionModel(
+        languageModel=render_language_model(connector, lang)
+    )
+
+def render_language_model(connector: "AlexaConnector", lang: LanguageCode) -> ask_schema.LanguageModel:
+    return ask_schema.LanguageModel(
+        invocationName=connector.invocation_name,
+        intents=[render_intent(i, connector, lang) for i in connector.agent_cls.intents],
+        types=[render_slot_type(e, connector, lang) for e in connector.agent_cls._entities_by_name.values()]
+        # TODO: complete
+    )
+
+#
+# Intent
+#
+
+def render_intent(intent_cls: type(Intent), connector: "AlexaConnector", lang: LanguageCode) -> ask_schema.LanguageModelIntent:
+    return ask_schema.LanguageModelIntent(
+        name=intent_cls.name.replace(".", "_"), # TODO: find better policy: this may cause ambiguous names
+        slots=render_intent_slots(intent_cls, connector),
+        samples=render_intent_samples(intent_cls, connector, lang)
+        # TODO: complete
+    )
+
+def render_intent_slots(intent_cls: type(Intent), connector: "AlexaConnector") -> List[ask_schema.LanguageModelIntentSlot]:
+    result = []
+    for param_name, param_metadata in intent_cls.parameter_schema.items():
+        entity_cls = param_metadata.entity_cls
+        slot_type = connector._entity_service_name(entity_cls)
+
+        result.append(ask_schema.LanguageModelIntentSlot(
+            name=param_name,
+            type=slot_type,
+            multipleValues=ask_schema.LanguageModelIntentSlotMultipleValues(
+                enabled=param_metadata.is_list
+            )
+        ))
+    return result
+
+
+def render_intent_samples(intent_cls: type(Intent), connector: "AlexaConnector", lang: LanguageCode) -> List[str]:
+    language_data = intent_language_data(connector.agent_cls, intent_cls, lang)
+    language_data = language_data[lang]
+    result = []
+    for utterance in language_data.example_utterances:
+        rendered_chunks = []
+        for chunk in utterance.chunks():
+            if isinstance(chunk, intent_language.TextUtteranceChunk):
+                rendered_chunks.append(chunk.text)
+            elif isinstance(chunk, intent_language.EntityUtteranceChunk):
+                rendered_chunks.append("{" + chunk.parameter_name + "}")
+            else:
+                raise ValueError(f"Unsupported utterance chunk type {type(chunk)}. This looks like a bug, please file an issue at https://github.com/dariowho/intents")
+        utterance = "".join(rendered_chunks)
+        # TODO: refine, especially for List parameters. Also, "{", "}" and
+        # "_" are only allowed in slot references
+        utterance = re.sub(r"[^a-zA-Z \-\{\}\_\.\']+", '', utterance)
+        result.append(utterance)
+    return result
+
+#
+# Slot Type
+#
+
+def render_slot_type(entity_cls: type(Entity), connector: "AlexaConnector", lang: LanguageCode) -> ask_schema.LanguageModelType:
+    language_data = entity_language_data(connector.agent_cls, entity_cls, lang)
+    language_data = language_data[lang]
+    return ask_schema.LanguageModelType(
+        name=entity_cls.name,
+        values=[render_slot_value(entity_cls, v, connector) for v in language_data]
+    )
+
+def render_slot_value(entity_cls: type(Entity), entity_entry: entity_language.EntityEntry, connector: "AlexaConnector") -> ask_schema.LanguageModelTypeValue:
+    return ask_schema.LanguageModelTypeValue(
+        id=connector.entity_value_id(entity_cls, entity_entry),
+        name=ask_schema.LanguageModelTypeValueName(
+            value=entity_entry.value,
+            synonyms=entity_entry.synonyms
+        )
+    )
+
+# from example_agent.agent import ExampleAgent
+# from intents.connectors._experimental.alexa import AlexaConnector
+
+# alexa = AlexaConnector(ExampleAgent, "any invocation")
+# # render(alexa)
+
+# alexa.export("./TMP_ALEXA")
---
+++ packages/intents/intents-0.2.0.tar.gz/intents-0.2.0/intents/connectors/dialogflow_es/export.py
@@ -0,0 +1,333 @@
+"""
+Here we export a :class:`Agent` to Dialogflow format.
+"""
+import os
+import json
+import shutil
+import logging
+import tempfile
+from uuid import uuid1
+from dataclasses import asdict
+from typing import List, Dict, Iterable
+
+from intents import Intent, language
+from intents.model.intent import _IntentMetaclass
+from intents.model.entity import SystemEntityMixin, _EntityMetaclass
+from intents.model.relations import related_intents
+import intents.connectors.dialogflow_es.agent_format as df
+from intents.connectors.dialogflow_es.entities import MAPPINGS as ENTITY_MAPPINGS
+
+logger = logging.getLogger(__name__)
+
+def export(connector: "intents.DialogflowEsConnector", output_path: str, agent_name="py-agent") -> None:
+    """
+    Export the given agent to the given path
+    """
+    agent_cls = connector.agent_cls
+
+    output_dir = os.path.join(tempfile.gettempdir(), 'dialogflow-agents-export', agent_name)
+    intents_dir = os.path.join(output_dir, 'intents')
+    entities_dir = os.path.join(output_dir, 'entities')
+
+    if os.path.isdir(intents_dir):
+        logger.warning("Removing existing intents folder: %s", intents_dir)
+        shutil.rmtree(intents_dir)
+
+    if os.path.isdir(entities_dir):
+        logger.warning("Removing existing entities folder: %s", entities_dir)
+        shutil.rmtree(entities_dir)
+
+    os.makedirs(intents_dir)
+    os.makedirs(entities_dir)
+
+    with open(os.path.join(output_dir, 'agent.json'), 'w') as f:
+        languages = agent_cls.languages
+        json.dump(asdict(render_agent(connector, agent_name, languages)), f, indent=2)
+
+    with open(os.path.join(output_dir, 'package.json'), 'w') as f:
+        json.dump({"version": "1.0.0"}, f, indent=2)
+
+    for intent in agent_cls.intents:
+        language_data = language.intent_language_data(agent_cls, intent)
+        rendered_intent = render_intent(connector, intent, language_data)
+        with open(os.path.join(intents_dir, f"{intent.name}.json"), "w") as f:
+            json.dump(asdict(rendered_intent), f, indent=2)
+        
+        for language_code, language_code_data in language_data.items():
+            rendered_intent_usersays = render_intent_usersays(agent_cls, intent, language_code, language_code_data.example_utterances)
+            filename = f"{intent.name}_usersays_{language_code.value}.json"
+            with open(os.path.join(intents_dir, filename), "w") as f:
+                usersays_data = [asdict(x) for x in rendered_intent_usersays]
+                json.dump(usersays_data, f, indent=2)
+
+    for entity_cls in agent_cls._entities_by_name.values():
+        language_data = language.entity_language_data(agent_cls, entity_cls)
+        rendered_entity = render_entity(entity_cls)
+        with open(os.path.join(entities_dir, f"{entity_cls.name}.json"), "w") as f:
+            json.dump(asdict(rendered_entity), f, indent=2)
+
+        for language_code, entries in language_data.items():
+            rendered_entity_entries = render_entity_entries(agent_cls, entries)
+            filename = f"{entity_cls.name}_entries_{language_code.value}.json"
+            with open(os.path.join(entities_dir, filename), "w") as f:
+                entries_data = [asdict(x) for x in rendered_entity_entries]
+                json.dump(entries_data, f, indent=2)
+
+    if output_path.endswith('.zip'):
+        output_path = output_path[:-4]
+    shutil.make_archive(output_path, 'zip', output_dir)
+
+#
+# Agent
+#
+
+def render_agent(connector: "intents.DialogflowEsConnector",  agent_name: str, languages: List[language.LanguageCode]):
+    google_assistant = df.AgentGoogleAssistant(
+        project=connector.gcp_project_id,
+        oAuthLinking=df.AgentGoogleAssistantOauthLinking()
+        # TODO: include Google Assistant configuration
+    )
+
+    if connector.webhook_configuration:
+        webhook = df.AgentWebhook(
+            available=True,
+            url=connector.webhook_configuration.url,
+            headers=connector.webhook_configuration.headers
+        )
+    else:
+        webhook = df.AgentWebhook()
+
+    languages = [l.value for l in languages]
+    return df.Agent(
+        displayName=agent_name,
+        webhook=webhook,
+        googleAssistant=google_assistant,
+        language=languages[0],
+        supportedLanguages=languages[1:]
+    )
+
+#
+# Intent
+#
+
+def get_input_contexts(connector: "DialogflowEsConnector", intent_cls: _IntentMetaclass) -> List[str]:
+    result = [connector._context_name(r.intent_cls) for r in related_intents(intent_cls).follow]
+    
+    # TODO: remove after deprecation
+    result.extend([c.name for c in intent_cls.input_contexts])
+
+    return result
+
+def get_output_contexts(
+    connector: "DialogflowEsConnector",
+    intent_cls: _IntentMetaclass,
+    visited: List[_IntentMetaclass]=None
+) -> List[df.AffectedContext]:
+    """
+    An Intent can output its own context (e.g. intent `OrderFish` can spawn
+    context `c_orderfish`). However, this should only happen if that context is
+    actually needed (i.e. if there are intents referencing it).
+
+    Also, this procedure is recursive: if intent `OrderFish` inherits from
+    intent `OrderItem`, and there are intents referencing `c_orderitem`, then
+    `OrderFish` should output `c_orderitem`
+    """
+    if not visited:
+        visited = []
+
+    if intent_cls is Intent:
+        return []
+
+    result = []
+    if connector._intent_needs_context(intent_cls):
+        name = connector._context_name(intent_cls)
+        result.append(df.AffectedContext(name, 5)) # TODO: allow custom lifespan
+
+    # TODO: remove after deprecation
+    if issubclass(intent_cls, Intent):
+        result.extend([
+            df.AffectedContext(c.name, c.lifespan) for c in intent_cls.output_contexts
+            if c.name not in [ec.name for ec in result]
+        ])
+
+    visited.append(intent_cls)
+    for super_cls in intent_cls.mro():
+        if super_cls not in visited and issubclass(super_cls, Intent):
+            result.extend(get_output_contexts(connector, super_cls, visited))
+
+    return result
+
+def render_intent(connector: "DialogflowEsConnector", intent_cls: _IntentMetaclass, language_data: Dict[language.LanguageCode, language.IntentLanguageData]):
+    response = df.Response(
+        affectedContexts=get_output_contexts(connector, intent_cls),
+        # affectedContexts=[df.AffectedContext(c.name, c.lifespan) for c in intent_cls.output_contexts],
+        parameters=render_parameters(intent_cls, language_data),
+        messages=render_responses(intent_cls, language_data, connector.rich_platforms),
+    )
+
+    return df.Intent(
+        id=str(uuid1()),
+        name=intent_cls.name,
+        contexts=get_input_contexts(connector, intent_cls),
+        # contexts=[c.name for c in intent_cls.input_contexts],
+        responses=[response],
+
+        # TODO: re-enable
+        # webhookUsed=intent_cls.metadata.intent_webhook_enabled,
+        # webhookForSlotFilling=intent_cls.metadata.slot_filling_webhook_enabled,
+        events=[df.Event(e.name) for e in intent_cls.events]
+    )
+
+def render_parameters(intent_cls: _IntentMetaclass, language_data: Dict[language.LanguageCode, language.IntentLanguageData]):
+    result = []
+    for param_name, param_metadata in intent_cls.parameter_schema.items():
+        entity_cls = param_metadata.entity_cls
+        # TODO: use Connector._entity_service_name()
+        if issubclass(entity_cls, SystemEntityMixin):
+            data_type = ENTITY_MAPPINGS[entity_cls].service_name
+        else:
+            data_type = entity_cls.name
+
+        prompts = []
+        for language_code, language_code_data in language_data.items():
+            for prompt in language_code_data.slot_filling_prompts.get(param_name, []):
+                prompts.append(df.Prompt(value=prompt, lang=language_code.value))
+
+        result.append(df.Parameter(
+            id=str(uuid1()),
+            name=param_name,
+            required=param_metadata.required,
+            dataType=f'@{data_type}',
+            value=f"${param_name}",
+            defaultValue=param_metadata.default if not param_metadata.required else '',
+            isList=param_metadata.is_list,
+            prompts=prompts
+        ))
+    return result
+
+def render_response(response: language.IntentResponse, language_code: language.LanguageCode, platform: str):
+    """
+    platform: None = "Default"
+    """
+    if isinstance(response, language.TextIntentResponse):
+        response: language.TextIntentResponse
+        return df.TextResponseMessage(
+            lang=language_code.value,
+            speech=response.choices,
+            platform=platform
+        )
+    elif isinstance(response, language.QuickRepliesIntentResponse):
+        response: language.QuickRepliesIntentResponse
+        return df.QuickRepliesResponseMessage(
+            lang=language_code.value,
+            replies=response.replies,
+            platform=platform
+        )
+    elif isinstance(response, language.ImageIntentResponse):
+        response: language.ImageIntentResponse
+        return df.ImageResponseMessage(
+            lang=language_code.value,
+            imageUrl=response.url,
+            title=response.title if response.title else "",
+            platform=platform
+        )
+    elif isinstance(response, language.CardIntentResponse):
+        response: language.CardIntentResponse
+        buttons = None
+        if response.link:
+            buttons = [df.CardResponseMessageButton(text="👁", postback=response.link)]
+        return df.CardResponseMessage(
+            lang=language_code.value,
+            title=response.title,
+            subtitle=response.subtitle,
+            imageUrl=response.image,
+            buttons=buttons,
+            platform=platform
+        )
+    elif isinstance(response, language.CustomPayloadIntentResponse):
+        response: language.CustomPayloadIntentResponse
+        return df.CustomPayloadResponseMessage(
+            lang=language_code.value,
+            payload={
+                response.name: response.payload
+            },
+            platform=platform
+        )
+    else:
+        raise ValueError(f"Unsupported response type: {response}")
+
+def render_responses(intent_cls: _IntentMetaclass, language_data: Dict[language.LanguageCode, language.IntentLanguageData], rich_platforms: Iterable[str]):
+    result = []
+
+    for language_code, language_code_data in language_data.items():
+        if not language_code_data.responses:
+            result.append(df.ResponseMessage(language_code.value))
+            continue
+
+        for response_group, responses in language_code_data.responses.items():
+            assert response_group in [language.IntentResponseGroup.DEFAULT, language.IntentResponseGroup.RICH]
+            if response_group == language.IntentResponseGroup.RICH:
+                platforms_to_render = rich_platforms
+            else:
+                platforms_to_render = (None,) # Dialogflow will put the response in "Default" when platform=None
+
+            for res in responses:
+                for platform in platforms_to_render:
+                    result.append(render_response(res, language_code, platform))
+
+    return result
+
+def render_utterance_chunk(chunk: language.UtteranceChunk):
+    if isinstance(chunk, language.TextUtteranceChunk):
+        return df.UsersaysTextChunk(text=chunk.text, userDefined=True)
+
+    if isinstance(chunk, language.EntityUtteranceChunk):
+        chunk: language.EntityUtteranceChunk
+        # TODO: user Connector._entity_service_name()
+        if issubclass(chunk.entity_cls, SystemEntityMixin):
+            meta = ENTITY_MAPPINGS[chunk.entity_cls].service_name
+        else:
+            meta = chunk.entity_cls.name
+
+        return df.UsersaysEntityChunk(
+            text=chunk.parameter_value,
+            alias=chunk.parameter_name,
+            meta=f'@{meta}',
+            userDefined=True
+        )
+
+    raise ValueError(f"Unsupported Utterance Chunk Type: {chunk}")
+
+def render_intent_usersays(agent_cls: type, intent: _IntentMetaclass, language_code: language.LanguageCode, examples: List[language.ExampleUtterance]):
+    result = []
+    for e in examples:
+        result.append(df.IntentUsersays(
+            id=str(uuid1()),
+            lang=language_code.value,
+            data=[render_utterance_chunk(c) for c in e.chunks()]
+        ))
+    return result
+
+#
+# Entity
+#
+
+def render_entity(entity_cls: _EntityMetaclass) -> df.Entity:
+    metadata = entity_cls.metadata
+    return df.Entity(
+        id=str(uuid1()),
+        name=entity_cls.name,
+        isRegexp=metadata.regex_entity,
+        automatedExpansion=metadata.automated_expansion,
+        allowFuzzyExtraction=metadata.fuzzy_matching
+    )
+
+def render_entity_entries(entity_cls: _EntityMetaclass, entries: List[language.EntityEntry]) -> List[df.EntityEntry]:
+    result = []
+    for e in entries:
+        result.append(df.EntityEntry(
+            value=e.value,
+            synonyms=e.synonyms
+        ))
+    return result
+
