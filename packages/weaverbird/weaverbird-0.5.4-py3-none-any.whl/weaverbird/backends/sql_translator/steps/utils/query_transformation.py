0d14aab3cbce420ab56c4205c5ab1ab11c1929d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:10:51 +0000

    Add package weaverbird/0.5.4/weaverbird-0.5.4-py3-none-any.whl

---
+++ packages/weaverbird/weaverbird-0.5.4-py3-none-any.whl/weaverbird/backends/sql_translator/steps/utils/query_transformation.py
@@ -0,0 +1,59 @@
+from weaverbird.pipeline.conditions import (
+    ComparisonCondition,
+    Condition,
+    ConditionComboAnd,
+    ConditionComboOr,
+    InclusionCondition,
+    MatchCondition,
+    NullCondition,
+)
+
+SQL_COMPARISON_OPERATORS = {
+    'eq': '=',
+    'ne': '!=',
+    'lt': '<',
+    'le': '<=',
+    'gt': '>',
+    'ge': '>=',
+}
+
+SQL_NULLITY_OPERATORS = {
+    'isnull': 'IS NULL',
+    'notnull': 'IS NOT NULL',
+}
+
+SQL_MATCH_OPERATORS = {
+    'matches': 'RLIKE',
+    'notmatches': 'NOT RLIKE',
+}
+
+SQL_INCLUSION_OPERATORS = {
+    'in': 'IN',
+    'nin': 'NOT IN',
+}
+
+
+def apply_condition(condition: Condition, query: str) -> str:
+    if isinstance(condition, ComparisonCondition):
+        try:
+            float(condition.value)
+            query += f'{condition.column} {SQL_COMPARISON_OPERATORS[condition.operator]} {condition.value}'
+        except ValueError:
+            query += f"{condition.column} {SQL_COMPARISON_OPERATORS[condition.operator]} '{condition.value}'"
+    elif isinstance(condition, NullCondition):
+        query += f'{condition.column} {SQL_NULLITY_OPERATORS[condition.operator]}'
+    elif isinstance(condition, MatchCondition):
+        query += f"{condition.column} {SQL_MATCH_OPERATORS[condition.operator]} '{condition.value}'"
+    elif isinstance(condition, InclusionCondition):
+        query += f"{condition.column} {SQL_INCLUSION_OPERATORS[condition.operator]} {str(tuple(condition.value))}"
+    elif isinstance(condition, ConditionComboAnd):
+        query = apply_condition(condition.and_[0], query)
+        for c in condition.and_[1:]:
+            query = apply_condition(c, f'{query} AND ')
+    elif isinstance(condition, ConditionComboOr):
+        query = apply_condition(condition.or_[0], query)
+        for c in condition.or_[1:]:
+            query = apply_condition(c, f'{query} OR ')
+    else:
+        raise NotImplementedError('Only comparison conditions are implemented')
+    return query
