fbf33175b43ddb8c572aed544ea6d45a413ab07e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:17:07 +0000

    Add package seven-cloudapp-frame/1.0.0.6/seven_cloudapp_frame-1.0.0.6.tar.gz

---
+++ packages/seven-cloudapp-frame/seven_cloudapp_frame-1.0.0.6.tar.gz/seven_cloudapp_frame-1.0.0.6/seven_cloudapp_frame/handlers/server/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+:Author: HuangJianYi
+:Date: 2021-07-15 11:57:53
+@LastEditTime: 2021-07-26 18:22:54
+@LastEditors: HuangJianYi
+:description: 
+"""
+__all__ = ["base_s", "report_s"]
