81a5ddfa7210abad4d8c785be5d7351614e126b1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:12:28 +0000

    Add package python-http-parser/0.4.0/python_http_parser-0.4.0-py3-none-any.whl

---
+++ packages/python-http-parser/python_http_parser-0.4.0-py3-none-any.whl/python_http_parser/__init__.pyi
@@ -0,0 +1,32 @@
+"""
+``python_http_parser`` module.
+"""
+__all__ = [
+    'decode',
+    'parse'
+]
+
+# Imports
+from typing import Dict, List, Optional, TypedDict, Union
+
+import python_http_parser.constants as constants
+
+class ParsedHTTPMessage(TypedDict):
+    status_code: Optional[int]
+    status_msg: Optional[str]
+    req_method: Optional[str]
+    req_uri: Optional[str]
+    http_ver: float
+    headers: Dict[str, Union[str, list]]
+    raw_headers: List[str]
+    body: Optional[str]
+
+
+def parse(
+    msg: Union[bytes, bytearray, str],
+    strictness_level: int = constants.PARSER_NORMAL,
+    is_response: bool = False
+) -> ParsedHTTPMessage: ...
+
+
+decode = parse
