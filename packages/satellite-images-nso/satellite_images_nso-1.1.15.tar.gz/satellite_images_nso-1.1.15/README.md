7a77f7ae8a726cf659c7e0fa3bce5faf1916e352
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:29:55 +0000

    Add package satellite-images-nso/1.1.15/satellite_images_nso-1.1.15.tar.gz

---
+++ packages/satellite-images-nso/satellite_images_nso-1.1.15.tar.gz/satellite_images_nso-1.1.15/README.md
@@ -0,0 +1,130 @@
+# Introduction 
+This python code is intended to automate/make easier the data extraction and cropping of satellite data from the netherlands space office (NSO).
+NSO provides free satellite images from the Netherlands, a downside however is that the NSO provides a very large region and as such a very large data file.
+This leads to a unnecessary large amount of data especially if you only want to study a smaller specific region.
+
+This python code crops a selected region out of the original satellite image based on a geojson, provided that the selected region is smaller than the original file.
+And then saves this cutout thus reducing the unnecessary saved data. 
+A option can also be set for calculating the Normalized difference vegetation index (NVDI, used in for example crop analysis) of the cutout region.
+We are working on extracting more variables on satellite images, like multi date normalisation in order to do atmospheric correction.
+
+This image gives a illustration: 
+![Alt text](example.png?raw=true "Title")
+
+
+
+
+
+
+*This satellite data is only intended for dutch legal entities, dutch institutions or dutch citizens.
+For the license terms of the NSO see this links: [https://www.spaceoffice.nl/nl/satellietdataportaal/toegang-data/licentievoorwaarden/](https://www.spaceoffice.nl/nl/satellietdataportaal/toegang-data/licentievoorwaarden/)
+
+# Getting Started
+
+0. Get a NSO account, register at [https://satellietdataportaal.nl/register.php](https://satellietdataportaal.nl/register.php)
+1. First get a GeoJSON file of the region you want to cut. [Geojson.io](https://geojson.io/#map=8/51.821/5.004) can you help you with that. Note the coordinates have to be in WGS84! ( Which should be standard for a geojson.) 
+2. Make a instance of nso_geojsonregion with instance of the geojson region you have, where you want to store the cropped files and the NSO account based on step 0.
+2. Retrieve download links for the specific region you want to have.
+3. Download the found links.
+
+# Example code.
+
+```python
+# This the way the import nso.
+import satellite_images_nso.api.nso_georegion as nso
+# The sat_manipulator gives other handy transmations on satellite data.
+import satellite_images_nso.api.sat_manipulator as sat_manipulator
+
+path_geojson = "/src/example/example.geojson"
+# The first parameter is the path to the geojson, the second the map where the cropped satellite data will be installed
+georegion = nso.nso_georegion(path_geojson,"/src/output/",\
+                              YOUR_USER_NAME_HERE,\
+                             YOUR_PASSWORD_HERE)
+
+# This method fetches all the download links to all the satelliet images which contain the region in the geojson.
+# Filter out for which satellite you want to download links from! SV for example stands for the Suoerview satellite
+links = georegion.retrieve_download_links()
+
+
+# This example filters out only 50 cm RGB Infrared Superview satellite imagery in the summer.
+season = "Summer"
+links_group = []
+for link in links:
+            if 'SV' in link and '50cm' in link and 'RGBI' in link:
+                if sat_manipulator.get_season_for_month(int(link.split("/")[len(link.split("/"))-1][4:6]))[0] == season:
+                    links_group.append(link)
+
+
+
+# Downloads a satelliet image from the NSO, make a crop out of it so it fits the geojson region and calculate the NVDI index.
+# The output will stored in the designated output folder.
+georegion.execute_link(links_group[0])
+# The parameters are : execute_link(self, link, delete_zip_file = True, delete_source_files = True, check_if_file_exists = True)
+# With the parameters you can decide if you want to keep the original satellite files, such  as wether to keep the downloaded zip file or the extracted source files from which the cutout will be made.
+
+
+# This function reads a .tif file, which is a format the satellite data is stored in,  and converts it to a pixel based geopandas dataframe.
+# For machine learning purposes.
+path_to_vector = "path/to/folder/*.tif"
+geo_df_pixel = sat_manipulator.tranform_vector_to_pixel_gpdf(path_to_vector)
+```
+See also the jupyter notebook in src/nso_notebook_example.ipynb
+# Installation.
+
+Install this package with: `pip install satellite_images_nso`
+
+Be sure you've installed GDAL already on your computer. Other python dependencies will install automatically (Fiona>=1.8.13, GDAL>=3.0.4, geopandas>=0.9.0, rasterio>=1.1.3 Shapely>=1.7.0).
+Or look into the requirements.txt file.
+
+If you don't have gdal installed on your computer or if it doesn't work follow the instruction below.
+
+## Install GDAL on Windows
+If you are a Windows user you have to install the GDAL dependency yourself via a wheels.
+
+Instead install these wheels with pip install XXX.XX.XX.whl.
+
+Go to https://www.lfd.uci.edu/~gohlke/pythonlibs/ for the wheels of these depencies:
+ 
+Depencencies are : "Fiona>=1.8.13", "GDAL>=3.0.4", "geopandas>=0.9.0","rasterio>=1.1.3","Shapely>=1.7.0"
+NOTE: You can't use normal pip for the installation of any of these depencies or else they won't find GDAL. So use all the .whl files!
+Then follow the fourth solution on this stackoverflow question:
+https://gis.stackexchange.com/questions/2276/installing-gdal-with-python-on-windows
+
+
+# Install GDAL on Databricks
+If you are using databricks use this code to set up a init script which installs GDAL.
+
+```python
+    dbutils.fs.mkdirs("dbfs:/databricks/FileStore/init_script")
+    dbutils.fs.put("/databricks/FileStore/init_script/gdal.sh","""
+        #!/bin/bash
+        set -ex
+        /databricks/python/bin/python -V
+        ./databricks/conda/etc/profile.d/conda.sh
+        conda activate /databricks/python
+        conda install -y gdal""", True)
+```
+
+## Install GDAL on MacOS
+Install GDAL by using Brew:  
+`brew install GDAL`
+
+# Run as a docker container
+```console
+docker run -it --entrypoint bash dockerhubpzh/satellite_images_nso_docker
+```
+See: https://hub.docker.com/r/dockerhubpzh/satellite_images_nso_docker
+
+# Local development
+Run `rebuild.bat` to build and install package on local computer.
+
+
+
+
+# Author
+Michael de Winter
+
+Daniel Overdevest
+
+Yilong Wen
+
---
+++ packages/satellite-images-nso/satellite_images_nso-1.1.15.tar.gz/satellite_images_nso-1.1.15/src/satellite_images_nso/api/nso_georegion.py
@@ -0,0 +1,173 @@
+import os
+import satellite_images_nso._nso_data_extraction.nso_api as nso_api
+import satellite_images_nso._manipulation.nso_manipulator as nso_manipulator
+from datetime import date
+import glob
+import geopandas as gpd
+import json
+from satellite_images_nso.__logger import logger_nso
+import shutil
+
+
+"""
+    This class constructs a nso georegion object.
+
+    WHich can be used for retrieving download links for satellite images for a parameter georegion.
+    Or cropping satellite images for the parameter georegion.
+   
+    Author: Michael de Winter
+"""
+
+logger = logger_nso.init_logger()
+
+def correct_file_path(path):
+    " File path does not need to end with /"
+    path = path.replace("\\","/")
+    if path.endswith("/"):
+        return path[:-1]
+    return path
+
+
+class nso_georegion:
+
+    def __init__(self, path_to_geojson: str, output_folder: str, username: str, password: str):
+        """
+            Init of the class.
+
+            @param path_to_geojson: Path where the geojson is located.
+            @param output_folder: Folder where the cropped and nvdi files will be stored.
+            @param username: the username of the nso account.
+            @param password: the password of the nso account
+        """
+        
+        self.path_to_geojson = correct_file_path(path_to_geojson)
+        # Name needs to be included in the geojson name.
+        self.region_name = path_to_geojson.split("/")[len(path_to_geojson.split("/"))-1].split('.')[0]
+        # georegion is a variable which contains the coordinates in the geojson, which should be WGS!
+        self.georegion = self.__getFeatures(self.path_to_geojson)[0]
+        if len(self.georegion) == 0 :
+            raise Exception("Geojson not loaded correctly. Weirdly this error is sometimes solved by reloading the session")
+
+        self.output_folder = correct_file_path(output_folder)
+       
+        self.username = username
+        self.password = password
+
+
+    def  __getFeatures(self,path):
+        """
+            Function to parse features from GeoDataFrame in such a manner that rasterio wants them
+    
+            @param path: The path to a geojson.
+            @return coordinates which rasterio wants to have.
+        """
+        return [json.loads(gpd.read_file(path).to_json())['features'][0]['geometry']['coordinates']]
+
+
+    def retrieve_download_links(self,start_date = "2014-01-01", end_date =date.today().strftime("%Y-%m-%d"),max_meters=3):
+        """
+            This functions retrieves download links for area chosen in the geojson for the nso.
+
+            @param georegion: Polygon with the stored georegion.
+            @param start_date: From when satelliet date needs to be looked at.
+            @param end_date: the end date of the period which needs to be looked at
+            @param max_meters: Maximum resolution which needs to be looked at.
+
+            @return: the found download links.
+        """
+        return nso_api.retrieve_download_links(self.georegion,self.username, self.password, start_date = "2014-01-01", end_date =date.today().strftime("%Y-%m-%d"),max_meters=3)
+
+    def crop_and_calculate_nvdi(self,path, calculate_nvdi = True):
+        """
+            Function for the crop and the calculating of the NVDI index.
+            Can be used as a standalone if you have already unzipped the file.
+
+            @oaram path: Path to a .tif file.
+            @param calculate_nvdi: Wether or not to also calculate the NVDI index.
+        """       
+        true_path = path
+        if '.tif' not in true_path:
+            for x in glob.glob(path+'/*.tif', recursive=True):
+                true_path = x
+        
+        if ".tif" not in true_path:
+            logger.info(true_path+" Error:  .tif not found") 
+            raise Exception(".tif not found")
+        else: 
+            cropped_path, nvdi_path, nvdi_matrix =  nso_manipulator.run(true_path, self.path_to_geojson, self.output_folder, calculate_nvdi )
+            logger.info("Cropped file is found at: "+cropped_path)
+            logger.info("The NDVI picture is found at: "+nvdi_path)
+            logger.info("NDVI numpy arrat i found at: "+nvdi_matrix)
+            
+                      
+    def execute_link(self, link, delete_zip_file = True, delete_source_files = True, check_if_file_exists = True):
+        """ 
+            Executes the download, croppend 67and the calculating of the NVDI for a specific link.
+        
+            @param link: Link to a file from the NSO.
+            @param geojson_path: Path to a geojson with the selected region.
+            @param delete_zip_file: whether or not to keep the original .zip file.
+            @param delete_source_files: whether or not to keep the extracted files.
+            @param check_if_file_exists: check wether the file is already downloaded or stored somewhere.
+        """
+        try:
+            download_archive_name = self.output_folder+"/"+link.split("/")[len(link.split("/"))-1]+"_"+link.split("/")[len(link.split("/"))-2]+'.zip'
+
+            nso_api.download_link(link,download_archive_name, self.username, self.password)
+            extracted_folder = nso_api.unzip_delete(download_archive_name,delete_zip_file)
+  
+            self.crop_and_calculate_nvdi(extracted_folder)
+        except Exception as e: 
+            logger.info("Error in downloading and or cropping: "+str(e))
+            print("Error in downloading and or cropping: "+str(e))
+            
+        if delete_source_files == True and 'extracted_folder' in globals():
+            shutil.rmtree(extracted_folder)
+
+        return "Succesfully cropped .tif file"
+
+
+    def check_already_downloaded_links(self):
+        """
+            Check which links have already been dowloaded.
+        """
+        downloaded_files = []
+
+        for file in glob.glob(self.output_folder+'/*'+str(self.region_name)+'*.tif'):
+            downloaded_files.append(file)
+
+        return downloaded_files
+    
+    def get_output_folder(self):
+        """
+            Get the output folder
+        """
+        return self.output_folder
+
+    def get_georegion(self):
+        """
+            Get the coordinates from the geojson.
+        """
+        return self.georegion 
+
+    def get_region_name(self):
+        """-
+            Get the name of the shape file.
+        """
+        return self.region_name
+
+  
+
+
+
+
+
+
+
+
+    
+
+
+
+
+
