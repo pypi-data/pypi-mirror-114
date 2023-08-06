7b89aa6a7dd38bbbee2d260e8c121d7b66c3cac0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:11:18 +0000

    Add package calver/2021.7.30/calver-2021.7.30.tar.gz

---
+++ packages/calver/calver-2021.7.30.tar.gz/calver-2021.7.30/src/calver/integration.py
@@ -0,0 +1,38 @@
+import datetime
+import os
+
+DEFAULT_FORMAT = "%Y.%m.%d"
+
+
+def get_pkginfo_contents():
+    path = os.path.join(os.path.abspath("."), "PKG-INFO")
+    with open(path, encoding="utf-8") as fp:
+        return fp.read()
+
+
+def pkginfo_version():
+    try:
+        content = get_pkginfo_contents()
+    except FileNotFoundError:
+        return
+
+    data = dict(x.split(": ", 1) for x in content.splitlines() if ": " in x)
+
+    version = data.get("Version")
+    if version != "UNKNOWN":
+        return version
+
+
+def version(dist, keyword, value):
+    if not value:
+        return
+    elif value is True:
+        generate_version = lambda: datetime.datetime.now().strftime(DEFAULT_FORMAT)
+    elif isinstance(value, str):
+        generate_version = lambda: datetime.datetime.now().strftime(value)
+    elif getattr(value, "__call__", None):
+        generate_version = value
+    else:
+        return
+
+    dist.metadata.version = pkginfo_version() or generate_version()
