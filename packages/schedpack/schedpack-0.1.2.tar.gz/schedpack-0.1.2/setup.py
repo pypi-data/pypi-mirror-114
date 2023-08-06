1ac0c03e440ca622bdc6dc0febdb873fc9bb0085
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:12:47 +0000

    Add package schedpack/0.1.2/schedpack-0.1.2.tar.gz

---
+++ packages/schedpack/schedpack-0.1.2.tar.gz/schedpack-0.1.2/setup.py
@@ -0,0 +1,24 @@
+from distutils.core import setup
+
+
+setup(
+    name = 'schedpack',
+    packages = ['schedpack'],
+    version = '0.1.2',
+    license = 'MIT',
+    description = 'Package for scheduling activities that last some time',
+    author = 'borisoid',
+    url = 'https://github.com/Borisoid/schedpack',
+    keywords = ['schedule', 'lasting', 'cron'],
+    install_requires = [
+        'croniter==1.0.15',
+    ],
+    classifiers = [
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.8',
+    ],
+)
