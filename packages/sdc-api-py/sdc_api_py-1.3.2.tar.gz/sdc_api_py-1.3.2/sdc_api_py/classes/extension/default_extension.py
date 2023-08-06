a58d7e1c9ed9f2efc84299f68591e8342bd7cee6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:08:25 +0000

    Add package sdc-api-py/1.3.2/sdc_api_py-1.3.2.tar.gz

---
+++ packages/sdc-api-py/sdc_api_py-1.3.2.tar.gz/sdc_api_py-1.3.2/sdc_api_py/classes/extension/default_extension.py
@@ -0,0 +1,43 @@
+import discord
+
+from ..Bots import Global
+from ..Lib import Querier
+from discord.ext.tasks import loop
+from discord.ext.commands import Cog
+
+
+class Monitoring(Cog):
+    def __init__(self, bot: discord.ext.commands.Bot):
+        self.bot = bot
+        self.querier = Querier()
+        self.monitorings.start()
+        self.last_server_count = -1
+
+    @loop(minutes=Global._time)
+    async def monitorings(self):
+        server_count = len(self.bot.guilds)
+        if server_count != self.last_server_count:
+            try:
+                res = await self.querier.execute_post_query(
+                    f"https://api.server-discord.com/v2/bots/{self.bot.user.id}/stats",
+                    headers={"Authorization": f"{Global.SDC_token}"},
+                    data={"shards": self.bot.shard_count or 1, "servers": len(self.bot.guilds)}
+                )
+                self.last_server_count = server_count
+                data = await res.json()
+                status = data["status"]
+                if status is True:
+                    if Global.logging_level:
+                        print(f"SDC: Статистика отправлена")
+                else:
+                    print(f"SDC: Произошла ошибка при отправке статистики: {data}")
+
+            except Exception as error:
+                print(f"SDC: Произошла ошибка при отправке статистики: {error}")
+        else:
+            if Global.logging_level:
+                print("SDC: Количество серверов не изменилось. Отправка статистики пропущена")
+
+
+def setup(bot):
+    bot.add_cog(Monitoring(bot))
