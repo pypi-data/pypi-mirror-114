45188f4fb452e896e823f18e9d46a7a9acf47625
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:27:59 +0000

    Add package lisa20/0.0.10/lisa20-0.0.10.tar.gz

---
+++ packages/lisa20/lisa20-0.0.10.tar.gz/lisa20-0.0.10/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="lisa20",
+    url="https://github.com/vk643/zipline-live2-vk",
+    version="0.0.10",
+    author="Andrew Platov",
+    author_email="andrew.platow95@gmail.com",
+    description="A small example package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    include_package_data=True,
+    license='Apache 2.0',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "./"},
+    python_requires=">=3.6",
+)
