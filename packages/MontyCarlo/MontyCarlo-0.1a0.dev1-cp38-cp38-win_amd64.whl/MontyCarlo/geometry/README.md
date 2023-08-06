346daaa78b0234495dade330c325215f41234c08
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:24:38 +0000

    Add package MontyCarlo/0.1a0.dev1/MontyCarlo-0.1a0.dev1-cp38-cp38-win_amd64.whl

---
+++ packages/MontyCarlo/MontyCarlo-0.1a0.dev1-cp38-cp38-win_amd64.whl/MontyCarlo/geometry/README.md
@@ -0,0 +1,3 @@
+THE CODE INSIDE THE FOLDER ```\sdf``` IS NOT MY WORK. 
+
+It is an open source implementation of sdf renderization through the marching cubes algorithm.
---
+++ packages/MontyCarlo/MontyCarlo-0.1a0.dev1-cp38-cp38-win_amd64.whl/MontyCarlo/types.pyx
@@ -0,0 +1,45 @@
+__doc__ = """
+types.pyx
+    A place to keep the frequently used data structures. (I will eventually move everything here).
+"""
+
+__author__ = "Rui Campos"
+
+print("Importing `.types`")
+
+
+import numpy as np
+
+nan = np.nan
+
+cdef struct double3:
+    double x, y, z
+    
+    
+class MAP(dict):
+    """A dictionary with the following syntax sugar:
+    
+    > map = MAP()
+    > map.x = 1
+    
+    is equivelent to:
+    
+    > map = MAP()
+    > map['x'] = 1
+    
+    """
+    
+    def __getattr__(self, key):
+        try:
+            return self[key]
+        except KeyError:
+            raise AttributeError
+
+    def __setattr__(self, key, value):
+        self[key] = value
+
+    def __delattr__(self, key):
+        try:
+            del self[key]
+        except KeyError:
+            raise AttributeError
