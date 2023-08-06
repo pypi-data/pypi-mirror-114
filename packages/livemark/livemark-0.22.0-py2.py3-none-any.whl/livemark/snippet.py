675a4841b2e38b601a1766fdb5bb695b0c441820
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:30:54 +0000

    Add package livemark/0.22.0/livemark-0.22.0-py2.py3-none-any.whl

---
+++ packages/livemark/livemark-0.22.0-py2.py3-none-any.whl/livemark/snippet.py
@@ -0,0 +1,41 @@
+class Snippet:
+    def __init__(self, input, *, header):
+        self.__input = input
+        self.__header = header
+        self.__output = None
+
+    @property
+    def input(self):
+        return self.__input
+
+    @property
+    def output(self):
+        return self.__output
+
+    @output.setter
+    def output(self, value):
+        self.__output = value
+
+    @property
+    def header(self):
+        return self.__header
+
+    @property
+    def language(self):
+        language = None
+        if len(self.__header) >= 1:
+            language = self.__header[0]
+        return language
+
+    @property
+    def modifier(self):
+        modifier = None
+        if len(self.__header) >= 2:
+            modifier = self.__header[1]
+        return modifier
+
+    # Process
+
+    def process(self, document):
+        for plugin in document.plugins:
+            plugin.process_snippet(self)
