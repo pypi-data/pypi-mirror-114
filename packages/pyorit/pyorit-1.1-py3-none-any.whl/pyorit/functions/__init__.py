597839eeb7b95285eddcff572cc52073ba2cb870
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:12:01 +0000

    Add package pyorit/1.1/pyorit-1.1-py3-none-any.whl

---
+++ packages/pyorit/pyorit-1.1-py3-none-any.whl/pyorit/functions/__init__.py
@@ -0,0 +1,26 @@
+# OrangIT - UserBot
+# Copyright (C) 2021 OrangIT
+#
+# This file is a part of < https://github.com/Projectgeez/OrangIT/ >
+# PLease read the GNU Affero General Public License in
+# <https://www.github.com/OrangIT/OrangIT/blob/main/LICENSE/>.
+
+
+DANGER = [
+    "SESSION",
+    "HEROKU_API",
+    "base64",
+    "bash",
+    "get_me()",
+    "phone",
+    "REDIS_PASSWORD",
+    "load_addons",
+    "load_plugins",
+    "os.system",
+    "sys.stdout",
+    "sys.stderr",
+    "subprocess",
+    "await locals()",
+    "aexec",
+    ".session.save()",
+]
---
+++ packages/pyorit/pyorit-1.1-py3-none-any.whl/pyorit/version.py
@@ -0,0 +1,3 @@
+__version__ = "1.1"
+
+OrangIT_version = "0.0.1"
