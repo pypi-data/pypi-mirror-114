4789f7fd6dc13e8c70cfd624ca1def36bd29223f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:14:09 +0000

    Add package stats-distrib/1.0/stats_distrib-1.0.tar.gz

---
+++ packages/stats-distrib/stats_distrib-1.0.tar.gz/stats_distrib-1.0/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='stats_distrib',
+      version='1.0',
+      description='Gaussian & Binomial distributions implementation in Python',
+      packages=['stats_distrib'],
+      zip_safe=False)
