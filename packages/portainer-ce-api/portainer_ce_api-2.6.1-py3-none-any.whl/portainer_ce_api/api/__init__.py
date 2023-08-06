1f7524f5f9396f5d7bbb7d02bc279b1abb4da4f0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:52:47 +0000

    Add package portainer-ce-api/2.6.1/portainer_ce_api-2.6.1-py3-none-any.whl

---
+++ packages/portainer-ce-api/portainer_ce_api-2.6.1-py3-none-any.whl/portainer_ce_api/api/__init__.py
@@ -0,0 +1,32 @@
+from __future__ import absolute_import
+
+# flake8: noqa
+
+# import apis into api package
+from portainer_ce_api.api.default_api import DefaultApi
+from portainer_ce_api.api.auth_api import AuthApi
+from portainer_ce_api.api.backup_api import BackupApi
+from portainer_ce_api.api.custom_templates_api import CustomTemplatesApi
+from portainer_ce_api.api.dockerhub_api import DockerhubApi
+from portainer_ce_api.api.edge_api import EdgeApi
+from portainer_ce_api.api.edge_groups_api import EdgeGroupsApi
+from portainer_ce_api.api.edge_jobs_api import EdgeJobsApi
+from portainer_ce_api.api.edge_stacks_api import EdgeStacksApi
+from portainer_ce_api.api.edge_templates_api import EdgeTemplatesApi
+from portainer_ce_api.api.endpoint_groups_api import EndpointGroupsApi
+from portainer_ce_api.api.endpoints_api import EndpointsApi
+from portainer_ce_api.api.motd_api import MotdApi
+from portainer_ce_api.api.registries_api import RegistriesApi
+from portainer_ce_api.api.resource_controls_api import ResourceControlsApi
+from portainer_ce_api.api.roles_api import RolesApi
+from portainer_ce_api.api.settings_api import SettingsApi
+from portainer_ce_api.api.stacks_api import StacksApi
+from portainer_ce_api.api.status_api import StatusApi
+from portainer_ce_api.api.tags_api import TagsApi
+from portainer_ce_api.api.team_memberships_api import TeamMembershipsApi
+from portainer_ce_api.api.teams_api import TeamsApi
+from portainer_ce_api.api.templates_api import TemplatesApi
+from portainer_ce_api.api.upload_api import UploadApi
+from portainer_ce_api.api.users_api import UsersApi
+from portainer_ce_api.api.webhooks_api import WebhooksApi
+from portainer_ce_api.api.websocket_api import WebsocketApi
