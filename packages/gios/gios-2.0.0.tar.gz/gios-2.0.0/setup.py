97a15b1a374378ad949c6edc4c8c7953e5968514
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:10:52 +0000

    Add package gios/2.0.0/gios-2.0.0.tar.gz

---
+++ packages/gios/gios-2.0.0.tar.gz/gios-2.0.0/setup.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+from setuptools import setup
+
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="gios",
+    version="2.0.0",
+    author="Maciej Bieniek",
+    description="Python wrapper for getting air quality data from GIOŚ servers.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    include_package_data=True,
+    url="https://github.com/bieniu/gios",
+    license="Apache-2.0 License",
+    packages=["gios"],
+    package_data={"gios": ["py.typed"]},
+    python_requires=">=3.6",
+    install_requires=list(val.strip() for val in open("requirements.txt")),
+    classifiers=[
+        "License :: OSI Approved :: Apache Software License",
+        "Programming Language :: Python",
+        "Operating System :: OS Independent",
+    ],
+    setup_requires=("pytest-runner"),
+    tests_require=list(val.strip() for val in open("requirements-test.txt")),
+)
