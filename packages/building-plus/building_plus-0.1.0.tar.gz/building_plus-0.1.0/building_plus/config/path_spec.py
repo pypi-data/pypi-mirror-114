6aa8434c24df84b272f913a07857257750231b62
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:14:40 +0000

    Add package building-plus/0.1.0/building_plus-0.1.0.tar.gz

---
+++ packages/building-plus/building_plus-0.1.0.tar.gz/building_plus-0.1.0/building_plus/config/path_spec.py
@@ -0,0 +1,27 @@
+import pathlib
+
+from .user import USER_DIR
+
+
+# Suffixes.
+CSV_SUFFIX = '.csv'
+EPW_SUFFIX = '.epw'
+HDF5_SUFFIX = '.h5'
+IDF_SUFFIX = '.idf'
+EIO_SUFFIX = '.eio'
+
+
+# Paths.
+TLD_ABSPATH = pathlib.Path(__file__).parents[2]
+DEMO_FILES_DIR = TLD_ABSPATH / 'building' / 'demo_files'
+USER_DIR_PATH = pathlib.Path(USER_DIR).expanduser()
+USER_DIR_DATA_RETRIEVAL = USER_DIR_PATH / 'data_retrieval'
+USER_DIR_EPLUS = USER_DIR_PATH / 'eplus_validation'
+USER_DIR_BUILDINGS = USER_DIR_PATH / 'eplus_buildings'
+USER_DIR_ENERGYPLUS_WEATHER = USER_DIR_PATH / 'eplus_weather'
+AUTO_CREATED_DIRS = [
+    USER_DIR_DATA_RETRIEVAL,
+    USER_DIR_EPLUS,
+    USER_DIR_BUILDINGS,
+    USER_DIR_ENERGYPLUS_WEATHER,
+]
