84e323ace836356c335637e9b90ff6f680dfec9b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:17:12 +0000

    Add package yhqylsh/0.0.1/yhqylsh-0.0.1.tar.gz

---
+++ packages/yhqylsh/yhqylsh-0.0.1.tar.gz/yhqylsh-0.0.1/setup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+from setuptools import setup
+
+setup(
+    name='yhqylsh',
+    version='0.0.1',
+    author='chuantongmeide',
+    author_email='dick@foxmail.com',
+    # url='xine.github.com',
+    description=u'helloKitty',
+    packages=['yhqylsh'],
+    install_requires=[],
+    entry_points={
+    }
+)
