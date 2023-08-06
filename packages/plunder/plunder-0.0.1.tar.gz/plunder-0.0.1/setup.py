a7305496a963db4f80e84152f8057ff88b0f4eb2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:27:27 +0000

    Add package plunder/0.0.1/plunder-0.0.1.tar.gz

---
+++ packages/plunder/plunder-0.0.1.tar.gz/plunder-0.0.1/setup.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+from distutils.core import setup
+
+setup(
+    name="plunder",
+    version="0.0.1",
+    description="",
+    long_description="",
+    url="",
+    author="Kevin Johnson",
+    author_email="appliedmathkj@gmail.com",
+    packages=[],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Programming Language :: Python :: 3.9",
+        "Operating System :: OS Independent",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+        "Intended Audience :: Developers",
+    ],
+)
