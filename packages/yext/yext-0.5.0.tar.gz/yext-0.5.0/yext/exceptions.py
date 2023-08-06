0dcf2824e73ac27fa38f2f0298c56672edf75db1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:14:29 +0000

    Add package yext/0.5.0/yext-0.5.0.tar.gz

---
+++ packages/yext/yext-0.5.0.tar.gz/yext-0.5.0/yext/exceptions.py
@@ -0,0 +1,10 @@
+class YextException(Exception):
+    pass
+
+class RequestException(YextException):
+
+    def __init__(self, message, status, codes):
+        super(YextException, self).__init__(message)
+        self.message = message
+        self.status = status
+        self.codes = codes
