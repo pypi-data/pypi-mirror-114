a940d46db9810acc415857e3993c3fd8fd7a24cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:40:16 +0000

    Add package algorythm/1.0.1/algorythm-1.0.1-py3-none-any.whl

---
+++ packages/algorythm/algorythm-1.0.1-py3-none-any.whl/algorythm/backend.py
@@ -0,0 +1,88 @@
+import pyaudio
+import numpy as np
+from scipy.fft import rfft, rfftfreq
+import time
+
+FORMAT = pyaudio.paFloat32 # use float to restrict values between (0, 1)
+CHANNELS = 1 # use 1 channel for now, can try to visualize stereo later
+RATE = 48000 # Audio sample rate, shouldn't need to be adjusted
+chunk = 1024 # lower number is less latency, higher number improves performance
+
+KEEP_FRAMES = 100 #number of frames to keep in the list (mostly just need most recent frame)
+
+recent_frames = []
+last_freqs = None
+last_levels = None
+stream = None
+
+p = pyaudio.PyAudio()
+
+def callback(in_data, frame_count, time_info, status):
+    global recent_frames, last_freqs, last_levels
+    
+    numpydata = np.frombuffer(in_data, dtype=np.float32)
+    
+    yf = np.abs(rfft(numpydata))
+    xf = rfftfreq(chunk, 1 / RATE)
+
+    #normalization of fft values - other methods could be investigated
+    yl = 1.0 / (chunk / 3) * yf
+    
+    bins = list(zip(xf, yl))
+    recent_frames.append(bins)
+    recent_frames = recent_frames[-1 * KEEP_FRAMES:]
+    
+    last_freqs = xf[:(chunk+1)//3]
+    last_levels = yl[:(chunk+1)//3]
+    
+    return (yl,pyaudio.paContinue)
+
+def start_stream(settings):
+    global stream,p,chunk
+    chunk = settings.b_count * 3 - 1
+
+    found_stereo = False
+    for i in range(p.get_device_count()):
+        dev = p.get_device_info_by_index(i)
+        if ('Stereo Mix' in dev['name'] and dev['hostApi'] == 3):
+            dev_index = dev['index']
+            found_stereo = True
+    if not found_stereo:
+        dev_index = p.get_default_input_device_info()['index']
+        print("Unable to find Stereo Mix device, using default Microphone instead.")
+
+    stream = p.open(format = FORMAT,
+                    channels = CHANNELS,
+                    rate = RATE,
+                    input = True,
+                    input_device_index = dev_index,
+                    frames_per_buffer = chunk,
+                    stream_callback=callback)
+
+    stream.start_stream()
+
+def stop_stream():
+    stream.stop_stream()
+    stream.close()
+    p.terminate()
+
+def restart_stream(settings):
+    # clear and remake stream to update chunk
+    global recent_frames, last_freqs, last_levels
+    stream.stop_stream()
+    stream.close()
+
+    recent_frames = []
+    last_freqs = None
+    last_levels = None
+
+    start_stream(settings)
+
+if __name__ == '__main__':
+    start_stream()
+    while stream.is_active():
+        if len(recent_frames) > 0:
+            print(recent_frames[-1][:5])
+
+        time.sleep(0.1)
+    stop_stream()
---
+++ packages/algorythm/algorythm-1.0.1-py3-none-any.whl/algorythm/collect_media_info.py
@@ -0,0 +1,91 @@
+import asyncio
+from PIL import Image
+import requests
+import tempfile
+from io import BytesIO
+import binascii
+import numpy as np
+from scipy import cluster
+import algorythm.spotipy_implementation as sp
+
+
+async def winrtapi():
+    import winrt
+    # Song info    
+    from winrt.windows.media.control import \
+        CurrentSessionChangedEventArgs, GlobalSystemMediaTransportControlsSessionManager as MediaManager
+
+    sessions = await MediaManager.request_async()
+
+    curr_session = sessions.get_current_session()
+    if curr_session:
+        return await curr_session.try_get_media_properties_async()
+    else:
+        return None
+
+def collect_title_artist():
+    info = asyncio.run(winrtapi())
+
+    if info is not None:
+        info_dict = {song_attr: info.__getattribute__(song_attr) for song_attr in dir(info) if song_attr[0] != '_'}
+
+        info_dict['genres'] = list(info_dict['genres'])
+
+        title_artist = [info_dict['title'], info_dict['artist']]
+        return title_artist
+    else:
+        return ["N/A", "N/A"]
+
+def get_background_img(img_url):
+    buffer = tempfile.SpooledTemporaryFile(max_size=1e9)
+    r = requests.get(img_url, stream=True)
+    if r.status_code == 200:
+        downloaded = 0
+        for chunk in r.iter_content(chunk_size=1024):
+            downloaded += len(chunk)
+            buffer.write(chunk)
+        buffer.seek(0)
+        i = Image.open(BytesIO(buffer.read()))
+    buffer.close()
+    return i
+
+def generate_colors_from_img(img, num_colors):
+    NUM_CLUSTERS = 5
+
+    rgb_img = img.convert('RGB')
+    ar = np.asarray(rgb_img)
+    shape = ar.shape
+    ar = ar.reshape(np.product(shape[:2]), shape[2]).astype(float)
+    codes, dist = cluster.vq.kmeans(ar, NUM_CLUSTERS)
+    vecs, dist = cluster.vq.vq(ar, codes)         # assign codes
+    counts, bins = np.histogram(vecs, len(codes))    # count occurrences
+    num_colors = len(counts) if len(counts) < num_colors else num_colors
+    
+    max_indeces = np.argpartition(counts, -1 * num_colors)[-1 * num_colors:]
+    colors = [binascii.hexlify(bytearray(int(c) for c in codes[i])).decode('ascii') for i in max_indeces]
+    return colors
+
+def generate_colors(count=0):
+    curr_media_info = collect_title_artist()
+
+    err = {'time_per_beat':1, 'colors':None, 'album_art':None}
+    # Check if no currently playing track was found
+    if curr_media_info == ["N/A", "N/A"] or '' in curr_media_info:
+        return err
+
+    try:
+        track_id = sp.search_for_id(*curr_media_info)
+        track_img_url = sp.get_album_art(track_id)
+        pil_img = get_background_img(track_img_url)
+        features = sp.get_audio_features(track_id)
+        tempo = float(features['track']['tempo'])
+        time_per_beat = 60.0 / tempo # in sec
+        time_sig = features['track']['time_signature']
+        count = time_sig if count == 0 else count
+        colors = generate_colors_from_img(pil_img, count)
+        return {'time_per_beat':time_per_beat*time_sig, 'colors':colors, 'album_art':pil_img}
+    except:
+        return err
+  
+if __name__ == '__main__':
+    print(generate_colors())
