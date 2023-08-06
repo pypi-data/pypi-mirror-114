75e88aa384d66ab4ca31437513eb4955e7818199
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:19:49 +0000

    Add package ConferenceCorpus/0.0.4/ConferenceCorpus-0.0.4.tar.gz

---
+++ packages/ConferenceCorpus/ConferenceCorpus-0.0.4.tar.gz/ConferenceCorpus-0.0.4/corpus/event.py
@@ -0,0 +1,103 @@
+'''
+Created on 26.07.2021
+
+@author: wf
+'''
+from lodstorage.entity import EntityManager
+from lodstorage.jsonable import JSONAble,JSONAbleList
+from lodstorage.lod import LOD
+from lodstorage.storageconfig import StorageConfig
+
+class Event(JSONAble):
+    '''
+    base class for Event entities
+    '''
+    def __init__(self):
+        '''
+        Constructor
+        '''
+        
+
+    def __str__(self):
+        '''
+        return my
+        '''
+        text=self.__class__.__name__
+        attrs=["pageTitle","acronym","eventId","title","year","source","url"]
+        delim=":"
+        for attr in attrs:
+            if hasattr(self, attr):
+                value=getattr(self,attr)
+                text+=f"{delim}{value}"
+                delim=":" 
+        return text
+    
+class EventSeries(JSONAble):
+    '''
+    base class for Event Series entities
+    '''
+    def __init__(self):
+        '''
+        Constructor
+        '''
+        pass
+    
+class EventSeriesManager(EntityManager):
+    '''
+    Event series list
+    '''
+    def __init__(self,name:str,clazz=None,tableName:str=None,primaryKey:str=None,config:StorageConfig=None,debug=False):
+        '''
+        constructor 
+        '''
+        super(EventSeriesManager, self).__init__(name=name,entityName="EventSeries",entityPluralName="EventSeries",primaryKey=primaryKey,listName="series",clazz=clazz,tableName=tableName,config=config,debug=debug)
+            
+class EventManager(EntityManager,JSONAbleList):
+    '''
+    Event entity list
+    '''
+    
+    def __init__(self,name:str,clazz=None,tableName:str=None,primaryKey:str=None,config:StorageConfig=None,debug=False):
+        '''
+        constructor 
+        '''
+        super(EventManager, self).__init__(name=name,entityName="Event",entityPluralName="Events",primaryKey=primaryKey,listName="events",clazz=clazz,tableName=tableName,config=config,debug=debug)
+
+    def linkSeriesAndEvent(self, eventSeriesManager:EventSeriesManager, seriesKey:str="series"):
+        '''
+        link Series and Event using the given foreignKey
+
+        Args:
+            seriesKey(str): the key to be use for lookup
+            eventSeriesManager(EventSeriesManager):
+        '''
+        # get foreign key hashtable
+        self.seriesLookup = LOD.getLookup(self.getList(), seriesKey, withDuplicates=True)
+        # get "primary" key hashtable
+        self.seriesAcronymLookup = LOD.getLookup(eventSeriesManager.getList(), "acronym", withDuplicates=True)
+
+        for seriesAcronym in self.seriesLookup.keys():
+            if seriesAcronym in self.seriesAcronymLookup:
+                seriesEvents = self.seriesLookup[seriesAcronym]
+                if self.verbose:
+                    print(f"{seriesAcronym}:{len(seriesEvents):4d}")
+            else:
+                if self.debug:
+                    print(f"Event Series Acronym {seriesAcronym} lookup failed")
+        if self.debug:
+            print("%d events/%d eventSeries -> %d linked" % (
+            len(self.getList()), len(eventSeriesManager.getList()), len(self.seriesLookup)))
+            
+    def getEventsInSeries(self,seriesAcronym):
+        """
+        Return all the events in a given series.
+        """
+        if seriesAcronym in self.seriesAcronymLookup:
+            seriesEvents = self.seriesLookup[seriesAcronym]
+            if self.debug:
+                print(f"{seriesAcronym}:{len(seriesEvents):4d}")
+        else:
+            if self.debug:
+                print(f"Event Series Acronym {seriesAcronym} lookup failed")
+            return None
+        return seriesEvents
