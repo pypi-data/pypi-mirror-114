59cfba604d0e4ac3ca540be7072d96156fd13a90
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:30:54 +0000

    Add package pyK8sManager/0.2.3/pyK8sManager-0.2.3.tar.gz

---
+++ packages/pyK8sManager/pyK8sManager-0.2.3.tar.gz/pyK8sManager-0.2.3/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.2.3' 
+DESCRIPTION = 'pyK8sManager'
+LONG_DESCRIPTION = 'pyK8sManager'
+
+setup(
+        name="pyK8sManager", 
+        version=VERSION,
+        author="Lucian Tin Udovicic",
+        author_email="luciantin@protonmail.com",
+        description=DESCRIPTION,
+        long_description=LONG_DESCRIPTION,
+        packages=find_packages(),
+        install_requires=["kubernetes"], 
+        
+        keywords=['python', 'first package'],
+        classifiers= [
+            "Development Status :: 3 - Alpha",
+            "Intended Audience :: Education",
+            "Programming Language :: Python :: 3",
+        ]
+)
