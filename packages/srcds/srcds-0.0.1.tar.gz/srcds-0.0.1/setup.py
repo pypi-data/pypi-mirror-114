77d9722aaa88436617c2e0f04927663c9517b286
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:11:47 +0000

    Add package srcds/0.0.1/srcds-0.0.1.tar.gz

---
+++ packages/srcds/srcds-0.0.1.tar.gz/srcds-0.0.1/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(
+    name="srcds",
+    version="0.0.1",
+    description="Stub package",
+    long_description="Stub package",
+    long_description_content_type="text/markdown",
+    author="Gabe Van Engel",
+    author_email="pypi@schizoid.net",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.9",
+    ],
+    packages=["src"],
+)
