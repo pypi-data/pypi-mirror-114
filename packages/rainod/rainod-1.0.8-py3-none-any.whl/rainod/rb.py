43e0d1c0e94c84239557126e0a96efaebfed7b96
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:42:35 +0000

    Add package rainod/1.0.8/rainod-1.0.8-py3-none-any.whl

---
+++ packages/rainod/rainod-1.0.8-py3-none-any.whl/rainod/rb.py
@@ -0,0 +1,51 @@
+import pandas as pd
+import numpy as np
+from haversine import haversine
+
+def test():
+    print("곽운일, 심명진, 김도영, 편도준")
+
+def test2():
+    t = pd.DataFrame(data=[1,2,3])
+    display(t)
+
+##########################################################csv변환
+def t_csv(x,y,z):
+    if y==40:
+        x.to_csv('land40_{}.csv'.format(z),encoding='cp949')
+    if y==4070:
+        x.to_csv('land40_70_{}.csv'.format(z),encoding='cp949')
+    if y==70:
+        x.to_csv('land70_{}.csv'.format(z),encoding='cp949')
+
+############################################################거리계산
+def make_distance(x,y,name):
+    df = pd.DataFrame()
+    
+    for i in range(len(x)):
+        for j in range(len(y)):
+            distance = haversine(x.loc[:,['위도','경도']].values[i],
+                                 y.loc[:,['위도','경도']].values[j],
+                                 unit = 'km')
+            if distance <= 1:
+                new_data ={'아파트' : x['주소'].values[i],
+                           '거리' : distance}
+                df = df.append(new_data,ignore_index=True)
+                
+    df = df.groupby(['아파트']).agg(['count', 'mean']).reset_index()
+    df.columns=['주소', name+' 개수', name+' 평균거리']
+    apt_df = pd.merge(x, df, left_on = '주소', right_on = '주소', how = 'left')
+    return apt_df
+
+###########################################################맵 마커 
+def cir_marker(x,y,name):
+    df2 = pd.DataFrame()
+    for i in range(len(x)):
+        for j in range(len(y)):
+            distance = haversine(x.loc[:,['위도','경도']].values[i],
+                                 y.loc[:,['위도','경도']].values[j],
+                                 unit = 'km')
+            if distance <= 1:
+                data ={str(name)+'위도' : y['위도'].values[j],str(name)+'경도': y['경도'].values[j]}
+                df2 = df2.append(data,ignore_index=True)
+    return df2
