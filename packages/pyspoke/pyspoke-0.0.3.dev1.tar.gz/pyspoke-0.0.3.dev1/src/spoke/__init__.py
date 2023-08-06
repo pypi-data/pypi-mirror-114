71e6c6f5ad5e400c715bf6212c99c4765f4f3437
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:51:35 +0000

    Add package pyspoke/0.0.3.dev1/pyspoke-0.0.3.dev1.tar.gz

---
+++ packages/pyspoke/pyspoke-0.0.3.dev1.tar.gz/pyspoke-0.0.3.dev1/src/spoke/__init__.py
@@ -0,0 +1,7 @@
+from . import connection
+from . import genid
+from . import message
+from . import pubsub
+from .simple import publish, call
+from .wait import wait
+from .version import __version__
