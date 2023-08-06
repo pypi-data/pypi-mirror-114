d5a349b68eeb021ef300ca8947fc6c7e03b3e271
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:16:53 +0000

    Add package aeolus/0.4.12/aeolus-0.4.12-py3-none-any.whl

---
+++ packages/aeolus/aeolus-0.4.12-py3-none-any.whl/aeolus/plot/mpl.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+"""Matplotlib-related utilities."""
+from matplotlib.lines import Line2D
+
+__all__ = ("add_custom_legend",)
+
+
+def add_custom_legend(ax, styles_and_labels, **leg_kw):
+    """
+    Add a custom legend to matplotlib axes.
+
+    Parameters
+    ----------
+    ax: matplotlib.axes._subplots.AxesSubplot
+        Axes where to put the legend.
+    styles_and_labels: dict
+        Dictionary with labels as keys and a dictionary of plot
+        keywords as values.
+    leg_kw: dict, optional
+        Keyword arguments passed to `legend()` function.
+
+    Example
+    -------
+    >>> import matplotlib.pyplot as plt
+    >>> ax = plt.axes()
+    >>> my_dict = dict(foo=dict(color='C0', marker="X"),
+                       bar=dict(color='C1', marker="o"))
+    >>> add_custom_legend(ax, my_dict, loc=2, title="blah")
+
+    """
+    lines = [Line2D([0], [0], **style) for style in styles_and_labels.values()]
+    leg = ax.legend(lines, styles_and_labels.keys(), **leg_kw)
+    if ax.legend_ is not None:
+        ax.add_artist(leg)
