02eb2e3c386400fcb2bfb6253c58e83fc680aba1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:12:46 +0000

    Add package ml-tracking-api/1.2.4/ml_tracking_api-1.2.4-py3-none-any.whl

---
+++ packages/ml-tracking-api/ml_tracking_api-1.2.4-py3-none-any.whl/ml_tracking/models/__init__.py
@@ -0,0 +1,19 @@
+# flake8: noqa
+
+# import all models into this package
+# if you have many models here with many references from one model to another this may
+# raise a RecursionError
+# to avoid this, import only the models that you directly need like:
+# from from ml_tracking.model.pet import Pet
+# or import this package, but before doing it, use:
+# import sys
+# sys.setrecursionlimit(n)
+
+from ml_tracking.model.create_data_row_command import CreateDataRowCommand
+from ml_tracking.model.data_row_dto import DataRowDto
+from ml_tracking.model.data_row_dto_paginated_list import DataRowDtoPaginatedList
+from ml_tracking.model.epoch_update_command import EpochUpdateCommand
+from ml_tracking.model.hosted_service_dto import HostedServiceDto
+from ml_tracking.model.iteration_update_command import IterationUpdateCommand
+from ml_tracking.model.register_model_run_command import RegisterModelRunCommand
+from ml_tracking.model.update_data_row_command import UpdateDataRowCommand
