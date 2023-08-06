26dcfc118b1e155c06b648fdbb14567ce34d9ff4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:30:41 +0000

    Add package Xplique/0.2.0/Xplique-0.2.0.tar.gz

---
+++ packages/Xplique/Xplique-0.2.0.tar.gz/Xplique-0.2.0/xplique/__init__.py
@@ -0,0 +1,14 @@
+"""
+Xplique
+-------
+
+The goal of Xplique is to provide a simple interface to the latest explanation
+techniques
+"""
+
+__version__ = '0.2.0'
+
+from . import attributions
+from . import concepts
+from . import features_visualizations
+from . import commons
