b4143274772fa2ea6fd080288912b98e68d18416
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:42:23 +0000

    Add package lightningmcqueen/1.0.1/lightningmcqueen-1.0.1.tar.gz

---
+++ packages/lightningmcqueen/lightningmcqueen-1.0.1.tar.gz/lightningmcqueen-1.0.1/README.md
@@ -0,0 +1,2 @@
+Announcing LightningMcQueen: the fastest Python development experience in the galaxy.
+For the first time in the history of the galaxy, you can build a hosted app with data persistence and user auth in just 1 line of code!
---
+++ packages/lightningmcqueen/lightningmcqueen-1.0.1.tar.gz/lightningmcqueen-1.0.1/setup.py
@@ -0,0 +1,22 @@
+import pathlib
+from setuptools import setup
+
+# The directory containing this file
+HERE = pathlib.Path(__file__).parent
+
+# The text of the README file
+README = (HERE / "README.md").read_text()
+
+# This call to setup() does all the work
+setup(
+    name="lightningmcqueen",
+    version="1.0.1",
+    description="The fastest Python development experience in the galaxy",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url="https://replit.com/@notjustinshaw/lightningmcqueen",
+    author="Justin Shaw",
+    author_email="realjustinshaw@gmail.com",
+    license="MIT",
+    packages=["lightningmcqueen"],
+)
