520d523008eea8324d8f6f736f0a26938f834c0c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:41:35 +0000

    Add package ailever/0.2.316/ailever-0.2.316-py3-none-any.whl

---
+++ packages/ailever/ailever-0.2.316-py3-none-any.whl/ailever/investment/parallelizer.py
@@ -0,0 +1,41 @@
+from ..path import refine
+
+from datetime import datetime
+import os
+import re
+import numpy as np
+import pandas as pd
+
+def parallelize(path='.', object_format='csv'):
+    prllz = Parallelizer(path=refine(path), object_format=object_format)
+    return prllz
+
+class Parallelizer:
+    def __init__(self, path, object_format, truncate=100):
+        self.origin_path = os.getcwd()
+        self.serialization_path = path
+        self.truncated_period = truncate
+        self.ndarray = getattr(self, '_'+object_format)(to='ndarray')
+        self.pdframe = getattr(self, '_'+object_format)(to='pdframe')
+        
+    def _csv(self, to):
+        serialized_objects = os.listdir(self.serialization_path)
+        ticker_names = list(map(lambda x: x[:-re.search('[.]', x[::-1]).span()[1]], serialized_objects))
+        
+        so_path = os.path.join(self.serialization_path, serialized_objects.pop(0))
+        base_init_frame = pd.read_csv(so_path)
+        base_period = base_init_frame['date'].values[-self.truncated_period:]
+        base_array = base_init_frame['close'].values[-self.truncated_period:]
+
+        for so in serialized_objects:
+            so_path = os.path.join(self.serialization_path, so)
+            base_array = np.c_[base_array, pd.read_csv(so_path)['close'].values[-self.truncated_period:]]
+
+        if to == 'ndarray':
+            return base_array
+        elif to == 'pdframe':
+            base_frame = pd.DataFrame(data=base_array, columns=ticker_names)
+            base_frame.insert(0, 'date', base_period)
+            return base_frame.set_index('date')
+
+    
