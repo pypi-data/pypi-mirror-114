103a7e297fb54864503afc0fa840ff27995d78c0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:42:24 +0000

    Add package lethean-vpn/1.0.0/lethean_vpn-1.0.0-py3-none-any.whl

---
+++ packages/lethean-vpn/lethean_vpn-1.0.0-py3-none-any.whl/lethean_vpn/__init__.py
@@ -0,0 +1,11 @@
+# flake8: noqa
+
+"""
+    Lethean VPN
+
+    Distributed Virtual Private Marketplace  # noqa: E501
+    Contact: contact@lethean.io
+"""
+
+
+__version__ = "1.0.0"
---
+++ packages/lethean-vpn/lethean_vpn-1.0.0-py3-none-any.whl/lethean_vpn/lib/__init__.py
@@ -0,0 +1,9 @@
+# flake8: noqa
+
+"""
+    Lethean VPN
+
+    Distributed Virtual Private Marketplace  # noqa: E501
+    Contact: contact@lethean.io
+"""
+
