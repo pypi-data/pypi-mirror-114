c7c1d82f2adf4e5d3fe3e1c7be4101618ee496f3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:25:25 +0000

    Add package CellDetection/0.2.2/CellDetection-0.2.2-py3-none-any.whl

---
+++ packages/CellDetection/CellDetection-0.2.2-py3-none-any.whl/celldetection/data/misc.py
@@ -0,0 +1,154 @@
+import numpy as np
+import torch
+from collections import OrderedDict
+from skimage import img_as_ubyte
+
+
+def transpose_spatial(inputs: np.ndarray, inputs_channels_last=True, spatial_dims=2, has_batch=False):
+    if spatial_dims == 0:
+        return inputs
+    has_batch = bool(has_batch)
+    a = ([0] * has_batch)
+    if inputs_channels_last:
+        # e. g. (0, 3, 1, 2)
+        b = list(range(spatial_dims + has_batch, inputs.ndim))  # n channels
+        c = list(range(has_batch, spatial_dims + has_batch))  # spatial dims
+    else:
+        # e.g. (0, 2, 3, 1)
+        b = list(range(inputs.ndim - spatial_dims, inputs.ndim))  # spatial dims
+        c = list(range(has_batch, inputs.ndim - spatial_dims))  # n channels
+    return np.transpose(inputs, a + b + c)
+
+
+def channels_last2channels_first(inputs: np.ndarray, spatial_dims=2, has_batch=False) -> np.ndarray:
+    """Channels last to channels first.
+
+    Args:
+        inputs: Input array.
+        spatial_dims: Number of spatial dimensions.
+        has_batch: Whether inputs has a batch dimension.
+
+    Returns:
+        Transposed array.
+    """
+    return transpose_spatial(inputs, inputs_channels_last=True, spatial_dims=spatial_dims, has_batch=has_batch)
+
+
+def channels_first2channels_last(inputs: np.ndarray, spatial_dims=2, has_batch=False) -> np.ndarray:
+    """Channels first to channels last.
+
+    Args:
+        inputs: Input array.
+        spatial_dims: Number of spatial dimensions.
+        has_batch: Whether inputs has a batch dimension.
+
+    Returns:
+        Transposed array.
+    """
+    return transpose_spatial(inputs, inputs_channels_last=False, spatial_dims=spatial_dims, has_batch=has_batch)
+
+
+def to_tensor(inputs: np.ndarray, spatial_dims=2, transpose=False, has_batch=False, dtype=None,
+              device=None) -> torch.Tensor:
+    """Array to Tensor.
+
+    Converts numpy array to Tensor and optionally transposes from channels last to channels first.
+
+    Args:
+        inputs: Input array.
+        transpose: Whether to transpose channels from channels last to channels first.
+        spatial_dims: Number of spatial dimensions.
+        has_batch: Whether inputs has a batch dimension.
+        dtype: Data type of output Tensor.
+        device: Device of output Tensor.
+
+    Returns:
+        Tensor.
+    """
+    return torch.as_tensor(
+        channels_last2channels_first(inputs, spatial_dims=bool(transpose) * spatial_dims, has_batch=has_batch),
+        device=device, dtype=dtype)
+
+
+def universal_dict_collate_fn(batch) -> OrderedDict:
+    results = OrderedDict({})
+    ref = batch[0]
+    for k in ref.keys():
+        if isinstance(ref[k], (list, tuple)):
+            max_dim = np.max([b[k][0].shape[0] for b in batch])
+            results[k] = np.stack(
+                [np.pad(b[k][0], ((0, max_dim - b[k][0].shape[0]),) + ((0, 0),) * (b[k][0].ndim - 1)) for b in batch],
+                axis=0)
+            results[k] = to_tensor(results[k], transpose=False, spatial_dims=0, has_batch=True)
+        else:
+            results[k] = np.stack([b[k] for b in batch], axis=0)
+            results[k] = to_tensor(results[k], transpose=True, spatial_dims=2, has_batch=True)
+    return results
+
+
+def normalize_percentile(image, percentile=99.9, to_uint8=True):
+    low, high = np.percentile(image, (100 - percentile, percentile))
+    img = (np.clip(image, low, high) - low) / (high - low)
+    return img_as_ubyte(img) if to_uint8 else img
+
+
+def random_crop(*arrays, height, width=None):
+    """Random crop.
+
+    Args:
+        *arrays: Input arrays that are to be cropped. None values accepted.
+            The shape of the first element is used as reference.
+        height: Output height.
+        width: Output width. Default is same as height.
+
+    Returns:
+        Cropped array if `arrays` contains a single array; a list of cropped arrays otherwise
+    """
+    if len(arrays) <= 0:
+        return None
+    if width is None:
+        width = height
+    h, w = arrays[0].shape[:2]
+    hh, ww = h - height, w - width
+    a, b = np.random.randint(0, hh) if hh > 0 else 0, np.random.randint(0, ww) if ww > 0 else 0
+    slices = (
+        slice(a, a + height),
+        slice(b, b + width)
+    )
+    results = [(None if v is None else v[slices]) for v in arrays]
+    if len(results) == 1:
+        results, = results
+    return results
+
+
+def rle2mask(code, shape, transpose=True, min_index=1, constant=1) -> np.ndarray:
+    """Run length encoding to mask.
+
+    Convert run length encoding to mask image.
+
+    Args:
+        code: Run length code.
+            As ndarray: array([idx0, len0, idx1, len1, ...]) or array([[idx0, len0], [idx1, len1], ...])
+            As list: [idx0, len0, idx1, len1, ...] or [[idx0, len0], [idx1, len1], ...]
+            As str: 'idx0 len0 idx1 len1 ...'
+        shape: Mask shape.
+        transpose: If True decode row by row, otherwise decode column by column.
+        min_index: Smallest pixel index. Depends on rle encoding.
+        constant: Pixels marked by rle are set to this value.
+
+    Returns:
+        Mask image.
+
+    """
+    image = np.zeros(np.multiply.reduce(shape))
+    code = np.array([int(i) for i in code.split(' ')] if isinstance(code, str) else code).ravel()
+    c0 = code.shape[0]
+    assert c0 % 2 == 0
+    for i in range(0, c0, 2):
+        idx, le = code[i:i + 2]
+        idx -= min_index
+        image[idx:idx + le] = constant
+    image = np.reshape(image, shape)
+    if transpose:
+        image = image.T
+    return image
---
+++ packages/CellDetection/CellDetection-0.2.2-py3-none-any.whl/celldetection/util/util.py
@@ -0,0 +1,266 @@
+import numpy as np
+import inspect
+import torch
+import torch.nn as nn
+from typing import Union, List, Tuple, Any, Dict as TDict
+from torch import Tensor
+from torchvision.models.utils import load_state_dict_from_url
+import hashlib
+import json
+from tqdm import tqdm
+from os.path import join
+from os import makedirs
+
+__all__ = ['Dict', 'lookup_nn', 'reduce_loss_dict', 'to_device', 'asnumpy', 'fetch_model', 'random_code_name',
+           'dict_hash', 'fetch_image', 'random_seed', 'tweak_module_', 'add_to_loss_dict',
+           'random_code_name_dir']
+
+
+class Dict(dict):
+    __getattr__ = dict.__getitem__  # alternative: dict.get if KeyError is not desired
+    __delattr__ = dict.__delitem__
+    __setattr__ = dict.__setitem__
+
+    def __init__(self, **kwargs):
+        super().__init__(kwargs)
+
+
+def lookup_nn(item: str, *a, src=nn, call=True, inplace=True, **kw):
+    """
+
+    Examples:
+        ```
+        >>> lookup_nn('batchnorm2d', 32)
+            BatchNorm2d(32, eps=1e-05, momentum=0.1, affine=True, track_running_stats=True)
+        >>> lookup_nn(torch.nn.BatchNorm2d, 32)
+            BatchNorm2d(32, eps=1e-05, momentum=0.1, affine=True, track_running_stats=True)
+        >>> lookup_nn('batchnorm2d', num_features=32)
+            BatchNorm2d(32, eps=1e-05, momentum=0.1, affine=True, track_running_stats=True)
+        >>> lookup_nn('tanh')
+            Tanh()
+        >>> lookup_nn('tanh', call=False)
+            torch.nn.modules.activation.Tanh
+        >>> lookup_nn('relu')
+            ReLU(inplace=True)
+        >>> lookup_nn('relu', inplace=False)
+            ReLU()
+        ```
+
+    Args:
+        item: Lookup item. None is equivalent to `identity`.
+        *a: Arguments passed to item if called.
+        src: Lookup source.
+        call: Whether to call item.
+        inplace: Default setting for items that take an `inplace` argument when called.
+            As default is True, `lookup_nn('relu')` returns a ReLu instance with `inplace=True`.
+        **kw:
+
+    Returns:
+        Looked up item.
+    """
+    if item is None:
+        v = nn.Identity
+    elif isinstance(item, str):
+        l_item = item.lower()
+        v = next((getattr(src, i) for i in dir(src) if i.lower() == l_item))
+    elif isinstance(item, nn.Module):
+        return item
+    else:
+        v = item
+    if call:
+        kwargs = {'inplace': inplace} if 'inplace' in inspect.getfullargspec(v).args else {}
+        kwargs.update(kw)
+        v = v(*a, **kw)
+    return v
+
+
+def reduce_loss_dict(losses: dict, divisor):
+    return sum((i for i in losses.values() if i is not None)) / divisor
+
+
+def add_to_loss_dict(d: dict, key: str, loss: torch.Tensor, weight=None):
+    dk = d[key]
+    torch.nan_to_num_(loss, 0., 0., 0.)
+    if weight is not None:
+        loss = loss * weight
+    d[key] = loss if dk is None else dk + loss
+
+
+def to_device(batch: Union[list, tuple, dict, Tensor], device):
+    if isinstance(batch, Tensor):
+        batch = batch.to(device)
+    elif isinstance(batch, dict):
+        batch = {k: to_device(b, device) for k, b in batch.items()}
+    elif isinstance(batch, (list, tuple)):
+        batch = type(batch)([to_device(b, device) for b in batch])
+    return batch
+
+
+def asnumpy(v):
+    if v is None:
+        return v
+    elif isinstance(v, torch.Tensor):
+        if str(v.device) != 'cpu':
+            v = v.cpu()
+        return v.data.numpy()
+    elif isinstance(v, (np.ndarray, int, float, bool, np.float, np.int, np.bool)):
+        return v
+    elif isinstance(v, (tuple, list)):
+        return [asnumpy(val) for val in v]
+    elif isinstance(v, dict):
+        r = dict()
+        for k, val in v.items():
+            r[k] = asnumpy(val)
+        return r
+    else:
+        raise ValueError(f'Type not supported: {type(v)}')
+
+
+def fetch_model(name, map_location=None, **kwargs):
+    """Fetch model.
+
+    Loads model or state dict from url.
+
+    Args:
+        name: Model name hosted on `celldetection.org` or url. Urls must start with 'http'.
+        map_location: A function, `torch.device`, string or a dict specifying how to remap storage locations.
+        **kwargs: From the doc of `torch.models.utils.load_state_dict_from_url`.
+
+    """
+    url = name if name.startswith('http') else f'https://celldetection.org/torch/models/{name}.pt'
+    return load_state_dict_from_url(url, map_location=map_location, **kwargs)
+
+
+def random_code_name(chars=4) -> str:
+    """Random code name.
+
+    Generates random code names that are somewhat pronounceable and memorable.
+
+    Examples:
+        ```
+        >>> from celldetection import util
+        >>> util.random_code_name()
+        kolo
+        >>> util.random_code_name(6)
+        lotexo
+        ```
+
+    Args:
+        chars: Number of characters.
+
+    Returns:
+        String
+    """
+    a, b = [i for i in 'aeiou'], [i for i in 'tskyrhzjgqmxlvnfcpwbd']
+    return ''.join([np.random.choice(b if j % 2 == 0 else a) for j in range(chars)])
+
+
+def random_code_name_dir(directory='./out', chars=6):
+    """Random code name directory.
+
+    Creates random code name and creates a subdirectory with said name under `directory`.
+    Code names that are already taken (subdirectory already exists) are not reused.
+
+    Args:
+        directory: Root directory.
+        chars: Number of characters for the code name.
+
+    Returns:
+        Tuple of code name and created directory.
+    """
+    try:
+        code_name = random_code_name(chars=chars)
+        out_dir = join(directory, code_name)
+        makedirs(out_dir)
+    except FileExistsError:
+        return random_code_name_dir(directory)
+    return code_name, out_dir
+
+
+def dict_hash(dictionary: TDict[str, Any]) -> str:
+    """MD5 hash of a dictionary.
+
+    References:
+        https://www.doc.ic.ac.uk/~nuric/coding/how-to-hash-a-dictionary-in-python.html
+
+    Args:
+        dictionary: A dictionary.
+
+    Returns:
+        Md5 hash of the dictionary as string.
+    """
+    dhash = hashlib.md5()
+    dhash.update(json.dumps(dictionary, sort_keys=True).encode())
+    return dhash.hexdigest()
+
+
+def fetch_image(url, numpy=True):
+    """Fetch image from URL.
+
+    Args:
+        url: URL
+        numpy: Whether to convert PIL Image to numpy array.
+
+    Returns:
+        PIL Image or numpy array.
+    """
+    import requests
+    from PIL import Image
+    img = Image.open(requests.get(url, stream=True).raw)
+    return np.asarray(img) if numpy else img
+
+
+def random_seed(seed, backends=False, deterministic_torch=True):
+    """Set random seed.
+
+    References:
+        https://pytorch.org/docs/stable/notes/randomness.html
+
+    Args:
+        seed: Random seed.
+        backends: Whether to also adapt backends. If set True cuDNN's benchmark feature is disabled. This
+            causes cuDNN to deterministically select an algorithm, possibly at the cost of reduced performance.
+            Also the selected algorithm is set to run deterministically.
+        deterministic_torch: Whether to set PyTorch operations to behave deterministically.
+
+    """
+    from torch import manual_seed
+    from torch.backends import cudnn
+    import random
+    random.seed(seed)
+    manual_seed(seed)
+    np.random.seed(seed)
+    if backends:
+        cudnn.deterministic = True
+        cudnn.benchmark = False
+    if deterministic_torch and 'use_deterministic_algorithms' in dir(torch):
+        torch.use_deterministic_algorithms(True)
+
+
+def tweak_module_(module: nn.Module, class_or_tuple, must_exist=True, **kwargs):
+    """Tweak module.
+
+    Sets attributes for all modules that are instances of given `class_or_tuple`.
+
+    Examples:
+        ```
+        >>> import celldetection as cd, torch.nn as nn
+        >>> model = cd.models.ResNet18(in_channels=3)
+        >>> cd.util.tweak_module_(model, nn.BatchNorm2d, momentum=0.05)  # sets momentum to 0.05
+        ```
+
+    Notes:
+        This is an in-place operation.
+
+    Args:
+        module: PyTorch `Module`.
+        class_or_tuple: All instances of given `class_or_tuple` are to be tweaked.
+        must_exist: If `True` an AttributeError is raised if keywords do not exist.
+        **kwargs: Attributes to be tweaked: `<attribute_name>=<value>`.
+    """
+    for mod in module.modules():
+        if isinstance(mod, class_or_tuple):
+            for k, v in kwargs.items():
+                if must_exist:
+                    getattr(mod, k)
+                setattr(mod, k, v)
