b44f59ca44def365a05c4cf0fe92cdb6e4c694d3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:29:26 +0000

    Add package vscode-ext/1.5.0/vscode-ext-1.5.0.tar.gz

---
+++ packages/vscode-ext/vscode-ext-1.5.0.tar.gz/vscode-ext-1.5.0/vscode/utils.py
@@ -0,0 +1,39 @@
+import json
+from .undef import undefined
+
+
+def uinput():
+    res = input()
+    if res.strip() == "undefined":
+        return undefined
+    else:
+        return res
+
+
+def json_input():
+    res = uinput()
+    if not res:
+        return res
+    try:
+        return json.loads(res)
+    except json.decoder.JSONDecodeError:
+        return res
+
+
+def send_ipc(code, args=None):
+    obj = json.dumps({"code": code, "args": args if args else []})
+    print(f"{obj}", flush=True, end="")
+
+
+def camel_to_snake(text: str) -> str:
+    return "".join(["_" + i.lower() if i.isupper() else i for i in text]).lstrip("_")
+
+
+def apply_func_to_keys(dictionary: dict, func) -> dict:
+    new = {}
+    for key in dictionary:
+        if isinstance(dictionary[key], dict):
+            new[func(key)] = apply_func_to_keys(dictionary[key], func)
+        else:
+            new[func(key)] = dictionary[key]
+    return new
