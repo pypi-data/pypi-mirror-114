c0ee6bc1182b43e95ca5a0bb8fe849bb5ea78947
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:08:34 +0000

    Add package drf_health_check/0.0.8/drf_health_check-0.0.8-py3-none-any.whl

---
+++ packages/drf_health_check/drf_health_check-0.0.8-py3-none-any.whl/drf_health_check/apps.py
@@ -0,0 +1,10 @@
+from django.apps import AppConfig
+
+from .settings import settings
+
+
+class HealthCheck(AppConfig):
+    name = "health_check"
+
+    def ready(self) -> None:
+        settings.build_settings()
