8410db1a60a9b23a0f401b22f6adafd9547ae505
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:42:06 +0000

    Add package rainod/1.0.6/rainod-1.0.6.tar.gz

---
+++ packages/rainod/rainod-1.0.6.tar.gz/rainod-1.0.6/setup.py
@@ -0,0 +1,18 @@
+import setuptools
+
+# with open('README.md', encoding='utf-8') as f: # README.md 내용 읽어오기
+#         long_description = f.read()
+
+setuptools.setup(
+    name='rainod', # 패키지 명
+    version='1.0.6',
+    description='2조 패키지',
+    author='dojun',
+    author_email='ehwns4337@naver.com',
+    url='',
+    license='MIT', # MIT에서 정한 표준 라이센스 따른다
+    #py_modules=['rb'], # 패키지에 포함되는 모듈
+    python_requires='>=3',
+    install_requires=[], # 패키지 사용을 위해 필요한 추가 설치 패키지
+    packages=setuptools.find_packages(), # 패키지가 들어있는 폴더들
+)
