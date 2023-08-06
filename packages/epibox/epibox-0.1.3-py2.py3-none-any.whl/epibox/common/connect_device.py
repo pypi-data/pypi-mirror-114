e46de68a1c9747ad393e6b4034d0ca721a0e27d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:49:22 +0000

    Add package epibox/0.1.3/epibox-0.1.3-py2.py3-none-any.whl

---
+++ packages/epibox/epibox-0.1.3-py2.py3-none-any.whl/epibox/common/connect_device.py
@@ -0,0 +1,37 @@
+import bitalino
+
+def connect_device(macAddress, client, devices):
+    
+    connected = False
+    devices = [d for d in devices if d] # remove None
+    print('devices: {}'.format(devices))
+    
+    if macAddress in [d.macAddress for d in devices]:
+        try:
+            print('{} state: {}'.format(macAddress, [d.state()for d in devices if d.macAddress==macAddress]))
+            connected = True
+        
+        except Exception as e:
+            print('error in connect_device: {}'.format(e))
+            del devices[[d.macAddress for d in devices].index(macAddress)]
+    
+    else:
+
+        try:
+            device = bitalino.BITalino(macAddress, timeout=5)
+            devices += [device]
+            if macAddress in [d.macAddress for d in devices]:
+                connected = True
+
+            
+        except Exception as e:
+            print(e)
+
+    devices = [d for d in devices if d] # remove None
+    
+    if not connected or macAddress not in [d.macAddress for d in devices]:
+        client.publish(topic='rpi', qos=2, payload="['MAC STATE', '{}', '{}']".format(macAddress, 'failed'))
+    else:
+        client.publish(topic='rpi', qos=2, payload="['MAC STATE', '{}', '{}']".format(macAddress, 'connected'))
+    
+    return connected, devices
