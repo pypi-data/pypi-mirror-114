a216686fbc82519a95c7e7c54cf7434f5d96dc3b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:24:33 +0000

    Add package pyunit-ner/2021.8.2/pyunit_ner-2021.8.2-py3-none-any.whl

---
+++ packages/pyunit-ner/pyunit_ner-2021.8.2-py3-none-any.whl/pyunit_ner/__init__.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3.7
+# -*- coding: utf-8 -*-
+# @Time  : 2019/11/7 10:26
+# @Author: Jtyoui@qq.com
+from .ner import reader_text
+
+__version__ = '2021.8.2'
+__author__ = 'Jtyoui'
+__description__ = '百度实体抽取模型'
+__email__ = 'jtyoui@qq.com'
+__names__ = 'pyUnit_ner'
+__url__ = 'https://github.com/PyUnit/pyunit-ner'
