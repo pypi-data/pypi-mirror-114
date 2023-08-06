8df2dcc4d934b0f43488cbbec40631e50a75a8f0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:42:23 +0000

    Add package versa/0.8.0/versa-0.8.0.tar.gz

---
+++ packages/versa/versa-0.8.0.tar.gz/versa-0.8.0/tools/py/reader/__init__.py
@@ -0,0 +1,4 @@
+# versa.reader
+raise DeprecationWarning('Please use versa.serial instead.')
+raise ImportError('Please use versa.serial instead.')
+
---
+++ packages/versa/versa-0.8.0.tar.gz/versa-0.8.0/tools/py/terms.py
@@ -0,0 +1,13 @@
+from amara3 import iri
+
+from . import iriref
+from . import I, VERSA_BASEIRI
+
+RDF_NS = I('http://www.w3.org/1999/02/22-rdf-syntax-ns#')
+RDFS_NS = I('http://www.w3.org/2000/01/rdf-schema#')
+
+VERSA_TYPE = VERSA_TYPE_REL = VTYPE_REL = VERSA_BASEIRI('type')
+RDF_TYPE = RDF_TYPE_REL = RDF_NS('type')
+
+VFPRINT_REL = VERSA_BASEIRI('fingerprint')
+
