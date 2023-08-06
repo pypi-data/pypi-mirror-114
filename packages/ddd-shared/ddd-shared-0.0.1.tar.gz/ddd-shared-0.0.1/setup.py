9dcc7d773a8b6872a8da470b532aa815918350bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:10:19 +0000

    Add package ddd-shared/0.0.1/ddd-shared-0.0.1.tar.gz

---
+++ packages/ddd-shared/ddd-shared-0.0.1.tar.gz/ddd-shared-0.0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name="ddd-shared",
+    author="Cisco Delgado",
+    author_email="fdelgados@gmail.com",
+    version="0.0.1",
+    packages=["shared"]
+)
