c5789a3015a0d8dd4c2d7787f8e4366486d7efb0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:42:01 +0000

    Add package distributions-hairong/0.1/distributions_hairong-0.1.tar.gz

---
+++ packages/distributions-hairong/distributions_hairong-0.1.tar.gz/distributions_hairong-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='distributions_hairong',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['distributions_hairong'],
+      zip_safe=False)
