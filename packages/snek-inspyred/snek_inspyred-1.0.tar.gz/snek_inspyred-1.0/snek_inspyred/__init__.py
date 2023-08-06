6cf14ffa133b41b07306b52515f71f2d5509cae5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:43:16 +0000

    Add package snek-inspyred/1.0/snek_inspyred-1.0.tar.gz

---
+++ packages/snek-inspyred/snek_inspyred-1.0.tar.gz/snek_inspyred-1.0/snek_inspyred/__init__.py
@@ -0,0 +1,15 @@
+from pygame import Surface
+
+SCREEN_WIDTH = 480
+SCREEN_HEIGHT = 480
+
+
+GRIDSIZE = 20
+GRID_WIDTH = SCREEN_HEIGHT / GRIDSIZE
+GRID_HEIGHT = SCREEN_WIDTH / GRIDSIZE
+
+# Games need buttons
+UP = (0, -1)
+DOWN = (0, 1)
+LEFT = (-1, 0)
+RIGHT = (1, 0)
---
+++ packages/snek-inspyred/snek_inspyred-1.0.tar.gz/snek_inspyred-1.0/snek_inspyred/game/__init__.py
@@ -0,0 +1,66 @@
+import pygame
+from snek_inspyred.assets import Snake, Food
+
+
+class Screen(object):
+    def __init__(self, width=480, height=480, lock_proportion=True):
+        if not isinstance(width, int) or not isinstance(height, int):
+            raise TypeError()
+
+        self.width = width
+        self.height = height
+        self.Grid = self.__Grid(scrn_h=self.height, scrn_w=self.width)
+        self.display = pygame.display.set_mode((self.width, self.height), 0, 32)
+
+    class __Grid(object):
+        def __init__(self, size: int = 10, scrn_w: int = 480, scrn_h: int = 480):
+            self.size = size
+            self.width = scrn_h // self.size
+            self.height = scrn_w // self.size
+
+        def draw(self, surface):
+            for y in range(0, self.height):
+                for x in range(0, self.width):
+                    if (x + y) %  2 == 0:
+                        r = pygame.Rect((x*self.size, y*self.size), (self.size, self.size))
+                        pygame.draw.rect(surface, (93, 216, 228), r)
+                    else:
+                        rr = pygame.Rect((x*self.size, y*self.size), (self.size, self.size))
+                        pygame.draw.rect(surface, (84, 194, 205), rr)
+
+
+def main():
+    pygame.init()
+
+    clock = pygame.time.Clock()
+
+    screen = Screen(height=800, width=800)
+    display = screen.display
+    surface = pygame.Surface(display.get_size())
+    surface = surface.convert()
+
+    screen.Grid.draw(surface)
+
+    snake = Snake(screen)
+    food = Food(screen)
+    myfont = pygame.font.SysFont('monospace', 16)
+
+    while True:
+        clock.tick(10)
+        snake.handle_keys()
+        screen.Grid.draw(surface)
+        snake.move()
+        if snake.get_head_position() == food.position:
+            snake.length += 1
+            snake.score += 1
+            food.randomize_position()
+        snake.draw(surface)
+        food.draw(surface)
+
+        display.blit(surface, (0, 0))
+        text = myfont.render("Score {0}".format(snake.score), 1, (0, 0, 0))
+        display.blit(text, (5, 10))
+        pygame.display.update()
+
+
+
