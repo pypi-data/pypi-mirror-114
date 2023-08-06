ee957f79ac5b890c3578a7e6dc73cad7135a48bd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:12:37 +0000

    Add package nonebot-plugin-flashre/1.0/nonebot_plugin_flashre-1.0.tar.gz

---
+++ packages/nonebot-plugin-flashre/nonebot_plugin_flashre-1.0.tar.gz/nonebot_plugin_flashre-1.0/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+
+setup(
+    name='nonebot_plugin_flashre',  # 对外我们模块的名字
+    version='1.0',  # 版本号
+    description='QQ闪照破解',  # 描述
+    author='LycanW',  # 作者
+    author_email='wanghen0410@126.com',
+    platforms=["all"],
+    url='https://github.com/LycanW/nonebot_plugin_flashre'
+)
