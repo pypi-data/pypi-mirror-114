5450159b733419df06a093816b694060ee9cab5b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:21:24 +0000

    Add package bdcctools/0.3.0/bdcctools-0.3.0-py3-none-any.whl

---
+++ packages/bdcctools/bdcctools-0.3.0-py3-none-any.whl/bdcctools/utils.py
@@ -0,0 +1,44 @@
+"""
+General helper functions.
+"""
+import pandas as pd
+
+
+def clean_text(s: pd.Series) -> pd.Series:
+    """
+    Removes special characters, leading, trailing and multiple whitespaces
+    from text values in a pandas Series.
+
+    Parameters
+    ----------
+    s: pandas Series to clean.
+
+    Returns
+    -------
+    Clean Series.
+    """
+    s = s.replace(r"\d+|[^\w\s]+", "", regex=True)
+    s = s.replace(r"\s+", " ", regex=True)
+    s = s.str.strip()
+
+    return s
+
+
+def standardize_text(s: pd.Series) -> pd.Series:
+    """
+    Standardizes text values by cleaning, converting to lowercase and
+    removing accents.
+
+    Parameters
+    ----------
+    s: pandas Series to standardize.
+
+    Returns
+    -------
+    Standardized Series.
+    """
+    s = clean_text(s)
+    s = s.str.lower()
+    s = s.str.normalize("NFKD").str.encode("ascii", errors="ignore").str.decode("utf-8")
+
+    return s
