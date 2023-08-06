7f3fe61e932b8dde44500a6d5afe60c52c6f76de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:22:19 +0000

    Add package Gaussian-and-Binomial-sethhamilton94/0.1/Gaussian_and_Binomial_sethhamilton94-0.1.tar.gz

---
+++ packages/Gaussian-and-Binomial-sethhamilton94/Gaussian_and_Binomial_sethhamilton94-0.1.tar.gz/Gaussian_and_Binomial_sethhamilton94-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='Gaussian_and_Binomial_sethhamilton94',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['Gaussian_and_Binomial_sethhamilton94'],
+      zip_safe=False)
