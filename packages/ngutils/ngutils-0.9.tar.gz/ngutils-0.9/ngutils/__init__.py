3baaa8da3ed3886f40d5d9d7b1967f6b22808251
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:14:44 +0000

    Add package ngutils/0.9/ngutils-0.9.tar.gz

---
+++ packages/ngutils/ngutils-0.9.tar.gz/ngutils-0.9/ngutils/__init__.py
@@ -0,0 +1,6 @@
+__version__ = 0.9
+
+from ngutils.functions import \
+    phrase_build, \
+    read_urls_contents, \
+    view_types
---
+++ packages/ngutils/ngutils-0.9.tar.gz/ngutils-0.9/setup.py
@@ -0,0 +1,33 @@
+import pathlib
+from setuptools import setup, find_packages
+
+HERE = pathlib.Path(__file__).parent
+
+VERSION = '0.9'
+PACKAGE_NAME = 'ngutils'
+AUTHOR = 'nugani'
+AUTHOR_EMAIL = 'ganibaev@gmail.com'
+URL = 'https://github.com/nigani'
+
+LICENSE = 'Apache License 2.0'
+DESCRIPTION = 'Tools for DataScience'
+LONG_DESCRIPTION = (HERE / "readme.md").read_text()
+LONG_DESC_TYPE = "text/markdown"
+
+INSTALL_REQUIRES = [
+'pandas',
+'requests'
+]
+
+setup(name=PACKAGE_NAME,
+      version=VERSION,
+      description=DESCRIPTION,
+      long_description=LONG_DESCRIPTION,
+      long_description_content_type=LONG_DESC_TYPE,
+      author=AUTHOR,
+      license=LICENSE,
+      author_email=AUTHOR_EMAIL,
+      url=URL,
+      install_requires=INSTALL_REQUIRES,
+      packages=find_packages()
+      )
