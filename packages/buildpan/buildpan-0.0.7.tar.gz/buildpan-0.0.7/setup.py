073a42184df05e37ddd06501202d1e8138d3dbf5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:31:20 +0000

    Add package buildpan/0.0.7/buildpan-0.0.7.tar.gz

---
+++ packages/buildpan/buildpan-0.0.7.tar.gz/buildpan-0.0.7/setup.py
@@ -0,0 +1,29 @@
+#setup.py
+
+"""
+    Title: setup.py
+    Module Name: setup.py
+    Author: Akash Dwivedi
+    Language: Python
+    Date Created: 26-07-2021
+    Date Modified: 29-07-2021
+    Description:
+        ###############################################################
+        ## Setup.py file for the package      ## 
+         ###############################################################
+ """
+from setuptools import _install_setup_requires, setup,find_packages
+
+with open('requirements.txt') as f:
+    required = f.read().splitlines()
+setup(
+    name='buildpan',
+    version='0.0.7',
+    packages=find_packages(),
+    install_requires=required,
+    entry_points={
+        'console_scripts': [
+            'buildpan =buildpan.buildpan:buildpan'
+        ]
+    }
+)
