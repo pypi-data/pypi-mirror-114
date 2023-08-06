a643ff0554442f99239dc03ef9917d205303c64a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:51:33 +0000

    Add package kd-med/0.0.8/kd_med-0.0.8.tar.gz

---
+++ packages/kd-med/kd_med-0.0.8.tar.gz/kd_med-0.0.8/kd_med/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+# @Time    : 7/27/21 6:01 PM
+# @Author  : Jingnan
+# @Email   : jiajingnan2222@gmail.com
+
+from kd_med.kd_loss import kd_loss, GetEncSConv
+from kd_med.pre_trained_enc import PreTrainedEnc
+
+__all__ = [kd_loss, GetEncSConv, PreTrainedEnc]
