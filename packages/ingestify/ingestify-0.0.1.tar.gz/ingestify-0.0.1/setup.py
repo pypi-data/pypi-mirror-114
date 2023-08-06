c2d124f9f154c9860da12af27885cb7dd870f317
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:49:41 +0000

    Add package ingestify/0.0.1/ingestify-0.0.1.tar.gz

---
+++ packages/ingestify/ingestify-0.0.1.tar.gz/ingestify-0.0.1/setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+
+
+def setup_package():
+    setup(
+        name="ingestify",
+        version='0.0.1',
+        author="Koen Vossen",
+        author_email="info@koenvossen.nl",
+        license="AGPL",
+    )
+
+
+if __name__ == "__main__":
+    setup_package()
