37d686f49ff156646f114e9646b080c68600e596
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:42:02 +0000

    Add package selfcord/0.1.2/selfcord-0.1.2.tar.gz

---
+++ packages/selfcord/selfcord-0.1.2.tar.gz/selfcord-0.1.2/selfcord/__init__.py
@@ -0,0 +1,20 @@
+"""
+Selfcord
+~~~~~~~~
+
+A discord.py addon for selfbots in mind.  
+
+© Benny 2021
+
+"""
+
+from .selfcord import *
+from .channel import *
+from .message import *
+from .wrapper import *
+from .utils import *
+from .embed import *
+from .user import *
+from .userinfo import *
+from .messages import *
+from .account import *
