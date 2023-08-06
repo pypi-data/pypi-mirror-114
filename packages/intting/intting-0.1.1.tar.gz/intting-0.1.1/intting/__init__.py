4f1e5d365248cd0f0ec29c450a04910b8aa18cf4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:09:57 +0000

    Add package intting/0.1.1/intting-0.1.1.tar.gz

---
+++ packages/intting/intting-0.1.1.tar.gz/intting-0.1.1/intting/__init__.py
@@ -0,0 +1,2 @@
+print('Thanks for you use the intting')
+print('Email:a1234567890001919@outlook.com')
---
+++ packages/intting/intting-0.1.1.tar.gz/intting-0.1.1/setup.py
@@ -0,0 +1,19 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+  long_description = fh.read()
+
+setuptools.setup(
+  name="intting",
+  version="0.1.1",
+  author="zjy_090820",
+  author_email="a1234567890001919@outlook.com",
+  description="easy to user int (beta)",
+  url="https://pypi.org",
+  packages=setuptools.find_packages(),
+  classifiers=[
+  "Programming Language :: Python :: 3",
+  "License :: OSI Approved :: MIT License",
+  "Operating System :: OS Independent",
+  ],
+)
