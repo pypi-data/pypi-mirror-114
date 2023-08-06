b55ca0b5d981379df36b7e7d8e910136308d8ac3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:21:15 +0000

    Add package pyasync-orm/0.1.2/pyasync_orm-0.1.2-py3-none-any.whl

---
+++ packages/pyasync-orm/pyasync_orm-0.1.2-py3-none-any.whl/pyasync_orm/models.py
@@ -0,0 +1,28 @@
+import inflection
+
+from pyasync_orm.fields import BigSerial
+from pyasync_orm.orm import ORM
+
+
+class Model:
+    table_name: str
+    orm: ORM
+
+    def __init_subclass__(cls, **kwargs):
+        super().__init_subclass__(**kwargs)
+        cls.table_name = inflection.tableize(cls.__name__)
+        cls.id = BigSerial(primary_key=True)
+        cls.orm = ORM(model_class=cls)
+
+    def __str__(self):
+        return f'<{self.__class__.__name__}: {self.id}>'
+
+    def __repr__(self):
+        return f'{self}'
+
+    @classmethod
+    def from_db(cls, data: dict) -> 'Model':
+        instance = cls()
+        for key, value in data.items():
+            setattr(instance, key, value)
+        return instance
