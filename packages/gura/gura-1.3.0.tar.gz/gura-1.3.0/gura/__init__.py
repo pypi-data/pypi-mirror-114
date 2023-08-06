ee9241b97b29a11053f7d9b315da74506c318cab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:51:03 +0000

    Add package gura/1.3.0/gura-1.3.0.tar.gz

---
+++ packages/gura/gura-1.3.0.tar.gz/gura-1.3.0/gura/__init__.py
@@ -0,0 +1,14 @@
+from gura.GuraParser import GuraParser, InvalidIndentationError, DuplicatedVariableError, DuplicatedKeyError, \
+    VariableNotDefinedError, DuplicatedImportError, loads, dumps
+from gura.Parser import ParseError
+
+__version__ = "1.3.0"
+
+loads = loads
+dumps = dumps
+ParseError = ParseError
+InvalidIndentationError = InvalidIndentationError
+DuplicatedVariableError = DuplicatedVariableError
+DuplicatedKeyError = DuplicatedKeyError
+VariableNotDefinedError = VariableNotDefinedError
+DuplicatedImportError = DuplicatedImportError
