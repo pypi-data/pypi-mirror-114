970efd68c649a9932ee9db6097fe1272a5f83f4a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:30:58 +0000

    Add package prasadi/0.0.1/prasadi-0.0.1.tar.gz

---
+++ packages/prasadi/prasadi-0.0.1.tar.gz/prasadi-0.0.1/prasadi/__init__.py
@@ -0,0 +1,10 @@
+## This is needed to allow Airflow to pick up specific metadata fields it needs for certain features. We recognize it's a bit unclean to define these in multiple places, but at this point it's the only workaround if you'd like your custom conn type to show up in the Airflow UI.
+def get_provider_info():
+    return {
+        "package-name": "prasadi", # Required
+        "name": "Sample Airflow Provider", # Required
+        "description": "A sample template for airflow providers.", # Required
+        "hook-class-names": ["prasadi.hooks.sample_hook.SampleHook"],
+        "extra-links": ["prasadi.operators.sample_operator.ExtraLink"],
+        "versions": ["0.0.1"] # Required
+    }
