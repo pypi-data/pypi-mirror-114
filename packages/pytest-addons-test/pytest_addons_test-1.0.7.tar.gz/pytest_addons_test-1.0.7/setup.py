e63e90ee157a581b463475f485e4fd42293da232
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:25:35 +0000

    Add package pytest-addons-test/1.0.7/pytest_addons_test-1.0.7.tar.gz

---
+++ packages/pytest-addons-test/pytest_addons_test-1.0.7.tar.gz/pytest_addons_test-1.0.7/setup.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['pytest_addons_test']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['pytest>=6.2.4,<7.0.0']
+
+entry_points = \
+{'pytest.parsers': ['.rst = pytest_addons_test:main']}
+
+setup_kwargs = {
+    'name': 'pytest-addons-test',
+    'version': '1.0.7',
+    'description': '用于测试pytest的插件',
+    'long_description': None,
+    'author': 'lin',
+    'author_email': None,
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+    'python_requires': '>=3.9,<4.0',
+}
+
+
+setup(**setup_kwargs)
