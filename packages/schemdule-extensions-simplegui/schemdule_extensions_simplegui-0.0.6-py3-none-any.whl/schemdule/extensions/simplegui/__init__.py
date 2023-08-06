4852318551c8cfe024306065c7f8fa114e85da8d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:42:56 +0000

    Add package schemdule-extensions-simplegui/0.0.6/schemdule_extensions_simplegui-0.0.6-py3-none-any.whl

---
+++ packages/schemdule-extensions-simplegui/schemdule_extensions_simplegui-0.0.6-py3-none-any.whl/schemdule/extensions/simplegui/__init__.py
@@ -0,0 +1,17 @@
+import PySimpleGUI as sg
+from typing import Any
+from schemdule.prompters import Prompter, PromptResult
+
+__version__ = "0.0.6"
+
+
+class MessageBoxPrompter(Prompter):
+    def __init__(self, final: bool = False, auto_close: bool = False) -> None:
+        super().__init__(final)
+        self.auto_close = auto_close
+
+    def prompt(self, message: str, payload: Any) -> Any:
+        sg.popup_scrolled(str(payload), title=f"Attention {message}", auto_close=self.auto_close,
+                          keep_on_top=True, background_color='white', text_color='black')
+
+        return self.success()
