e46477b1df2d2365f362dead6c6af0a99c9fea07
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:19:37 +0000

    Add package k3portlock/0.1.0/k3portlock-0.1.0.tar.gz

---
+++ packages/k3portlock/k3portlock-0.1.0.tar.gz/k3portlock-0.1.0/k3portlock/__init__.py
@@ -0,0 +1,27 @@
+"""
+Portlock is a cross-process lock that is implemented with `tcp` port binding.
+Since no two processes could bind on a same TCP port.
+
+Portlock tries to bind **3** ports on loopback ip `127.0.0.1`.
+If a Portlock instance succeeds on binding **2** ports out of 3,
+it is considered this instance has acquired the lock.
+
+"""
+
+# from .proc import CalledProcessError
+# from .proc import ProcError
+
+__version__ = "0.1.0"
+__name__ = "k3portlock"
+
+from .portlock import (
+    PortlockError,
+    PortlockTimeout,
+    Portlock,
+)
+
+__all__ = [
+    'PortlockError',
+    'PortlockTimeout',
+    'Portlock',
+]
