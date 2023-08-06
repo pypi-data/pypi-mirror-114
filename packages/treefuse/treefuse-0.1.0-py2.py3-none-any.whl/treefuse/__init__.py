715e67669d9a0034e11ccbaaad7b5b13342c4b13
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:29:36 +0000

    Add package treefuse/0.1.0/treefuse-0.1.0-py2.py3-none-any.whl

---
+++ packages/treefuse/treefuse-0.1.0-py2.py3-none-any.whl/treefuse/__init__.py
@@ -0,0 +1,10 @@
+"""Top-level package for TreeFuse."""
+
+__author__ = """Daniel Watkins"""
+__email__ = 'daniel@daniel-watkins.co.uk'
+__version__ = '0.1.0'
+
+from .treefuse import treefuse_main
+
+
+__all__ = ["treefuse_main"]
