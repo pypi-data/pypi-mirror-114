2e427d64d7c2f8979ad56a5a7a78358d0c09dfd7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:13:08 +0000

    Add package ycc/0.3.9/ycc-0.3.9-py3-none-any.whl

---
+++ packages/ycc/ycc-0.3.9-py3-none-any.whl/p/__init__.py
@@ -0,0 +1,16 @@
+#a_3y
+# __init__.py
+#from . import *
+#from pdsystem import * #The moudle of Maker:神秘的·  ps:需要pip下载,在终端输入pip install pdsystem
+#pdios()
+#from p.yh import *
+#from p.yz import *
+#from p.y import *
+from p.beginnings import *
+from colorama import init
+from datetime import datetime as sj
+from time import sleep as dd
+from math import pi as pai1
+from sys import exit as tc
+start=sj.now()
+
