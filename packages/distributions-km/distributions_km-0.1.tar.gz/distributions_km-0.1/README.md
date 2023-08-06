e84ef6ab202e1455dc5e1bea34ad32817b3b4f1f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:30:55 +0000

    Add package distributions-km/0.1/distributions_km-0.1.tar.gz

---
+++ packages/distributions-km/distributions_km-0.1.tar.gz/distributions_km-0.1/README.md
@@ -0,0 +1,10 @@
+# distributions_km package
+
+	Sample package with Gaussian and Binomial modules. 
+
+# Files
+	The package consists General Distribution, Gaussian and Binomial distribution file
+    
+    
+    
+# Installation
---
+++ packages/distributions-km/distributions_km-0.1.tar.gz/distributions_km-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='distributions_km',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['distributions_km'],
+      author = 'Muralidhar Kuluru',
+      author_email = 'muralidhark36@gmail.com',
+      zip_safe=False)
