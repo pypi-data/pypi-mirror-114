60639196aac9669020293029037e3626d07902d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:41:49 +0000

    Add package ailever/0.2.319/ailever-0.2.319-py3-none-any.whl

---
+++ packages/ailever/ailever-0.2.319-py3-none-any.whl/ailever/investment/screening_markets.py
@@ -0,0 +1,31 @@
+from .parallelizer import parallelize
+
+import os
+import numpy as np
+from numpy import linalg
+import pandas as pd
+
+
+
+def reits_screening(path, period):
+    print(f'[AILEVER] Recommandations based on latest {period} days.')
+
+    prllz = parallelize(path=path,
+                        object_format='csv',
+                        base_column='close',
+                        date_column='date',
+                        truncate=period)
+
+    base = prllz.ndarray
+    tickers = prllz.pdframe.columns
+    mapper = {idx:ticker for idx, ticker in enumerate(tickers)}
+
+    x, y = np.arange(base.shape[0]), base
+    bias = np.ones_like(x)
+    X = np.c_[bias, x]
+
+    b = linalg.inv(X.T@X) @ X.T @ y
+    yhat = X@b
+    recommand = yhat[-1] - yhat[-2]
+    return list(map(lambda x: mapper[x][:-4], np.argsort(recommand)[::-1]))
+
