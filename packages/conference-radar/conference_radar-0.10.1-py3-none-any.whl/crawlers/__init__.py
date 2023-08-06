fddb3558bffa6e75973fe90169a10d2d956803f7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:25:15 +0000

    Add package conference-radar/0.10.1/conference_radar-0.10.1-py3-none-any.whl

---
+++ packages/conference-radar/conference_radar-0.10.1-py3-none-any.whl/crawlers/__init__.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+
+from .pycon.pycon_crawler import PyConCrawler
+from .python.python_crawler import PythonCrawler
+from .pydata.pydata_crawler import PyDataCrawler
+from .confs_tech.confs_tech_crawler import ConfsTechCrawler
