40892d5c5c30310cbb2d2f214c507f92cfebfdbd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:13:01 +0000

    Add package uffd/0.1.2/uffd-0.1.2.tar.gz

---
+++ packages/uffd/uffd-0.1.2.tar.gz/uffd-0.1.2/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+import os
+
+with open('README.md', 'r', encoding='utf-8') as f:
+	long_description = f.read()
+
+setup(
+	name='uffd',
+	version=os.environ.get('PACKAGE_VERSION', 'local'),
+	description='UserFerwaltungsFrontend: Ldap based single sign on and user management web software',
+	long_description=long_description,
+	long_description_content_type='text/markdown',
+	url='https://git.cccv.de/uffd/uffd',
+	classifiers=[
+		'Programming Language :: Python :: 3',
+		'Development Status :: 4 - Beta',
+		'License :: OSI Approved :: GNU Affero General Public License v3 or later (AGPLv3+)',
+		'Operating System :: OS Independent',
+		'Topic :: System :: Systems Administration :: Authentication/Directory :: LDAP',
+		'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+		'Environment :: Web Environment',
+		'Framework :: Flask',
+	],
+	author='CCCV',
+	author_email='it@cccv.de',
+	license='AGPL3',
+	packages=find_packages(),
+	zip_safe=False,
+	python_requires='>=3.7',
+)
