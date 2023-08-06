6c0d9c1f241b72d1aa73a6ff1285c8deb0057edd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:32:26 +0000

    Add package D47calib/0.0.4/D47calib-0.0.4-py3-none-any.whl

---
+++ packages/D47calib/D47calib-0.0.4-py3-none-any.whl/D47calib/__init__.py
@@ -0,0 +1,14 @@
+'''
+D47calib library
+'''
+
+__author__    = 'Mathieu Daëron'
+__contact__   = 'daeron@lsce.ipsl.fr'
+__copyright__ = 'Copyright (c) 2021 Mathieu Daëron'
+__license__   = 'Modified BSD License - https://opensource.org/licenses/BSD-3-Clause'
+__date__      = '2021-07-27'
+
+def bar():
+	print('bar')
+	
+	
