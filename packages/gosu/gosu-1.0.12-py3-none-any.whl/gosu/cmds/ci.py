e43fc2e2524ef9fc8a96892aca8006d6d906718b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:26:14 +0000

    Add package gosu/1.0.12/gosu-1.0.12-py3-none-any.whl

---
+++ packages/gosu/gosu-1.0.12-py3-none-any.whl/gosu/cmds/ci.py
@@ -0,0 +1,42 @@
+import os
+import re
+import subprocess
+
+import semver
+import typer
+
+app = typer.Typer()
+
+
+def git(*args):
+    return subprocess.check_output(["git"] + list(args))
+
+
+def get_version():
+    try:
+        return git("describe", "--tags").decode().strip()
+    except subprocess.CalledProcessError:
+        return "1.0.0"
+
+
+@app.command()
+def push_repo():
+    git(
+        "remote",
+        "set-url",
+        "--push",
+        "origin",
+        re.sub(r".+@([^/]+)/", r"git@\1:", os.environ["CI_REPOSITORY_URL"]),
+    )
+    git("push", "-o", "ci.skip", "origin", get_version())
+
+
+@app.command()
+def bump_version():
+    v = get_version()
+    n = semver.bump_patch(v)
+    if "-" not in v:
+        return
+
+    print(f"bump from {v} to {n}")
+    git("tag", n)
