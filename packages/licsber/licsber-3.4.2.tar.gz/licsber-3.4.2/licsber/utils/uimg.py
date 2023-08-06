a804d728327c303e3bfba463cd88c022c86f4b8a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:16:35 +0000

    Add package licsber/3.4.2/licsber-3.4.2.tar.gz

---
+++ packages/licsber/licsber-3.4.2.tar.gz/licsber-3.4.2/licsber/utils/uimg.py
@@ -0,0 +1,31 @@
+import cv2
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+def parse_img(inputs):
+    """
+    将输入转化为图片, 支持路径/numpy array/content(图片二进制).
+    :param inputs: 任意输入
+    :return: numpy array
+    """
+    if type(inputs) == str:
+        return cv2.imread(inputs)
+    elif type(inputs) == bytes:
+        return cv2.imdecode(np.frombuffer(inputs, np.uint8), cv2.COLOR_RGBA2RGB)
+    else:
+        return inputs
+
+
+def imshow(img):
+    """
+    转换奇怪的bgr和rgb
+    :param img: numpy array
+    """
+    if len(img.shape) != 3:
+        plt.imshow(img)
+        return
+
+    b, g, r = cv2.split(img)
+    rgb = cv2.merge([r, g, b])
+    plt.imshow(rgb)
---
+++ packages/licsber/licsber-3.4.2.tar.gz/licsber-3.4.2/setup.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+import setuptools
+
+with open('README.md', 'r', encoding='utf-8') as f:
+    long_description = f.read()
+
+setuptools.setup(
+    name='licsber',
+    version='3.4.2',
+    author='Licsber',
+    author_email='licsber@gmail.com',
+    url='https://www.cnblogs.com/licsber/',
+    description=u'个人娱乐工具箱.',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    packages=setuptools.find_packages(),
+    include_package_data=True,
+    install_requires=[
+        'pymongo',
+        'requests',
+        'beautifulsoup4',
+        'pycryptodome',
+        'numpy',
+        'minio',
+        'matplotlib',
+        'opencv-python',
+        'h5py',
+        'paddlepaddle~=2.0',
+        'tqdm',
+    ],
+    entry_points={
+        'console_scripts': [
+            'licsber=licsber.shell.hello:licsber',
+            'memobird=licsber.shell:memobird',
+            'count-dir=licsber.shell.dir_ops:count_dir',
+            'flatten-dir=licsber.shell.dir_ops:flatten_dir',
+            'empty-dir=licsber.shell.dir_ops:empty_dir',
+            'archive=licsber.shell.dir_ops:archive',
+            'rename=licsber.shell.dir_ops:rename',
+            'save-115=licsber.shell.cloud_drive:save_115_link',
+            'save-115-dir=licsber.shell.cloud_drive:save_115_dir',
+            'conv=licsber.shell.cloud_drive:conv',
+        ],
+    },
+    classifiers=[
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Programming Language :: Python :: 3 :: Only',
+        'Operating System :: OS Independent',
+    ],
+    license='GPLv3',
+)
