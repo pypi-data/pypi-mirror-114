0fc519e3aa239d9e839096f07406fbaf4af3ac59
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:42:23 +0000

    Add package lightningmcqueen/1.0.1/lightningmcqueen-1.0.1-py3-none-any.whl

---
+++ packages/lightningmcqueen/lightningmcqueen-1.0.1-py3-none-any.whl/lightningmcqueen/__init__.py
@@ -0,0 +1,4 @@
+# __init__.py
+
+# Version of the lightningmcqueen package
+__version__ = "1.0.1"
---
+++ packages/lightningmcqueen/lightningmcqueen-1.0.1-py3-none-any.whl/lightningmcqueen/__main__.py
@@ -0,0 +1,14 @@
+import flask
+from replit import db, web
+
+app = flask.Flask(__name__)
+users = web.UserStore()
+
+@app.route("/")
+@web.authenticated
+def index():
+  hits = users.current.get("hits", 0) + 1
+  users.current["hits"] = hits
+  return f"You have visited this page {hits} times"
+
+web.run(app)
