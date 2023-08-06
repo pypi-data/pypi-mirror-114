aef2f141bd51a61e719df61e7fe86a906f9c4c89
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:22:16 +0000

    Add package rycronofy/0.4/rycronofy-0.4.tar.gz

---
+++ packages/rycronofy/rycronofy-0.4.tar.gz/rycronofy-0.4/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='rycronofy',
+    version='0.4',
+    author='Ryan Mohta',
+    author_email='ryanmohta@gmail.com',
+    description='A basic Python wrapper over the Cronofy calendar scheduling library',
+    packages=find_packages(),
+    install_requires=['requests'],
+    keywords=['python', 'cronofy']
+)
