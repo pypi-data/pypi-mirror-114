9e4e172869316e922ec32672b0724b4d870f29bd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:23:57 +0000

    Add package mad-notifications/0.2.8/mad_notifications-0.2.8.tar.gz

---
+++ packages/mad-notifications/mad_notifications-0.2.8.tar.gz/mad_notifications-0.2.8/mad_notifications/apps.py
@@ -0,0 +1,8 @@
+from django.apps import AppConfig
+from django.utils.translation import gettext_lazy as _
+
+
+class NotificationsConfig(AppConfig):
+    name = 'mad_notifications'
+
+    verbose_name = _('Notifications')
