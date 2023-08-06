4c2be78f654c2d0ba33d5ab6fb086cebd63f1e2f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:13:05 +0000

    Add package librapid/0.2.6/librapid-0.2.6-cp37-cp37m-manylinux2010_i686.whl

---
+++ packages/librapid/librapid-0.2.6-cp37-cp37m-manylinux2010_i686.whl/librapid/__init__.py
@@ -0,0 +1,21 @@
+# coding: utf8
+
+import os
+import platform
+import distutils.sysconfig
+import shutil
+from pathlib import Path
+
+from . import progress
+
+# If using windows, tell the os where the DLL for blas is
+# If blas was not installed, this doesn't really do anything
+if platform.system() == "Windows":
+	import win32api
+	# this_directory = Path(__file__).parent
+	this_directory = os.path.join(__file__, os.pardir)
+
+	print("Loading DLL from", os.path.join(this_directory, "blas"))
+	win32api.SetDllDirectory(os.path.join(this_directory, "blas"))
+
+from librapid_ import *
