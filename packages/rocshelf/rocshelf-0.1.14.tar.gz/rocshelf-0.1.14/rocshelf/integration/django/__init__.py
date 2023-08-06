861597da2ad7ca13e66fde113fb3b675f9d52d0c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:48:49 +0000

    Add package rocshelf/0.1.14/rocshelf-0.1.14.tar.gz

---
+++ packages/rocshelf/rocshelf-0.1.14.tar.gz/rocshelf-0.1.14/rocshelf/integration/django/__init__.py
@@ -0,0 +1,13 @@
+""" Интеграция Rocshelf в django
+
+Настройки settings.py
+
+ROCSHELF_DIST_PATH - Путь до папки с скомпилированными исходниками
+ROCSHELF_CACHE_PATH - Путь до кеша компиляции
+
+"""
+
+import django
+
+if django.VERSION < (3, 2):
+    default_app_config = 'rocshelf.integration.django.RocshelfAppConfig'
