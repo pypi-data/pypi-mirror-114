2b0de4b83016edaf7375bf2bd177573d9314caf3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:26:00 +0000

    Add package x735-v2.5/0.1.1/x735-v2.5-0.1.1.tar.gz

---
+++ packages/x735-v2.5/x735-v2.5-0.1.1.tar.gz/x735-v2.5-0.1.1/setup.py
@@ -0,0 +1,17 @@
+import setuptools
+
+setuptools.setup(
+    name="x735-v2.5",
+    version="0.1.1",
+    description="Library for board x735",
+    author="sh.kiruh@gmail.com",
+    install_requires=[
+        "pigpio==1.78",
+        "RPi.GPIO==0.7.0",
+        "click==8.0.1",
+    ],
+    extras_require={"dev": ["pre-commit"]},
+    entry_points={
+        "console_scripts": ["x735fan=main:fan"],
+    },
+)
