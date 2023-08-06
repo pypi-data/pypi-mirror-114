4832b0c79a799b343b4df61cf884e2a028490699
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:12:55 +0000

    Add package librapid/0.2.6/librapid-0.2.6.tar.gz

---
+++ packages/librapid/librapid-0.2.6.tar.gz/librapid-0.2.6/librapid/librapid.hpp
@@ -0,0 +1,9 @@
+#ifndef LIBRAPID_INCLUDE
+#define LIBRAPID_INCLUDE
+
+#include <librapid/utils/utils.hpp>
+#include <librapid/math/rapid_math.hpp>
+#include <librapid/ndarray/ndarray.hpp>
+#include <librapid/network/network.hpp>
+
+#endif // LIBRAPID_INCLUDE
