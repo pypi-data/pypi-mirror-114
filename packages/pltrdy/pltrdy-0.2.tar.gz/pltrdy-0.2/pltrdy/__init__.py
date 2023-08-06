5c1e872211430ee5540ce6556153f65cf2d60de1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:13:59 +0000

    Add package pltrdy/0.2/pltrdy-0.2.tar.gz

---
+++ packages/pltrdy/pltrdy-0.2.tar.gz/pltrdy-0.2/pltrdy/__init__.py
@@ -0,0 +1,13 @@
+import pltrdy.percentiles
+import pltrdy.assertion
+import pltrdy.explore_results
+import pltrdy.gen_script
+import pltrdy.pickle
+import pltrdy.rouge
+import pltrdy.stats
+import pltrdy.time
+import pltrdy.wc  # noqa F401
+
+from pltrdy.describe import describe_str, describe_lines, describe, tab_print
+
+__version__ = "0.2"
