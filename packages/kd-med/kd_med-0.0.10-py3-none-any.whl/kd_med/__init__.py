b006f9721229b1c5d366e7d954b8af34d8d8ae73
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:30:53 +0000

    Add package kd-med/0.0.10/kd_med-0.0.10-py3-none-any.whl

---
+++ packages/kd-med/kd_med-0.0.10-py3-none-any.whl/kd_med/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+# @Time    : 7/27/21 6:01 PM
+# @Author  : Jingnan
+# @Email   : jiajingnan2222@gmail.com
+
+from kd_med.kd_loss import kd_loss, get_enc_plus_conv
+from kd_med.pre_trained_enc import pre_trained_enc
+
+__all__ = [kd_loss, get_enc_plus_conv, pre_trained_enc]
