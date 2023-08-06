4b720a7feb1a1037ff22547013b8947aa70ac4c8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:27:39 +0000

    Add package calculador-de-elementos-estruturais/0.4.4/calculador_de_elementos_estruturais-0.4.4.tar.gz

---
+++ packages/calculador-de-elementos-estruturais/calculador_de_elementos_estruturais-0.4.4.tar.gz/calculador_de_elementos_estruturais-0.4.4/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='calculador_de_elementos_estruturais',
+    version='0.4.4',
+    license='Boost Software License',
+    author='Doglas Rocha',
+    keywords=['Vigas', 'Pilares', 'Ligação Corte Duplo'],
+    description=u'Calculador de Elementos Estruturais',
+    packages=['calculador_de_elementos_estruturais'],
+    install_requires=['pandas'])
