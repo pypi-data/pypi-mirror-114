601cd019fead477ada040642c75f0a8454f73389
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:10:18 +0000

    Add package combinedUA/1.0.0/combinedUA-1.0.0-py3-none-any.whl

---
+++ packages/combinedUA/combinedUA-1.0.0-py3-none-any.whl/combinedUA/__init__.py
@@ -0,0 +1,40 @@
+from random import choice
+
+from util import read_file, get_base_dir
+
+base_dir = get_base_dir(__file__)
+ios = read_file(base_dir + "/db/ios.txt").splitlines()
+safari_build = read_file(base_dir + "/db/safari_build.txt").splitlines()
+safari_version = read_file(base_dir + "/db/safari_version.txt").splitlines()
+webkit = read_file(base_dir + "/db/webkit.txt").splitlines()
+chrome = read_file(base_dir + "/db/chrome.txt").splitlines()
+android = read_file(base_dir + "/db/android.txt").splitlines()
+
+
+def get_ua_ios_safari():
+    os = "(iPhone; CPU iPhone OS {} like Mac OS X)".format(choice(ios).replace(".", "_"))
+    engine = "AppleWebKit/{} (KHTML, like Gecko)".format(choice(webkit))
+    browser = "Version/{} Mobile/15E148 Safari/{}".format(choice(safari_version), choice(safari_build))
+    return f"Mozilla/5.0 {os} {engine} {browser}"
+
+
+def get_ua_ios_chrome():
+    os = "(iPhone; CPU iPhone OS {} like Mac OS X)".format(choice(ios).replace(".", "_"))
+    engine = "AppleWebKit/{} (KHTML, like Gecko)".format(choice(webkit))
+    browser = "CriOS/{} Mobile/15E148 Safari/{}".format(choice(chrome), choice(safari_build))
+    return f"Mozilla/5.0 {os} {engine} {browser}"
+
+
+def get_ua_android_chrome():
+    os = "(Linux; Android {}; M6 Note)".format(choice(android))
+    engine = "AppleWebKit/{} (KHTML, like Gecko)".format(choice(webkit))
+    browser = "Chrome/{} Mobile Safari/{}".format(choice(chrome), choice(safari_build))
+    return f"Mozilla/5.0 {os} {engine} {browser}"
+
+
+ua = [get_ua_ios_safari, get_ua_ios_chrome, get_ua_android_chrome]
+
+
+def get_ua():
+    f = choice(ua)
+    return f()
