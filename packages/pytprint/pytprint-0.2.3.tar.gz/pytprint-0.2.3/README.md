30ba90814085b6910fe9cd04b72e75a87a5a391a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:40:15 +0000

    Add package pytprint/0.2.3/pytprint-0.2.3.tar.gz

---
+++ packages/pytprint/pytprint-0.2.3.tar.gz/pytprint-0.2.3/README.md
@@ -0,0 +1,10 @@
+# pytprint
+Thread-safe print module for Python. It's exactly as simple as it sounds.
+
+## Install
+
+`pip install git+https://github.com/rory-linehan/pytprint@0.2.3`
+
+## Usage
+
+`from pytprint import print`
---
+++ packages/pytprint/pytprint-0.2.3.tar.gz/pytprint-0.2.3/setup.py
@@ -0,0 +1,12 @@
+# Copyright 2020 Rory Linehan
+# this code is distributed under the terms of the GNU General Public License
+
+from distutils.core import setup
+
+setup(
+    name='pytprint',
+    version='0.2.3',
+    packages=['pytprint',],
+    license='GNU GPL',
+    long_description=open('README.md').read(),
+)
