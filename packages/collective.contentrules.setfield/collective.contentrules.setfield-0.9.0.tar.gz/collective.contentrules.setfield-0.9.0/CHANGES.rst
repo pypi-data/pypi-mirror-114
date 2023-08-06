a7c3c7ea2bcb2ec5ba6fe887890ef05ef7f1ee0c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:29:57 +0000

    Add package collective.contentrules.setfield/0.9.0/collective.contentrules.setfield-0.9.0.tar.gz

---
+++ packages/collective.contentrules.setfield/collective.contentrules.setfield-0.9.0.tar.gz/collective.contentrules.setfield-0.9.0/CHANGES.rst
@@ -0,0 +1,26 @@
+Changelog
+=========
+
+0.9.0 (2021-07-27)
+------------------
+
+- Initial release.
+  [instification]
+
+- Add configurable option to set values on all content that would match the executing rule.
+  [instification]
+
+- Add configurable option to preserve modification date of updated objects.
+  [instification]
+
+- Add 'Objects parent modified' trigger and handler to contentrules.
+  [instification]
+
+- Add unit tests.
+  [JeffersonBledsoe]
+
+- Ensure only dexterity fields can be set and values are valid or error thrown
+  [djay]
+
+- Fix bug where object with no workflow would prevent rule being fired
+  [djay]
---
+++ packages/collective.contentrules.setfield/collective.contentrules.setfield-0.9.0.tar.gz/collective.contentrules.setfield-0.9.0/collective/contentrules/setfield/testing.py
@@ -0,0 +1,27 @@
+from plone import api
+from plone.app.testing import (PLONE_FIXTURE, FunctionalTesting,
+                               IntegrationTesting, PloneSandboxLayer)
+
+
+class SetFieldLayer(PloneSandboxLayer):
+
+    defaultBases = (PLONE_FIXTURE,)
+
+    def setUpZope(self, app, configurationContext):
+        import collective.contentrules.setfield
+
+        self.loadZCML(package=collective.contentrules.setfield)
+
+    def setUpPloneSite(self, portal):
+        if api.env.plone_version().startswith("5"):
+            self.applyProfile(portal, "plone.app.contenttypes:default")
+
+
+FIXTURE = SetFieldLayer()
+
+INTEGRATION_TESTING = IntegrationTesting(
+    bases=(FIXTURE,), name="collective.contentrules.setfield:IntegrationTests"
+)
+FUNCTIONAL_TESTING = FunctionalTesting(
+    bases=(FIXTURE,), name="collective.contentrules.setfield:FunctionalTests"
+)
