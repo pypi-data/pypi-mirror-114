10a99b2f598ea6dceb9256114e9f237f8ad9749d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:31:18 +0000

    Add package pypipackageexamplejortega/0.0.2/pypipackageexamplejortega-0.0.2.tar.gz

---
+++ packages/pypipackageexamplejortega/pypipackageexamplejortega-0.0.2.tar.gz/pypipackageexamplejortega-0.0.2/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = "pypipackageexamplejortega",
+    version = "0.0.2",
+    license = "MIT",
+    description = "Paquete de prueba",
+    author = "Javier Ortega",
+    # install_requires = ["math", "numpy"]
+    # author_email = "javier9552@gmail.com"
+    packages = find_packages(),
+    url = "https://github.com/JavierOrtegaFP/pypipackageexamplejortega"
+)
