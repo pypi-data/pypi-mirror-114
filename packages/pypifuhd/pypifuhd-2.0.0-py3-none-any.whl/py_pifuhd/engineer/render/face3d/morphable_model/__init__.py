d414f8db4c31aee772c05e0b774fddc4fdccce8d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:23:51 +0000

    Add package pypifuhd/2.0.0/pypifuhd-2.0.0-py3-none-any.whl

---
+++ packages/pypifuhd/pypifuhd-2.0.0-py3-none-any.whl/py_pifuhd/engineer/render/face3d/morphable_model/__init__.py
@@ -0,0 +1,9 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+# from .. import mesh
+from .. import mesh_numpy
+
+from .morphabel_model import MorphabelModel
+from . import load
