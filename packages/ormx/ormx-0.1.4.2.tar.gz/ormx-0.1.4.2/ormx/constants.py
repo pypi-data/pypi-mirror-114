dd7f83bf8aa921ef349914859de83a3ca100639d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:12:28 +0000

    Add package ormx/0.1.4.2/ormx-0.1.4.2.tar.gz

---
+++ packages/ormx/ormx-0.1.4.2.tar.gz/ormx-0.1.4.2/ormx/constants.py
@@ -0,0 +1,25 @@
+SELECT_TABLES_SQL = "SELECT name FROM sqlite_master WHERE type = 'table';"
+CREATE_TABLE_SQL = "CREATE TABLE {name} ({fields});"
+INSERT_SQL = 'INSERT INTO {name} ({fields}) VALUES ({placeholders});'
+SELECT_ALL_SQL = 'SELECT {fields} FROM {name};'
+SELECT_FIRST_SQL = 'SELECT * FROM {name} ORDER BY ROWID DESC LIMIT 1;'
+SELECT_WHERE_SQL = 'SELECT {fields} FROM {name} WHERE {query};'
+
+SQLITE_TYPE_MAP = {
+    int: "INTEGER",
+    float: "REAL",
+    str: "TEXT",
+    bytes: "BLOB",
+    bool: "INTEGER",  # 0 or 1
+}
+
+
+__all__ = [
+    "SELECT_TABLES_SQL",
+    "CREATE_TABLE_SQL",
+    "INSERT_SQL",
+    "SELECT_ALL_SQL",
+    "SELECT_FIRST_SQL",
+    "SELECT_WHERE_SQL",
+    "SQLITE_TYPE_MAP"
+]
