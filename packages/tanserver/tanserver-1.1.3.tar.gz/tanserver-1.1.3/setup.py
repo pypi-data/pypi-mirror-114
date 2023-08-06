2e85bbcdb1cef4402d8fbcbe6f3c2807fef1fbc0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:27:07 +0000

    Add package tanserver/1.1.3/tanserver-1.1.3.tar.gz

---
+++ packages/tanserver/tanserver-1.1.3.tar.gz/tanserver-1.1.3/setup.py
@@ -0,0 +1,2 @@
+from distutils.core import setup
+setup(name='tanserver',version='1.1.3',description='Tanserver Python APIs',author='Chen Daye',author_email='tanserver@outlook.com',py_modules=['tanserver'])
