54522305c62f3fe7dc8309b35c485ba746e608dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:30:59 +0000

    Add package parqser/1.0.10/parqser-1.0.10.tar.gz

---
+++ packages/parqser/parqser-1.0.10.tar.gz/parqser-1.0.10/parqser/__init__.py
@@ -0,0 +1,10 @@
+"""
+parqser
+
+Finally, a good parser
+"""
+from . import *
+
+__all__ = ['page', 'parser', 'saver', 'scrapper', 'session', 'web_component']
+__version__ = "1.0.10"
+__author__ = 'Ilya Shamov'
