3f6456c190a75bd642444c109ae08be90c2c6dac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:14:41 +0000

    Add package ansi-escape-sequences/1.0.0/ansi-escape-sequences-1.0.0.tar.gz

---
+++ packages/ansi-escape-sequences/ansi-escape-sequences-1.0.0.tar.gz/ansi-escape-sequences-1.0.0/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+with open("README.md", 'r') as fh:
+    long_description = fh.read()
+
+setup(
+    name="ansi-escape-sequences",
+    version="1.0.0",
+    description="Simple tool to use ansi escape sequences",
+    py_mosules=["ansi_escape_sequences"],
+    package_dir={'': "src"},
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    classifiers=[
+        'License :: OSI Approved :: MIT License'
+    ],
+    url="https://gitlab.com/Commandcracker/ansi.py",
+)
