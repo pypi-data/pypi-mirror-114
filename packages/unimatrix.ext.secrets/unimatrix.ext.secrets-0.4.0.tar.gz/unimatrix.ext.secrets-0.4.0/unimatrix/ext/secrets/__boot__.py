16c9a503d8bff9614823efaa18936201a77e6571
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:21:21 +0000

    Add package unimatrix.ext.secrets/0.4.0/unimatrix.ext.secrets-0.4.0.tar.gz

---
+++ packages/unimatrix.ext.secrets/unimatrix.ext.secrets-0.4.0.tar.gz/unimatrix.ext.secrets-0.4.0/unimatrix/ext/secrets/__boot__.py
@@ -0,0 +1,16 @@
+# pylint: skip-file
+from unimatrix.conf import settings
+
+from .conf import load
+from .registry import registry
+
+
+async def on_setup(settings=settings):
+    for alias, params in dict.items(getattr(settings, 'SECRETS', {})):
+        registry.add({**params, 'alias': alias})
+
+    load()
+
+
+async def boot():
+    pass
