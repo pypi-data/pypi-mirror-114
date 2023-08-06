16cc7c5bd6709ff18ffa462e038f64cbf1a46ee6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:42:45 +0000

    Add package nome/0.1.0/nome-0.1.0-py3-none-any.whl

---
+++ packages/nome/nome-0.1.0-py3-none-any.whl/nome/__init__.py
@@ -0,0 +1,3 @@
+"""A Python CLI to check if a package name is available in PyPI or npm."""
+
+__version__ = "0.1.0"
---
+++ packages/nome/nome-0.1.0-py3-none-any.whl/nome/constants.py
@@ -0,0 +1,6 @@
+PYPI_BASE_URL: str = "https://pypi.org/project/"
+NPM_BASE_URL: str = "https://www.npmjs.com/package/"
+
+HTTP_STATUS_CODE_MSG: str = "<comment>HTTP response status code: {code} ({desc})</comment>"
+AVAILABLE_MSG: str = "<fg=green;options=bold>{name}</> is available!"
+NOT_AVAILABLE_MSG: str = "<fg=red;options=bold>{name}</> ({url}) is not available..."
