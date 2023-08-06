87b5e96e34f2bdb853c927be8dceb13afcf26c96
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:11:50 +0000

    Add package bdrxml/1.4/bdrxml-1.4.tar.gz

---
+++ packages/bdrxml/bdrxml-1.4.tar.gz/bdrxml-1.4/README.md
@@ -0,0 +1,5 @@
+bdrxml
+======
+[![Tests Status](https://github.com/Brown-University-Library/bdrxml/workflows/CI%20tests/badge.svg)](https://github.com/Brown-University-Library/bdrxml/actions)
+
+Reading and writing XML for the Fedora-based BDR.
---
+++ packages/bdrxml/bdrxml-1.4.tar.gz/bdrxml-1.4/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(name='bdrxml',
+    version='1.4',
+    url='https://github.com/Brown-University-Library/bdrxml',
+    author='Brown University Library',
+    author_email='bdr@brown.edu',
+    packages=find_packages(),
+    package_data={'bdrxml': ['schemas/*.*']},
+    install_requires=[
+        'eulxml==1.1.3',
+        'ply==3.8',
+        'rdflib'
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
+
