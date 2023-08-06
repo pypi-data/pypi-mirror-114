04bb2cc5b72ff839d8ffcb7cc2cf332eed0573f3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:15:08 +0000

    Add package webmesh/0.1.1/webmesh-0.1.1.tar.gz

---
+++ packages/webmesh/webmesh-0.1.1.tar.gz/webmesh-0.1.1/src/webmesh/message_protocols.py
@@ -0,0 +1,20 @@
+from abc import ABC, abstractmethod
+from typing import Tuple, Any, Optional
+
+
+class AbstractMessageProtocol(ABC):
+    @abstractmethod
+    def unpack(self, message: Any) -> Tuple[str, Any]:
+        pass
+
+    @abstractmethod
+    def pack(self, target: Optional[str], message: Any) -> Any:
+        pass
+
+
+class SimpleDictProtocol(AbstractMessageProtocol):
+    def unpack(self, message: Any) -> Tuple[str, Any]:
+        return message['path'], message['data'] if 'data' in message else None
+
+    def pack(self, target: Optional[str], message: Any) -> Any:
+        return {'path': target, 'data': message}
