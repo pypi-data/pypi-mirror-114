6ca5d4c419bd4cc16b3fb1a25e90bbf56f036f5a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:30:49 +0000

    Add package chromatic-lightcurves/0.0.2/chromatic-lightcurves-0.0.2.tar.gz

---
+++ packages/chromatic-lightcurves/chromatic-lightcurves-0.0.2.tar.gz/chromatic-lightcurves-0.0.2/chromatic/imports.py
@@ -0,0 +1,45 @@
+# basics
+import numpy as np
+import matplotlib.pyplot as plt, matplotlib
+import copy, pkg_resources, os, glob, fnmatch, pickle
+from tqdm import tqdm
+
+import warnings, textwrap
+
+
+def custom_formatwarning(message, *args, **kwargs):
+    # ignore everything except the message
+    return f"\n🌈 Warning: {textwrap.dedent(str(message))}"
+
+
+warnings.formatwarning = custom_formatwarning
+
+# astropy
+from astropy.io import ascii, fits
+import astropy.units as u
+import astropy.constants as con
+from astropy.visualization import quantity_support
+
+# slightly fancier visualization tools
+from matplotlib.gridspec import GridSpec, GridSpecFromSubplotSpec
+
+from scipy.interpolate import interp1d
+
+# For modelling transits.
+import batman
+
+# define a driectory where we can put any necessary data files
+data_directory = pkg_resources.resource_filename("chromatic", "data")
+
+
+def expand_filenames(filepath):
+    """
+    A wrapper to expand a string or list into a list of filenames.
+    """
+    if type(filepath) == list:
+        filenames = filepath
+    elif "*" in filepath:
+        filenames = np.sort(glob.glob(filepath))
+    else:
+        filenames = [filepath]
+    return filenames
