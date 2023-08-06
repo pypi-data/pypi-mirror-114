1386cb76b7b2c02f3afb97f5e2ba4ffa6d470e1c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:22:25 +0000

    Add package TestPackage-0495/1.0.0/TestPackage_0495-1.0.0-py3-none-any.whl

---
+++ packages/TestPackage-0495/TestPackage_0495-1.0.0-py3-none-any.whl/testpackage/__init__.py
@@ -0,0 +1,10 @@
+class TestPack:
+    def __init__(self, packageName, packageVersion):
+        self.packageName = packageName
+        self.packageVersion = packageVersion
+    def name(self):
+        return self.packageName
+
+    def version(self):
+        return self.packageVersion
+    
