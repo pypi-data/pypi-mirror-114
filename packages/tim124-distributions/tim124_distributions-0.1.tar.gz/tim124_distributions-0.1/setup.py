c786ee95be5fc8eb84c8ee717a2714c3ad19d2df
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:10:16 +0000

    Add package tim124-distributions/0.1/tim124_distributions-0.1.tar.gz

---
+++ packages/tim124-distributions/tim124_distributions-0.1.tar.gz/tim124_distributions-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name = 'tim124_distributions',
+      version = '0.1',
+      description = 'Gaussian and Binomial distributions',
+      packages = ['tim124_distributions'],
+      author = "Tim Chan",
+      author_email = "timchansdp@gmail.com",
+      zip_safe=False)
