c1d173abe1c7501054fe249773947d1eb78091e2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:42:59 +0000

    Add package kd-med/0.0.7/kd_med-0.0.7.tar.gz

---
+++ packages/kd-med/kd_med-0.0.7.tar.gz/kd_med-0.0.7/kd_med/kd_loss.py
@@ -0,0 +1,123 @@
+# -*- coding: utf-8 -*-
+# @Time    : 7/26/21 6:59 PM
+# @Author  : Jingnan
+# @Email   : jiajingnan2222@gmail.com
+import math
+
+import torch
+import torch.nn as nn
+from kd_med.pre_trained_enc import PreTrainedEnc
+
+
+class EncPlusConv(nn.Module):
+    """
+    the out_chn of enc_s must be equal to the in_chn of conv.
+    """
+    def __init__(self, enc_s, conv):
+        super().__init__()
+        self.enc_s = enc_s
+        self.conv = conv
+
+    def forward(self, x):
+        out = self.enc_s(x)
+        out = self.conv(out)
+        return out
+
+
+class GetEncSConv:
+    """
+    Using class method to make sure the network only be created once and reusable many times.
+    """
+    enc_t = None
+    enc_s = None
+    enc_plus_conv = None
+    chn_t = None
+    chn_s = None
+    dims = 3
+
+
+    @classmethod
+    def set_conv_config(cls, dim1_t, dim1_s):
+        # o = (n - f + 2 * p) / s + 1
+        # f = n - ((o - 1) * s - 2 * p)
+        # p = ((o - 1) * s - n + f)/2
+        if dim1_t == dim1_s:  # no need of conv at all
+            cls.enc_plus_conv = cls.enc_s
+            return cls.enc_plus_conv
+        elif dim1_t > dim1_s:
+            raise Exception(f'teacher model depth is less than student model, dim1_t: {dim1_t}, dim1_s: {dim1_s}')
+        else:  # teacher model is deeper
+            s = math.ceil(dim1_s / dim1_t) # down sample using stride at first, pad more if over down-sampling
+            conv_sz = s + 1  # conv size should be bigger than stride
+            exprected_in_size = s * (dim1_t - 1) + conv_sz
+            p = math.ceil((exprected_in_size - dim1_s) / 2)
+            if cls.dims == 3:
+                conv = nn.Conv3d(cls.chn_s, cls.chn_t, kernel_size=(conv_sz, conv_sz, conv_sz),
+                                 stride=(s, s, s), padding=p)
+            else:
+                conv = nn.Conv2d(cls.chn_s, cls.chn_t, kernel_size=(conv_sz, conv_sz),
+                                 stride=(s, s), padding=p)
+            cls.enc_plus_conv = EncPlusConv(cls.enc_s, conv)
+
+    @classmethod
+    def get(cls, enc_t: nn.Module, enc_s: nn.Module, dims: int = 3):
+        """
+        Chn_in = 1 always here.
+        :param enc_t:
+        :param enc_s:
+        :param dims:
+        :return:
+        """
+        # print(f"cls.enc_t: {cls.enc_t}, cls.enc_s: {cls.enc_s}")
+        if cls.enc_t is not enc_t:  # when coming encoder is not the same as the stored one
+
+            cls.dims = dims
+            cls.enc_t = enc_t
+            cls.enc_s = enc_s
+            if dims == 3:
+                input_tmp = torch.ones((2, 1, 128, 128, 128))
+                out_t = cls.enc_t(input_tmp)
+                out_s = cls.enc_s(input_tmp)
+                batch_size, cls.chn_t, dim1_t, dim2_t, dim3_t = out_t.shape
+                batch_size, cls.chn_s, dim1_s, dim2_s, dim3_s = out_s.shape
+            else:
+                input_tmp = torch.ones((2, 1, 128, 128))
+                out_t = cls.enc_t(input_tmp)
+                out_s = cls.enc_s(input_tmp)
+                batch_size, cls.chn_t, dim1_t, dim2_t = out_t.shape
+                batch_size, cls.chn_s, dim1_s, dim2_s = out_s.shape
+
+            cls.set_conv_config(dim1_t, dim1_s)
+
+        return cls.enc_plus_conv
+
+
+def kd_loss(batch_x: torch.Tensor,
+            enc_s: nn.Module,
+            net_t_name: str ='resnet3d_34'):
+    """
+    The enc_s will share the same memory with enc_s_conv, and the enc_s_conv will be optimized by the loss of kd.
+    todo: I have to put enc_s_conv to outsize otherwise the last conv will not be updated at all !!!
+    :param batch_x:
+    :param enc_s:
+    :param net_t_name:
+    :return:
+    """
+
+    if len(batch_x.shape) == 5:
+        dims: int = 3
+    elif len(batch_x.shape) == 4:
+        dims = 2
+    else:
+        raise Exception(f'shape of batch_x: {batch_x.shape} is not correct')
+
+    enc_t = PreTrainedEnc.get(net_t_name)
+    # enc_s share the same memory with the enc_s in enc_s_conv, only create it once and reuse it
+    enc_s_conv = GetEncSConv().get(enc_t, enc_s, dims)
+    with torch.no_grad():
+        out_t = enc_t(batch_x)
+    out_s = enc_s_conv(batch_x)
+    loss = nn.MSELoss()
+    kd_loss = loss(out_t, out_s)
+    return kd_loss
+
---
+++ packages/kd-med/kd_med-0.0.7.tar.gz/kd_med-0.0.7/tests/test_kd_loss.py
@@ -0,0 +1,138 @@
+# -*- coding: utf-8 -*-
+# @Time    : 7/28/21 8:34 PM
+# @Author  : Jingnan
+# @Email   : jiajingnan2222@gmail.com
+import unittest
+import tempfile
+import os
+import torch
+import torch.nn as nn
+
+from parameterized import parameterized
+from kd_med.kd_loss import kd_loss
+
+
+class Cnn2_3dEnc(nn.Module):
+    def __init__(self, fc1_nodes=1024, fc2_nodes=1024, num_classes: int = 5, base: int = 8, level_node = 0):
+        super().__init__()
+        self.level_node = level_node
+        self.features = nn.Sequential(
+            nn.Conv3d(1, base, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.MaxPool3d(kernel_size=3, stride=2),
+            nn.Conv3d(base, base * 2, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.MaxPool3d(kernel_size=3, stride=2),
+            # nn.Conv3d(base * 2, base * 4, kernel_size=3, padding=1),
+            # nn.ReLU(inplace=True),
+            # nn.MaxPool3d(kernel_size=3, stride=2),
+        )
+        self.avgpool = nn.AdaptiveAvgPool3d((6, 6, 6))
+        self.classifier = nn.Sequential(
+            nn.Dropout(),
+            nn.Linear(base * 2 * 6 * 6 * 6, fc1_nodes),
+            nn.ReLU(inplace=True),
+            nn.Dropout(),
+            nn.Linear(fc1_nodes, fc2_nodes),
+            nn.ReLU(inplace=True),
+            nn.Dropout(),
+            nn.Linear(fc2_nodes, num_classes),
+        )
+
+    def forward(self, input):  # input would be a tuple of size (1,) if only one element is input
+        if self.level_node == 0:
+            x = input
+        else:
+            x, level = input[0], input[1]
+
+        x = self.features(x)
+        # x = self.avgpool(x)
+        # x = torch.flatten(x, 1)
+        # x = self.classifier(x)
+        return x
+
+
+class Vgg11_3dEnc(nn.Module):
+    def __init__(self, fc1_nodes=1024, fc2_nodes=1024, num_classes: int = 5, base: int = 8, level_node = 0):
+        super().__init__()
+        self.num_classes = num_classes
+        self.base = base
+        self.level_node = level_node
+
+        self.features = nn.Sequential(
+            nn.Conv3d(1, base, 3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.BatchNorm3d(base),
+            nn.MaxPool3d(kernel_size=3, stride=2),
+
+            nn.Conv3d(base, base * 2, 3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.BatchNorm3d(base * 2),
+            nn.MaxPool3d(kernel_size=3, stride=2),
+
+            nn.Conv3d(base * 2, base * 4, 3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.BatchNorm3d(base * 4),
+            nn.Conv3d(base * 4, base * 4, 3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.BatchNorm3d(base * 4),
+            nn.MaxPool3d(kernel_size=3, stride=2),
+
+            nn.Conv3d(base * 4, base * 8, 3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.BatchNorm3d(base * 8),
+            nn.Conv3d(base * 8, base * 8, 3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.BatchNorm3d(base * 8),
+            nn.MaxPool3d(kernel_size=3, stride=2),
+
+            nn.Conv3d(base * 8, base * 16, 3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.BatchNorm3d(base * 16),
+            nn.Conv3d(base * 16, base * 16, 3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.BatchNorm3d(base * 16),
+            nn.MaxPool3d(kernel_size=3, stride=2),)
+
+        self.avgpool = nn.AdaptiveAvgPool3d((6, 6, 6))
+        self.ft = nn.Flatten()
+        self.dp1 = nn.Dropout()
+
+        if self.level_node != 0:
+            nb_fc0 = base * 16 * 6 * 6 * 6 + 1
+        else:
+            nb_fc0 = base * 16 * 6 * 6 * 6
+
+        self.ln1 = nn.Linear(nb_fc0, fc1_nodes)
+        self.rl1 = nn.ReLU(inplace=True)
+
+        self.dp2 = nn.Dropout()
+        self.ln2 = nn.Linear(fc1_nodes, fc2_nodes)
+        self.rl2 = nn.ReLU(inplace=True)
+
+        self.dp3 = nn.Dropout()
+        self.ln3 = nn.Linear(fc2_nodes, self.num_classes)
+
+    def forward(self, input):  # input would be a tuple of size (1,) if only one element is input
+        x = self.features(input)
+
+        return x
+
+dims = 3
+batch_x = torch.ones((2, 1, 96, 96, 64))
+enc_s_ls = [Cnn2_3dEnc()]  # todo: VGG11
+enc_t_name_ls = ['resnet3d_18', 'resnet3d_18', 'resnet3d_34','resnet3d_50', 'resnet3d_101']
+
+
+
+class Testkd_loss(unittest.TestCase):
+    def test_kd_loss(self):
+        for enc_s in enc_s_ls:
+            for enc_t_name in enc_t_name_ls:
+                kd_loss(batch_x, enc_s, enc_t_name)
+
+
+
+if __name__ == "__main__":
+    unittest.main()
+
