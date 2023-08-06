22082f6f7e5ce22a93857f1e631d9d9c81ac021f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:23:44 +0000

    Add package botd/47/botd-47.tar.gz

---
+++ packages/botd/botd-47.tar.gz/botd-47/setup.py
@@ -0,0 +1,61 @@
+# This file is placed in the Public Domain.
+
+import os
+import shutil
+
+from setuptools import setup
+from setuptools.command.install import install
+
+p = "/usr/local/share/botd/botd.service"
+d = "/etc/systemd/system/botd.service"
+
+def read():
+    return open("README.rst", "r").read()
+
+
+class Install(install):
+
+    def post_install(self):
+        if not os.path.exists(d):
+            shutil.copyfile(p, d)
+
+    def run(self):
+        super().do_egg_install()
+        self.post_install()
+
+setup(
+    name="botd",
+    version="47",
+    url="https://github.com/bthate/botd",
+    author="Bart Thate",
+    author_email="bthate@dds.nl",
+    description="24/7 channel daemon",
+    long_description=read(),
+    license="Public Domain",
+    zip_safe=True,
+    include_package_data=True,
+    py_modules=["ob", "trm"],
+    packages=["bot"],
+    data_files=[
+        (
+            "share/botd/",
+            [
+                "files/bot.1.md",
+                "files/botctl.8.md",
+                "files/botd.8.md",
+                "files/botd.service",
+                "files/botd",
+            ],
+        ),
+    ],
+    scripts=["bin/bot", "bin/botcmd", "bin/botctl", "bin/botd"],
+    cmdclass={'install': Install},
+    test_suite="test.run",
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "License :: Public Domain",
+        "Operating System :: Unix",
+        "Programming Language :: Python",
+        "Topic :: Utilities",
+    ],
+)
