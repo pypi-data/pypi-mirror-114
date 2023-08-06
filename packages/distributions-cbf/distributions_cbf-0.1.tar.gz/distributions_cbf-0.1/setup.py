3f386d73c1c4fb5d5686f3f0b1f322657ef28d9f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:19:31 +0000

    Add package distributions-cbf/0.1/distributions_cbf-0.1.tar.gz

---
+++ packages/distributions-cbf/distributions_cbf-0.1.tar.gz/distributions_cbf-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='distributions_cbf',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['distributions_cbf'],
+      author= 'Cesar Blanco',
+      author_email= 'cblanco@learning.eoi.es',
+      zip_safe=False)
