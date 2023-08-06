bbaef72c3036330ec60a4fbd1d6c7627efd25df9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:14:00 +0000

    Add package detection-tools/0.2/detection_tools-0.2.tar.gz

---
+++ packages/detection-tools/detection_tools-0.2.tar.gz/detection_tools-0.2/setup.py
@@ -0,0 +1,17 @@
+"""Setup script for detection tools"""
+
+from setuptools import find_packages
+from setuptools import setup
+
+
+REQUIRED_PACKAGES = ['Pillow>=1.0', 'Matplotlib>=2.1', 'Cython>=0.28.1']
+
+setup(
+    name='detection_tools',
+    version='0.2',
+    install_requires=REQUIRED_PACKAGES,
+    include_package_data=True,
+    packages=[p for p in find_packages() if
+    p.startswith('detection_tools')],
+    description='Tensorflow Object Detection Tools Library',
+)
