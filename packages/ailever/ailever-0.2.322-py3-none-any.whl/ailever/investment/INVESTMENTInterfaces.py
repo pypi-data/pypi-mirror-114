722e83ec080062a3d0396e98242ff44b2c832ab0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:25:46 +0000

    Add package ailever/0.2.322/ailever-0.2.322-py3-none-any.whl

---
+++ packages/ailever/ailever-0.2.322-py3-none-any.whl/ailever/investment/INVESTMENTInterfaces.py
@@ -0,0 +1,3 @@
+from .initializer import initialize
+from .parallelizer import parallelize
+from .screening_markets import reits_screening
---
+++ packages/ailever/ailever-0.2.322-py3-none-any.whl/ailever/investment/__init__.py
@@ -0,0 +1,6 @@
+from .INVESTMENTInterfaces import initialize
+from .INVESTMENTInterfaces import parallelize
+from .INVESTMENTInterfaces import reits_screening
+
+
+
