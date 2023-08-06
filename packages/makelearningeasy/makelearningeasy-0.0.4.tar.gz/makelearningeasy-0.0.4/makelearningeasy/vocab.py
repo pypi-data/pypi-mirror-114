e22347a81fc6805d5391d38e4327be2103751d31
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:12:22 +0000

    Add package makelearningeasy/0.0.4/makelearningeasy-0.0.4.tar.gz

---
+++ packages/makelearningeasy/makelearningeasy-0.0.4.tar.gz/makelearningeasy-0.0.4/makelearningeasy/vocab.py
@@ -0,0 +1,49 @@
+"""
+vocab
+"""
+
+from selenium import webdriver
+from IPython.display import Image
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from IPython.display import display, Image
+import PIL.Image
+from io import BytesIO
+import cv2
+import numpy as np
+import time
+
+def meaning(word):
+
+    wd = webdriver.Chrome("/Users/sainagimmidisetty/Downloads/chromedriver")
+    wd.maximize_window()
+    wd.get("https://www.google.com/")
+    meaning = WebDriverWait(wd, 20).until(EC.presence_of_element_located((By.XPATH, "//input[@title='Search']")))
+    meaning.send_keys(word)
+     # + " " +"meaning")
+    meaning.send_keys(Keys.ENTER)
+    wd.save_screenshot('img/pic.png')
+    return Image("img/pic.png")
+
+def wikipedia(word):
+
+    wd = webdriver.Chrome("/Users/sainagimmidisetty/Downloads/chromedriver")
+    wd.maximize_window()
+    word.replace(" ","_")
+    wd.get("https://en.wikipedia.org/wiki/" + word +)
+    time.sleep(10)
+    wd.save_screenshot('img/pic.png')
+    return Image("img/pic.png".
+
+def show_image(img, fmt='png'):
+    a = np.uint8(img)
+    f = BytesIO()
+    PIL.Image.fromarray(a).save(f, fmt)
+    display(Image(data=f.getvalue()))
+
+def display_all_images(lower_bound,upper_bound):
+    for i in range(lower_bound,upper_bound):
+        img = cv2.imread("img/pic " + str(i) + ".png")
+        show_image(img)
---
+++ packages/makelearningeasy/makelearningeasy-0.0.4.tar.gz/makelearningeasy-0.0.4/setup.py
@@ -0,0 +1,40 @@
+from setuptools import setup
+import os
+import sys
+
+if sys.version_info < (3, 5):
+    sys.exit('Sorry, Python < 3.5 is not supported.')
+# Utility function to read the README file.
+# Used for the long_description.  It's nice, because now 1) we have a top level
+# README file and 2) it's easier to type in the README file than to put a raw
+# string in below ...
+
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+
+with open('makelearningeasy/__init__.py', 'rb') as fid:
+    for line in fid:
+        line = line.decode('utf-8')
+        if line.startswith('__version__'):
+            version = line.strip().split()[-1][1:-1]
+            break
+
+download_url = ('https://github.com/MyOpenSource-Projects/makelearningeasy/\
+                blob/main/dist/makelearningeasy-0.0.4.whl')
+
+setup(name = 'makelearningeasy',
+      packages = ['makelearningeasy'],
+      version = '0.0.4',
+      description = 'A personal project to improve my learning process',
+      author = 'David Foster Wallace',
+      author_email = 'davidfosterwallace@gmail.com',
+      license = 'Apache2',
+      url = 'https://github.com/MyOpenSource-Projects/makelearningeasy',
+      install_requires = [ 'selenium', 'opencv-python', 'numpy', 'Pillow'],
+      download_url = 'https://github.com/MyOpenSource-Projects/makelearningeasy/archive/0.0.4.tar.gz',
+      keywords = ['makelearningeasy', 'easylearning'],
+      classifiers = [],
+      python_requires='>=3',
+      )
