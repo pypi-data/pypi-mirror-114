8dd10355527f1d77dd3637d26d9f7b0f97f71881
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:30:36 +0000

    Add package FITSxtractor/0.2/FITSxtractor-0.2.tar.gz

---
+++ packages/FITSxtractor/FITSxtractor-0.2.tar.gz/FITSxtractor-0.2/setup.py
@@ -0,0 +1,29 @@
+from distutils.core import setup
+
+setup(
+  name = 'FITSxtractor',
+  packages = ['FITSxtractor'],
+  version = '0.2',
+  license='MIT',
+  description = 'This package extracts xml metadata from FITS output to a csv or xlsx file',
+  author = 'ovanov',
+  author_email = 'ovanov@protonmail.com',
+  url = 'https://github.com/ovanov/FITSxtractor',
+  keywords = ['FITS', 'xml', 'csv', 'CLI programm'],
+  install_requires=[
+          'pandas>=1.3.1',
+          'openpyxl>=3.0.7',
+          'tqdm==4.61.2'
+      ],
+  classifiers=[
+    'Development Status :: 3 - Alpha',
+    'Intended Audience :: Developers',
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7',
+    'Programming Language :: Python :: 3.8',
+    'Programming Language :: Python :: 3.9',
+  ],
+)
