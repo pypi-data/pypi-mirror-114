aa8493daa1a2516a6a7fb7315bc07a68d6912eb0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:13:59 +0000

    Add package pitci/0.2.0/pitci-0.2.0.tar.gz

---
+++ packages/pitci/pitci-0.2.0.tar.gz/pitci-0.2.0/pitci/dispatchers.py
@@ -0,0 +1,41 @@
+"""
+Module containing functions used to return the correct conformal predictor
+class given the underlying model type.
+"""
+
+from functools import singledispatch
+
+
+@singledispatch
+def get_absolute_error_conformal_predictor(model):
+    """Function to return the appropriate child class of
+    AbsoluteErrorConformalPredictor depending on the type
+    of the model arg.
+    """
+
+    raise NotImplementedError(
+        f"model type not supported for AbsoluteErrorConformalPredictor children; {type(model)}"
+    )
+
+
+@singledispatch
+def get_leaf_node_scaled_conformal_predictor(model):
+    """Function to return the appropriate child class of
+    LeafNodeScaledConformalPredictor depending on the type
+    of the model arg.
+    """
+
+    raise NotImplementedError(
+        f"model type not supported for LeafNodeScaledConformalPredictor children; {type(model)}"
+    )
+
+
+@singledispatch
+def get_leaf_node_split_conformal_predictor(model, n_bins=3):
+    """Function to return the appropriate child class of
+    SplitConformalPredictor depending on the type of the model arg.
+    """
+
+    raise NotImplementedError(
+        f"model type not supported for SplitConformalPredictor children; {type(model)}"
+    )
