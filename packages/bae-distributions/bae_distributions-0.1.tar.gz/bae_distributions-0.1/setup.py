534a8dfcffecba70aa04c99565778ef5d3d02182
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:27:56 +0000

    Add package bae-distributions/0.1/bae_distributions-0.1.tar.gz

---
+++ packages/bae-distributions/bae_distributions-0.1.tar.gz/bae_distributions-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='bae_distributions',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['bae_distributions'],
+      author = 'Inbae Kang',
+      author_email = 'kibae92@gmail.com',
+      zip_safe=False)
