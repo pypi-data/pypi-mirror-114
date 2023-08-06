1879de06c5311a0bc437d4afdd970bb7b0852e04
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:40:17 +0000

    Add package EnergyPlus-EpJSON-Transition-Tool/0.2/EnergyPlus%20EpJSON%20Transition%20Tool-0.2.tar.gz

---
+++ packages/EnergyPlus-EpJSON-Transition-Tool/EnergyPlus%20EpJSON%20Transition%20Tool-0.2.tar.gz/EnergyPlus EpJSON Transition Tool-0.2/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name="EnergyPlus EpJSON Transition Tool",
+    version="0.2",
+    description="A tool for converting EpJSON files which may need changing from one version to another",
+    url='https://energyplus.net',
+    license='ModifiedBSD',
+)
