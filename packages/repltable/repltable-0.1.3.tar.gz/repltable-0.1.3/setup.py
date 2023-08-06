fffa36db664c2c284a6874da54451ace5399ab67
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:13:41 +0000

    Add package repltable/0.1.3/repltable-0.1.3.tar.gz

---
+++ packages/repltable/repltable-0.1.3.tar.gz/repltable-0.1.3/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['repltable']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['replit>=3.2.1,<4.0.0']
+
+setup_kwargs = {
+    'name': 'repltable',
+    'version': '0.1.3',
+    'description': 'Table support for the replit database',
+    'long_description': None,
+    'author': 'terabyte.',
+    'author_email': 'mikey@mikeyo.ml',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
