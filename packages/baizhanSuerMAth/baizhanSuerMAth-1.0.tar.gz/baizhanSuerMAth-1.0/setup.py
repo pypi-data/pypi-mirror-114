19713fe8bf26e2173a19812c1d4d1eadabc1d2b1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:10:50 +0000

    Add package baizhanSuerMAth/1.0/baizhanSuerMAth-1.0.tar.gz

---
+++ packages/baizhanSuerMAth/baizhanSuerMAth-1.0.tar.gz/baizhanSuerMAth-1.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+    name='baizhanSuerMAth',
+    version='1.0',
+    description='这是第一个对外发布的模块，测试哦',
+    author='gaoqi',
+    author_email='gaoqi110@163.com',
+    py_modules=['baizhanSUperMAth.demo1','baizhanSuperMAth.demo2']
+)
