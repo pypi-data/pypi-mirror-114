b6bd68d8fc6365f9a60cfbe5dbfd0dff9938a6f7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:40:30 +0000

    Add package custom-output/0.0.1/custom_output-0.0.1.tar.gz

---
+++ packages/custom-output/custom_output-0.0.1.tar.gz/custom_output-0.0.1/setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+from setuptools import setup
+
+setup(
+    name = 'custom_output',
+    version = '0.0.1',
+    author = 'thomassoni',
+    author_email = 'thomassoniofc@gmail.com',
+    packages = ['custom_output'], 
+    url = 'https://github.com/thomassoni/custom_output',
+    project_urls = {
+        'Código fonte': 'https://github.com/thomassoni/custom_output',
+        'tracker' : 'https://github.com/thomassoni/custom_output/issues'
+
+    }
+)
