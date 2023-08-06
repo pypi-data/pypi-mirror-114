75bccadf898513cb89fc5af56e12a351afe7144d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:14:22 +0000

    Add package RockyRoad/0.0.52/RockyRoad-0.0.52-py3-none-any.whl

---
+++ packages/RockyRoad/RockyRoad-0.0.52-py3-none-any.whl/rockyroad/modules/warranty.py
@@ -0,0 +1,52 @@
+from .module_imports import *
+
+
+@headers({"Ocp-Apim-Subscription-Key": key})
+class Warranty(Consumer):
+    """Inteface to Warranty resource for the RockyRoad API."""
+
+    def __init__(self, Resource, *args, **kw):
+        self._base_url = Resource._base_url
+        super().__init__(base_url=Resource._base_url, *args, **kw)
+
+    def creditRequest(self):
+        return self.__Credit_Request(self)
+
+    @headers({"Ocp-Apim-Subscription-Key": key})
+    class __Credit_Request(Consumer):
+        """Inteface to Warranty Credit Request resource for the RockyRoad API."""
+
+        def __init__(self, Resource, *args, **kw):
+            super().__init__(base_url=Resource._base_url, *args, **kw)
+
+        @returns.json
+        @get("warranty/credit-requests")
+        def list(
+            self,
+            uid: Query(type=str) = None,
+            claimReference: Query(type=str) = None,
+        ):
+            """This call will return detailed warranty credit request information for the specified criteria."""
+
+        @returns.json
+        @delete("warranty/credit-requests")
+        def delete(self, uid: Query(type=str)):
+            """This call will delete the maintenance interval for the specified uid."""
+
+        @returns.json
+        @json
+        @post("warranty/credit-requests")
+        def insert(self, creditRequest: Body):
+            """This call will create a warranty credit request with the specified parameters."""
+
+        @returns.json
+        @json
+        @patch("warranty/credit-requests")
+        def update(self, creditRequest: Body):
+            """This call will update the warranty credit request with the specified parameters."""
+
+        @returns.json
+        @multipart
+        @post("warranty/uploadfiles")
+        def upload(self, files: Part):
+            """This call will create a warranty credit request with the specified parameters."""
