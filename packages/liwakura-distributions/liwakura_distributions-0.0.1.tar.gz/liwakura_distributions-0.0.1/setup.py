9ec1a42e0dd386eec41570167f0cbc7e241ea580
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:40:11 +0000

    Add package liwakura-distributions/0.0.1/liwakura_distributions-0.0.1.tar.gz

---
+++ packages/liwakura-distributions/liwakura_distributions-0.0.1.tar.gz/liwakura_distributions-0.0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='liwakura_distributions',
+      version='0.0.1',
+      description='Gaussian distributions',
+      packages=['liwakura_distributions'],
+      author = 'Lain Iwakura',
+      author_email = 'dad0bc1a.4c35.4a3f.851c.bae30d+pypi@gmail.com',
+      zip_safe=False)
