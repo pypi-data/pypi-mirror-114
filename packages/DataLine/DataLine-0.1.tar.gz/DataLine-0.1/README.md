8cd86139c6893177789cf27d5a79d31bdfd10369
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:51:00 +0000

    Add package DataLine/0.1/DataLine-0.1.tar.gz

---
+++ packages/DataLine/DataLine-0.1.tar.gz/DataLine-0.1/README.md
@@ -0,0 +1,18 @@
+# DataLine
+
+Simple class to aid the process of data cleaning.
+
+Install
+-------
+
+python setup.py install
+pip install dataline
+
+Use:
+----
+
+Please see example/example1.py.
+
+
+
+
---
+++ packages/DataLine/DataLine-0.1.tar.gz/DataLine-0.1/setup.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+from __future__ import print_function
+from __future__ import division
+from __future__ import absolute_import
+from __future__ import unicode_literals
+
+from setuptools import setup, find_packages
+
+INSTALL_REQUIRES = []
+
+if __name__ == "__main__":
+    setup(name='DataLine',
+        version='0.1'
+        , author=''
+        , author_email='tiagobotari@gmail.com'
+        , description="Simple class to aid the process of data cleaning."
+        , license='https://github.com/tiagobotari/dataline'
+        , url="https://github.com/tiagobotari/dataline"
+        , packages=find_packages()
+        , install_requires=INSTALL_REQUIRES
+        , zip_safe=False
+        , python_requires='>=3.6'
+    )
