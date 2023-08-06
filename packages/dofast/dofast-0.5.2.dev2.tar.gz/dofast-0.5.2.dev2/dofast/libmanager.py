e0ef2d8e8a9793e1c5d3f158ae6b3fa52e7c90eb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:08:44 +0000

    Add package dofast/0.5.2.dev2/dofast-0.5.2.dev2.tar.gz

---
+++ packages/dofast/dofast-0.5.2.dev2.tar.gz/dofast-0.5.2.dev2/dofast/libmanager.py
@@ -0,0 +1,22 @@
+import codefast as cf
+import setuptools
+
+
+def release():
+    cf.utils.shell('python3 setup.py sdist bdist_wheel', print_str=True)
+    cf.info('Package packed.')
+
+    cf.utils.shell('python3 -m twine upload -r formal --skip-existing dist/*',
+                   print_str=True)
+    cf.info('Formal package released.')
+
+    cf.utils.shell('python3 -m twine upload -r test --skip-existing dist/*',
+                   print_str=True)
+    cf.info('Test package released.')
+
+    cf.utils.shell('rm -r dist/*')
+    cf.utils.shell('rm -r build/*')
+    cf.utils.shell('rm -r *egg-info*')
+    cf.utils.shell('rm -r *__pycache__*')
+    cf.info('Byproducts cleaned.')
+    
