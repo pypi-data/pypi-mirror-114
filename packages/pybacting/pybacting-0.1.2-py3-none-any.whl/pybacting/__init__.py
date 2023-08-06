9a9b8897081fae6817e41158dc0eeace1ec6a4ae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:50:14 +0000

    Add package pybacting/0.1.2/pybacting-0.1.2-py3-none-any.whl

---
+++ packages/pybacting/pybacting-0.1.2-py3-none-any.whl/pybacting/__init__.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+
+"""A python wrapper around `bacting <https://github.com/egonw/bacting>`_."""
+
+from .api import cdk  # noqa:F401
+from .io import from_smiles  # noqa:F401
