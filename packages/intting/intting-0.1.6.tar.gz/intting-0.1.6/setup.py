a8906e70b6e99bf6fd59824a46f5bbabc6c13789
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:23:49 +0000

    Add package intting/0.1.6/intting-0.1.6.tar.gz

---
+++ packages/intting/intting-0.1.6.tar.gz/intting-0.1.6/setup.py
@@ -0,0 +1,20 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+  long_description = fh.read()
+
+setuptools.setup(
+  name="intting",
+  version="0.1.6",
+  author="zjy_090820",
+  author_email="a1234567890001919@outlook.com",
+  description="easy to user int (beta)",
+  long_description='This packge can help you to ctrl int better,and we will to hard to update the packge!',
+  url="https://pypi.org",
+  packages=setuptools.find_packages(),
+  classifiers=[
+  "Programming Language :: Python :: 3",
+  "License :: OSI Approved :: MIT License",
+  "Operating System :: OS Independent",
+  ],
+)
