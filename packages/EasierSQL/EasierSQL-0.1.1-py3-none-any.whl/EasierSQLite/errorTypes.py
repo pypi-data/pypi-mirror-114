e5886c13d8c39f91bc6d58c1fa195c69b103dac4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:32:04 +0000

    Add package EasierSQL/0.1.1/EasierSQL-0.1.1-py3-none-any.whl

---
+++ packages/EasierSQL/EasierSQL-0.1.1-py3-none-any.whl/EasierSQLite/errorTypes.py
@@ -0,0 +1,29 @@
+class createTableError(Exception):
+    pass
+
+class selectingError(Exception):
+    pass
+
+class insertingError(Exception):
+    pass
+
+class delete_table_error(Exception):
+    pass
+
+class create_column_error(Exception):
+    pass
+
+class delete_row_error(Exception):
+    pass
+
+class renameError(Exception):
+    pass
+
+class database_attach_error(Exception):
+    pass
+
+class database_detach_error(Exception):
+    pass
+
+class custom_query_error(Exception):
+    pass
