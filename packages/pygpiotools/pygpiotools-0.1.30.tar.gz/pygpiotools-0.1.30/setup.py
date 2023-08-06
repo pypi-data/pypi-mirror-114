089ddb1955e5ae816ff4c8dfff0ac7b74cd8028d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:16:04 +0000

    Add package pygpiotools/0.1.30/pygpiotools-0.1.30.tar.gz

---
+++ packages/pygpiotools/pygpiotools-0.1.30.tar.gz/pygpiotools-0.1.30/setup.py
@@ -0,0 +1,43 @@
+import os
+import pygpiotools
+from setuptools import setup
+from distutils.core import Extension
+
+fh = open("README.md", "r")
+long_description = fh.read()
+
+if os.name == 'nt':
+       #Windows is the usual mess. We'll need a native extension
+       pygptools = Extension('pygpiotools',
+                           sources = ['gpio-windows.c', "pl2303.c"])
+       modules = [pygptools]
+       define_macros = [('PYGPIOTOOLS_VERSION', pygpiotools.__version__)],
+       install_requires = []
+       packages = []
+else:
+       #libusb for all the unix folks, no native stuff
+       modules = []
+       install_requires = [
+              "pyusb>=1.0.0",
+              ]
+       packages = ["pygpiotools"]
+
+install_requires.append("parse")
+print(install_requires)
+
+setup( 
+       name = 'pygpiotools',
+       version = pygpiotools.__version__,
+       packages=packages,
+       include_package_data=True,
+       install_requires = install_requires,
+       description = 'Python tools & API to control GPIO pins of common USB_to_UART converters',
+       ext_modules = modules,
+       long_description_content_type='text/markdown',
+       long_description = long_description,
+       url = "http://github.com/RC-MODULE/rumboot-tools",
+       author = "Andrew Andrianov",
+       author_email = "andrew@ncrmnt.org",
+       license = "GPL",
+       platforms = "any",
+       )
