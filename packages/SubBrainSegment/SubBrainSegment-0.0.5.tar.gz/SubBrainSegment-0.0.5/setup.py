b93bcce8988f4421cf48b376709742f21a7e95fd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:42:36 +0000

    Add package SubBrainSegment/0.0.5/SubBrainSegment-0.0.5.tar.gz

---
+++ packages/SubBrainSegment/SubBrainSegment-0.0.5.tar.gz/SubBrainSegment-0.0.5/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+classifiers = [
+    'Intended Audience :: Developers',
+    'Programming Language :: Python :: 3.6',
+    'Environment :: GPU :: NVIDIA CUDA :: 10.0',
+    'License :: OSI Approved :: MIT License'
+]
+
+setup(
+     name='SubBrainSegment',
+     version='0.0.5',
+     description='Package for subcortical brain segmentation',
+     url='https://github.com/JENNSHIUAN',
+     author='JENNSHIUAN',
+     author_email='danny092608@gmail.com',
+     License='MIT',
+     classifiers=classifiers,
+     keywords='subcortical brain segmentation',
+     package=find_packages(),
+     install_requires=[
+             'numpy',
+             'nibabel',
+             'nilearn',
+             'SimpleITK',
+             'tables'
+         ]
+)
