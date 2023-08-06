aec1aa4db06990f0a267f3aee7041eae571b571b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:16:14 +0000

    Add package avocado-framework/90.0/avocado_framework-90.0-py3-none-any.whl

---
+++ packages/avocado-framework/avocado_framework-90.0-py3-none-any.whl/avocado/core/task/statemachine.py
@@ -0,0 +1,215 @@
+import asyncio
+import collections
+import multiprocessing
+import time
+
+
+class TaskStateMachine:
+    """Represents all phases that a task can go through its life."""
+
+    def __init__(self, tasks, status_repo):
+        self._requested = collections.deque(tasks)
+        self._status_repo = status_repo
+        self._triaging = []
+        self._ready = []
+        self._started = []
+        self._finished = []
+        self._lock = asyncio.Lock()
+
+    @property
+    def requested(self):
+        return self._requested
+
+    @property
+    def triaging(self):
+        return self._triaging
+
+    @property
+    def ready(self):
+        return self._ready
+
+    @property
+    def started(self):
+        return self._started
+
+    @property
+    def finished(self):
+        return self._finished
+
+    @property
+    def lock(self):
+        return self._lock
+
+    @property
+    async def complete(self):
+        async with self._lock:
+            pending = any([self._requested, self._triaging,
+                           self._ready, self._started])
+        return not pending
+
+
+class Worker:
+
+    def __init__(self, state_machine, spawner,
+                 max_triaging=None, max_running=None, task_timeout=None):
+        self._state_machine = state_machine
+        self._spawner = spawner
+        if max_triaging is None:
+            max_triaging = multiprocessing.cpu_count()
+        self._max_triaging = max_triaging
+        if max_running is None:
+            max_running = 2 * multiprocessing.cpu_count() - 1
+        self._max_running = max_running
+        self._task_timeout = task_timeout
+
+    async def bootstrap(self):
+        """Reads from requested, moves into triaging."""
+        try:
+            async with self._state_machine.lock:
+                if len(self._state_machine.triaging) < self._max_triaging:
+                    runtime_task = self._state_machine.requested.popleft()
+                    self._state_machine.triaging.append(runtime_task)
+                else:
+                    return
+        except IndexError:
+            return
+
+    async def triage(self):
+        """Reads from triaging, moves into either: ready or finished."""
+        async def check_finished_dependencies(runtime_task):
+            for task in runtime_task.task.dependencies:
+                # check if this dependency `task` failed on triage
+                # this is needed because this kind of task fail does not
+                # have information in the status repo
+                for finished_rt_task in self._state_machine.finished:
+                    if (finished_rt_task.task is task and
+                            finished_rt_task.status == 'FAILED ON TRIAGE'):
+                        await fail_on_triage(runtime_task)
+                        return
+                # from here, this dependency `task` ran, so, let's check
+                # the its latest data in the status repo
+                latest_task_data = \
+                    self._state_machine._status_repo.get_latest_task_data(
+                        str(task.identifier))
+                # maybe, the latest data is not available yet
+                if latest_task_data is None:
+                    async with self._state_machine.lock:
+                        self._state_machine.triaging.append(runtime_task)
+                    await asyncio.sleep(0.1)
+                    return
+                # if this dependency task failed, skip the parent task
+                if latest_task_data['result'] not in ['pass']:
+                    await fail_on_triage(runtime_task)
+                    return
+            # everything is fine!
+            return True
+
+        async def fail_on_triage(runtime_task):
+            async with self._state_machine.lock:
+                self._state_machine.finished.append(runtime_task)
+                runtime_task.status = 'FAILED ON TRIAGE'
+
+        try:
+            async with self._state_machine.lock:
+                runtime_task = self._state_machine.triaging.pop(0)
+        except IndexError:
+            return
+
+        # a task waiting dependencies already checked its requirements
+        if runtime_task.status != 'WAITING DEPENDENCIES':
+            # check for requirements a task may have
+            requirements_ok = (
+                    await self._spawner.check_task_requirements(runtime_task))
+            if not requirements_ok:
+                await fail_on_triage(runtime_task)
+                return
+
+        # handle task dependencies
+        if runtime_task.task.dependencies:
+            # check of all the dependency tasks finished
+            async with self._state_machine.lock:
+                finished_tasks = [rt_task.task for rt_task
+                                  in self._state_machine.finished]
+            if not runtime_task.task.dependencies.issubset(finished_tasks):
+                async with self._state_machine.lock:
+                    self._state_machine.triaging.append(runtime_task)
+                    runtime_task.status = 'WAITING DEPENDENCIES'
+                await asyncio.sleep(0.1)
+                return
+
+            # dependencies finished, let's check if they finished
+            # successfully, so we can move on with the parent task
+            dependencies_ok = await check_finished_dependencies(runtime_task)
+            if not dependencies_ok:
+                return
+
+        # the task is ready to run
+        async with self._state_machine.lock:
+            self._state_machine.ready.append(runtime_task)
+
+    async def start(self):
+        """Reads from ready, moves into either: started or finished."""
+        try:
+            async with self._state_machine.lock:
+                runtime_task = self._state_machine.ready.pop(0)
+        except IndexError:
+            return
+
+        # enforce a rate limit on the number of started (currently
+        # running) tasks.  this is a global limit, but the spawners
+        # can also be queried with regards to their capacity to handle
+        # new tasks
+        should_wait = False
+        async with self._state_machine.lock:
+            if len(self._state_machine.started) >= self._max_running:
+                self._state_machine.ready.insert(0, runtime_task)
+                runtime_task.status = 'WAITING'
+                should_wait = True
+        if should_wait:
+            await asyncio.sleep(0.1)
+            return
+
+        start_ok = await self._spawner.spawn_task(runtime_task)
+        if start_ok:
+            runtime_task.status = None
+            if self._task_timeout is not None:
+                runtime_task.execution_timeout = time.monotonic() + self._task_timeout
+            async with self._state_machine.lock:
+                self._state_machine.started.append(runtime_task)
+        else:
+            async with self._state_machine.lock:
+                self._state_machine.finished.append(runtime_task)
+
+    async def monitor(self):
+        """Reads from started, moves into finished."""
+        try:
+            async with self._state_machine.lock:
+                runtime_task = self._state_machine.started.pop(0)
+        except IndexError:
+            return
+
+        if self._spawner.is_task_alive(runtime_task):
+            try:
+                if runtime_task.execution_timeout is None:
+                    remaining = None
+                else:
+                    remaining = runtime_task.execution_timeout - time.monotonic()
+                await asyncio.wait_for(self._spawner.wait_task(runtime_task),
+                                       remaining)
+                runtime_task.status = 'FINISHED'
+            except asyncio.TimeoutError:
+                runtime_task.status = 'FINISHED W/ TIMEOUT'
+
+        async with self._state_machine.lock:
+            self._state_machine.finished.append(runtime_task)
+
+    async def run(self):
+        """Pushes Tasks forward and makes them do something with their lives."""
+        while True:
+            is_complete = await self._state_machine.complete
+            if is_complete:
+                break
+            await self.bootstrap()
+            await self.triage()
+            await self.start()
+            await self.monitor()
---
+++ packages/avocado-framework/avocado_framework-90.0-py3-none-any.whl/avocado/plugins/runner_nrunner.py
@@ -0,0 +1,324 @@
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+#
+# See LICENSE for more details.
+#
+# Copyright: Red Hat Inc. 2019-2020
+# Authors: Cleber Rosa <crosa@redhat.com>
+
+"""
+NRunner based implementation of job compliant runner
+"""
+
+import asyncio
+import multiprocessing
+import random
+
+from avocado.core import nrunner
+from avocado.core.dispatcher import SpawnerDispatcher
+from avocado.core.messages import MessageHandler
+from avocado.core.plugin_interfaces import CLI, Init
+from avocado.core.plugin_interfaces import Runner as RunnerInterface
+from avocado.core.requirements.resolver import RequirementsResolver
+from avocado.core.settings import settings
+from avocado.core.status.repo import StatusRepo
+from avocado.core.status.server import StatusServer
+from avocado.core.task.runtime import RuntimeTask
+from avocado.core.task.statemachine import TaskStateMachine, Worker
+from avocado.core.test_id import TestID
+
+
+class RunnerInit(Init):
+
+    name = 'nrunner'
+    description = 'nrunner initialization'
+
+    def initialize(self):
+        section = 'nrunner'
+        help_msg = 'Shuffle the tasks to be executed'
+        settings.register_option(section=section,
+                                 key='shuffle',
+                                 default=False,
+                                 help_msg=help_msg,
+                                 key_type=bool)
+
+        help_msg = ('URI for listing the status server. Usually '
+                    'a "HOST:PORT" string')
+        settings.register_option(section=section,
+                                 key='status_server_listen',
+                                 default='127.0.0.1:8888',
+                                 metavar="HOST:PORT",
+                                 help_msg=help_msg)
+
+        help_msg = ('URI for connecting to the status server, usually '
+                    'a "HOST:PORT" string. Use this if your status server '
+                    'is in another host, or different port')
+        settings.register_option(section=section,
+                                 key='status_server_uri',
+                                 default='127.0.0.1:8888',
+                                 metavar="HOST:PORT",
+                                 help_msg=help_msg)
+
+        help_msg = ('Buffer size that status server uses.  This should '
+                    'generally not be a concern to most users, but '
+                    'it can be tunned in case a runner generates very large '
+                    'status messages, which is common if a test generates a '
+                    'lot of output. Default is 33554432 (32MiB)')
+        settings.register_option(section=section,
+                                 key='status_server_buffer_size',
+                                 key_type=int,
+                                 default=2 ** 25,
+                                 help_msg=help_msg)
+
+        help_msg = ('Number of maximum number tasks running in parallel. You '
+                    'can disable parallel execution by setting this to 1. '
+                    'Defaults to the amount of CPUs on this machine.')
+        settings.register_option(section=section,
+                                 key='max_parallel_tasks',
+                                 default=multiprocessing.cpu_count(),
+                                 key_type=int,
+                                 help_msg=help_msg)
+
+        help_msg = ("Spawn tasks in a specific spawner. Available spawners: "
+                    "'process' and 'podman'")
+        settings.register_option(section=section,
+                                 key="spawner",
+                                 default='process',
+                                 help_msg=help_msg)
+
+        help_msg = "The amount of time a test has to complete in seconds."
+        settings.register_option(section='task.timeout',
+                                 key='running',
+                                 default=None,
+                                 key_type=int,
+                                 help_msg=help_msg)
+
+
+class RunnerCLI(CLI):
+
+    name = 'nrunner'
+    description = 'nrunner command line options for "run"'
+
+    def configure(self, parser):
+        super(RunnerCLI, self).configure(parser)
+        parser = parser.subcommands.choices.get('run', None)
+        if parser is None:
+            return
+
+        parser = parser.add_argument_group('nrunner specific options')
+        settings.add_argparser_to_option(namespace='nrunner.shuffle',
+                                         parser=parser,
+                                         long_arg='--nrunner-shuffle',
+                                         action='store_true')
+
+        settings.add_argparser_to_option(
+            namespace='nrunner.status_server_listen',
+            parser=parser,
+            long_arg='--nrunner-status-server-listen',
+            metavar='HOST_PORT')
+
+        settings.add_argparser_to_option(
+            namespace='nrunner.status_server_uri',
+            parser=parser,
+            long_arg='--nrunner-status-server-uri',
+            metavar='HOST_PORT')
+
+        settings.add_argparser_to_option(
+            namespace='nrunner.max_parallel_tasks',
+            parser=parser,
+            long_arg='--nrunner-max-parallel-tasks',
+            metavar='NUMBER_OF_TASKS')
+
+        settings.add_argparser_to_option(
+            namespace='nrunner.spawner',
+            parser=parser,
+            long_arg='--nrunner-spawner',
+            metavar='SPAWNER')
+
+    def run(self, config):
+        pass
+
+
+class Runner(RunnerInterface):
+
+    name = 'nrunner'
+    description = 'nrunner based implementation of job compliant runner'
+
+    @staticmethod
+    def _get_requirements_runtime_tasks(runnable, prefix):
+        if runnable.requirements is None:
+            return
+
+        # creates the runnables for the requirements
+        requirements_runnables = RequirementsResolver.resolve(runnable)
+        requirements_runtime_tasks = []
+        # creates the tasks and runtime tasks for the requirements
+        for requirement_runnable in requirements_runnables:
+            name = '%s-%s' % (requirement_runnable.kind,
+                              requirement_runnable.kwargs.get('name'))
+            # the human UI works with TestID objects, so we need to
+            # use it to name other tasks
+            task_id = TestID(prefix,
+                             name,
+                             None)
+            # with --dry-run we don't want to run requirement
+            if runnable.kind == 'dry-run':
+                requirement_runnable.kind = 'noop'
+            # creates the requirement task
+            requirement_task = nrunner.Task(requirement_runnable,
+                                            identifier=task_id,
+                                            category='requirement')
+            # make sure we track the dependencies of a task
+            # runtime_task.task.dependencies.add(requirement_task)
+            # created the requirement runtime task
+            requirements_runtime_tasks.append(RuntimeTask(requirement_task))
+
+        return requirements_runtime_tasks
+
+    @staticmethod
+    def _create_runtime_tasks_for_test(test_suite, runnable, no_digits,
+                                       index, variant):
+        """Creates runtime tasks for both tests, and for its requirements."""
+        result = []
+        # test related operations
+        if test_suite.name:
+            prefix = "{}-{}".format(test_suite.name, index)
+        else:
+            prefix = index
+        test_id = TestID(prefix,
+                         runnable.uri,
+                         variant,
+                         no_digits)
+        # handles the test task
+        task = nrunner.Task(runnable,
+                            identifier=test_id,
+                            known_runners=nrunner.RUNNERS_REGISTRY_PYTHON_CLASS)
+        runtime_task = RuntimeTask(task)
+        result.append(runtime_task)
+
+        # handles the requirements
+        requirements_runtime_tasks = (
+            Runner._get_requirements_runtime_tasks(runnable,
+                                                   prefix))
+        # extend the list of tasks with the requirements runtime tasks
+        if requirements_runtime_tasks is not None:
+            for requirement_runtime_task in requirements_runtime_tasks:
+                # make sure we track the dependencies of a task
+                runtime_task.task.dependencies.add(
+                    requirement_runtime_task.task)
+            result.extend(requirements_runtime_tasks)
+
+        return result
+
+    @staticmethod
+    def _get_all_runtime_tasks(test_suite):
+        runtime_tasks = []
+        test_result_total = test_suite.variants.get_number_of_tests(test_suite.tests)
+        no_digits = len(str(test_result_total))
+        execution_order = test_suite.config.get('run.execution_order')
+        if execution_order == "variants-per-test":
+            for index, (runnable, variant) in enumerate(((test, variant)
+                                                         for test in test_suite.tests
+                                                         for variant in test_suite.variants.itertests()),
+                                                        start=1):
+                runtime_tasks.extend(Runner._create_runtime_tasks_for_test(
+                    test_suite,
+                    runnable,
+                    no_digits,
+                    index,
+                    variant))
+        elif execution_order == "tests-per-variant":
+            for index, (runnable, variant) in enumerate(((test, variant)
+                                                         for variant in test_suite.variants.itertests()
+                                                         for test in test_suite.tests),
+                                                        start=1):
+                runtime_tasks.extend(Runner._create_runtime_tasks_for_test(
+                    test_suite,
+                    runnable,
+                    no_digits,
+                    index,
+                    variant))
+        return runtime_tasks
+
+    def _start_status_server(self, status_server_listen):
+        # pylint: disable=W0201
+        self.status_repo = StatusRepo()
+        # pylint: disable=W0201
+        self.status_server = StatusServer(status_server_listen,
+                                          self.status_repo)
+        asyncio.ensure_future(self.status_server.serve_forever())
+
+    async def _update_status(self, job):
+        tasks_by_id = {str(runtime_task.task.identifier): runtime_task.task
+                       for runtime_task in self.runtime_tasks}
+        message_handler = MessageHandler()
+        while True:
+            try:
+                (task_id, _, _, index) = \
+                    self.status_repo.status_journal_summary.pop(0)
+
+            except IndexError:
+                await asyncio.sleep(0.05)
+                continue
+
+            message = self.status_repo.get_task_data(task_id, index)
+            task = tasks_by_id.get(task_id)
+            message_handler.process_message(message, task, job)
+
+    def run_suite(self, job, test_suite):
+        summary = set()
+
+        test_suite.tests, _ = nrunner.check_runnables_runner_requirements(
+            test_suite.tests)
+        job.result.tests_total = test_suite.variants.get_number_of_tests(test_suite.tests)
+
+        listen = test_suite.config.get('nrunner.status_server_listen')
+        self._start_status_server(listen)
+
+        # pylint: disable=W0201
+        self.runtime_tasks = self._get_all_runtime_tasks(test_suite)
+        if test_suite.config.get('nrunner.shuffle'):
+            random.shuffle(self.runtime_tasks)
+        test_ids = [rt.task.identifier for rt in self.runtime_tasks
+                    if rt.task.category == 'test']
+        tsm = TaskStateMachine(self.runtime_tasks, self.status_repo)
+        spawner_name = test_suite.config.get('nrunner.spawner')
+        spawner = SpawnerDispatcher(test_suite.config)[spawner_name].obj
+        max_running = min(test_suite.config.get('nrunner.max_parallel_tasks'),
+                          len(self.runtime_tasks))
+        timeout = test_suite.config.get('task.timeout.running')
+        workers = [Worker(state_machine=tsm,
+                          spawner=spawner,
+                          max_running=max_running,
+                          task_timeout=timeout).run()
+                   for _ in range(max_running)]
+        asyncio.ensure_future(self._update_status(job))
+        loop = asyncio.get_event_loop()
+        try:
+            loop.run_until_complete(asyncio.wait_for(asyncio.gather(*workers),
+                                                     job.timeout or None))
+        except (KeyboardInterrupt, asyncio.TimeoutError):
+            summary.add("INTERRUPTED")
+
+        # Wait until all messages may have been processed by the
+        # status_updater. This should be replaced by a mechanism
+        # that only waits if there are missing status messages to
+        # be processed, and, only for a given amount of time.
+        # Tests with non received status will always show as SKIP
+        # because of result reconciliation.
+        loop.run_until_complete(asyncio.sleep(0.05))
+
+        job.result.end_tests()
+        self.status_server.close()
+
+        # Update the overall summary with found test statuses, which will
+        # determine the Avocado command line exit status
+        summary.update([status.upper() for status in
+                        self.status_repo.get_result_set_for_tasks(test_ids)])
+        return summary
